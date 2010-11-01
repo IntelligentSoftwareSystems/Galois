a5334c7e6fcd0491a63b1e34d3089cf53cdba36a
amin <amin@522c16a8-e32f-0410-b4cc-8071c0b41197>
2010-11-01 23:40:30 +0000

    git-svn-id: https://svn.ices.utexas.edu/repos/iss/projects/GaloisCpp/trunk@5236 522c16a8-e32f-0410-b4cc-8071c0b41197

--- include/Galois/Galois.h
+++ include/Galois/Galois.h
@@ -15,139 +15,239 @@
 #include "Galois/Runtime/WorkList.h"
 
 namespace GaloisRuntime {
-  
-  template<class WorkListTy, class Function> 
-  class GaloisWork : public Executable {
-    typedef typename WorkListTy::value_type value_type;
-    typedef GWL_LIFO_SB<value_type> localWLTy;
-    //typedef GWL_ChaseLev_Dyn<value_type> localWLTy;
-    //typedef GWL_Idempotent_FIFO_SB<value_type> localWLTy;
 
-    WorkListTy& global_wl;
-    Function f;
-    int conflicts;
-    unsigned long tTime;
-    unsigned long pTime;
-    CPUSpaced<localWLTy> local_wl;
+template<class WorkListTy, class Function>
+class GaloisWork: public Executable {
+	typedef typename WorkListTy::value_type value_type;
+	typedef GWL_LIFO_SB<value_type> localWLTy;
+	//typedef GWL_ChaseLev_Dyn<value_type> localWLTy;
+	//typedef GWL_Idempotent_FIFO_SB<value_type> localWLTy;
 
-  public:
-    GaloisWork(WorkListTy& _wl, Function _f)
-      :global_wl(_wl), f(_f), conflicts(0), tTime(0), pTime(0)
-    {}
-    
-    ~GaloisWork() {
-      std::cout << "STAT: Conflicts " << conflicts << "\n";
-      std::cout << "STAT: TotalTime " << tTime << "\n";
-      std::cout << "STAT: ProcessTime " << pTime << "\n";
-      assert(global_wl.empty());
-    }
+	WorkListTy& global_wl;
+	Function f;
+	int conflicts;
+	unsigned long tTime;
+	unsigned long pTime;
+	CPUSpaced<localWLTy> local_wl;
 
-    void doProcess(value_type val, localWLTy& wlLocal, GaloisRuntime::TimeAccumulator& ProcessTime, int& lconflicts) {
-      ProcessTime.start();
-      SimpleRuntimeContext cnx;
-      setThreadContext(&cnx);
-      try {
-	f(val, wlLocal);
-      } catch (int a) {
-	ProcessTime.stop();
-	++lconflicts;
-	global_wl.push(val);
-	return;
-      }	
-      setThreadContext(0);
-      ProcessTime.stop();
-      return;
-    }
-
-    virtual void preRun(int tmax) {
-      local_wl.resize(tmax);
-    }
-
-    virtual void postRun() {
-      for (int i = 0; i < local_wl.size(); ++i)
-	assert(local_wl[i].empty());
-      assert(global_wl.empty());
-    }
-    
-    virtual void operator() (int ID, int tmax) {
-      localWLTy& wlLocal = local_wl[ID];
-
-      int lconflicts = 0;
-      GaloisRuntime::Timer TotalTime;
-      GaloisRuntime::TimeAccumulator ProcessTime;
-
-      TotalTime.start();
-      /*
-      do {
-	global_wl.moveTo(wlLocal, 256);
-	while (!wlLocal.empty()) {
-	  bool suc;
-	  value_type val = wlLocal.pop(suc);
-	  if (suc)
-	    doProcess(val, wlLocal, ProcessTime, lconflicts);
+public:
+	GaloisWork(WorkListTy& _wl, Function _f) :
+		global_wl(_wl), f(_f), conflicts(0), tTime(0), pTime(0) {
 	}
-      } while (!global_wl.empty());
-      */
-      do {
-      	do {
-      	  //move some items out of the global list
-      	  global_wl.moveTo(wlLocal, 256);
-	  
-      	  bool suc;
-      	  do {
-      	    value_type val = wlLocal.pop(suc);
-      	    if (suc)
-      	      doProcess(val, wlLocal, ProcessTime, lconflicts);
-      	  } while (suc);
-      	} while (!global_wl.empty());
-	
-      	//Try to steal work
-      	for (int i = 0; i < tmax; ++i) {
-      	  bool suc = false;
-      	  value_type val = local_wl[(i + ID) % tmax].steal(suc);
-      	  //Don't push it on the queue before we can execute it
-      	  if (suc) {
-      	    doProcess(val, wlLocal,ProcessTime, lconflicts);
-      	    //One item is enough
-      	    break;
-      	  }
-      	}
-      } while (!wlLocal.empty() || !global_wl.empty());
-      TotalTime.stop();
-      __sync_fetch_and_add(&tTime, TotalTime.get());
-      __sync_fetch_and_add(&pTime, ProcessTime.get());
-      __sync_fetch_and_add(&conflicts, lconflicts);
-    }
-  };
 
-  template<class WorkListTy, class Function>
-  void for_each_simple (WorkListTy& wl, Function f)
-  {
-    //wl.sort();
-    GaloisWork<WorkListTy, Function> GW(wl, f);
-    ThreadPool& PTP = getSystemThreadPool();
-    PTP.run(&GW);
-  }
-  
+	~GaloisWork() {
+		std::cout << "STAT: Conflicts " << conflicts << "\n";
+		std::cout << "STAT: TotalTime " << tTime << "\n";
+		std::cout << "STAT: ProcessTime " << pTime << "\n";
+		assert(global_wl.empty());
+	}
+
+	void doProcess(value_type val, localWLTy& wlLocal,
+			GaloisRuntime::TimeAccumulator& ProcessTime, int& lconflicts) {
+		ProcessTime.start();
+		SimpleRuntimeContext cnx;
+		setThreadContext(&cnx);
+		try {
+			f(val, wlLocal);
+		} catch (int a) {
+			ProcessTime.stop();
+			++lconflicts;
+			global_wl.push(val);
+			return;
+		}
+		setThreadContext(0);
+		ProcessTime.stop();
+		return;
+	}
+
+	virtual void preRun(int tmax) {
+		local_wl.resize(tmax);
+	}
+
+	virtual void postRun() {
+		for (int i = 0; i < local_wl.size(); ++i)
+			assert(local_wl[i].empty());
+		assert(global_wl.empty());
+	}
+
+	virtual void operator()(int ID, int tmax) {
+		localWLTy& wlLocal = local_wl[ID];
+
+		int lconflicts = 0;
+		GaloisRuntime::Timer TotalTime;
+		GaloisRuntime::TimeAccumulator ProcessTime;
+
+		TotalTime.start();
+		/*
+		 do {
+		 global_wl.moveTo(wlLocal, 256);
+		 while (!wlLocal.empty()) {
+		 bool suc;
+		 value_type val = wlLocal.pop(suc);
+		 if (suc)
+		 doProcess(val, wlLocal, ProcessTime, lconflicts);
+		 }
+		 } while (!global_wl.empty());
+		 */
+		do {
+			do {
+				//move some items out of the global list
+				global_wl.moveTo(wlLocal, 256);
+
+				bool suc;
+				do {
+					value_type val = wlLocal.pop(suc);
+					if (suc)
+						doProcess(val, wlLocal, ProcessTime, lconflicts);
+				} while (suc);
+			} while (!global_wl.empty());
+
+			//Try to steal work
+			for (int i = 0; i < tmax; ++i) {
+				bool suc = false;
+				value_type val = local_wl[(i + ID) % tmax].steal(suc);
+				//Don't push it on the queue before we can execute it
+				if (suc) {
+					doProcess(val, wlLocal, ProcessTime, lconflicts);
+					//One item is enough
+					break;
+				}
+			}
+		} while (!wlLocal.empty() || !global_wl.empty());
+		TotalTime.stop();
+		__sync_fetch_and_add(&tTime, TotalTime.get());
+		__sync_fetch_and_add(&pTime, ProcessTime.get());
+		__sync_fetch_and_add(&conflicts, lconflicts);
+	}
+};
+
+template<class WorkListTy, class Function>
+class GaloisWork2: public Executable {
+	typedef typename WorkListTy::value_type value_type;
+	typedef GWL_LIFO_SB<value_type> localWLTy;
+	//typedef GWL_ChaseLev_Dyn<value_type> localWLTy;
+	//typedef GWL_Idempotent_FIFO_SB<value_type> localWLTy;
+
+	WorkListTy& global_wl;
+	Function f;
+	int conflicts;
+	unsigned long tTime;
+	unsigned long pTime;
+	CPUSpaced<localWLTy> local_wl;
+	int threadsWorking;
+
+public:
+	GaloisWork2(WorkListTy& _wl, Function _f) :
+		global_wl(_wl), f(_f), conflicts(0), tTime(0), pTime(0) {
+		threadsWorking = 0;
+	}
+
+	~GaloisWork2() {
+		std::cout << "STAT: Conflicts " << conflicts << "\n";
+		std::cout << "STAT: TotalTime " << tTime << "\n";
+		std::cout << "STAT: ProcessTime " << pTime << "\n";
+		assert(global_wl.empty());
+	}
+
+	void doProcess(value_type val, localWLTy& wlLocal,
+			GaloisRuntime::TimeAccumulator& ProcessTime, int& lconflicts) {
+		ProcessTime.start();
+		SimpleRuntimeContext cnx;
+		setThreadContext(&cnx);
+		try {
+			f(val, wlLocal);
+		} catch (int a) {
+			ProcessTime.stop();
+			++lconflicts;
+			global_wl.push(val);
+			return;
+		}
+		setThreadContext(0);
+		ProcessTime.stop();
+		return;
+	}
+
+	virtual void preRun(int tmax) {
+		local_wl.resize(tmax);
+	}
+
+	virtual void postRun() {
+		for (int i = 0; i < local_wl.size(); ++i)
+			assert(local_wl[i].empty());
+		assert(global_wl.empty());
+	}
+
+	virtual void operator()(int ID, int tmax) {
+		localWLTy& wlLocal = local_wl[ID];
+
+		int lconflicts = 0;
+		GaloisRuntime::Timer TotalTime;
+		GaloisRuntime::TimeAccumulator ProcessTime;
+
+		TotalTime.start();
+
+		do {
+			__sync_fetch_and_add(&threadsWorking, +1);
+			do {
+				do {
+					//move some items out of the global list
+					global_wl.moveTo(wlLocal, 2);
+
+					bool suc;
+					do {
+						value_type val = wlLocal.pop(suc);
+						if (suc)
+							doProcess(val, wlLocal, ProcessTime, lconflicts);
+					} while (suc);
+				} while (!global_wl.empty());
+
+				//Try to steal work
+				for (int i = 0; i < tmax; ++i) {
+					bool suc = false;
+					value_type val = local_wl[(i + ID) % tmax].steal(suc);
+					//Don't push it on the queue before we can execute it
+					if (suc) {
+						doProcess(val, wlLocal, ProcessTime, lconflicts);
+						//One item is enough
+						break;
+					}
+				}
+			} while (!wlLocal.empty() || !global_wl.empty());
+			__sync_fetch_and_add(&threadsWorking, -1);
+		} while (__sync_fetch_and_add(&threadsWorking, 0) > 0);
+		TotalTime.stop();
+		__sync_fetch_and_add(&tTime, TotalTime.get());
+		__sync_fetch_and_add(&pTime, ProcessTime.get());
+		__sync_fetch_and_add(&conflicts, lconflicts);
+	}
+};
+
+template<class WorkListTy, class Function>
+void for_each_simple(WorkListTy& wl, Function f) {
+	//wl.sort();
+	GaloisWork2<WorkListTy, Function> GW(wl, f);
+	ThreadPool& PTP = getSystemThreadPool();
+	PTP.run(&GW);
+}
 
 }
 
 //The user interface
 namespace Galois {
 
-  template<typename T>
-  class WorkList {
-  public:
-    virtual void push(T) = 0;
-  };
+template<typename T>
+class WorkList {
+public:
+	virtual void push(T) = 0;
+};
 
-  static __attribute__((unused)) void setMaxThreads(int T) {
-    GaloisRuntime::getSystemThreadPool().resize(T);
-  }
+static __attribute__((unused)) void setMaxThreads(int T) {
+	GaloisRuntime::getSystemThreadPool().resize(T);
+}
 
-  template<typename WorkListTy, typename Function>
-  void for_each (WorkListTy& wl, Function f) {
-    GaloisRuntime::for_each_simple(wl, f);
-  }
+template<typename WorkListTy, typename Function>
+void for_each(WorkListTy& wl, Function f) {
+	GaloisRuntime::for_each_simple(wl, f);
+}
 
 }
