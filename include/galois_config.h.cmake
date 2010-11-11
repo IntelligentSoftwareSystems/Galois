29b72bf00d06a642ecfbe096f85747449e8a02ee
lenharth <lenharth@522c16a8-e32f-0410-b4cc-8071c0b41197>
2010-11-11 20:40:13 +0000

    Add support for cray thread pool
    git-svn-id: https://svn.ices.utexas.edu/repos/iss/projects/GaloisCpp/trunk@5432 522c16a8-e32f-0410-b4cc-8071c0b41197

---
+++ include/galois_config.h.cmake
@@ -0,0 +1,7 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#cmakedefine WITH_PTHREAD_POOL
+#cmakedefine WITH_CRAY_POOL
+
+#endif // CONFIG_H
---
+++ src/ThreadPool_cray.cpp
@@ -0,0 +1,95 @@
+/*! \file 
+ *  \brief cray thread pool implementation
+ */
+
+#include "galois_config.h"
+#ifdef WITH_CRAY_POOL
+
+#include <list>
+#include <cassert>
+
+#include "Galois/Runtime/ThreadPool.h"
+
+using namespace GaloisRuntime;
+
+namespace {
+
+  class Semaphore {
+    sem_t sem;
+  public:
+    explicit Semaphore(int val) {
+      sem_init(&sem, 0, val);
+    }
+    ~Semaphore() {
+      sem_destroy(&sem);
+    }
+
+    void release(int n = 1) {
+      while (n) {
+	--n;
+	sem_post(&sem);
+      }
+    }
+
+    void acquire(int n = 1) {
+      while (n) {
+	--n;
+	sem_wait(&sem);
+      }
+    }
+    
+  };
+
+  class ThreadPool_cray : public ThreadPool {
+    
+    int tmax;
+    int num;
+    Executable* work;
+
+    int mkID() {
+      return int_fetch_add(&tmax, 1);
+    }
+
+    void launch(void) {
+      int myID = mkID();
+      (*work)(myID, num);
+    }
+
+  public:
+    ThreadPool_cray() 
+      :tmax(0), num(1)
+    {}
+
+    virtual void run(Executable* E) {
+      work = E;
+      work->preRun(numThreads());
+      ThreadPool_cray* p = this;
+      future int children[num];
+      future children$(p) {
+	p->launch();
+	return 0;
+      }
+      for (int i = 0; i < num; ++i)
+	touch(&children$[i])
+      work->postRun();
+    }
+
+    virtual void resize(int num) {
+      this->num = num;
+    }
+
+    virtual int size() {
+      return num;
+    }
+  };
+}
+
+
+//! Implement the global threadpool
+static ThreadPool_cray pool;
+
+ThreadPool& GaloisRuntime::getSystemThreadPool() {
+  return pool;
+}
+
+#endif
