0edc0bb1d780877a0a774640c9c7be5f61f851f9
lenharth <lenharth@522c16a8-e32f-0410-b4cc-8071c0b41197>
2011-02-02 20:35:24 +0000

    Out of line Termination Detection
    git-svn-id: https://svn.ices.utexas.edu/repos/iss/projects/GaloisCpp/trunk@5893 522c16a8-e32f-0410-b4cc-8071c0b41197

--- include/Galois/Runtime/Termination.h
+++ include/Galois/Runtime/Termination.h
@@ -2,7 +2,6 @@
 
 namespace GaloisRuntime {
 
-
 //Dikstra dual-ring termination algorithm
 class TerminationDetection {
 
@@ -18,67 +17,15 @@ class TerminationDetection {
   bool lastWasWhite;
 public:
 
-  TerminationDetection()
-    :data(0), globalTerm(false), lastWasWhite(false)
-  {
-    data.get(0).hasToken = true;
-  }
+  TerminationDetection();
 
-  void workHappened() {
+  inline void workHappened() {
     data.get().processIsBlack = true;
   }
 
-  void localTermination() {
-    tokenHolder& th = data.get();
-    tokenHolder& thn = data.getNext();
-    if (ThreadPool::getMyID() == 1) {
-      //master
-      if (th.hasToken) {
-	if (th.tokenIsBlack || th.processIsBlack) {
-	  //failed circulation
-	  lastWasWhite = false;
-	  th.processIsBlack = false;
-	  th.hasToken = false;
-	  thn.tokenIsBlack = false;
-	  __sync_synchronize();
-	  thn.hasToken = true;
-	} else {
-	  if (lastWasWhite) {
-	    //This was the second time around
-	    globalTerm = true;
-	  } else {
-	    //Start a second round of voting
-	    lastWasWhite = true;
-	    th.hasToken = false;
-	    thn.tokenIsBlack = false;
-	    __sync_synchronize();
-	    thn.hasToken = true;
-	  }
-	}
-      } else {
-	//Do nothing while waiting for the token
-      }
-    } else {
-      //Normal thread
-      if (th.processIsBlack) {
-	//Black process colors the token
-	//color resets to white
-	th.processIsBlack = false;
-	th.tokenIsBlack = false;
-	th.hasToken = false;
-	thn.tokenIsBlack = true;
-	__sync_synchronize();
-	thn.hasToken = true;
-      } else {
-	//white process pass the token
-	thn.tokenIsBlack = th.tokenIsBlack;
-	th.hasToken = false;
-	__sync_synchronize();
-	thn.hasToken = true;
-      }
-    }
-  }
+  void localTermination();
 
+  // Returns
   bool globalTermination() {
     return globalTerm;
   }
---
+++ src/Termination.cpp
@@ -0,0 +1,71 @@
+//Dikstra dual-ring termination algorithm
+
+#include "Galois/Runtime/PerCPU.h"
+#include "Galois/Runtime/Termination.h"
+//#include "Galois/Runtime/SimpleLock.h"
+
+#include <iostream>
+
+using namespace GaloisRuntime;
+
+TerminationDetection::TerminationDetection()
+  :data(0), globalTerm(false), lastWasWhite(false)
+{
+  data.get(0).hasToken = true;
+  data.get(0).tokenIsBlack = true;
+}
+
+void TerminationDetection::localTermination() {
+  tokenHolder& th = data.get();
+  tokenHolder& thn = data.getNext();
+  //  static SimpleLock<int, true> L;
+  //  L.lock();
+  //  std::cerr << "th " << &th << " thn " << &thn << "\n";
+  //  L.unlock();
+  if (ThreadPool::getMyID() == 1) {
+    //master
+    if (th.hasToken) {
+      if (th.tokenIsBlack || th.processIsBlack) {
+	//failed circulation
+	lastWasWhite = false;
+	th.processIsBlack = false;
+	th.hasToken = false;
+	thn.tokenIsBlack = false;
+	__sync_synchronize();
+	thn.hasToken = true;
+      } else {
+	if (lastWasWhite) {
+	  //This was the second time around
+	  globalTerm = true;
+	} else {
+	  //Start a second round of voting
+	  lastWasWhite = true;
+	  th.hasToken = false;
+	  thn.tokenIsBlack = false;
+	  __sync_synchronize();
+	  thn.hasToken = true;
+	}
+      }
+    } else {
+      //Do nothing while waiting for the token
+    }
+  } else {
+    //Normal thread
+    if (th.processIsBlack) {
+      //Black process colors the token
+      //color resets to white
+      th.processIsBlack = false;
+      th.tokenIsBlack = false;
+      th.hasToken = false;
+      thn.tokenIsBlack = true;
+      __sync_synchronize();
+      thn.hasToken = true;
+    } else {
+      //white process pass the token
+      thn.tokenIsBlack = th.tokenIsBlack;
+      th.hasToken = false;
+      __sync_synchronize();
+      thn.hasToken = true;
+    }
+  }
+}
