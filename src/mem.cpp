abf76255887adece9b9be684e1633ccbf54da005
lenharth <lenharth@522c16a8-e32f-0410-b4cc-8071c0b41197>
2011-02-03 22:19:16 +0000

    Don't try unsupported mmap flags
    git-svn-id: https://svn.ices.utexas.edu/repos/iss/projects/GaloisCpp/trunk@5916 522c16a8-e32f-0410-b4cc-8071c0b41197

--- src/mem.cpp
+++ src/mem.cpp
@@ -9,15 +9,24 @@ using namespace MM;
 
 static const int _PROT = PROT_READ | PROT_WRITE;
 static const int _MAP_BASE = MAP_ANONYMOUS | MAP_PRIVATE;
+#ifdef MAP_POPULATE
 static const int _MAP_POP  = MAP_POPULATE | _MAP_BASE;
+#endif
+#ifdef MAP_HUGETLB
 static const int _MAP_HUGE = MAP_HUGETLB | _MAP_POP;
+#endif
 
 void* mmapWrapper::_alloc() {
+  void* ptr = 0;
+#ifdef MAP_HUGETLB
   //First try huge
-  void* ptr = mmap(0, AllocSize, _PROT, _MAP_HUGE, -1, 0);
+  ptr = mmap(0, AllocSize, _PROT, _MAP_HUGE, -1, 0);
+#endif
+#ifdef MAP_POPULATE
   //Then try populate
   if (!ptr || ptr == MAP_FAILED)
     ptr = mmap(0, AllocSize, _PROT, _MAP_POP, -1, 0);
+#endif
   //Then try normal
   if (!ptr || ptr == MAP_FAILED)
     ptr = mmap(0, AllocSize, _PROT, _MAP_BASE, -1, 0);
