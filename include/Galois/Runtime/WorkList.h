d5059e53c0bd7ee2cd7b0c26ee1a0030a9b7aa90
lenharth <lenharth@522c16a8-e32f-0410-b4cc-8071c0b41197>
2011-02-10 18:04:17 +0000

    Get rid of stl adaptor, add hashed priority queue, add local queue thing, which may not be a good idea
    git-svn-id: https://svn.ices.utexas.edu/repos/iss/projects/GaloisCpp/trunk@5970 522c16a8-e32f-0410-b4cc-8071c0b41197

--- include/Galois/Runtime/WorkList.h
+++ include/Galois/Runtime/WorkList.h
@@ -1,13 +1,9 @@
 // Scalable Local worklists -*- C++ -*-
-// This contains final (leaf) worklists.  Some support stealing, some don't
-// All classes conform to:
-// void push(T)
-// bool,T pop()
-// bool empty()
-// void fillInitial(begin, end)
+// This contains final worklists.
 
 #include <queue>
 #include <stack>
+#include <limits>
 
 #include "Galois/Runtime/SimpleLock.h"
 #include "Galois/Runtime/PerCPU.h"
@@ -15,8 +11,44 @@
 
 #include <boost/utility.hpp>
 
+//#include <iostream>
+
 namespace GaloisRuntime {
 namespace WorkList {
+
+// Worklists may not be copied.
+// Worklists should be default instantiatable
+// All classes (should) conform to:
+template<typename T>
+class AbstractWorkList {
+public:
+  //! T is the value type of the WL
+  typedef T value_type;
+  //! Optional for top-level queues
+  typedef AbstractWorkList<T> ConcurrentTy;
+  //! Optional for top-level queues
+  typedef AbstractWorkList<typename T::X> SingleThreadTy;
+  
+  //! push a value onto the queue
+  void push(value_type);
+  //! push an aborted value onto the queue
+  void aborted(value_type);
+  //! pop a value from the queue.
+  std::pair<bool, value_type> pop();
+  //! pop a value from the queue trying not as hard to take locks
+  std::pair<bool, value_type> try_pop();
+  //! return if the queue *may* be empty
+  bool empty();
+  //! return the number of items in the list
+  //! this is not called size because it may not be constant
+  unsigned count();
+  
+  //! called in sequential mode to seed the worklist
+  template<typename iter>
+  void fillInitial(iter begin, iter end);
+
+
+};
 
 template<bool concurrent>
 class PaddedLock;
@@ -27,30 +59,34 @@ class PaddedLock<true> {
 
 public:
   void lock() { Lock.data.lock(); }
+  bool try_lock() { return Lock.data.try_lock(); }
   void unlock() { Lock.data.unlock(); }
 };
 template<>
 class PaddedLock<false> {
 public:
   void lock() {}
+  bool try_lock() { return true; }
   void unlock() {}
 };
 
+template<typename T, class Compare = std::less<T>, bool concurrent = true>
+class PriQueue : private boost::noncopyable, private PaddedLock<concurrent> {
 
-
-template<typename MQ, bool concurrent = true>
-class STLAdaptor : private boost::noncopyable, private PaddedLock<concurrent> {
-
-  MQ wl;
+  std::priority_queue<T, std::vector<T>, Compare> wl;
 
   using PaddedLock<concurrent>::lock;
+  using PaddedLock<concurrent>::try_lock;
   using PaddedLock<concurrent>::unlock;
 
 public:
-  typedef STLAdaptor<MQ, true>  ConcurrentTy;
-  typedef STLAdaptor<MQ, false> SingleThreadTy;
+  typedef PriQueue<T, Compare, true>  ConcurrentTy;
+  typedef PriQueue<T, Compare, false> SingleThreadTy;
 
-  typedef typename MQ::value_type value_type;
+  friend class PriQueue<T, Compare, true>;
+  friend class PriQueue<T, Compare, false>;
+
+  typedef T value_type;
 
   void push(value_type val) {
     lock();
@@ -70,10 +106,106 @@ public:
       return std::make_pair(true, retval);
     }
   }
-    
+
+  std::pair<bool, value_type> try_pop() {
+    if (try_lock()) {
+      if (!wl.empty()) {
+	value_type retval = wl.top();
+	wl.pop();
+	unlock();
+	return std::make_pair(true, retval);
+      }
+      unlock();
+    }
+    return std::make_pair(false, value_type());
+  }
+   
   bool empty() {
     lock();
     bool retval = wl.empty();
+    unlock();
+    return retval;
+  }
+
+  unsigned count() {
+    lock();
+    unsigned ret = wl.size();
+    unlock();
+    return ret;
+  }
+
+  void aborted(value_type val) {
+    push(val);
+  }
+
+  //Not Thread Safe
+  template<typename Iter>
+  void fill_initial(Iter ii, Iter ee) {
+    while (ii != ee) {
+      wl.push(*ii++);
+    }
+  }
+};
+
+template<typename T, bool concurrent = true>
+class LIFO : private boost::noncopyable, private PaddedLock<concurrent> {
+  std::vector<T> wl;
+
+  using PaddedLock<concurrent>::lock;
+  using PaddedLock<concurrent>::try_lock;
+  using PaddedLock<concurrent>::unlock;
+
+public:
+  typedef LIFO<T, true>  ConcurrentTy;
+  typedef LIFO<T, false> SingleThreadTy;
+
+  friend class LIFO<T, true>;
+  friend class LIFO<T, false>;
+
+  typedef T value_type;
+
+  void push(value_type val) {
+    lock();
+    wl.push_back(val);
+    unlock();
+  }
+
+  std::pair<bool, value_type> pop() {
+    lock();
+    if (wl.empty()) {
+      unlock();
+      return std::make_pair(false, value_type());
+    } else {
+      value_type retval = wl.back();
+      wl.pop_back();
+      unlock();
+      return std::make_pair(true, retval);
+    }
+  }
+
+  std::pair<bool, value_type> try_pop() {
+    if (try_lock()) {
+      if (!wl.empty()) {
+	value_type retval = wl.back();
+	wl.pop_back();
+	unlock();
+	return std::make_pair(true, retval);
+      }
+      unlock();
+    }
+    return std::make_pair(false, value_type());
+  }
+
+  bool empty() {
+    lock();
+    bool retval = wl.empty();
+    unlock();
+    return retval;
+  }
+
+  bool count() {
+    lock();
+    unsigned retval = wl.size();
     unlock();
     return retval;
   }
@@ -89,26 +221,83 @@ public:
       wl.push(*ii++);
     }
   }
-
 };
 
-template<typename Q>
-class stdqueueFix : public Q {
+template<typename T, bool concurrent = true>
+class FIFO : private boost::noncopyable, private PaddedLock<concurrent> {
+  std::deque<T> wl;
+
+  using PaddedLock<concurrent>::lock;
+  using PaddedLock<concurrent>::try_lock;
+  using PaddedLock<concurrent>::unlock;
+
 public:
-  typename Q::value_type top() {
-    return Q::front();
+  typedef FIFO<T, true>  ConcurrentTy;
+  typedef FIFO<T, false> SingleThreadTy;
+
+  friend class FIFO<T, true>;
+  friend class FIFO<T, false>;
+
+  typedef T value_type;
+
+  void push(value_type val) {
+    lock();
+    wl.push_back(val);
+    unlock();
+  }
+
+  std::pair<bool, value_type> pop() {
+    lock();
+    if (wl.empty()) {
+      unlock();
+      return std::make_pair(false, value_type());
+    } else {
+      value_type retval = wl.front();
+      wl.pop_front();
+      unlock();
+      return std::make_pair(true, retval);
+    }
+  }
+
+  std::pair<bool, value_type> try_pop() {
+    if (try_lock()) {
+      if (!wl.empty()) {
+	value_type retval = wl.front();
+	wl.pop_front();
+	unlock();
+	return std::make_pair(true, retval);
+      }
+      unlock();
+    }
+    return std::make_pair(false, value_type());
+  }
+    
+  bool empty() {
+    lock();
+    bool retval = wl.empty();
+    unlock();
+    return retval;
+  }
+
+  unsigned count() {
+    lock();
+    unsigned retval = wl.size();
+    unlock();
+    return retval;
+  }
+
+  void aborted(value_type val) {
+    push(val);
+  }
+
+  //Not Thread Safe
+  template<typename Iter>
+  void fill_initial(Iter ii, Iter ee) {
+    while (ii != ee) {
+      wl.push(*ii++);
+    }
   }
 };
-
-template<typename T, bool concurrent = true>
-class LIFO : public STLAdaptor<std::stack<T>, concurrent > {};
-
-template<typename T, bool concurrent = true>
-class FIFO : public STLAdaptor<stdqueueFix<std::queue<T> >, concurrent > {};
-
-template<typename T, class Compare = std::less<T>, bool concurrent = true >
-class PriQueue : public STLAdaptor<std::priority_queue<T, std::vector<T>, Compare>, concurrent > {};
-
 
 template<typename T, int ChunkSize = 64, bool pushToLocal = true, typename BackingTy = LIFO<T> >
 class ChunkedFIFO : private boost::noncopyable {
@@ -250,6 +439,21 @@ class OrderedByIntegerMetric : private boost::noncopyable {
     y = 0;
   }
 
+  // void print() {
+  //   static int iter = 0;
+  //   ++iter;
+  //   if (iter % 1024 == 0) {
+  //     unsigned c[32];
+  //     for (int i = 0; i < 32; ++i)
+  // 	c[i] = 0;
+  //     for (unsigned int i = 0; i < size; ++i)
+  // 	c[i % 32] += data[i].count();
+  //     for (int i = 0; i < 31; ++i)
+  // 	std::cout << c[i] << ",";
+  //     std::cout << c[31] << "\n";
+  //   }
+  // }
+
  public:
 
   typedef T value_type;
@@ -276,11 +480,12 @@ class OrderedByIntegerMetric : private boost::noncopyable {
   }
 
   std::pair<bool, value_type> pop()  __attribute__((noinline)) {
+    // print();
     unsigned int& cur = cursor.get();
     std::pair<bool, value_type> ret;
     //Find a successful pop
     assert(cur < size);
-    ret = data[cur].pop();
+    ret = data[cur].try_pop();
     if (ret.first)
       return ret;
 
@@ -288,7 +493,7 @@ class OrderedByIntegerMetric : private boost::noncopyable {
     //assuming queues tend to be full, this should let us pick up good
     //items sooner
     for (cur = 0; cur < size; ++cur) {
-      ret = data[cur].pop();
+      ret = data[cur].try_pop();
       if (ret.first)
 	return ret;
     }
@@ -297,12 +502,24 @@ class OrderedByIntegerMetric : private boost::noncopyable {
     return ret;
   }
 
+  std::pair<bool, value_type> try_pop()  __attribute__((noinline)) {
+    return pop();
+  }
+
   bool empty() const {
     for (unsigned int i = 0; i < size; ++i)
       if (!data[i].empty())
 	return false;
     return true;
   }
+
+  unsigned count() {
+    unsigned c = 0;
+    for (unsigned int i = 0; i < size; ++i)
+      c += data[i].size();
+    return c;
+  }
+
   void aborted(value_type val) {
     push(val);
   }
@@ -432,6 +649,13 @@ class StealingLocalWL : private boost::noncopyable {
     return data.getNext().pop();
   }
 
+  std::pair<bool, value_type> try_pop()  __attribute__((noinline)) {
+    std::pair<bool, value_type> ret = data.get().try_pop();
+    if (ret.first)
+      return ret;
+    return data.getNext().try_pop();
+  }
+
   bool empty() {
     return data.get().empty();
   }
@@ -449,5 +673,141 @@ class StealingLocalWL : private boost::noncopyable {
 };
 
 
+template<typename T, typename GlobalQueueTy, typename LocalQueueTy>
+class LocalQueues {
+
+  PerCPU<typename LocalQueueTy::SingleThreadTy> local;
+  GlobalQueueTy global;
+  bool starved;
+
+public:
+  LocalQueues() :local(0), starved(false) {}
+
+  typedef T value_type;
+  
+  void push(value_type val) {
+    if (starved) {
+      global.push(val);
+      starved = 0;
+    } else {
+      local.get().push(val);
+    }
+  }
+
+  void aborted(value_type val) {
+    //Fixme: should be configurable
+    global.push(val);
+  }
+
+  std::pair<bool, value_type> pop() {
+    std::pair<bool, value_type> ret = local.get().pop();
+    if (ret.first)
+      return ret;
+    ret = global.pop();
+    if (!ret.first)
+      starved = true;
+    return ret;
+  }
+
+  std::pair<bool, value_type> try_pop() {
+    std::pair<bool, value_type> ret = local.get().try_pop();
+    if (ret.first)
+      return ret;
+    ret = global.try_pop();
+    if (!ret.first)
+      starved = true;
+    return ret;
+  }
+
+  bool empty() {
+    if (!local.get().empty()) return false;
+    return global.empty();
+  }
+
+  template<typename iter>
+  void fillInitial(iter begin, iter end) {
+    while (begin != end)
+      global.push(*begin++);
+  }
+};
+
+template<class T, class Indexer, typename ContainerTy = FIFO<T> >
+class ApproxOrderByIntegerMetric : private boost::noncopyable {
+
+  ContainerTy data[2048];
+  
+  Indexer I;
+  PerCPU<int> cursor;
+
+  int num() {
+    return 2048;
+  }
+
+ public:
+
+  typedef T value_type;
+  
+  ApproxOrderByIntegerMetric(const Indexer& x = Indexer())
+    :I(x), cursor(0)
+  {  }
+  
+  void push(value_type val) __attribute__((noinline)) {   
+    unsigned int index = I(val, std::numeric_limits<unsigned int>::max());
+    index %= num();
+    assert(index < num());
+    data[index].push(val);
+  }
+
+  std::pair<bool, value_type> pop()  __attribute__((noinline)) {
+    // print();
+    int& cur = cursor.get();
+    std::pair<bool, value_type> ret = data[cur].pop();
+    if (ret.first)
+      return ret;
+
+    //must move cursor
+    for (int i = 0; i < num(); ++i) {
+      cur = (cur + 1) % num();
+      ret = data[cur].try_pop();
+      if (ret.first)
+	return ret;
+    }
+    return std::pair<bool, value_type>(false, value_type());
+  }
+
+  std::pair<bool, value_type> try_pop()  __attribute__((noinline)) {
+    return pop();
+  }
+
+  bool empty() {
+    for (int i = 0; i < num(); ++i)
+      if (!data[i].empty())
+	return false;
+    return true;
+  }
+
+  unsigned count() {
+    unsigned c = 0;
+    for (unsigned int i = 0; i < num(); ++i)
+      c += data[i].count();
+    return c;
+  }
+
+  void aborted(value_type val) {
+    push(val);
+  }
+
+  //Not Thread Safe
+  //Not ideal
+  template<typename Iter>
+  void fill_initial(Iter ii, Iter ee) {
+    while (ii != ee) {
+      push(*ii++);
+    }
+  }
+};
+
+
+//End namespace
 }
 }
