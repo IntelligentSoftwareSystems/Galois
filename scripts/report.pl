68390698bdb489c3a685fd5e51b081a998164f2e
lenharth <lenharth@522c16a8-e32f-0410-b4cc-8071c0b41197>
2011-02-17 19:45:34 +0000

    summarize run script output
    git-svn-id: https://svn.ices.utexas.edu/repos/iss/projects/GaloisCpp/trunk@5991 522c16a8-e32f-0410-b4cc-8071c0b41197

---
+++ scripts/report.pl
@@ -0,0 +1,40 @@
+use List::Util qw(sum reduce);
+
+#Gather
+my $curthread = 0;
+
+while(<>) {
+    if (/THREADS: (\d+)/) {
+	$curthread = $1;
+    }
+    if (/STAT: (\w+)\s+(\d+)/) {
+	push(@{$stats{$curthread}{"$1"} }, $2);
+	$k{"$1"} = 1;
+    }
+}
+
+#output
+foreach my $th (sort keys %stats) {
+    print ",$th";
+} 
+print "\n";
+foreach my $st (sort keys %k) {
+    print "$st";
+    foreach my $th (sort keys %stats) {
+	@values = @{$stats{$th}{$st}};
+	my $avg = sum(@values)/@values;
+	print ",$avg";
+    }
+    print "\n";
+    print "$st Stdev";
+    foreach my $th (sort keys %stats) {
+	@values = @{$stats{$th}{$st}};
+	my $avg = sum(@values)/@values;
+	my $stdev = reduce {$a + ($b - $avg) * ($b - $avg)} 0, @values;
+	$stdev = $stdev / @values;
+	$stdev = sqrt($stdev);
+	print ",$stdev";
+    }
+    print "\n";
+}
+
