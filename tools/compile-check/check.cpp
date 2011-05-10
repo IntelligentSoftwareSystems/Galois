1b6e4b31d36f6ed1664d2493684c03b7577ecaf5
lenharth <lenharth@522c16a8-e32f-0410-b4cc-8071c0b41197>
2011-05-10 17:59:33 +0000

    check rethreading in compile-time WL check
    git-svn-id: https://svn.ices.utexas.edu/repos/iss/projects/GaloisCpp/trunk@7039 522c16a8-e32f-0410-b4cc-8071c0b41197

--- tools/compile-check/check.cpp
+++ tools/compile-check/check.cpp
@@ -3,6 +3,9 @@
 template<typename T>
 struct checker {
   T wl;
+  typename T::template rethread<true>::WL wl2;
+  typename T::template rethread<true>::WL wl3;
+
   checker() {
     int a[2] = {1,2};
     wl.fill_initial(&a[0], &a[2]);
@@ -10,6 +13,19 @@ struct checker {
     wl.pop();
     wl.empty();
     wl.aborted(34);
+
+    wl2.fill_initial(&a[0], &a[2]);
+    wl2.push(0);
+    wl2.pop();
+    wl2.empty();
+    wl2.aborted(34);
+
+    wl3.fill_initial(&a[0], &a[2]);
+    wl3.push(0);
+    wl3.pop();
+    wl3.empty();
+    wl3.aborted(34);
+
   }
 };
 
