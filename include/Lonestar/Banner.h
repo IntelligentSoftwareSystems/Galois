37ec45cfdd666d30d1e1d855b9b901478602ffb9
ddn <ddn@522c16a8-e32f-0410-b4cc-8071c0b41197>
2011-06-15 21:36:00 +0000

    Fix copyright years
    git-svn-id: https://svn.ices.utexas.edu/repos/iss/projects/GaloisCpp/trunk@7122 522c16a8-e32f-0410-b4cc-8071c0b41197

--- include/Lonestar/Banner.h
+++ include/Lonestar/Banner.h
@@ -19,7 +19,7 @@ kind.
 
 void printBanner(std::ostream& OS, const char* app, const char* desc, const char* url) {
   OS << "\nLonestar Benchmark Suite v3.0 (C++)\n"
-       << "Copyright (C) 2007, 2008, 2009, 2010 The University of Texas at Austin\n"
+       << "Copyright (C) 2011 The University of Texas at Austin\n"
        << "http://iss.ices.utexas.edu/lonestar/\n"
        << "\n"
        << "application: " << app << "\n";
