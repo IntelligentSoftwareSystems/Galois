43c6c47d0daa2d64ae691c2a9706a953e2386a0b
ddn <ddn@522c16a8-e32f-0410-b4cc-8071c0b41197>
2011-06-28 01:32:24 +0000

    Fix some unused return value warnings
    git-svn-id: https://svn.ices.utexas.edu/repos/iss/projects/GaloisCpp/trunk@7197 522c16a8-e32f-0410-b4cc-8071c0b41197

--- include/Galois/Graphs/Serialize.h
+++ include/Galois/Graphs/Serialize.h
@@ -1,23 +1,32 @@
-//Write graph structure out to file in V1 structure format -*- C++ -*-
-/*
-Galois, a framework to exploit amorphous data-parallelism in irregular
-programs.
+/** Write graphs out to file -*- C++ -*-
+ * @file
+ * @section License
+ *
+ * Galois, a framework to exploit amorphous data-parallelism in irregular
+ * programs.
+ *
+ * Copyright (C) 2011, The University of Texas at Austin. All rights reserved.
+ * UNIVERSITY EXPRESSLY DISCLAIMS ANY AND ALL WARRANTIES CONCERNING THIS
+ * SOFTWARE AND DOCUMENTATION, INCLUDING ANY WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR ANY PARTICULAR PURPOSE, NON-INFRINGEMENT AND WARRANTIES OF
+ * PERFORMANCE, AND ANY WARRANTY THAT MIGHT OTHERWISE ARISE FROM COURSE OF
+ * DEALING OR USAGE OF TRADE.  NO WARRANTY IS EITHER EXPRESS OR IMPLIED WITH
+ * RESPECT TO THE USE OF THE SOFTWARE OR DOCUMENTATION. Under no circumstances
+ * shall University be liable for incidental, special, indirect, direct or
+ * consequential damages or loss of profits, interruption of business, or
+ * related expenses which may arise from use of Software or Documentation,
+ * including but not limited to those resulting from defects in Software and/or
+ * Documentation, or loss or inaccuracy of data of any kind.
+ *
+ * @author Andrew Lenharth <andrewl@lenharth.org>
+ */
+#ifndef GALOIS_GRAPHS_SERIALIZE_H
+#define GALOIS_GRAPHS_SERIALIZE_H
 
-Copyright (C) 2011, The University of Texas at Austin. All rights reserved.
-UNIVERSITY EXPRESSLY DISCLAIMS ANY AND ALL WARRANTIES CONCERNING THIS SOFTWARE
-AND DOCUMENTATION, INCLUDING ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR ANY
-PARTICULAR PURPOSE, NON-INFRINGEMENT AND WARRANTIES OF PERFORMANCE, AND ANY
-WARRANTY THAT MIGHT OTHERWISE ARISE FROM COURSE OF DEALING OR USAGE OF TRADE.
-NO WARRANTY IS EITHER EXPRESS OR IMPLIED WITH RESPECT TO THE USE OF THE
-SOFTWARE OR DOCUMENTATION. Under no circumstances shall University be liable
-for incidental, special, indirect, direct or consequential damages or loss of
-profits, interruption of business, or related expenses which may arise from use
-of Software or Documentation, including but not limited to those resulting from
-defects in Software and/or Documentation, or loss or inaccuracy of data of any
-kind.
-*/
-
+#include <error.h>
+#include <errno.h>
 #include <sys/stat.h>
+#include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
 
@@ -27,22 +36,30 @@ kind.
 namespace Galois {
 namespace Graph {
 
+//! Writes graph out to binary file
 template<typename Graph>
 bool outputGraph(const char* file, Graph& G) {
   //ASSUME LE machine
   mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
   int fd = open(file, O_WRONLY | O_CREAT |O_TRUNC, mode);
   
+  ssize_t retval;
   //version
   uint64_t tmp64 = 1;
-  write(fd, &tmp64, sizeof(uint64_t));
+  retval = write(fd, &tmp64, sizeof(uint64_t));
+  if (retval == -1)
+    error_at_line(1, errno, __FILE__, __LINE__, "error writing file");
 
   tmp64 = sizeof(typename Graph::EdgeDataTy);
-  write(fd, &tmp64, sizeof(uint64_t));
+  retval = write(fd, &tmp64, sizeof(uint64_t));
+  if (retval == -1)
+    error_at_line(1, errno, __FILE__, __LINE__, "error writing file");
 
   //num nodes
   tmp64 = G.size();
-  write(fd, &tmp64, sizeof(uint64_t));
+  retval = write(fd, &tmp64, sizeof(uint64_t));
+  if (retval == -1)
+    error_at_line(1, errno, __FILE__, __LINE__, "error writing file");
 
   //num edges and outidx computation
   tmp64 = 0;
@@ -58,10 +75,15 @@ bool outputGraph(const char* file, Graph& G) {
     offset += G.neighborsSize(*ii);
     outIdx.push_back(offset);
   }
-  write(fd, &tmp64, sizeof(uint64_t));
+  retval = write(fd, &tmp64, sizeof(uint64_t));
+  if (retval == -1)
+    error_at_line(1, errno, __FILE__, __LINE__, "error writing file");
 
   //outIdx
-  write(fd, &outIdx[0], sizeof(uint64_t) * outIdx.size());
+  retval = write(fd, &outIdx[0], sizeof(uint64_t) * outIdx.size());
+  if (retval == -1)
+    error_at_line(1, errno, __FILE__, __LINE__, "error writing file");
+
 
   //outs
   count = 0;
@@ -70,13 +92,17 @@ bool outputGraph(const char* file, Graph& G) {
     for (typename Graph::neighbor_iterator ni = G.neighbor_begin(*ii),
 	   ne = G.neighbor_end(*ii); ni != ne; ++ni) {
       uint32_t tmp32 = NodeIDs[*ni];
-      write(fd, &tmp32, sizeof(uint32_t));
+      retval = write(fd, &tmp32, sizeof(uint32_t));
+      if (retval == -1)
+        error_at_line(1, errno, __FILE__, __LINE__, "error writing file");
       ++count;
     }
   }
   if (count % 2) {
     uint32_t tmp32 = 0;
-    write(fd, &tmp32, sizeof(uint32_t));
+    retval = write(fd, &tmp32, sizeof(uint32_t));
+    if (retval == -1)
+      error_at_line(1, errno, __FILE__, __LINE__, "error writing file");
   }
 
   //edgeData
@@ -84,7 +110,10 @@ bool outputGraph(const char* file, Graph& G) {
 	 ee = G.active_end(); ii != ee; ++ii) {
     for (typename Graph::neighbor_iterator ni = G.neighbor_begin(*ii),
 	   ne = G.neighbor_end(*ii); ni != ne; ++ni) {
-      write(fd, &G.getEdgeData(*ii, *ni), sizeof(typename Graph::EdgeDataTy));
+      retval = write(fd, &G.getEdgeData(*ii, *ni),
+          sizeof(typename Graph::EdgeDataTy));
+      if (retval == -1)
+        error_at_line(1, errno, __FILE__, __LINE__, "error writing file");
     }
   }
 
@@ -93,7 +122,7 @@ bool outputGraph(const char* file, Graph& G) {
   return true;
 }
 
-//Parsable 
+//! Writes graph out to an ASCII file
 template<typename Graph>
 bool outputTextEdgeData(const char* ofile, Graph& G) {
   std::ofstream file(ofile);
@@ -110,3 +139,4 @@ bool outputTextEdgeData(const char* ofile, Graph& G) {
 
 }
 }
+#endif
