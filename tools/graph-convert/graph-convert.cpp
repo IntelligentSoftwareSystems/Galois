16f536342947badca5f9ec33c607a1439a43134e
ddn <ddn@522c16a8-e32f-0410-b4cc-8071c0b41197>
2011-06-28 23:33:54 +0000

    Reorganization of tools directory
    git-svn-id: https://svn.ices.utexas.edu/repos/iss/projects/GaloisCpp/trunk@7209 522c16a8-e32f-0410-b4cc-8071c0b41197

---
+++ tools/graph-convert/graph-convert.cpp
@@ -0,0 +1,148 @@
+/** Graph converter -*- C++ -*-
+ * @file
+ * @section License
+ *
+ * Galois, a framework to exploit amorphous data-parallelism in irregular
+ * programs.
+ *
+ * Copyright (C) 2011, The University of Texas at Austin. All rights reserved.
+ * UNIVERSITY EXPRESSLY DISCLAIMS ANY AND ALL WARRANTIES CONCERNING THIS
+ * SOFTWARE AND DOCUMENTATION, INCLUDING ANY WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR ANY PARTICULAR PURPOSE, NON-INFRINGEMENT AND WARRANTIES OF
+ * PERFORMANCE, AND ANY WARRANTY THAT MIGHT OTHERWISE ARISE FROM COURSE OF
+ * DEALING OR USAGE OF TRADE.  NO WARRANTY IS EITHER EXPRESS OR IMPLIED WITH
+ * RESPECT TO THE USE OF THE SOFTWARE OR DOCUMENTATION. Under no circumstances
+ * shall University be liable for incidental, special, indirect, direct or
+ * consequential damages or loss of profits, interruption of business, or
+ * related expenses which may arise from use of Software or Documentation,
+ * including but not limited to those resulting from defects in Software and/or
+ * Documentation, or loss or inaccuracy of data of any kind.
+ *
+ * @author Dimitrios Prountzos <dprountz@cs.utexas.edu>
+ */
+#include "Galois/Graphs/LCGraph.h"
+#include "Galois/Graphs/FileGraph.h"
+#include "Galois/Galois.h"
+#include "Galois/IO/gr.h"
+
+#include "Galois/Graphs/Serialize.h"
+
+#include <iostream>
+#include <vector>
+
+static const char* help = "[-rmat | -dimacs] <input file> <output file>";
+
+void readRMAT(const char *infilename, const char *outfilename) {
+  typedef Galois::Graph::FirstGraph<int,int,true> Graph;
+  typedef Graph::GraphNode GNode;
+  Graph graph;
+
+  std::ifstream infile(infilename);
+
+  // Skip to first non-comment line
+  while (true) {
+    if (infile.peek() != '%') {
+      break;
+    }
+    infile.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+  }
+
+  int nnodes, nedges;
+  infile >> nnodes >> nedges;
+  infile.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+  std::vector<GNode> nodes;
+  nodes.resize(nnodes);
+  for (int i = 0; i < nnodes; ++i) {
+    GNode n = graph.createNode(i);
+    nodes[i] = n;
+    graph.addNode(n, Galois::Graph::NONE);
+  }
+
+  for (int edge_num = 0; edge_num < nedges; ++edge_num) {
+    int cur_id, cur_edges;
+    infile >> cur_id >> cur_edges;
+    for (int j = 0; j < cur_edges; ++j) {
+      int neighbor_id, weight;
+      infile >> neighbor_id >> weight;
+      graph.addEdge(nodes[cur_id], nodes[neighbor_id], weight);
+    }
+
+    infile.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+  }
+
+  std::cout << "Finished reading graph. "
+    << "Nodes: " << nnodes << " Edges: " << nedges << "\n";
+
+  outputGraph(outfilename, graph);
+}
+
+void readDIMACS(const char *infilename, const char *outfilename) {
+  typedef Galois::Graph::FirstGraph<int,int,true> Graph;
+  typedef Graph::GraphNode GNode;
+  Graph graph;
+
+  std::ifstream infile(infilename);
+
+  while (true) {
+    if (infile.peek() != 'c') {
+      break;
+    }
+    infile.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+  }
+
+  std::string tmp;
+  int nnodes, nedges;
+  infile >> tmp >> tmp >> nnodes >> nedges;
+  infile.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+  std::vector<GNode> nodes;
+  nodes.resize(nnodes);
+  for (int i = 0; i < nnodes; ++i) {
+    GNode n = graph.createNode(i);
+    nodes[i] = n;
+    graph.addNode(n, Galois::Graph::NONE);
+  }
+
+  for (int edge_num = 0; edge_num < nedges; ++edge_num) {
+    int cur_id, neighbor_id, weight;
+    infile >> tmp >> cur_id >> neighbor_id >> weight;
+    assert(tmp.compare("a") == 0);
+    assert(0 <= cur_id && cur_id < nnodes);
+    assert(0 <= neighbor_id && neighbor_id < nnodes);
+    graph.addEdge(nodes[cur_id], nodes[neighbor_id], weight);
+
+    infile.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+  }
+
+  std::cout << "Finished reading graph. "
+    << "Nodes: " << nnodes << " Edges: " << nedges << "\n";
+
+  outputGraph(outfilename, graph);
+}
+
+int main(int argc, const char** argv) {
+  bool use_dimacs = true;
+  for (int index = 1; index < argc; ++index) {
+    const char* tok = argv[index];
+    if (strcmp(tok, "-help") == 0) {
+      std::cout << help;
+      return 0;
+    } else if (strcmp(tok, "-dimacs") == 0) {
+      use_dimacs = true;
+    } else if (strcmp(tok, "-rmat") == 0) {
+      use_dimacs = false;
+    } else if (argc - index == 2) {
+      if (use_dimacs) 
+        readDIMACS(argv[index], argv[index+1]);
+      else
+        readRMAT(argv[index], argv[index+1]);
+      return 0;
+    } else {
+      std::cerr << "unknown arguments, use -help for usage information\n";
+      return 1;
+    }
+  }
+  std::cerr << help;
+  return 1;
+}
--- tools/random-graph/random-graph.py
+++ tools/random-graph/random-graph.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-Generates random 2d points for use with Delaunay Triangulation.
+Generates random graphs
 
 @section License
 
@@ -17,7 +17,7 @@ of Software or Documentation, including but not limited to those resulting from
 defects in Software and/or Documentation, or loss or inaccuracy of data of any
 kind.
 
-@author Donald Nguyen <ddn@cs.utexas.edu>
+@author Andrew Lenharth <andrewl@lenharth.org>
 """
 
 from __future__ import print_function
@@ -25,29 +25,48 @@ import sys
 import optparse
 import random
 
-def main(npoints, seed, options):
+#my $num = 268435456; #28
+#my $num = 134217728; #27
+#my $num =  67108864; #26
+#my $num =  33554432; #25
+#my $num =  16777216; #24
+#my $num =   8388608; #23
+#my $num =   4194304; #22
+#my $num =   2097152; #21
+
+def main(num_nodes, seed, options):
   random.seed(seed)
-  print('%d 2 0 0' % npoints)
-  if not options.realistic:
-    for i in xrange(npoints):
-      x = random.random() * npoints
-      y = random.random() * npoints
-      # repr to get maximum precision in ASCII'ed representation
-      print('%d %s %s 0' % (i, repr(x), repr(y)))
-  else:
-    # produce spatially coherent bins of about 10000 points each
-    pass
+  max_weight = int(options.max_weight)
+  num_edges = int(options.density) * num_nodes
+
+  print('p sp %d %d' % (num_nodes, num_edges))
+
+  def nextN():
+    return random.randint(0, num_nodes - 1)
+
+  def nextE():
+    return random.randint(1, max_weight)
+
+  # Form a connected graph
+  for index in xrange(num_nodes):
+    print('a %d %d %d' % (index, index + 1, nextE()))
+  print('a %d %d %d' % (num_nodes - 1, 0, nextE()))
+
+  for index in xrange(num_edges - num_nodes):
+    print('a %d %d %d' % (nextN(), nextN(), nextE()))
 
 
 if __name__ == '__main__':
-  usage = 'usage: %prog [options] <num points> <seed>'
+  usage = 'usage: %prog [options] <num nodes> <seed>'
   parser = optparse.OptionParser(usage=usage)
-  parser.add_option('-r', '--realistic', dest="realistic",
-      default=False, action='store_true',
-      help='use a more realistic strategy for generating points')
+  parser.add_option('--max-weight', dest="max_weight",
+      default=10000, action='store',
+      help='edge weights are uniformly selected from integers \
+          between (0, max-weight]')
+  parser.add_option('--density', dest="density",
+      default=4, action='store',
+      help='total number of edges is numnodes * density')
   (options, args) = parser.parse_args()
   if len(args) != 2:
     parser.error('missing arguments')
-  if options.realistic:
-    parser.error('not yet implemented')
   main(int(args[0]), int(args[1]), options)
