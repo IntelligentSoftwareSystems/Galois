a7666151cbbb4aa4e236d5f434c0d3049acfd8e2
lenharth <lenharth@522c16a8-e32f-0410-b4cc-8071c0b41197>
2011-06-29 21:53:24 +0000

    Async SP
    git-svn-id: https://svn.ices.utexas.edu/repos/iss/projects/GaloisCpp/trunk@7214 522c16a8-e32f-0410-b4cc-8071c0b41197

--- apps/surveypropagation/SurveyPropagation.cpp
+++ apps/surveypropagation/SurveyPropagation.cpp
@@ -1,26 +1,3 @@
-/** Survey propagation -*- C++ -*-
- * @file
- * @section License
- *
- * Galois, a framework to exploit amorphous data-parallelism in irregular
- * programs.
- *
- * Copyright (C) 2011, The University of Texas at Austin. All rights reserved.
- * UNIVERSITY EXPRESSLY DISCLAIMS ANY AND ALL WARRANTIES CONCERNING THIS
- * SOFTWARE AND DOCUMENTATION, INCLUDING ANY WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR ANY PARTICULAR PURPOSE, NON-INFRINGEMENT AND WARRANTIES OF
- * PERFORMANCE, AND ANY WARRANTY THAT MIGHT OTHERWISE ARISE FROM COURSE OF
- * DEALING OR USAGE OF TRADE.  NO WARRANTY IS EITHER EXPRESS OR IMPLIED WITH
- * RESPECT TO THE USE OF THE SOFTWARE OR DOCUMENTATION. Under no circumstances
- * shall University be liable for incidental, special, indirect, direct or
- * consequential damages or loss of profits, interruption of business, or
- * related expenses which may arise from use of Software or Documentation,
- * including but not limited to those resulting from defects in Software and/or
- * Documentation, or loss or inaccuracy of data of any kind.
- *
- * @author Martin Burtscher <burtscher@txstate.edu>
- * @author Andrew Lenharth <andrewl@lenharth.org>
- */
 #include "Galois/Timer.h"
 #include "Galois/Graphs/Graph.h"
 #include "Galois/Galois.h"
@@ -31,11 +8,12 @@
 
 #include <cstdlib>
 
+using namespace std;
+
 static const char* name = "Survey Propagation";
 static const char* description = "Solves SAT problems using survey propagation\n";
-static const char* url = 
-  "http://iss.ices.utexas.edu/lonestar/surveypropagation.html";
-static const char* help = "<seed> <num_clauses> <num_variables> <k>";
+static const char* url = "http://iss.ices.utexas.edu/lonestar/surveypropagation.html";
+static const char* help = "seed clauses variables k";
 
 
 //SAT problem:
@@ -72,30 +50,10 @@ static const char* help = "<seed> <num_clauses> <num_variables> <k>";
 //  a pi product
 //Graph is undirected (for now)
 
-struct literal {
-  int name;
-  bool solved;
-  bool value;
-  std::vector<int> functions;
-  literal() :name(0), solved(0), value(0) {}
-};
-
-struct clause {
-  int name;
-  //var num, negated
-  std::vector<std::pair<int, bool> > variables;
-  clause() :name(0) {}
-};
-
-std::vector<clause> clauses;
-std::vector<literal> literals;
-
 struct SPEdge {
-  double PIu;
-  double PIs;
-  double PI0;
   double eta;
   bool isNegative;
+
   SPEdge() {}
   SPEdge(bool isNeg) :isNegative(isNeg) {
     eta = (double)rand() / (double)RAND_MAX;
@@ -103,34 +61,31 @@ struct SPEdge {
 };
 
 struct SPNode {
-  literal* lit;
-  clause* cla;
-  double BiasP;
-  double BiasN;
-  double Bias0;
+  bool isClause;
+  int name;
+  bool solved;
+  bool value;
+  
   double Bias;
-  SPNode(literal* l) :lit(l), cla(0) {}
-  SPNode(clause* c) :lit(0), cla(c) {}
-};
 
+  SPNode(int n, bool b) :isClause(b), name(n), solved(false), value(false) {}
+};
 
 typedef Galois::Graph::FirstGraph<SPNode, SPEdge, false> Graph;
 typedef Galois::Graph::FirstGraph<SPNode, SPEdge, false>::GraphNode GNode;
 
 static Graph graph;
 
-static std::vector<GNode> gliterals;
-static std::vector<GNode> gclauses;
+static std::vector<GNode> literals;
+static std::vector<GNode> clauses;
 
-static Galois::accumulator<unsigned int> unconverged;
 static Galois::accumulator<unsigned int> nontrivial;
 
 static Galois::reduce_max<double> maxBias;
 static Galois::reduce_average<double> averageBias;
 
 //interesting parameters:
-static const double epsilon = 0.00001;
-static const int tmax = 100;
+static const double epsilon = 0.000001;
 
 void initalize_random_formula(int M, int N, int K) {
   //M clauses
@@ -141,10 +96,16 @@ void initalize_random_formula(int M, int N, int K) {
   clauses.resize(M);
   literals.resize(N);
 
-  for (int m = 0; m < M; ++m)
-    clauses[m].name = m;
-  for (int n = 0; n < N; ++n)
-    literals[n].name = n;
+  for (int m = 0; m < M; ++m) {
+    GNode N = graph.createNode(SPNode(m, true));
+    graph.addNode(N, Galois::Graph::NONE);
+    clauses[m] = N;
+  }
+  for (int n = 0; n < N; ++n) {
+    GNode N = graph.createNode(SPNode(n, false));
+    graph.addNode(N, Galois::Graph::NONE);
+    literals[n] = N;
+  }
 
   for (int m = 0; m < M; ++m) {
     //Generate K unique values
@@ -155,8 +116,7 @@ void initalize_random_formula(int M, int N, int K) {
       if (std::find(touse.begin(), touse.end(), newK) 
 	  == touse.end()) {
 	touse.push_back(newK);
-	clauses[m].variables.push_back(std::make_pair(newK, (bool) (rand() % 2)));
-	literals[newK].functions.push_back(m);
+	graph.addEdge(clauses[m], literals[newK], SPEdge((bool)(rand() % 2)), Galois::Graph::NONE);
       }
     }
   }
@@ -167,15 +127,19 @@ void print_formula() {
     if (m != 0)
       std::cout << " & ";
     std::cout << "c" << m << "( ";
-    for (unsigned k = 0; k < clauses[m].variables.size(); ++k) {
-      if (k != 0)
+    GNode N = clauses[m];
+    for (Graph::neighbor_iterator ii = graph.neighbor_begin(N, Galois::Graph::NONE), ee = graph.neighbor_end( N, Galois::Graph::NONE); ii != ee; ++ii) {
+      if (ii != graph.neighbor_begin(N, Galois::Graph::NONE))
 	std::cout << " | ";
-      if (clauses[m].variables[k].second)
+      SPEdge& E = graph.getEdgeData(N, ii, Galois::Graph::NONE);
+      if (E.isNegative)
 	std::cout << "-";
-      int n = clauses[m].variables[k].first;
-      std::cout << "v" << n;
-      if (literals[n].solved)
-	std::cout << "[" << (literals[n].value ? 1 : 0) << "]";
+      
+      SPNode& V = graph.getData(*ii, Galois::Graph::NONE);
+      std::cout << "v" << V.name;
+      if (V.solved)
+	std::cout << "[" << (V.value ? 1 : 0) << "]";
+      std::cout << "{" << E.eta << "," << V.Bias << "," << (V.value ? 1 : 0) << "}";
       std::cout << " ";
     }
     std::cout << " )";
@@ -183,134 +147,86 @@ void print_formula() {
   std::cout << "\n";
 }
 
-//transform the clauses and literals into a graph
-void build_graph() {
-
-  gliterals.resize(literals.size());
-  for (unsigned i = 0; i < literals.size(); ++i) {
-    GNode N = graph.createNode(&literals[i]);
-    graph.addNode(N, Galois::Graph::NONE);
-    gliterals[i] = N;
-  }
-
-  gclauses.resize(clauses.size());
-  for (unsigned i = 0; i < clauses.size(); ++i) {
-    GNode N = graph.createNode(&clauses[i]);
-    graph.addNode(N, Galois::Graph::NONE);
-    gclauses[i] = N;
-    for (unsigned j = 0; j < clauses[i].variables.size(); ++j)
-      graph.addEdge(N, gliterals[clauses[i].variables[j].first],
-          SPEdge(clauses[i].variables[j].second), Galois::Graph::NONE);
-  }
-}
-
-void print_graph() {
-  for (unsigned i = 0; i < clauses.size(); ++i) {
-    GNode N = gclauses[i];
-    for (Graph::neighbor_iterator
-        ii = graph.neighbor_begin(N, Galois::Graph::NONE),
-        ee = graph.neighbor_end(N, Galois::Graph::NONE); ii != ee; ++ii) {
-      std::cout << "c"
-	<< graph.getData(N, Galois::Graph::NONE).cla->name
-	<< "->v"
-	<< graph.getData(*ii, Galois::Graph::NONE).lit->name
-	<< " eta: "
-	<< graph.getEdgeData(N, *ii, Galois::Graph::NONE).eta
-	<< " PI[u,s,0]: "
-	<< graph.getEdgeData(N, *ii, Galois::Graph::NONE).PIu
-	<< " "
-	<< graph.getEdgeData(N, *ii, Galois::Graph::NONE).PIs
-	<< " "
-	<< graph.getEdgeData(N, *ii, Galois::Graph::NONE).PI0
-	<< "\n";
-    }
+void print_fixed() {
+  for (unsigned n = 0; n < literals.size(); ++n) {
+    GNode N = literals[n];
+    SPNode& V = graph.getData(N, Galois::Graph::NONE);
+    if (V.solved)
+      std::cout << V.name << "[" << (V.value ? 1 : 0) << "] ";
   }
   std::cout << "\n";
 }
 
-static bool isVu(bool JajNegative, bool JbjNegative) {
-  if (JajNegative)
-    return JbjNegative;
-  else
-    return !JbjNegative;
-}
-
-static bool isVs(bool JajNegative, bool JbjNegative) {
-  if (JajNegative)
-    return !JbjNegative;
-  else
-    return JbjNegative;
-}
-
-//Update all pi products on all edges of the variable j
-struct update_pi {
-  template<typename Context>
-  void operator()(GNode j, const Context& ctx) {
-    assert(j.getData(Galois::Graph::NONE).lit);
-    
-    //for each function a
-    for (Graph::neighbor_iterator
-        aii = graph.neighbor_begin(j, Galois::Graph::NONE),
-        aee = graph.neighbor_end(j, Galois::Graph::NONE); aii != aee; ++aii) {
-      SPEdge& jae = graph.getEdgeData(j, *aii, Galois::Graph::NONE);
-      //Now we have j->a, compute each pi product
-      double prodVuu = 1.0;
-      double prodVus = 1.0;
-      double prodVss = 1.0;
-      double prodVsu = 1.0;
-      double prodVa = 1.0;
-      //for each b
-      for (Graph::neighbor_iterator 
-          bii = graph.neighbor_begin(j, Galois::Graph::NONE),
-          bee = graph.neighbor_end(j, Galois::Graph::NONE); bii != bee; ++bii) {
-	SPEdge& bje = graph.getEdgeData(j, *bii, Galois::Graph::NONE);
-	double etabj = bje.eta;
-	if (bii != aii) { //all of these terms ignore the source
-	  if (isVu(jae.isNegative, bje.isNegative)) {
-	    prodVuu *= (1.0 - etabj); //1st term
-	    prodVsu *= (1.0 - etabj); //2nd term
-	  } else {
-	    prodVus *= (1.0 - etabj); //1st term
-	    prodVss *= (1.0 - etabj); //2nd term
-	  }
-	  //3rd term
-	  prodVa *= (1 - etabj);
-	}
-      }
-      jae.PIu = (1.0 - prodVuu) * prodVus;
-      jae.PIs = (1.0 - prodVss) * prodVsu;
-      jae.PI0 = prodVa;
-    }
+int count_fixed() {
+  int retval = 0;
+  for (unsigned n = 0; n < literals.size(); ++n) {
+    GNode N = literals[n];
+    SPNode& V = graph.getData(N, Galois::Graph::NONE);
+    if (V.solved)
+      ++retval;
   }
-};
+  return retval;
+}
 
-//update all eta products (surveys) on all edges of the function a
 struct update_eta {
+
+  double eta_for_a_i(GNode a, GNode i) {
+    double etaNew = 1.0;
+    //for each j
+    for (Graph::neighbor_iterator jii = graph.neighbor_begin(a), 
+	   jee = graph.neighbor_end(a); jii != jee; ++jii) {
+      GNode j = *jii;
+      if (j != i) {
+	bool ajNegative = graph.getEdgeData(a,jii).isNegative;
+	double prodP = 1.0;
+	double prodN = 1.0;
+	double prod0 = 1.0;
+	//for each b
+	for (Graph::neighbor_iterator bii = graph.neighbor_begin(j), 
+	       bee = graph.neighbor_end(j); 
+	     bii != bee; ++bii) {
+	  GNode b = *bii;
+	  SPEdge& Ebj = graph.getEdgeData(j, bii);
+	  if (b != a)
+	    prod0 *= (1.0 - Ebj.eta);
+	  if (Ebj.isNegative)
+	    prodN *= (1.0 - Ebj.eta);
+	  else
+	    prodP *= (1.0 - Ebj.eta);
+	}
+	double PIu, PIs;
+	if (ajNegative) {
+	  PIu = (1.0 - prodN) * prodP;
+	  PIs = (1.0 - prodP) * prodN;
+	} else {
+	  PIs = (1.0 - prodN) * prodP;
+	  PIu = (1.0 - prodP) * prodN;
+	}
+	double PI0 = prod0;
+	etaNew *= (PIu / (PIu + PIs + PI0));
+      }
+    }
+    return etaNew;
+  }
+
   template<typename Context>
-  void operator()(GNode a, const Context& ctx) {
-    assert(a.getData(Galois::Graph::NONE).cla);
-    
+  void operator()(GNode a, Context& ctx) {
     //for each i
-    for (Graph::neighbor_iterator 
-        iii = graph.neighbor_begin(a, Galois::Graph::NONE),
-        iee = graph.neighbor_end(a, Galois::Graph::NONE); iii != iee; ++iii) {
-      SPEdge& aie = graph.getEdgeData(a, *iii, Galois::Graph::NONE);
-      double prod = 1.0;
-      //for each j
-      for (Graph::neighbor_iterator
-          jii = graph.neighbor_begin(a, Galois::Graph::NONE),
-          jee = graph.neighbor_end(a, Galois::Graph::NONE); jii != jee; ++jii) {
-	if (jii != iii) { //ignore i
-	  SPEdge& jae = graph.getEdgeData(a, *jii, Galois::Graph::NONE);
-	  prod *= (jae.PIu / (jae.PIu + jae.PIs + jae.PI0));
+    for (Graph::neighbor_iterator iii = graph.neighbor_begin(a), 
+	   iee = graph.neighbor_end(a); iii != iee; ++iii) {
+      GNode i = *iii;
+      double e = eta_for_a_i(a, i);
+      double olde = graph.getEdgeData(a,iii).eta;
+      graph.getEdgeData(a,iii).eta = e;
+      //std::cout << olde << ',' << e << " ";
+      if (fabs(olde - e) > epsilon) {
+	for (Graph::neighbor_iterator bii = graph.neighbor_begin(i), 
+	   bee = graph.neighbor_end(i); bii != bee; ++bii) {
+	  GNode b = *bii;
+	  if (a != b)
+	    ctx.push(b);
 	}
       }
-      double delta = aie.eta < prod ? prod - aie.eta : aie.eta - prod;
-      if (delta > epsilon)
-	unconverged += 1;
-      if (prod > epsilon)
-	nontrivial += 1;
-      aie.eta = prod;
     }
   }
 };
@@ -320,8 +236,8 @@ struct update_biases {
   template<typename Context>
   void operator()(GNode i, const Context& ctx) {
     SPNode& idata = i.getData(Galois::Graph::NONE);
-    assert(idata.lit);
-    
+    if (idata.solved) return;
+
     double pp1 = 1.0;
     double pp2 = 1.0;
     double pn1 = 1.0;
@@ -329,12 +245,12 @@ struct update_biases {
     double p0 = 1.0;
 
     //for each function a
-    for (Graph::neighbor_iterator
-        aii = graph.neighbor_begin(i, Galois::Graph::NONE),
-        aee = graph.neighbor_end(i, Galois::Graph::NONE); aii != aee; ++aii) {
+    for (Graph::neighbor_iterator aii = graph.neighbor_begin(i, Galois::Graph::NONE), aee = graph.neighbor_end(i, Galois::Graph::NONE); aii != aee; ++aii) {
       SPEdge& aie = graph.getEdgeData(i, *aii, Galois::Graph::NONE);
 
       double etaai = aie.eta;
+      if (etaai > epsilon)
+	nontrivial += 1;
       if (aie.isNegative) {
 	pp2 *= (1.0 - etaai);
 	pn1 *= (1.0 - etaai);
@@ -347,59 +263,35 @@ struct update_biases {
     double pp = (1.0 - pp1) * pp2;
     double pn = (1.0 - pn1) * pn2;
     
-    idata.BiasP = pp / (pp + pn + p0);
-    idata.BiasN = pn / (pp + pn + p0);
-    idata.Bias0 = 1.0 - idata.BiasP - idata.BiasN;
+    double BiasP = pp / (pp + pn + p0);
+    double BiasN = pn / (pp + pn + p0);
+    //    double Bias0 = 1.0 - BiasP - BiasN;
 
-    double d = idata.BiasP - idata.BiasN;
+    double d = BiasP - BiasN;
     if (d < 0.0)
-      d = idata.BiasN - idata.BiasP;
+      d = BiasN - BiasP;
     idata.Bias = d;
+    idata.value = (BiasP > BiasN);
 
+    assert(d != NAN && -d != NAN);
     maxBias.insert(d);
     averageBias.insert(d);
   }
 };
 
 //return true if converged
-bool SP_algorithm() {
-  //0) at t = 0, for every edge a->i, randomly initialize the
-  //   message sigma a->i(t=0) in [0,1]
+void SP_algorithm() {
+  //0) at t = 0, for every edge a->i, randomly initialize the message sigma a->i(t=0) in [0,1]
   //1) for t = 1 to tmax:
-  //1.1) sweep the set of edges in a random order, and update sequentially
-  //     the warnings on all the edges of the graph, generating the values
-  //     sigma a->i (t) using SP_update
-  //1.2) if (|sigma a->i(t) - sigma a->i (t-1) < E on all the edges, the
-  //     iteration has converged and generated sigma* a->i = sigma a->i(t),
-  //     goto 2
-  //2) if t = tmax return un-converged.  if (t < tmax) then return the set
-  //   of fixed point warnings sigma* a->i = sigma a->i (t)
+  //1.1) sweep the set of edges in a random order, and update sequentially the warnings on all the edges of the graph, generating the values sigma a->i (t) using SP_update
+  //1.2) if (|sigma a->i(t) - sigma a->i (t-1) < E on all the edges, the iteration has converged and generated sigma* a->i = sigma a->i(t), goto 2
+  //2) if t = tmax return un-converged.  if (t < tmax) then return the set of fixed point warnings sigma* a->i = sigma a->i (t)
   
-  int x = tmax;
-  do {
-    unconverged.reset(0);
-    nontrivial.reset(0);
-    --x;
-
-    // for (unsigned int i = 0; i < gliterals.size(); ++i)
-    //   update_pi()(gliterals[i], 0);
-
-    Galois::for_each(gliterals.begin(), gliterals.end(), update_pi());
-    
-    //print_graph();
-    
-    // for (unsigned int i = 0; i < gclauses.size(); ++i)
-    //   update_eta()(gclauses[i], 0);
-
-    Galois::for_each(gclauses.begin(), gclauses.end(), update_eta());
-   
-    //print_graph();
-
-    std::cout << "U: " << unconverged.get() << " NT: " << nontrivial.get() << "\n";
-
-  } while (x && unconverged.get());
-
-  return unconverged.get() == 0;
+  Galois::for_each(clauses.begin(), clauses.end(), update_eta());
+  maxBias.reset(0.0);
+  averageBias.reset(0.0);
+  nontrivial.reset(0);
+  Galois::for_each(literals.begin(), literals.end(), update_biases());
 }
 
 struct fix_variables {
@@ -407,22 +299,27 @@ struct fix_variables {
   fix_variables(double d) :limit(d) {}
   template<typename Context>
   void operator()(GNode i, const Context& ctx) {
-    SPNode& idata = i.getData(Galois::Graph::NONE);
+    SPNode& idata = i.getData();
+    if (idata.solved) return;
     if (idata.Bias > limit) {
-      idata.lit->solved = true;
-      idata.lit->value = (idata.BiasP > idata.BiasN);
+      idata.solved = true;
       //TODO: simplify graph
+      //for each b
+      for (Graph::neighbor_iterator bii = graph.neighbor_begin(i), 
+	     bee = graph.neighbor_end(i); 
+	   bii != bee; ++bii) {
+	bii->getData().solved = true;
+	bii->getData().value = true;
+      }
+      graph.removeNode(i);
     }
   }
 };
 
 void decimate() {
-  maxBias.reset(0.0);
-  averageBias.reset(0.0);
-  Galois::for_each(gliterals.begin(), gliterals.end(), update_biases());
-  std::cout << "\n" << maxBias.get() << " " << averageBias.get() << "\n";
+  std::cout << "\nNonTrivial " << nontrivial.get() << " MaxBias " << maxBias.get() << " Average Bias " << averageBias.get() << "\n";
   double d = ((maxBias.get() - averageBias.get()) * 0.25) + averageBias.get();
-  Galois::for_each(gliterals.begin(), gliterals.end(), fix_variables(d));
+  Galois::for_each(literals.begin(), literals.end(), fix_variables(d));
 }
 
 bool survey_inspired_decimation() {
@@ -436,21 +333,17 @@ bool survey_inspired_decimation() {
   //   b) fix largest |W+ - W-| to x =  W+ > W-
   //   c) clean the graph
   //2.2) if all surveys are trivial(n = 0), output simplified subformula
-  //4) if solved, output SAT, if no contradiction, continue at 1, if
-  //   contridiction, stop
+  //4) if solved, output SAT, if no contradiction, continue at 1, if contridiction, stop
   do {
-    if (SP_algorithm()) {
-      if (nontrivial.get()) {
-	decimate();
-      } else {
-	std::cout << "SIMPLIFIED\n";
-	return false;
-      }
+    SP_algorithm();
+    if (nontrivial.get()) {
+      std::cout << "DECIMATED\n";
+      decimate();
     } else {
-      std::cout << "UNCONVERGED\n";
-      return false; // unconverged
+      std::cout << "SIMPLIFIED\n";
+      return false;
     }
-  } while (false); // while (true);
+  } while (true); // while (true);
   return true;
 }
 
@@ -473,12 +366,17 @@ int main(int argc, const char** argv) {
 
   initalize_random_formula(M,N,K);
   //print_formula();
-  build_graph();
+  //build_graph();
   //print_graph();
 
+  Galois::startTiming();
   survey_inspired_decimation();
+  Galois::stopTiming();
 
   //print_formula();
+  //print_fixed();
+
+  std::cout << "Fixed " << count_fixed() << " variables\n";
 
   return 0;
 }
