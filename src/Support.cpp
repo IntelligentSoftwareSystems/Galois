b08d9697bfd8b62d6232e70451786ada012cdf7d
ddn <ddn@522c16a8-e32f-0410-b4cc-8071c0b41197>
2011-07-01 23:42:38 +0000

    Encapsulation is fun!
    git-svn-id: https://svn.ices.utexas.edu/repos/iss/projects/GaloisCpp/trunk@7252 522c16a8-e32f-0410-b4cc-8071c0b41197

--- src/Support.cpp
+++ src/Support.cpp
@@ -30,72 +30,52 @@
 #include <cmath>
 #include <cstdio>
 
-typedef std::pair<const char*, const char*> strPair;
 static GaloisRuntime::SimpleLock<int, true> lock;
-typedef std::map<strPair, unsigned long> StatsMap;
-static StatsMap* stats;
-typedef std::map<int, std::vector<unsigned long> > DistStatsValue;
-typedef std::map<strPair, DistStatsValue> DistStatsMap;
-static DistStatsMap* distStats;
-static int gcounter = 0;
-
-typedef std::vector<unsigned long>::iterator vli;
-static void summarizeList(int iternum, const char* first, const char* second, vli b, vli e) {
-  long size = std::distance(b,e);
-  long min = *std::min_element(b, e);
-  long max = *std::max_element(b, e);
-  double ave = std::accumulate(b, e, 0.0) / size;
- 
-  double acc = 0.0;
-  for (vli it = b; it != e; ++it) {
-    acc += (*it - ave) * (*it - ave);
-  }
-
-  double stdev = 0.0;
-  if (size > 1) {
-    stdev = sqrt(acc / (size - 1));
-  }
-
-  printf("STAT DISTRIBUTION %d %s %s n: %ld ave: %.1f min: %ld max: %ld stdev: %.1f\n",
-	 iternum, first, second, size, ave, min, max, stdev);
-}
-
-static void genericReport(bool error, const char* text1,
-    const char* text2, const char* val) {
-  lock.lock();
-  FILE *out = error ? stderr : stdout;
-  fprintf(out, "%s %s %s\n", text1, text2, val);
-  lock.unlock();
-}
-
-void GaloisRuntime::reportStatSum(const char* text, unsigned long val, const char* loopname) {
-  if (!stats)
-    stats = new StatsMap();
-  (*stats)[std::make_pair(text,loopname)] += val;
-}
-
-void GaloisRuntime::reportStatAvg(const char* text, unsigned long val, const char* loopname) {
-  if (!distStats)
-    distStats = new DistStatsMap();
-  (*distStats)[std::make_pair(text,loopname)][gcounter].push_back(val);
-}
-
-void GaloisRuntime::statDone() {
-  ++gcounter;
-}
 
 namespace {
 class PrintStats {
+  typedef std::pair<const char*, const char*> strPair;
+  typedef std::map<strPair, unsigned long> StatsMap;
+  typedef std::map<int, std::vector<unsigned long> > DistStatsValue;
+  typedef std::map<strPair, DistStatsValue> DistStatsMap;
+  typedef std::vector<unsigned long>::iterator long_iterator;
+  StatsMap stats;
+  DistStatsMap distStats;
+  int gcounter;
+
+  void summarizeList(int iternum, const char* first, const char* second,
+      long_iterator b, long_iterator e) {
+    long size = std::distance(b,e);
+    long min = *std::min_element(b, e);
+    long max = *std::max_element(b, e);
+    double ave = std::accumulate(b, e, 0.0) / size;
+   
+    double acc = 0.0;
+    for (long_iterator it = b; it != e; ++it) {
+      acc += (*it - ave) * (*it - ave);
+    }
+
+    double stdev = 0.0;
+    if (size > 1) {
+      stdev = sqrt(acc / (size - 1));
+    }
+
+    printf("STAT DISTRIBUTION %d %s %s n: %ld ave: %.1f "
+        "min: %ld max: %ld stdev: %.1f\n",
+           iternum, first, second, size, ave, min, max, stdev);
+  }
+
 public:
+  PrintStats() : gcounter(0) { }
   ~PrintStats() {
-    for (StatsMap::iterator ii = stats->begin(), ee = stats->end();
+    for (StatsMap::iterator ii = stats.begin(), ee = stats.end();
         ii != ee; ++ii) {
       printf("STAT SINGLE %s %s %ld\n",
           ii->first.first, 
           ii->first.second ? ii->first.second : "(null)",
           ii->second);
     }
-    for (DistStatsMap::iterator ii = distStats->begin(), ee = distStats->end();
+    for (DistStatsMap::iterator ii = distStats.begin(), ee = distStats.end();
         ii != ee; ++ii) {
       for (DistStatsValue::iterator i = ii->second.begin(), e = ii->second.end();
           i != e; ++i) {
@@ -106,12 +86,48 @@ public:
             i->second.end());
       } 
     }
-    delete stats;
-    delete distStats;
+  }
+
+  void reportStatAvg(const char* text, unsigned long val, const char* loopname) {
+    distStats[std::make_pair(text,loopname)][gcounter].push_back(val);
+  }
+
+  void reportStatSum(const char* text, unsigned long val, const char* loopname) {
+    stats[std::make_pair(text,loopname)] += val;
+  }
+
+  void incIteration() {
+    ++gcounter;
   }
 };
 }
-PrintStats P;
+PrintStats* P;
+
+static void genericReport(bool error, const char* text1,
+    const char* text2, const char* val) {
+  lock.lock();
+  FILE *out = error ? stderr : stdout;
+  fprintf(out, "%s %s %s\n", text1, text2, val);
+  lock.unlock();
+}
+
+void GaloisRuntime::reportStatSum(const char* text, unsigned long val, const char* loopname) {
+  if (!P)
+    P = new PrintStats;
+  P->reportStatSum(text, val, loopname);
+}
+
+void GaloisRuntime::reportStatAvg(const char* text, unsigned long val, const char* loopname) {
+  if (!P)
+    P = new PrintStats;
+  P->reportStatAvg(text, val, loopname);
+}
+
+void GaloisRuntime::statDone() {
+  if (!P)
+    P = new PrintStats;
+  P->incIteration();
+}
 
 //Report Warnings
 void GaloisRuntime::reportWarning(const char* text) {
