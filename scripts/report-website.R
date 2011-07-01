bcf52646dae31bc9e99fb6942106fd0bcc191eb2
ddn <ddn@522c16a8-e32f-0410-b4cc-8071c0b41197>
2011-07-01 19:19:39 +0000

    - update status - report-website optionally shows serial time
    git-svn-id: https://svn.ices.utexas.edu/repos/iss/projects/GaloisCpp/trunk@7244 522c16a8-e32f-0410-b4cc-8071c0b41197

--- scripts/report-website.R
+++ scripts/report-website.R
@@ -7,7 +7,7 @@ library(ggplot2)
 args <- commandArgs(trailingOnly=TRUE)
 
 if (length(args) < 1) {
-  cat("usage: Rscript report-website.R <report.csv>\n")
+  cat("usage: Rscript report-website.R <report.csv> [serial time]\n")
   quit(save = "no", status=1)
 } 
 
@@ -15,6 +15,9 @@ df <- read.csv(args[1])
 
 T1 <- mean(subset(df, Threads == 1)$Time)
 qplot(Threads, Time, data=df) + geom_line() + ylim(0, max(df$Time))
+if (length(args) > 1) {
+  last_plot() + geom_hline(yintercept=as.numeric(args[2]))
+}
 ggsave("time.png", width=4, height=3, dpi=100)
 qplot(Threads, T1 / Time, data=df) + geom_smooth()
 ggsave("speedup.png", width=4, height=3, dpi=100)
