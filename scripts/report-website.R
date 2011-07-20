51b4d3483e47f752bd741a52d4f5b39576ba1813
lenharth <lenharth@522c16a8-e32f-0410-b4cc-8071c0b41197>
2011-07-20 15:54:42 +0000

    Give axis labels for website pictures
    git-svn-id: https://svn.ices.utexas.edu/repos/iss/projects/GaloisCpp/trunk@7401 522c16a8-e32f-0410-b4cc-8071c0b41197

--- scripts/report-website.R
+++ scripts/report-website.R
@@ -14,10 +14,10 @@ if (length(args) < 1) {
 df <- read.csv(args[1])
 
 T1 <- mean(subset(df, Threads == 1)$Time)
-qplot(Threads, Time, data=df) + geom_line() + ylim(0, max(df$Time))
+qplot(Threads, Time, data=df,ylab="Time (ms)") + geom_line() + ylim(0, max(df$Time))
 if (length(args) > 1) {
   last_plot() + geom_hline(yintercept=as.numeric(args[2]))
 }
 ggsave("time.png", width=4, height=3, dpi=100)
-qplot(Threads, T1 / Time, data=df) + geom_smooth()
+qplot(Threads, T1 / Time, data=df,ylab="Self-relative Speedup") + geom_smooth()
 ggsave("speedup.png", width=4, height=3, dpi=100)
