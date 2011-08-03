dee934f21ba2349396fd12e3326c517f1fef395b
ddn <ddn@522c16a8-e32f-0410-b4cc-8071c0b41197>
2011-08-03 20:26:54 +0000

    Added support for cpack just in case we want a more traditional build Added script to make tarballs for simple distribution
    git-svn-id: https://svn.ices.utexas.edu/repos/iss/projects/GaloisCpp/trunk@7468 522c16a8-e32f-0410-b4cc-8071c0b41197

---
+++ scripts/make_dist.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Make distribution tarball
+
+NAME="Galois-2.1.0"
+
+if [[ ! -e COPYRIGHT ]]; then
+  echo "Run this from the root source directory" 1>&2
+  exit 1
+fi
+
+touch "$NAME.tar.gz" # Prevent . from changing during tar
+(svn status | grep '^\?' | sed -e 's/^\? *//'; \
+  echo "*.swp"; \
+  echo "status.html"; \
+  echo "*~"; \
+  echo "$NAME.tar.gz") | \
+  tar --exclude-from=- --exclude-vcs --transform "s,^\./,$NAME/," -cz -f "$NAME.tar.gz" .
