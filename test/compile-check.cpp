42057023bd52c70227d85bba248ed4340ee8a844
lenharth <lenharth@522c16a8-e32f-0410-b4cc-8071c0b41197>
2011-09-07 21:52:26 +0000

    don't test things we don't use
    git-svn-id: https://svn.ices.utexas.edu/repos/iss/projects/GaloisCpp/trunk@7839 522c16a8-e32f-0410-b4cc-8071c0b41197

--- test/compile-check.cpp
+++ test/compile-check.cpp
@@ -32,20 +32,14 @@ struct checker {
     wl.fill_initial(&a[0], &a[2]);
     wl.push(0);
     wl.pop();
-    wl.empty();
-    wl.aborted(34);
 
     wl2.fill_initial(&a[0], &a[2]);
     wl2.push(0);
     wl2.pop();
-    wl2.empty();
-    wl2.aborted(34);
 
     wl3.fill_initial(&a[0], &a[2]);
     wl3.push(0);
     wl3.pop();
-    wl3.empty();
-    wl3.aborted(34);
   }
 };
 
