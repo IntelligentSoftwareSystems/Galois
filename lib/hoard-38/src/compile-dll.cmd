64a18ff34a95360d68db6911a8910bca61ee8093
ddn <ddn@522c16a8-e32f-0410-b4cc-8071c0b41197>
2011-12-23 18:39:19 +0000

    Remove hoard
    git-svn-id: https://svn.ices.utexas.edu/repos/iss/projects/GaloisCpp/trunk@8926 522c16a8-e32f-0410-b4cc-8071c0b41197

--- lib/hoard-38/src/compile-dll.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Building libhoard.dll and libhoard.lib (ignore linker warnings).
-: We need to execute this if we change libhoard.cpp.
-@echo off
-cl /Iheaplayers /Iheaplayers/util /c /MD /DNDEBUG /Ox /Zp8 libhoard.cpp
-cl /Zi /LD libhoard.obj /o libhoard.dll /Ox /link /def:libhoard.def /force:multiple kernel32.lib /subsystem:console /entry:_DllMainCRTStartup@12
-: Embed the manifest in the DLL.
-mt -manifest libhoard.dll.manifest -outputresource:libhoard.dll;2
- 
