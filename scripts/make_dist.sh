df135547ee8f9a8d4762365ea38e08b6156c842e
ddn <ddn@522c16a8-e32f-0410-b4cc-8071c0b41197>
2012-01-09 18:07:23 +0000

    Update version (again)
    git-svn-id: https://svn.ices.utexas.edu/repos/iss/projects/GaloisCpp/trunk@8956 522c16a8-e32f-0410-b4cc-8071c0b41197

--- scripts/make_dist.sh
+++ scripts/make_dist.sh
@@ -2,7 +2,7 @@
 #
 # Make distribution tarball
 
-NAME="Galois-2.1.2"
+NAME="Galois-2.1.3"
 
 if [[ ! -e COPYRIGHT ]]; then
   echo "Run this from the root source directory" 1>&2
@@ -12,10 +12,9 @@ fi
 touch "$NAME.tar.gz" # Prevent . from changing during tar
 (svn status | grep '^\?' | sed -e 's/^\? *//'; \
   echo "*.swp"; \
-  echo "status.html"; \
+  echo "*~"; \
   echo "apps/bp"; \
   echo "apps/linear"; \
   echo "apps/betweennesscentrality"; \
-  echo "*~"; \
   echo "$NAME.tar.gz") | \
   tar --exclude-from=- --exclude-vcs --transform "s,^\./,$NAME/," -cz -f "$NAME.tar.gz" .
