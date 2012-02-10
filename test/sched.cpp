0d79c3ee48e369c10930a4fd37b1aa8e0c71d039
lenharth <lenharth@522c16a8-e32f-0410-b4cc-8071c0b41197>
2012-02-10 19:29:17 +0000

    sched microbenchmark
    git-svn-id: https://svn.ices.utexas.edu/repos/iss/projects/GaloisCpp/trunk@9230 522c16a8-e32f-0410-b4cc-8071c0b41197

---
+++ test/sched.cpp
@@ -0,0 +1,81 @@
+/** Scheduler Microbenchmark -*- C++ -*-
+ * @file
+ * @section License
+ *
+ * Galois, a framework to exploit amorphous data-parallelism in irregular
+ * programs.
+ *
+ * Copyright (C) 2011, The University of Texas at Austin. All rights reserved.
+ * UNIVERSITY EXPRESSLY DISCLAIMS ANY AND ALL WARRANTIES CONCERNING THIS
+ * SOFTWARE AND DOCUMENTATION, INCLUDING ANY WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR ANY PARTICULAR PURPOSE, NON-INFRINGEMENT AND WARRANTIES OF
+ * PERFORMANCE, AND ANY WARRANTY THAT MIGHT OTHERWISE ARISE FROM COURSE OF
+ * DEALING OR USAGE OF TRADE.  NO WARRANTY IS EITHER EXPRESS OR IMPLIED WITH
+ * RESPECT TO THE USE OF THE SOFTWARE OR DOCUMENTATION. Under no circumstances
+ * shall University be liable for incidental, special, indirect, direct or
+ * consequential damages or loss of profits, interruption of business, or
+ * related expenses which may arise from use of Software or Documentation,
+ * including but not limited to those resulting from defects in Software and/or
+ * Documentation, or loss or inaccuracy of data of any kind.
+ *
+ * @section Description
+ *
+ * Test scalability of schedulers
+ *
+ * @author Andrew Lenharth <andrewl@lenharth.org>
+ */
+
+#include "Galois/Statistic.h"
+#include "Galois/Galois.h"
+
+#include "Lonestar/BoilerPlate.h"
+
+#include "Galois/Runtime/WorkListAlt.h"
+
+static const char* name = "Schedeler Micro Benchmark";
+static const char* desc = "Measure stuff\n";
+static const char* url = 0;
+
+static llvm::cl::opt<int> sval(llvm::cl::Positional, llvm::cl::desc("<start value>"), llvm::cl::Required);
+static llvm::cl::opt<int> ival(llvm::cl::Positional, llvm::cl::desc("<init num>"), llvm::cl::Required);
+
+struct process {
+  template<typename Context>
+  void operator()(int item, Context& lwl) {
+    for (int i = 0; i < item; ++i)
+      lwl.push(item - 1);
+  }
+};
+
+int main(int argc, char** argv) {
+  LonestarStart(argc, argv, std::cout, name, desc, url);
+
+  std::vector<int> v((int)ival, (int)sval);
+
+  std::cout << "Initial: " << (int)ival << " using " << (int)sval << "\n";
+
+  Galois::StatTimer T0("T0");
+  T0.start();
+  using namespace GaloisRuntime::WorkList;
+  Galois::for_each<ChunkedLIFO<64> >(v.begin(), v.end(), process());
+  T0.stop();
+
+  Galois::StatTimer T1("T1");
+  T1.start();
+  using namespace GaloisRuntime::WorkList;
+  Galois::for_each<Alt::ChunkedAdaptor<LIFO<>, 64, true > >(v.begin(), v.end(), process());
+  T1.stop();
+
+  Galois::StatTimer T2("T2");
+  T2.start();
+  using namespace GaloisRuntime::WorkList;
+  Galois::for_each<dChunkedLIFO<64> >(v.begin(), v.end(), process());
+  T2.stop();
+
+  Galois::StatTimer T3("T3");
+  T3.start();
+  using namespace GaloisRuntime::WorkList;
+  Galois::for_each<Alt::ChunkedAdaptor<Alt::LevelStealingAlt<Alt::LIFO_SB<> >, 64, true > >(v.begin(), v.end(), process());
+  T3.stop();
+
+}
