0e2d4b40eb31df63f2b968723016c5431747183a
ddn <ddn@522c16a8-e32f-0410-b4cc-8071c0b41197>
2012-03-02 00:49:00 +0000

    Better libnuma searching
    git-svn-id: https://svn.ices.utexas.edu/repos/iss/projects/GaloisCpp/trunk@9390 522c16a8-e32f-0410-b4cc-8071c0b41197

--- cmake/Modules/FindLIBNUMA.cmake
+++ cmake/Modules/FindLIBNUMA.cmake
@@ -1,8 +1,2 @@
-include(CheckCSourceCompiles)
-
-set (LIBNUMA_FOUND "NO")
-
-find_library(LIBNUMA numa)
-if(LIBNUMA_FOUND)
-  CHECK_C_SOURCE_COMPILES("${CMAKE_MODULE_PATH}/FindLIBNUMA.c" LIBNUMA_FOUND)
-endif()
+include(CheckLibraryExists)
+CHECK_LIBRARY_EXISTS(numa numa_allocate "" LIBNUMA_FOUND)
