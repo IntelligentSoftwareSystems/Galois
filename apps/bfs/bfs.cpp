100dd151381004dbf648f818bc57e3f6a810686b
ddn <ddn@522c16a8-e32f-0410-b4cc-8071c0b41197>
2012-03-27 15:11:57 +0000

    typo
    git-svn-id: https://svn.ices.utexas.edu/repos/iss/projects/GaloisCpp/trunk@9602 522c16a8-e32f-0410-b4cc-8071c0b41197

--- apps/bfs/bfs.cpp
+++ apps/bfs/bfs.cpp
@@ -709,7 +709,7 @@ int main(int argc, char **argv) {
   }
 
   std::cout
-    << "MEMINFO P2: " << GaloisRuntime::MM::pageAllocInfo() << " pages " <<
+    << "MEMINFO P2: " << GaloisRuntime::MM::pageAllocInfo() << " pages "
 //    << "and "
 //    << "(large: " << GaloisRuntime::MM::largeAllocInfo() << " bytes)"
     << "\n";
