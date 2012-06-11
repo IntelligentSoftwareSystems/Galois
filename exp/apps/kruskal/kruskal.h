756c09e328d06c6b4fc46ca2d624a010100aed2b
ahassaan <ahassaan@522c16a8-e32f-0410-b4cc-8071c0b41197>
2012-06-11 22:39:39 +0000

    Different two-loop implementations of kruskal and boruvka
    git-svn-id: https://svn.ices.utexas.edu/repos/iss/projects/GaloisCpp/trunk@10256 522c16a8-e32f-0410-b4cc-8071c0b41197

---
+++ exp/apps/kruskal/kruskal.h
@@ -0,0 +1,463 @@
+/** Kruskal MST -*- C++ -*-
+ * @file
+ * @section License
+ *
+ * Galois, a framework to exploit amorphous data-parallelism in irregular
+ * programs.
+ *
+ * Copyright (C) 2011, The University of Texas at Austin. All rights reserved.
+ * UNIVERSITY EXPRESSLY DISCLAIMS ANY AND ALL WARRANTIES CONCERNING THIS
+ * SOFTWARE AND DOCUMENTATION, INCLUDING ANY WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR ANY PARTICULAR PURPOSE, NON-INFRINGEMENT AND WARRANTIES OF
+ * PERFORMANCE, AND ANY WARRANTY THAT MIGHT OTHERWISE ARISE FROM COURSE OF
+ * DEALING OR USAGE OF TRADE.  NO WARRANTY IS EITHER EXPRESS OR IMPLIED WITH
+ * RESPECT TO THE USE OF THE SOFTWARE OR DOCUMENTATION. Under no circumstances
+ * shall University be liable for incidental, special, indirect, direct or
+ * consequential damages or loss of profits, interruption of business, or
+ * related expenses which may arise from use of Software or Documentation,
+ * including but not limited to those resulting from defects in Software and/or
+ * Documentation, or loss or inaccuracy of data of any kind.
+ *
+ * @section Description
+ *
+ * Kruskal MST.
+ *
+ * @author <ahassaan@ices.utexas.edu>
+ */
+
+#ifndef _KRUSKAL_H_ 
+#define _KRUSKAL_H_ 
+
+#include <vector>
+#include <string>
+#include <sstream>
+#include <limits>
+#include <iostream>
+#include <set>
+
+#include <cstdlib>
+#include <cstdio>
+
+#include <boost/iterator/counting_iterator.hpp>
+
+#include "Galois/Timer.h"
+#include "Galois/Statistic.h"
+#include "Galois/Galois.h"
+#include "Galois/GaloisUnsafe.h"
+#include "Galois/Graphs/Graph.h"
+#include "Galois/Graphs/FileGraph.h"
+#include "Galois/Graphs/LCGraph.h"
+#include "Galois/Runtime/WorkList.h"
+#include "llvm/Support/CommandLine.h"
+
+#include "Lonestar/BoilerPlate.h"
+
+#include "kruskalData.h"
+
+namespace cll = llvm::cl;
+
+static const char* const name = "Kruskal MST";
+static const char* const desc = "Compute minimum weight spanning tree of an undirected graph";
+static const char* const url = "kruskal";
+
+static cll::opt<std::string> filename(cll::Positional, cll::desc("<input file>"), cll::Required);
+
+
+
+template <typename KNode_tp>
+class Kruskal {
+
+public:
+  typedef Kruskal<KNode_tp> Base_ty;
+
+  typedef unsigned Weight_ty;
+  typedef std::vector<KNode_tp*> VecKNode_ty;
+  typedef std::vector<KEdge<KNode_tp> > VecKEdge_ty;
+  typedef std::set<KEdge<KNode_tp>, 
+          typename KEdge<KNode_tp>::NodeIDcomparator> SetKEdge_ty;
+
+
+protected:
+  virtual const std::string getVersion () const = 0;
+
+  //! doesn't do anything by default. Sub-classes may choose to override 
+  //! in order to to specific initialization
+  virtual void initRemaining (VecKNode_ty& nodes, VecKEdge_ty& edges) { };
+
+  virtual void runMST (VecKNode_ty& nodes, VecKEdge_ty& edges,
+      size_t& mstWeight, size_t& totalIter) = 0;
+
+
+  void readGraph (const std::string& filename, VecKNode_ty& nodes, SetKEdge_ty& edges) {
+
+    typedef Galois::Graph::LC_CSR_Graph<unsigned, unsigned> InGraph;
+    typedef InGraph::GraphNode InGNode;
+
+    InGraph ingraph;
+    ingraph.structureFromFile (filename);
+
+    // numbering nodes 0..N-1, where N is number of nodes
+    // in the graph
+    unsigned idCntr = 0;
+    for (InGraph::iterator n = ingraph.begin (), endn = ingraph.end ();
+        n != endn; ++n) {
+      ingraph.getData (*n, Galois::NONE) = idCntr;
+      ++idCntr;
+    }
+
+
+    nodes.resize (ingraph.size (), NULL);
+
+    size_t numEdges = 0;
+
+    for (InGraph::iterator n = ingraph.begin (), endn = ingraph.end ();
+        n != endn; ++n) {
+
+      InGNode src = ingraph.getData (*n, Galois::NONE);
+
+      if (nodes[src] == NULL) {
+        nodes[src] = new KNode_tp (src);
+      }
+
+      for (InGraph::edge_iterator e = ingraph.edge_begin (src, Galois::NONE),
+          ende = ingraph.edge_end (src, Galois::NONE); e != ende; ++e) {
+
+        InGNode dst = ingraph.getEdgeDst (*e);
+
+        if (nodes[dst] == NULL) {
+          nodes[dst] = new KNode_tp (dst);
+        }
+
+
+        if (src != dst) {
+          KEdge<KNode_tp> ke (nodes[src], nodes[dst], ingraph.getEdgeData (*e));
+
+          std::pair<typename SetKEdge_ty::iterator, bool> res = edges.insert (ke);
+
+          if (res.second) {
+            ++numEdges;
+          }
+
+        } else {
+          std::fprintf (stderr, "Warning: Ignoring self edge (%d, %d, %d)\n",
+              src, dst, ingraph.getEdgeData (*e));
+        }
+      }
+
+    }
+
+    std::cout << "Graph read with nodes=" << ingraph.size () << ", edges=" << numEdges << std::endl;
+
+
+  }
+  
+
+
+  virtual void readPBBSfile (const std::string& filename, VecKNode_ty& nodes, SetKEdge_ty& edges) {
+
+    typedef unsigned NodeData;
+    typedef float EdgeData;
+
+    static const unsigned WEIGHT_SCALE = 10000;
+
+    nodes.clear ();
+    edges.clear ();
+
+    std::cout << "Reading input from: " << filename << std::endl;
+
+    // std::ifstream inFile (filename.c_str ());
+    FILE* inFile = fopen (filename.c_str (), "r");
+
+    // std::string header;
+    char header[128];
+
+    // inFile >> header;
+    fscanf (inFile, "%s", header);
+
+    // inFile.seekg (0, std::ios::beg);
+
+
+    size_t numEdges = 0;
+
+    // while (!inFile.eof ()) {
+    while (!feof (inFile)) {
+      NodeData srcIdx;
+      NodeData dstIdx;
+      EdgeData w;
+
+      // inFile >> srcIdx;
+      // inFile >> dstIdx;
+      // inFile >> w;
+
+      fscanf (inFile, "%d", &srcIdx);
+      fscanf (inFile, "%d", &dstIdx);
+      fscanf (inFile, "%g", &w);
+
+
+      if (nodes.size () <= srcIdx) {
+        nodes.resize ((srcIdx + 1), NULL);
+      }
+
+      if (nodes.size () <= dstIdx) {
+        nodes.resize ((dstIdx + 1), NULL);
+      }
+
+      if (nodes[srcIdx] == NULL) {
+        nodes[srcIdx] = new KNode_tp (srcIdx);
+      }
+
+      if (nodes[dstIdx] == NULL) {
+        nodes[dstIdx] = new KNode_tp (dstIdx);
+      }
+
+      Weight_ty integ_wt = (WEIGHT_SCALE * w);
+
+      if (srcIdx != dstIdx) {
+
+        KEdge<KNode_tp> ke (nodes[srcIdx], nodes[dstIdx], integ_wt);
+        std::pair<typename SetKEdge_ty::iterator, bool> res = edges.insert (ke);
+
+        if (res.second) {
+          ++numEdges;
+        }
+
+      } else {
+          std::fprintf (stderr, "Warning: Ignoring self edge (%d, %d, %d)\n",
+              srcIdx, dstIdx, integ_wt);
+      }
+    }
+    // inFile.close ();
+    fclose (inFile);
+
+
+    std::cout << "PBBS graph read with nodes = " << nodes.size ()
+      << ", edges = " << numEdges << std::endl;
+
+
+  }
+
+public:
+
+  virtual void run (int argc, char* argv[]) {
+    LonestarStart (argc, argv, name, desc, url);
+
+    //TODO
+    // read the graph from file into a FileGraph
+    // create nodes and edges
+    // compute a set of edges
+    // run kruskal, which should return mst weight as int
+    // verify
+
+
+    VecKNode_ty nodes;
+    SetKEdge_ty edges;
+
+    size_t mstWeight = 0;
+    size_t totalIter = 0;
+
+    readGraph (filename, nodes, edges);
+    // readPBBSfile (filename, nodes, edges);
+    //
+    VecKNode_ty nodesVec (nodes.begin (), nodes.end ());
+    VecKEdge_ty edgesVec (edges.begin (), edges.end ());
+
+
+    initRemaining (nodesVec, edgesVec);
+    
+    Galois::StatTimer t ("Time taken by runMST: ");
+
+    t.start ();
+    runMST (nodesVec, edgesVec, mstWeight, totalIter);
+    t.stop ();
+
+    printResults (mstWeight, totalIter);
+
+    if (!skipVerify) {
+      verify (nodes, edges, mstWeight);
+    }
+
+    freeVecPtr (nodes);
+
+  }
+
+private:
+
+  void printResults (const size_t mstSum, const size_t iter) const {
+    std::cout << getVersion () << ", MST sum=" << mstSum << ", iterations=" << iter << std::endl;
+  }
+
+  template <typename T>
+  static void freeVecPtr (std::vector<T*>& vec) {
+    for (typename std::vector<T*>::iterator i = vec.begin (), ei = vec.end ();
+        i != ei; ++i) {
+
+      delete *i;
+      *i = NULL;
+    }
+
+  }
+
+  struct PrimNode {
+    typedef std::vector<PrimNode*> VecPNode_ty;
+    typedef std::vector<Weight_ty> VecWeight_ty;
+    typedef boost::counting_iterator<unsigned> Adj_iterator_ty;
+
+
+    unsigned id;
+    size_t weight;
+    bool inMST;
+    VecPNode_ty adj;
+    VecWeight_ty adjWts;
+    
+
+    PrimNode (unsigned id): 
+      id (id), 
+      weight (std::numeric_limits<size_t>::max ()), 
+      inMST (false) {}
+
+    void addEdge (PrimNode* pn, Weight_ty w) {
+      assert (pn != NULL);
+      assert (std::find (adj.begin (), adj.end (), pn) == adj.end ());
+
+      adj.push_back (pn);
+      adjWts.push_back (w);
+      assert (adj.size () == adjWts.size ());
+    }
+
+    Adj_iterator_ty adj_begin () const { 
+      return Adj_iterator_ty (0);
+    }
+
+    Adj_iterator_ty adj_end () const {
+      return Adj_iterator_ty (adj.size ());
+    }
+
+    PrimNode* getDst (Adj_iterator_ty i) const {
+      return adj[*i];
+    }
+
+    Weight_ty getWeight (Adj_iterator_ty i) const {
+      return adjWts[*i];
+    }
+
+  };
+
+  struct PrimUpdate {
+    PrimNode* dst;
+    size_t weight;
+
+    PrimUpdate (PrimNode* dst, size_t weight)
+      : dst (dst), weight (weight) {
+        assert (dst != NULL);
+      }
+
+
+    bool operator < (const PrimUpdate& that) const {
+      if (this->weight == that.weight) {
+        return (this->dst->id < that.dst->id);
+
+      } else {
+        return (this->weight < that.weight);
+      }
+    }
+  };
+
+  static unsigned getID (KNode_tp* nd) {
+    return nd->id;
+  }
+
+  size_t runPrim (const VecKNode_ty& nodes, const SetKEdge_ty& edges) const {
+
+    std::vector<PrimNode*> primNodes (nodes.size (), NULL);
+
+    for (typename VecKNode_ty::const_iterator n = nodes.begin (), endn = nodes.end ();
+        n != endn; ++n) {
+      unsigned index = getID (*n);
+      primNodes[index] = new PrimNode (index);
+    }
+
+    for (typename SetKEdge_ty::const_iterator e = edges.begin (), ende = edges.end ();
+        e != ende; ++e) {
+
+      unsigned srcIdx = getID (e->src);
+      unsigned dstIdx = getID (e->dst);
+
+      assert (primNodes[srcIdx] != NULL);
+      assert (primNodes[dstIdx] != NULL);
+
+      // add undirected edge
+      primNodes[srcIdx]->addEdge (primNodes[dstIdx], e->weight);
+      primNodes[dstIdx]->addEdge (primNodes[srcIdx], e->weight);
+
+    }
+
+    std::set<PrimUpdate> workset;
+
+    PrimNode* root = primNodes[0];
+    PrimUpdate upd (root, 0);
+    workset.insert (upd);
+
+
+    size_t iter = 0;
+    size_t mstSum = 0;
+
+    while (!workset.empty ()) {
+      ++iter;
+      PrimUpdate upd = *(workset.begin ());
+      workset.erase (workset.begin ());
+
+      PrimNode& src = *(upd.dst);
+
+      if (!src.inMST) {
+        src.inMST = true;
+        src.weight = upd.weight;
+
+        mstSum += upd.weight;
+
+        for (typename PrimNode::Adj_iterator_ty i = src.adj_begin (), endi = src.adj_end (); i != endi; ++i) {
+
+          PrimNode& dst = *(src.getDst (i));
+          Weight_ty wt = src.getWeight (i);
+
+          if (!dst.inMST) {
+            PrimUpdate addUpd (&dst, wt);
+            workset.insert (addUpd);
+          }
+
+        }
+      } // end if;
+    }
+
+    std::cout << "Number of iterations taken by Prim = " << iter << std::endl;
+
+    freeVecPtr (primNodes);
+    return mstSum;
+  }
+
+
+
+
+
+  bool verify (const VecKNode_ty& nodes, const SetKEdge_ty& edges, const size_t kruskalSum) const {
+    Galois::StatTimer pt("Prim's Time:");
+    pt.start ();
+    size_t primSum = runPrim (nodes, edges);
+    pt.stop ();
+
+    if (primSum != kruskalSum) {
+      std::cerr << "ERROR. Incorrect MST weight=" << kruskalSum 
+        << ", correct weight from Prim is=" << primSum << std::endl;
+      abort ();
+
+    } else {
+      std::cout << "OK. Correct MST weight=" << kruskalSum << std::endl;
+    }
+
+    return false;
+  }
+};
+
+
+
+
+#endif // _KRUSKAL_H_ 
---
+++ exp/apps/kruskal/kruskalNoAdj.h
@@ -0,0 +1,563 @@
+/** Kruskal parallel but without keeping adj lists -*- C++ -*-
+ * @file
+ * @section License
+ *
+ * Galois, a framework to exploit amorphous data-parallelism in irregular
+ * programs.
+ *
+ * Copyright (C) 2011, The University of Texas at Austin. All rights reserved.
+ * UNIVERSITY EXPRESSLY DISCLAIMS ANY AND ALL WARRANTIES CONCERNING THIS
+ * SOFTWARE AND DOCUMENTATION, INCLUDING ANY WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR ANY PARTICULAR PURPOSE, NON-INFRINGEMENT AND WARRANTIES OF
+ * PERFORMANCE, AND ANY WARRANTY THAT MIGHT OTHERWISE ARISE FROM COURSE OF
+ * DEALING OR USAGE OF TRADE.  NO WARRANTY IS EITHER EXPRESS OR IMPLIED WITH
+ * RESPECT TO THE USE OF THE SOFTWARE OR DOCUMENTATION. Under no circumstances
+ * shall University be liable for incidental, special, indirect, direct or
+ * consequential damages or loss of profits, interruption of business, or
+ * related expenses which may arise from use of Software or Documentation,
+ * including but not limited to those resulting from defects in Software and/or
+ * Documentation, or loss or inaccuracy of data of any kind.
+ *
+ * @section Description
+ *
+ * No adjacency lists per component. Adj information (min outgoing edge)
+ * is recomputed when executing
+ *
+ * @author <ahassaan@ices.utexas.edu>
+ */
+
+#ifndef _KRUSKAL_NO_ADJ_H_
+#define _KRUSKAL_NO_ADJ_H_
+
+#include <algorithm>
+
+#include <cstdio>
+
+#include <boost/iterator/counting_iterator.hpp>
+
+#include "Galois/Statistic.h"
+#include "Galois/Runtime/PerThreadWorkList.h"
+#include "Galois/Runtime/DoAll.h"
+
+#include "Galois/util/Marked.h"
+#include "Galois/Accumulator.h"
+
+
+#include "kruskalData.h"
+#include "kruskalFunc.h"
+#include "kruskal.h"
+
+
+typedef Galois::GSimpleReducible<size_t, std::plus<size_t> > Accumulator_ty;
+
+
+template <typename KNode_tp>
+struct WLfactory {
+  typedef typename GaloisRuntime::PerThreadWLfactory<KEdge<KNode_tp> >::PerThreadVector WL_ty;
+
+  typedef typename GaloisRuntime::PerThreadWLfactory<Markable<KEdge<KNode_tp> > >::PerThreadVector WLmarkable_ty;
+};
+
+
+template <typename KNode_tp>
+struct FindLoop {
+
+  Accumulator_ty& matchIter;
+
+  FindLoop (
+      Accumulator_ty& _matchIter)
+    :
+      matchIter (_matchIter)
+  {}
+
+  void operator () (KEdge<KNode_tp>& edge) {
+    matchIter.get () += 1;
+
+    KNode_tp* rep1 = kruskal::findPC (edge.src);
+
+    KNode_tp* rep2 = kruskal::findPC (edge.dst);
+
+    if (rep1 != rep2) {
+
+      rep1->claimAsMin (edge);
+      rep2->claimAsMin (edge);
+    }
+  }
+};
+
+template <typename KNode_tp>
+struct LinkUpLoop {
+
+  typedef typename WLfactory<KNode_tp>::WL_ty WL_ty;
+
+  WL_ty& nextWorkList;
+  unsigned round;
+  Accumulator_ty& numUnions;
+  Accumulator_ty& mstSum;
+  Accumulator_ty& mergeIter;
+
+  LinkUpLoop (
+      WL_ty& _nextWorkList,
+      unsigned _round,
+      Accumulator_ty& _numUnions,
+      Accumulator_ty& _mstSum,
+      Accumulator_ty& _mergeIter)
+    :
+      nextWorkList (_nextWorkList),
+      round (_round),
+      numUnions (_numUnions),
+      mstSum (_mstSum),
+      mergeIter (_mergeIter)
+  {}
+
+  void operator () (KEdge<KNode_tp>& edge) {
+
+
+    KNode_tp* rep1 = edge.src->getRep ();
+    KNode_tp* rep2 = edge.dst->getRep ();
+
+
+    // not  a self-edge
+    if (rep1 != rep2) {
+      mergeIter.get () += 1;
+
+      bool succ = false;
+
+      if (rep1->minEdge == &edge) {
+        kruskal::linkUp (rep1, rep2);
+        succ = true;
+
+      } else if (rep2->minEdge == &edge) {
+
+        kruskal::linkUp (rep2, rep1);
+        succ = true;
+
+      } else {
+
+        nextWorkList.get ().push_back (edge);
+      }
+
+
+      if (succ) {
+        numUnions.get () += 1;
+        mstSum.get () += edge.weight;
+        edge.inMST = true;
+      }
+
+      // reset minEdge for next round
+      if (rep1->minEdge == &edge) {
+        rep1->minEdge = NULL;
+      }
+
+      if (rep2->minEdge == &edge) {
+        rep2->minEdge = NULL;
+      }
+
+
+    }
+
+    // std::cout << "edge = " << edge.str () << ", rep1 = " << rep1->str ()
+    // << ", rep2 = " << rep2->str () << std::endl;
+    // 
+    // 
+    // std::printf ("&edge = %p, rep1->minEdge = %p, rep2->minEdge = %p\n",
+    // &edge, rep1->minEdge, rep2->minEdge);
+  }
+
+};
+
+
+template <typename KNode_tp>
+void kruskalNoAdjNonSrc (
+    std::vector<KNode_tp*>& nodes, 
+    std::vector<KEdge<KNode_tp> >& edges, 
+    size_t& totalWeight, 
+    size_t& totalIter) {
+
+  // fill in a per thread currWorkList of marked edges and sort (for efficiency);
+  // loop where edges compete for lightest using Find . Mark self edges
+  // loop where edges check if they won and merge using union
+  // loop to remove the marked edges
+
+  totalIter = 0;
+  totalWeight = 0;
+
+  unsigned round = 0;
+  size_t totalUnions = 0;
+
+
+  Accumulator_ty matchIter (0);
+  Accumulator_ty mstSum (0);
+  Accumulator_ty mergeIter (0);
+  Accumulator_ty numUnions (0);
+
+  Galois::TimeAccumulator matchTimer;
+  Galois::TimeAccumulator mergeTimer;
+
+
+  typedef typename WLfactory<KNode_tp>::WL_ty WL_ty;
+
+
+  WL_ty* currWorkList = new WL_ty ();
+  WL_ty* nextWorkList = new WL_ty ();
+
+  GaloisRuntime::beginSampling ();
+
+  bool first = true;
+
+  while (true) {
+
+    if (first) {
+      first = false;
+
+      matchTimer.start ();
+
+      GaloisRuntime::do_all_coupled (
+          edges.begin (), edges.end (),
+          FindLoop<KNode_tp> (matchIter), "match_loop");
+
+      matchTimer.stop ();
+
+
+      mergeTimer.start ();
+
+      GaloisRuntime::do_all_coupled (
+          edges.begin (), edges.end (),
+          LinkUpLoop<KNode_tp> (*nextWorkList, round, numUnions, mstSum, mergeIter), "merge_loop");
+
+      mergeTimer.stop ();
+
+    } else {
+
+      matchTimer.start ();
+
+      GaloisRuntime::do_all_coupled (
+          *currWorkList,
+          FindLoop<KNode_tp> (matchIter), "match_loop");
+
+      matchTimer.stop ();
+
+
+      mergeTimer.start ();
+
+      GaloisRuntime::do_all_coupled (
+          *currWorkList,
+          LinkUpLoop<KNode_tp> (*nextWorkList, round, numUnions, mstSum, mergeIter), "merge_loop");
+
+      mergeTimer.stop ();
+    }
+
+    size_t u = numUnions.reduce () - totalUnions;
+    assert (u > 0 && "No merges in this round, no progress???");
+    
+    totalUnions += u;
+
+
+    if (totalUnions == (nodes.size () - 1)) {
+      break;
+    }
+
+
+    ++round;
+    std::swap (currWorkList, nextWorkList);
+    nextWorkList->clear_all ();
+    
+  }
+
+  GaloisRuntime::endSampling ();
+
+  totalWeight = mstSum.reduce ();
+  totalIter = matchIter.reduce ();
+
+
+  std::cout << "Number of match-merge rounds: " << round << std::endl;
+
+  std::cout << "FindLoop iterations = " << matchIter.reduce () << std::endl;
+  std::cout << "LinkUpLoop iterations = " << mergeIter.reduce () << std::endl;
+
+  std::cout << "Total time taken by FindLoop: " << matchTimer.get () << std::endl;
+  std::cout << "Total time taken by LinkUpLoop : " << mergeTimer.get () << std::endl;
+
+  delete currWorkList; currWorkList = NULL;
+  delete nextWorkList; nextWorkList = NULL;
+
+}
+
+
+
+
+// low parallelism threshold, at which kruskal switches to serial
+static const size_t LOW_PAR_THRESH = 128;
+
+template <typename KNode_tp>
+struct FindLoopMarked: public FindLoop<KNode_tp> {
+
+  FindLoopMarked (Accumulator_ty& _matchIter): FindLoop<KNode_tp> (_matchIter) {}
+  
+  void operator () (Markable<KEdge<KNode_tp> >& edge) {
+    if (!edge.marked ()) {
+      FindLoop<KNode_tp>::operator () (edge);
+    }
+  }
+
+};
+
+
+template <typename KNode_tp>
+struct UnionLoop {
+
+  unsigned round;
+  Accumulator_ty& numUnions;
+  Accumulator_ty& mstSum;
+  Accumulator_ty& mergeIter;
+
+  UnionLoop (
+      unsigned _round,
+      Accumulator_ty& _numUnions,
+      Accumulator_ty& _mstSum,
+      Accumulator_ty& _mergeIter)
+    :
+      round (_round),
+      numUnions (_numUnions),
+      mstSum (_mstSum),
+      mergeIter (_mergeIter)
+  {}
+
+  void operator () (Markable<KEdge<KNode_tp> >& edge) {
+
+    if (!edge.marked ()) {
+      mergeIter.get () += 1;
+
+
+      KNode_tp* rep1 = edge.src->getRep ();
+      KNode_tp* rep2 = edge.dst->getRep ();
+
+      // std::cout << "edge = " << edge.str () << ", rep1 = " << rep1->str ()
+        // << ", rep2 = " << rep2->str () << std::endl;
+// 
+// 
+      // std::printf ("&edge = %p, rep1->minEdge = %p, rep2->minEdge = %p\n",
+          // &edge, rep1->minEdge, rep2->minEdge);
+
+      if ((rep1->minEdge == &edge) &&
+          (rep2->minEdge == &edge)) {
+
+        kruskal::unionByRank (rep1, rep2);
+
+
+        numUnions.get () += 1;
+        mstSum.get () += edge.weight;
+        edge.inMST = true;
+
+        // mark the edge
+        edge.mark (round);
+      }
+
+      // reset minEdge for next round
+      if (rep1->minEdge == &edge) {
+        rep1->minEdge = NULL;
+      }
+
+      if (rep2->minEdge == &edge) {
+        rep2->minEdge = NULL;
+      }
+
+    }
+  }
+};
+
+
+template <typename KNode_tp>
+void kruskalNoAdjSrc (std::vector<KNode_tp*>& nodes, 
+    std::vector<KEdge<KNode_tp> >& edges, 
+    size_t& totalWeight, size_t& totalIter) {
+
+  // fill in a per thread workList of marked edges and sort (for efficiency);
+  // loop where edges compete for lightest using Find . Mark self edges
+  // loop where edges check if they won and merge using union
+  // loop to remove the marked edges
+
+  totalIter = 0;
+  totalWeight = 0;
+
+  unsigned round = 0;
+  size_t totalUnions = 0;
+
+
+  Accumulator_ty matchIter (0);
+  Accumulator_ty mstSum (0);
+  Accumulator_ty mergeIter (0);
+  Accumulator_ty numUnions (0);
+
+  Galois::TimeAccumulator matchTimer;
+  Galois::TimeAccumulator mergeTimer;
+  Galois::TimeAccumulator removeTimer;
+
+  typedef typename WLfactory<KNode_tp>::WLmarkable_ty WL_ty;
+
+
+  WL_ty workList;
+
+  Galois::StatTimer t_wl_init ("WorkList initialization time: ");
+
+  t_wl_init.start ();
+  workList.fill_init (edges.begin (), edges.end (), &WL_ty::Cont_ty::push_back);
+  t_wl_init.stop ();
+
+
+
+  GaloisRuntime::beginSampling ();
+  
+  while (true) {
+
+
+    matchTimer.start ();
+
+    GaloisRuntime::do_all_coupled (
+        workList,
+        FindLoop<KNode_tp> (matchIter), "match_loop");
+
+    matchTimer.stop ();
+
+
+    mergeTimer.start ();
+
+    // std::cout << "Starting merge_loop" << std::endl;
+
+    GaloisRuntime::do_all_coupled (
+        workList,
+        UnionLoop<KNode_tp> (round, numUnions, mstSum, mergeIter), "merge_loop");
+
+    mergeTimer.stop ();
+
+    size_t u = numUnions.reduce () - totalUnions;
+    assert (u > 0);
+    
+    totalUnions += u;
+
+
+    if (totalUnions == (nodes.size () - 1)) {
+      break;
+    }
+
+    removeTimer.start ();
+    removeMarked (workList);
+    removeTimer.stop ();
+
+
+    if (u < LOW_PAR_THRESH) {
+      break;
+    }
+
+
+    ++round;
+    
+  }
+
+  GaloisRuntime::endSampling ();
+
+  totalWeight = mstSum.reduce ();
+  totalIter = matchIter.reduce ();
+
+
+  if (totalUnions < (nodes.size () - 1)) {
+
+    Galois::StatTimer t_serial ("Time for serially processing remainging edges");
+
+    t_serial.start ();
+
+    typedef std::vector<Markable<KEdge<KNode_tp> > > SWL_ty;
+
+    SWL_ty serialWorkList;
+
+    for (unsigned r = 0; r < workList.numRows (); ++r) {
+      serialWorkList.insert (
+          serialWorkList.end (), 
+          workList[r].begin (), workList[r].end ());
+    }
+
+    std::sort (serialWorkList.begin (), serialWorkList.end (), 
+        typename KEdge<KNode_tp>::Comparator ());
+
+
+    size_t niter = 0;
+    for (typename SWL_ty::iterator i = serialWorkList.begin (), ei = serialWorkList.end ();
+        i != ei; ++i) {
+
+      ++niter;
+
+      assert (!i->marked ());
+
+      if (kruskal::contract (*i)) {
+        ++totalUnions;
+        totalWeight += i->weight;
+      }
+
+      if (totalUnions == (nodes.size () - 1)) {
+        break;
+      }
+
+    }
+
+    t_serial.stop ();
+
+    std::cout << "Number of edges processed serially = " << niter << std::endl;
+
+    totalIter += niter;
+
+    // final check. no non-self edges remaining
+    for (typename WL_ty::iterator i = serialWorkList.begin (), ei = serialWorkList.end (); 
+        i != ei; ++i) {
+
+      assert (!kruskal::NotSelfEdge<KNode_tp> () (*i));
+    }
+  }
+
+
+
+
+  std::cout << "Number of match-merge rounds: " << round << std::endl;
+
+  std::cout << "FindLoop iterations = " << matchIter.reduce () << std::endl;
+  std::cout << "UnionLoop iterations = " << mergeIter.reduce () << std::endl;
+
+
+
+  std::cout << "Total time taken by FindLoop: " << matchTimer.get () << std::endl;
+  std::cout << "Total time taken by UnionLoop : " << mergeTimer.get () << std::endl;
+  std::cout << "Total time taken by RemoveMarked : " << removeTimer.get () << std::endl;
+
+
+
+}
+
+class KruskalNoAdjNonSrc: public Kruskal<KNodeMin> {
+protected:
+
+  virtual const std::string getVersion () const { return "Kruskal no-adj sources, and, non-sources satisfying Boruvka Property selected"; }
+
+  virtual void runMST (Base_ty::VecKNode_ty& nodes, Base_ty::VecKEdge_ty& edges,
+      size_t& totalWeight, size_t& totalIter) {
+
+    kruskalNoAdjNonSrc (nodes, edges, totalWeight, totalIter);
+  }
+};
+
+class KruskalNoAdjSrc: public Kruskal<KNodeMin> {
+protected:
+
+  virtual const std::string getVersion () const { return "Kruskal no-adj, only sources selected"; }
+
+  virtual void runMST (Base_ty::VecKNode_ty& nodes, Base_ty::VecKEdge_ty& edges,
+      size_t& totalWeight, size_t& totalIter) {
+
+    kruskalNoAdjSrc (nodes, edges, totalWeight, totalIter);
+  }
+};
+
+
+
+
+#endif //  _KRUSKAL_NO_ADJ_H_
+
+
