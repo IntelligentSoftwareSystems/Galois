6f6c0006df39345351167a8dfd4a7ad975b15ed3
ddn <ddn@522c16a8-e32f-0410-b4cc-8071c0b41197>
2012-06-19 20:32:02 +0000

    Det maximal independent set
    git-svn-id: https://svn.ices.utexas.edu/repos/iss/projects/GaloisCpp/trunk@10371 522c16a8-e32f-0410-b4cc-8071c0b41197

--- apps/independentset/IndependentSet.cpp
+++ apps/independentset/IndependentSet.cpp
@@ -26,6 +26,9 @@
 #include "Galois/Galois.h"
 #include "Galois/Statistic.h"
 #include "Galois/Graphs/LCGraph.h"
+#ifdef GALOIS_USE_EXP
+#include "Galois/Runtime/ParallelWorkInline.h"
+#endif
 #include "llvm/Support/CommandLine.h"
 
 #include "Lonestar/BoilerPlate.h"
@@ -45,6 +48,23 @@ const char* url = NULL;
 
 static cll::opt<std::string> filename(cll::Positional, cll::desc("<input file>"), cll::Required);
 
+enum DetAlgo {
+  nondet,
+  detBase,
+  detPrefix,
+  detDisjoint
+};
+
+#ifdef GALOIS_USE_DET
+static cll::opt<DetAlgo> detAlgo(cll::desc("Deterministic algorithm:"),
+    cll::values(
+      clEnumVal(nondet, "Non-deterministic"),
+      clEnumVal(detBase, "Base execution"),
+      clEnumVal(detPrefix, "Prefix execution"),
+      clEnumVal(detDisjoint, "Disjoint execution"),
+      clEnumValEnd), cll::init(detBase));
+#endif
+
 enum MatchFlag {
   UNMATCHED, OTHER_MATCHED, MATCHED
 };
@@ -59,22 +79,33 @@ typedef Graph::GraphNode GNode;
 
 Graph graph;
 
+template<int Version=detBase>
 struct Process {
   typedef int tt_does_not_need_parallel_push;
+  typedef int tt_needs_per_iter_alloc; // For LocalState
 
-  void operator()(GNode src, Galois::UserContext<GNode>& ctx) {
+  struct LocalState {
+    bool mod;
+    LocalState(Process<Version>* self, Galois::PerIterAllocTy& alloc): mod(false) { }
+  };
+
+  bool build(GNode src) {
     for (Graph::edge_iterator ii = graph.edge_begin(src),
         ei = graph.edge_end(src); ii != ei; ++ii) {
       GNode dst = graph.getEdgeDst(ii);
       Node& data = graph.getData(dst);
       if (data.flag != UNMATCHED)
-        return;
+        return false;
     }
 
-    Node& me = graph.getData(src, Galois::WRITE);
+    Node& me = graph.getData(src, Galois::NONE);
     if (me.flag != UNMATCHED)
-      return;
+      return false;
+    return true;
+  }
 
+  void modify(GNode src) {
+    Node& me = graph.getData(src, Galois::NONE);
     for (Graph::edge_iterator ii = graph.edge_begin(src, Galois::NONE),
         ei = graph.edge_end(src, Galois::NONE); ii != ei; ++ii) {
       GNode dst = graph.getEdgeDst(ii);
@@ -83,6 +114,34 @@ struct Process {
     }
 
     me.flag = MATCHED;
+  }
+
+  void operator()(GNode src, Galois::UserContext<GNode>& ctx) {
+    bool* modp;
+#ifdef GALOIS_USE_DET
+    if (Version == detDisjoint) {
+      bool used;
+      LocalState* localState = (LocalState*) ctx.getLocalState(used);
+      modp = &localState->mod;
+      if (used) {
+        if (*modp)
+          modify(src);
+        return;
+      }
+    }
+#endif
+
+    if (Version == detDisjoint) {
+      *modp = build(src);
+    } else {
+      bool mod = build(src);
+      if (Version == detPrefix)
+        return;
+      else
+        graph.getData(src, Galois::WRITE); // Failsafe point
+      if (mod)
+        modify(src);
+    }
   }
 };
 
@@ -129,13 +188,38 @@ bool verify() {
 }
 
 int main(int argc, char** argv) {
+  Galois::StatManager statManager;
   LonestarStart(argc, argv, name, desc, url);
 
   graph.structureFromFile(filename.c_str());
 
   Galois::StatTimer T;
   T.start();
+  using namespace GaloisRuntime::WorkList;
+  typedef BulkSynchronous<dChunkedLIFO<256> > BSWL;
+#ifdef GALOIS_USE_EXP
+  typedef BulkSynchronousInline<> BSInline;
+#else
+  typedef BSWL BSInline;
+#endif
+
+#ifdef GALOIS_USE_DET
+  switch (detAlgo) {
+    case nondet: 
+      Galois::for_each(graph.begin(), graph.end(), Process<>()); break;
+    case detBase:
+      Galois::for_each_det<false>(graph.begin(), graph.end(), Process<>()); break;
+    case detPrefix:
+      Galois::for_each_det<false>(graph.begin(), graph.end(), Process<detPrefix>(), Process<>());
+      break;
+    case detDisjoint:
+      Galois::for_each_det<true>(graph.begin(), graph.end(), Process<detDisjoint>()); break;
+    default: std::cerr << "Unknown algorithm" << detAlgo << "\n"; abort();
+  }
+#else
+  //Galois::for_each<BSInline>(graph.begin(), graph.end(), Process());
   Galois::for_each(graph.begin(), graph.end(), Process());
+#endif
   T.stop();
 
   std::cout << "Cardinality of maximal independent set: " 
