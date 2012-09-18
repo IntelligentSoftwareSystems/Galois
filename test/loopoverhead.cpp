6bcd9e116b4a70c840df22ce8d518ad3db3eeef0
lenharth <lenharth@522c16a8-e32f-0410-b4cc-8071c0b41197>
2012-09-18 18:58:06 +0000

    loop overhead test
    git-svn-id: https://svn.ices.utexas.edu/repos/iss/projects/GaloisCpp/trunk@11536 522c16a8-e32f-0410-b4cc-8071c0b41197

--- test/loopoverhead.cpp
+++ test/loopoverhead.cpp
@@ -5,21 +5,39 @@
 #include <cstdlib>
 
 int RandomNumber () { return (rand()%1000000); }
+const unsigned iter = 16*1024;
 
 struct emp {
   template<typename T>
-  void operator()(const T& t) {
-  }
+  void operator()(const T& t) { GaloisRuntime::LL::compilerBarrier(); }
+  template<typename T, typename C>
+  void operator()(const T& t, const C& c) { GaloisRuntime::LL::compilerBarrier(); }
 };
 
-int main() {
+void t_stl() {
 
   std::vector<unsigned> V(1024);
   unsigned M = GaloisRuntime::LL::getMaxThreads();
 
-  const unsigned iter = 16*1024;
+  std::cout << "stl:\nIterxSize\n";
 
-  std::cout << "IterxSize\n";
+  std::cout << "Using " << 1 << " threads\n";
+   
+  Galois::Timer t;
+  t.start();
+  for (unsigned x = 0; x < iter; ++x)
+    std::for_each(V.begin(), V.end(), emp());
+  t.stop();
+  
+  std::cout << "STL(" << iter << "x" << V.size() << "): " << t.get() << "\n";
+}
+
+void t_doall() {
+
+  std::vector<unsigned> V(1024);
+  unsigned M = GaloisRuntime::LL::getMaxThreads();
+
+  std::cout << "doall:\nIterxSize\n";
 
   while (M) {
     
@@ -36,6 +54,36 @@ int main() {
 
     M >>= 1;
   }
+}
 
+void t_foreach() {
+
+  std::vector<unsigned> V(1024);
+  unsigned M = GaloisRuntime::LL::getMaxThreads();
+
+  std::cout << "foreach:\nIterxSize\n";
+
+  while (M) {
+    
+    Galois::setActiveThreads(M); //GaloisRuntime::LL::getMaxThreads());
+    std::cout << "Using " << M << " threads\n";
+   
+    Galois::Timer t;
+    t.start();
+    for (unsigned x = 0; x < iter; ++x)
+      Galois::for_each<GaloisRuntime::WorkList::RandomAccessRange<false,std::vector<unsigned>::iterator> >(V.begin(), V.end(), emp());
+    //      Galois::for_each<GaloisRuntime::WorkList::dChunkedFIFO<> >(V.begin(), V.end(), emp());
+    t.stop();
+
+    std::cout << "Galois(" << iter << "x" << V.size() << "): " << t.get() << "\n";
+
+    M >>= 1;
+  }
+}
+
+int main() {
+  t_stl();
+  t_doall();
+  t_foreach();
   return 0;
 }
