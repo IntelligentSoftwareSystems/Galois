f6d19a400d6f43bdf61edc015626c7bcc32a26f5
ahassaan <ahassaan@522c16a8-e32f-0410-b4cc-8071c0b41197>
2012-11-14 21:13:48 +0000

    setup.sh: a small script to load all the modules needed for building Galois updates CMakeLists for detecting boost and vtune
    git-svn-id: https://svn.ices.utexas.edu/repos/iss/projects/GaloisCpp/trunk@12024 522c16a8-e32f-0410-b4cc-8071c0b41197

---
+++ setup.sh
@@ -0,0 +1,15 @@
+module load sl6;
+module load cmake;
+module load boost;
+module load gcc;
+# or module load intel;
+module load mkl;
+# TOOLS="$HOME/tools"
+# 
+# OMPI_PRE="$TOOLS/openmpi"
+# BOOST_PRE="$TOOLS/boost"
+# 
+# export LD_LIBRARY_PATH="$OMPI_PRE/lib:$BOOST_PRE/lib:$LD_LIBRARY_PATH"
+# 
+# export PATH="$OMPI_PRE/bin:$PATH"
+
