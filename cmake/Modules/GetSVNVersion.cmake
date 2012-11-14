f007811f0229490e4557d4519fcfbb0047a4d230
ddn <ddn@522c16a8-e32f-0410-b4cc-8071c0b41197>
2012-11-14 22:51:08 +0000

    clear vars after use
    git-svn-id: https://svn.ices.utexas.edu/repos/iss/projects/GaloisCpp/trunk@12028 522c16a8-e32f-0410-b4cc-8071c0b41197

--- cmake/Modules/GetSVNVersion.cmake
+++ cmake/Modules/GetSVNVersion.cmake
@@ -6,7 +6,7 @@ find_file(_MODULE "GetSVNVersion-write.cmake" PATHS ${CMAKE_MODULE_PATH})
 add_custom_command(OUTPUT DUMMY ${PROJECT_BINARY_DIR}/include/Galois/svnversion.h
   COMMAND ${CMAKE_COMMAND} -DSOURCE_DIR=${CMAKE_SOURCE_DIR} -P ${_MODULE})
 
-set(_MODULE)
+set(_MODULE off)
 
 set_source_files_properties(${PROJECT_BINARY_DIR}/include/Galois/svnversion.h
   PROPERTIES GENERATED TRUE
