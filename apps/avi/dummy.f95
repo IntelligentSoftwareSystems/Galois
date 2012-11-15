667d3d0754445237dd4735f358bb750707de8f18
lenharth <lenharth@522c16a8-e32f-0410-b4cc-8071c0b41197>
2012-11-15 18:21:10 +0000

    force cmake to include fortran libraries so lapack linking works
    git-svn-id: https://svn.ices.utexas.edu/repos/iss/projects/GaloisCpp/trunk@12039 522c16a8-e32f-0410-b4cc-8071c0b41197

---
+++ apps/avi/dummy.f95
@@ -0,0 +1,8 @@
+!***************************************************
+!  File:        Minimal95.F95
+!  Language:    Fortran 95
+!  Description: This program does nothing 
+!***************************************************
+
+PROGRAM Minimal95
+END PROGRAM Minimal95
