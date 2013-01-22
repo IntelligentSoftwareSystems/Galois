f70a7d2452ca87caa7ed92b1ac4d951f2a2a3ff2
lenharth <lenharth@522c16a8-e32f-0410-b4cc-8071c0b41197>
2013-01-22 16:49:07 +0000

    fix c++11
    git-svn-id: https://svn.ices.utexas.edu/repos/iss/projects/GaloisCpp/trunk@12603 522c16a8-e32f-0410-b4cc-8071c0b41197

--- cmake/Modules/FindCXX11.cmake
+++ cmake/Modules/FindCXX11.cmake
@@ -5,12 +5,12 @@ set(CXX11_FLAG_CANDIDATES -std=c++11 -std=c++0x)
 
 # Don't do anything when already set
 if(DEFINED CXX11_FLAGS)
-  set(CXX0X_FLAG_CANDIDATES)
-  set(CXX0X_FOUND_INTERNAL "YES")
+  set(CXX11_FLAG_CANDIDATES)
+  set(CXX11_FOUND_INTERNAL "YES")
 endif()
 
 foreach(FLAG ${CXX11_FLAG_CANDIDATES})
-  unset(CXX0X_FLAG_DETECTED CACHE)
+  unset(CXX11_FLAG_DETECTED CACHE)
   message(STATUS "Try C++11 flag = [${FLAG}]")
   check_cxx_compiler_flag("${FLAG}" CXX11_FLAG_DETECTED)
   if(CXX11_FLAG_DETECTED)
@@ -21,7 +21,7 @@ foreach(FLAG ${CXX11_FLAG_CANDIDATES})
 endforeach()
 
 find_package_handle_standard_args(CXX11 DEFAULT_MSG CXX11_FOUND_INTERNAL CXX11_FLAGS)
-mark_as_advanced(CXX0X_FLAGS)
+mark_as_advanced(CXX11_FLAGS)
 
 include(CheckCXX11Features)
 
