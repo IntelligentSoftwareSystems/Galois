49e803461627f50aff8cf21a5c6906c17852b96e
lenharth <lenharth@522c16a8-e32f-0410-b4cc-8071c0b41197>
2013-02-20 19:25:04 +0000

    update module script
    git-svn-id: https://svn.ices.utexas.edu/repos/iss/projects/GaloisCpp/trunk@13049 522c16a8-e32f-0410-b4cc-8071c0b41197

--- exp/scripts/iss_load_modules.sh
+++ exp/scripts/iss_load_modules.sh
@@ -5,14 +5,13 @@ if [ "$(basename -- $0)" == "iss_load_modules.sh" ]; then
   exit 1
 fi
 
-module use /net/faraday/workspace/local/modules/modulefiles
-
 module load sl6
 module load cmake
 module load boost/1.52
 module load lapack
 
 if [ "$1" != "min" ]; then
+  module use /net/faraday/workspace/local/modules/modulefiles
   module load gdb/7.5
   module load gcc/4.7.2-scale
   #module load tbb
