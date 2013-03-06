e06a022d1db36452f438400c998ac23e53dd0a12
lenharth <lenharth@522c16a8-e32f-0410-b4cc-8071c0b41197>
2013-03-06 18:06:42 +0000

    make dgraph harder
    git-svn-id: https://svn.ices.utexas.edu/repos/iss/projects/GaloisCpp/branches/distributed@13133 522c16a8-e32f-0410-b4cc-8071c0b41197

--- exp/test/dgraph.cpp
+++ exp/test/dgraph.cpp
@@ -11,6 +11,15 @@ using namespace Galois::Graph;
 typedef ThirdGraph<int,int,EdgeDirection::Out> G;
 typedef Galois::Runtime::Distributed::gptr<G> Gptr;
 
+int num = 1000;
+
+bool check(unsigned x, Gptr Gr, unsigned& r) {
+  auto b = Gr->begin();
+  std::advance(b, x);
+  r = std::distance(b, Gr->end());
+  return r == (num - x);
+}
+
 struct op {
   Gptr Gr;
 
@@ -19,10 +28,13 @@ struct op {
 
   template<typename Context>
   void operator()(unsigned x, Context& cnx) const {
-    auto b = Gr->begin();
-    std::advance(b, x);
-    if (std::distance(b, Gr->end()) != 100 - x)
-      std::cerr << "Mismatch\n";
+    static Galois::Runtime::LL::SimpleLock<true> L;
+    unsigned r;
+    if (!check(x, Gr, r)) {
+      L.lock();
+      std::cout << "Mismatch " << x << " " << num - x << " " << r << "\n";
+      L.unlock();
+    }
   }
 
   // serialization functions
@@ -46,6 +58,10 @@ struct cop {
   void operator()(unsigned x, Context& cnx) const {
     G::NodeHandle node = Gr->createNode(x);
     Gr->addNode(node);
+    // static Galois::Runtime::LL::SimpleLock<true> L;
+    // L.lock();
+    // std::cout << x << " " << Galois::Runtime::Distributed::networkHostID << " " << Galois::Runtime::LL::getTID() << "\n";
+    // L.unlock();
   }
 
   // serialization functions
@@ -67,17 +83,23 @@ int main(int argc, char** argv) {
   Galois::Runtime::Distributed::networkStart();
 
   Gptr Gr(new G());
-  
-  Galois::for_each<>(boost::counting_iterator<unsigned>(0), boost::counting_iterator<unsigned>(100), cop(Gr));
 
-  Galois::for_each<>(boost::counting_iterator<unsigned>(0), boost::counting_iterator<unsigned>(100), op(Gr));
+  Galois::for_each<>(boost::counting_iterator<unsigned>(0), boost::counting_iterator<unsigned>(num), cop(Gr));
+  std::cout << "\nDone Building\n";
 
-  for (int x = 0; x < 100; ++x) {
-    auto b = Gr->begin();
-    std::advance(b, x);
-    if (std::distance(b, Gr->end()) != 100 - x)
-      std::cerr << "Mismatch\n";
-  }
+  //  volatile int br = 1;
+  //  while (br) {}
+  std::cout << "\nTotal Size " << std::distance(Gr->begin(), Gr->end()) << " (should be " << num << ")\n";
+  unsigned r;
+  for (int x = 0; x < num; ++x)
+    if (!check(x, Gr, r))
+      std::cout << "Mismatch " << x << " " << num - x << " " << r << "\n";
+  std::cout << "\nDone Local Check\n";
+  // br = 0;
+  // while (br) {}
+  Galois::for_each<>(boost::counting_iterator<unsigned>(0), boost::counting_iterator<unsigned>(num), op(Gr));
+
+  std::cout << "\nDone Remote Check\n";
 
   // master_terminate();
   Galois::Runtime::Distributed::networkTerminate();
