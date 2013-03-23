aa9382b0f507d4c9a9a162e125a82be8dc7ff710
lenharth <lenharth@522c16a8-e32f-0410-b4cc-8071c0b41197>
2013-03-23 20:10:46 +0000

    measure send time
    git-svn-id: https://svn.ices.utexas.edu/repos/iss/projects/GaloisCpp/branches/distributed@13321 522c16a8-e32f-0410-b4cc-8071c0b41197

--- exp/test/networktest.cpp
+++ exp/test/networktest.cpp
@@ -1,4 +1,5 @@
 #include "Galois/Runtime/Network.h"
+#include "Galois/Timer.h"
 
 #include <iostream>
 
@@ -18,15 +19,26 @@ void landingPad(RecvBuffer& foo) {
   }
 }
 
+void lp2(RecvBuffer&) {}
+
 int main(int argc, char** argv) {
   NetworkInterface& net = getSystemNetworkInterface();
   
   std::cout << "testing " << networkHostID << " " << networkHostNum << "\n";
 
   if (networkHostID == 0) {
+    Galois::Timer T;
+    T.start();
     SendBuffer buf;
     gSerialize(buf,(int) networkHostID);
     net.broadcastMessage(&landingPad, buf);
+    for (unsigned int i = 0; i < 1000000; ++i) {
+      net.handleReceives();
+      SendBuffer buf2;
+      net.sendMessage(1, &lp2, buf2);
+    }
+    T.stop();
+    std::cout << "Time " << T.get() << "\n";
   }
   while (true) {
     net.handleReceives();
