3fed849a2cf9ce73458a419a1695c041d34ae33d
mdhanapal <mdhanapal@522c16a8-e32f-0410-b4cc-8071c0b41197>
2013-04-08 01:42:54 +0000

    error in distributed bag
    git-svn-id: https://svn.ices.utexas.edu/repos/iss/projects/GaloisCpp/branches/distributed@13443 522c16a8-e32f-0410-b4cc-8071c0b41197

---
+++ exp/test/bag.cpp
@@ -0,0 +1,59 @@
+#include "Galois/Galois.h"
+#include "Galois/Graphs/Bag.h"
+#include "Lonestar/BoilerPlate.h"
+
+#include <boost/iterator/counting_iterator.hpp>
+
+typedef Galois::Graph::Bag<int>::pointer IntPtrs;
+
+struct InsertBody : public Galois::Runtime::Lockable {
+  IntPtrs pBodies;
+  InsertBody() { }
+  InsertBody(IntPtrs& b): pBodies(b) { }
+  template<typename Context>
+  void operator()(int in, const Context& cnx) {
+    pBodies->push(in);
+  }
+  // serialization functions
+  typedef int tt_has_serialize;
+  void serialize(Galois::Runtime::Distributed::SerializeBuffer& s) const {
+    gSerialize(s,pBodies);
+  }
+  void deserialize(Galois::Runtime::Distributed::DeSerializeBuffer& s) {
+    gDeserialize(s,pBodies);
+  }
+};
+
+struct PrintInt : public Galois::Runtime::Lockable {
+  typedef int tt_does_not_need_stats;
+  PrintInt() { }
+
+  template<typename Context>
+  void operator()(int i, Context& cnx) {
+    std::stringstream ss;
+    ss << "i: " << i << " host: " << Galois::Runtime::Distributed::networkHostID << "\n";
+    std::cout << ss.str();
+  }
+
+  // serialization functions
+  typedef int tt_has_serialize;
+  void serialize(Galois::Runtime::Distributed::SerializeBuffer& s) const { }
+  void deserialize(Galois::Runtime::Distributed::DeSerializeBuffer& s) { }
+};
+
+int main(int argc, char** argv) {
+  LonestarStart(argc, argv, nullptr, nullptr, nullptr);
+
+  // check the host id and initialise the network
+  Galois::Runtime::Distributed::networkStart();
+
+  IntPtrs pBodies = Galois::Graph::Bag<int>::allocate();
+  Galois::for_each<>(boost::counting_iterator<unsigned>(0), boost::counting_iterator<unsigned>(10), InsertBody(pBodies));
+
+  Galois::for_each_local<>(pBodies,PrintInt());
+
+  // master_terminate();
+  Galois::Runtime::Distributed::networkTerminate();
+
+  return 0;
+}
