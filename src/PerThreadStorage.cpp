7e9a4d3f2cb632dd8c5de261f719271dab43452d
ahassaan <ahassaan@522c16a8-e32f-0410-b4cc-8071c0b41197>
2013-10-04 03:05:01 +0000

    a scheme to track freed locations in PerThreadStorage. These locations are then searched when the normal bump ptr style allocation fails to find enough memory.
    git-svn-id: https://svn.ices.utexas.edu/repos/iss/projects/GaloisCpp/trunk@14279 522c16a8-e32f-0410-b4cc-8071c0b41197

--- src/PerThreadStorage.cpp
+++ src/PerThreadStorage.cpp
@@ -39,7 +39,7 @@ Galois::Runtime::PerBackend& Galois::Runtime::getPPSBackend() {
   return b;
 }
 
-//#define MORE_MEM_HACK
+#define MORE_MEM_HACK
 #ifdef MORE_MEM_HACK
 const size_t allocSize = Galois::Runtime::MM::pageSize * 10;
 inline void* alloc() {
@@ -54,23 +54,90 @@ inline void* alloc() {
 #endif
 #undef MORE_MEM_HACK
 
-unsigned Galois::Runtime::PerBackend::allocOffset(unsigned size) {
-  size = (size + 15) & ~15;
-  unsigned retval = __sync_fetch_and_add(&nextLoc, size);
-  if (retval + size > allocSize) {
-    GALOIS_DIE("no more memory");
+unsigned Galois::Runtime::PerBackend::nextPow2 (unsigned size) {
+  unsigned i = MIN_SIZE;
+  while (unsigned (1<<i) < size) {
+    ++i;
   }
+  if (i >= MAX_SIZE) { 
+    GALOIS_DIE ("PTS size too big");
+  }
+  return i;
+}
+
+unsigned Galois::Runtime::PerBackend::allocOffset(const unsigned sz) {
+  unsigned retval = allocSize;
+
+  unsigned size = (1 << nextPow2 (sz));
+
+  if ((nextLoc + size) <= allocSize) {
+    // simple path, where we allocate bump ptr style
+    retval = __sync_fetch_and_add (&nextLoc, size);
+
+  } else {
+    // find a free offset
+    unsigned pow2 = nextPow2(sz);
+    if (!freeOffsets[pow2].empty ()) {
+      retval = freeOffsets[pow2].back ();
+      freeOffsets[pow2].pop_back ();
+    } else {
+      // find a bigger size 
+      unsigned next = pow2;
+
+      for (; (next < MAX_SIZE) && (freeOffsets[next].empty ()); ++next) {}
+
+      if (next == MAX_SIZE) {
+        GALOIS_DIE ("PTS out of memory error");
+
+      } else {
+        // found a bigger free offset
+        // we used the first piece equal to required size and produce 
+        // vending machine change for the rest
+        assert (!freeOffsets[next].empty ());
+        retval = freeOffsets[next].back ();
+        freeOffsets[next].pop_back ();
+
+        // remaining chunk
+        unsigned end = retval + (1 << next);
+        unsigned start = retval + size; 
+        for (unsigned i = next - 1; (start < end) && (i >= 0); --i) {
+          freeOffsets[i].push_back (start);
+          start += (1 << i);
+        }
+      }
+    }
+  }
+
+  assert (retval != allocSize);
+
+  return retval;
+
+  
+  // size = (size + 15) & ~15;
+  // unsigned retval = __sync_fetch_and_add(&nextLoc, size);
+  // if (retval + size > allocSize) {
+    // GALOIS_DIE("no more memory");
+  // }
   return retval;
 }
 
-void Galois::Runtime::PerBackend::deallocOffset(unsigned offset, unsigned size) {
+void Galois::Runtime::PerBackend::deallocOffset(const unsigned offset, const unsigned sz) {
+
+  unsigned size = (1 << nextPow2 (sz));
+  if (__sync_bool_compare_and_swap(&nextLoc, offset + size, offset)) {
+    ; // allocation was at the end , so recovered some memory
+  } else {
+    // allocation not at the end
+    freeOffsets[nextPow2 (sz)].push_back (offset);
+  }
+
   // Simplest way to recover memory; relies on mostly stack-like nature of
   // allocations
-  size = (size + 15) & ~15;
+  // size = (size + 15) & ~15;
   // Should only be executed by main thread but make lock-free for fun
-  if (__sync_bool_compare_and_swap(&nextLoc, offset + size, offset)) {
-    ; // Recovered some memory
-  }
+  // if (__sync_bool_compare_and_swap(&nextLoc, offset + size, offset)) {
+    // ; // Recovered some memory
+  // }
 }
 
 void* Galois::Runtime::PerBackend::getRemote(unsigned thread, unsigned offset) {
@@ -117,3 +184,30 @@ void Galois::Runtime::initPTS() {
     Galois::Runtime::ppsBase = getPPSBackend().initPerPackage();
   }
 }
+
+#ifdef GALOIS_USE_EXP
+char* Galois::Runtime::PerBackend::initPerThread_cilk () {
+  assert (heads.size () == LL::getMaxThreads ());
+  unsigned id = LL::getTID ();
+  assert (heads[id] != nullptr);
+
+  return heads[id];
+}
+
+char* Galois::Runtime::PerBackend::initPerPackage_cilk () {
+  assert (heads.size () == LL::getMaxThreads ());
+  unsigned id = LL::getTID ();
+  assert (heads[id] != nullptr);
+
+  return heads[id];
+}
+
+void Galois::Runtime::initPTS_cilk () {
+  if (!Galois::Runtime::ptsBase) {
+    Galois::Runtime::ptsBase = getPTSBackend ().initPerThread_cilk ();
+  }
+  if (!Galois::Runtime::ppsBase) {
+    Galois::Runtime::ppsBase = getPPSBackend ().initPerPackage_cilk ();
+  }
+}
+#endif // GALOIS_USE_EXP
