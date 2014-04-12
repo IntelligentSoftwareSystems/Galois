343630505f3dcd48c831575410069a51f5a29633
Andrew Lenharth <andrew@lenharth.org>
2014-04-11 21:43:36 -0500

    compare to omp

--- test/loopoverhead.cpp
+++ test/loopoverhead.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <omp.h>
 
 int RandomNumber () { return (rand()%1000000); }
 const unsigned iter = 16*1024;
@@ -48,6 +49,34 @@ void t_stl() {
   t.stop();
   
   std::cout << "STL(" << iter << "x" << V.size() << "): " << t.get() << "\n";
+}
+
+void t_omp() {
+
+  std::vector<unsigned> V(1024);
+  unsigned M = Galois::Runtime::LL::getMaxThreads();
+
+  std::cout << "omp:\nIterxSize\n";
+
+  while (M) {
+    
+    omp_set_num_threads(M); //Galois::Runtime::LL::getMaxThreads());
+    std::cout << "Using " << M << " threads\n";
+   
+    Galois::Timer t;
+    t.start();
+    for (unsigned x = 0; x < iter; ++x) {
+      emp f;
+#pragma omp parallel for
+      for (int n = 0; n < V.size(); ++n)
+        f(n);
+    }
+    t.stop();
+
+    std::cout << "OMP(" << iter << "x" << V.size() << "): " << t.get() << "\n";
+
+    M >>= 1;
+  }
 }
 
 void t_doall(bool burn, bool steal) {
@@ -103,8 +132,12 @@ void t_foreach(bool burn) {
 }
 
 int main() {
+#pragma omp parallel for
+  for (int x = 0; x< 100; ++x)
+    {}
   t_inline();
   t_stl();
+  t_omp();
   t_doall(false,false);
   t_doall(false,true);
   t_foreach(false);
