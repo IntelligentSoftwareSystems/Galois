40951fd097be5039d988f4f33014986d98cf465c
Andrew Lenharth <andrew@lenharth.org>
2014-04-17 10:21:35 -0500

    4.9 fix

--- include/Galois/Graph/LC_InlineEdge_Graph.h
+++ include/Galois/Graph/LC_InlineEdge_Graph.h
@@ -118,23 +118,23 @@ protected:
   uint64_t numNodes;
   uint64_t numEdges;
 
-  template<bool _C = HasCompressedNodePtr>
-  NodeInfo* getDst(edge_iterator ii, typename std::enable_if<_C>::type* x = 0) const {
+  template<bool C_b = HasCompressedNodePtr>
+  NodeInfo* getDst(edge_iterator ii, typename std::enable_if<C_b>::type* x = 0) const {
     return const_cast<NodeInfo*>(&nodeData[ii->dst]);
   }
 
-  template<bool _C = HasCompressedNodePtr>
-  NodeInfo* getDst(edge_iterator ii, typename std::enable_if<!_C>::type* x = 0) const {
+  template<bool C_b = HasCompressedNodePtr>
+  NodeInfo* getDst(edge_iterator ii, typename std::enable_if<!C_b>::type* x = 0) const {
     return ii->dst;
   }
 
-  template<typename Container,typename Index, bool _C = HasCompressedNodePtr>
-  void setEdgeDst(Container& c, edge_iterator edge, Index idx, typename std::enable_if<_C>::type* = 0) {
+  template<typename Container,typename Index, bool C_b = HasCompressedNodePtr>
+  void setEdgeDst(Container& c, edge_iterator edge, Index idx, typename std::enable_if<C_b>::type* = 0) {
     edge->dst = idx;
   }
 
-  template<typename Container,typename Index, bool _C = HasCompressedNodePtr>
-  void setEdgeDst(Container& c, edge_iterator edge, Index idx, typename std::enable_if<!_C>::type* = 0) {
+  template<typename Container,typename Index, bool C_b = HasCompressedNodePtr>
+  void setEdgeDst(Container& c, edge_iterator edge, Index idx, typename std::enable_if<!C_b>::type* = 0) {
     edge->dst = &c[idx];
   }
 
