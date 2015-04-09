6116dfc6a73bb8b1ab04b8fd3b35bda59d157b95
Andrew Lenharth <andrew@lenharth.org>
2015-04-09 16:43:23 -0500

    fixup ptrlocks

--- include/Galois/Runtime/ll/PtrLock.h
+++ include/Galois/Runtime/ll/PtrLock.h
@@ -44,35 +44,59 @@ namespace Galois {
 namespace Runtime {
 namespace LL {
 
-/// PtrLock is a spinlock and a pointer.  If the second template
-/// parameter is false, the lock is a noop.  This wraps a pointer and
+/// PtrLock is a spinlock and a pointer. 
+/// This wraps a pointer and
 /// uses the low order bit for the lock flag
 /// Copying a lock is unsynchronized (relaxed ordering)
-template<typename T, bool isALock>
-class PtrLock;
 
-template<typename T>
-class PtrLock<T, true> {
+namespace details {
+
+class PtrLockBase {
   std::atomic<uintptr_t> _lock;
 
-  GALOIS_ATTRIBUTE_NOINLINE
-  void slow_lock() {
-    uintptr_t oldval;
-    do {
-      while ((_lock.load(std::memory_order_acquire) & 1) != 0) {
-        asmPause();
-      }
-      oldval = _lock.fetch_or(1, std::memory_order_acq_rel);
-    } while (oldval & 1);
-    assert(_lock);
+  void slow_lock();
+
+protected:
+
+  //! CAS only works on unlocked values
+  //! the lock bit will prevent a successful cas
+  inline bool _CAS(uintptr_t oldval, uintptr_t newval) {
+    assert(!(oldval & 1) && !(newval & 1));
+    uintptr_t old = oldval;
+    return _lock.compare_exchange_strong(old, newval);
   }
 
-public:
-  constexpr PtrLock() : _lock(0) {}
-  //relaxed order for copy
-  PtrLock(const PtrLock& p) : _lock(p._lock.load(std::memory_order_relaxed)) {}
+  //! CAS that works on locked values; this can be very dangerous
+  //! when used incorrectly
+  inline bool _stealing_CAS(uintptr_t oldval, uintptr_t newval) {
+    uintptr_t old = 1 | oldval;
+    return _lock.compare_exchange_strong(old, 1 | newval);
+  }
 
-  PtrLock& operator=(const PtrLock& p) {
+  inline void _unlock_and_set(uintptr_t val) {
+    assert(is_locked());
+    assert(!(val & 1));
+    _lock.store( val, std::memory_order_release);
+  }
+  
+  inline uintptr_t _getValue() const {
+    return _lock.load(std::memory_order_relaxed) & ~(uintptr_t)1;
+  }
+
+  inline void _setValue(uintptr_t val) {
+    val |= (_lock & 1);
+    //relaxed OK since this doesn't clear lock
+    _lock.store(val, std::memory_order_relaxed);
+  }
+
+
+  constexpr PtrLockBase() : _lock(0) {}
+  //relaxed order for copy
+  PtrLockBase(const PtrLockBase& p) : _lock(p._lock.load(std::memory_order_relaxed)) {}
+
+public:
+
+  PtrLockBase& operator=(const PtrLockBase& p) {
     if (&p == this) return *this;
     //relaxed order for initialization
     _lock.store(p._lock.load(std::memory_order_relaxed), std::memory_order_relaxed);
@@ -102,23 +126,6 @@ public:
     _lock.store(0, std::memory_order_release);
   }
 
-  inline void unlock_and_set(T* val) {
-    assert(is_locked());
-    assert(!((uintptr_t)val & 1));
-    _lock.store((uintptr_t) val, std::memory_order_release);
-  }
-  
-  inline T* getValue() const {
-    return (T*)(_lock.load(std::memory_order_relaxed) & ~(uintptr_t)1);
-  }
-
-  inline void setValue(T* val) {
-    uintptr_t nval = (uintptr_t)val;
-    nval |= (_lock & 1);
-    //relaxed OK since this doesn't clear lock
-    _lock.store(nval, std::memory_order_relaxed);
-  }
-
   inline bool try_lock() {
     uintptr_t oldval = _lock.load(std::memory_order_relaxed);
     if ((oldval & 1) != 0)
@@ -131,45 +138,58 @@ public:
     return _lock.load(std::memory_order_acquire) & 1;
   }
 
+};
+
+} // namespace details
+
+template<typename T>
+class PtrLock : public details::PtrLockBase {
+public:
+  constexpr PtrLock() : PtrLockBase() {}
+  //relaxed order for copy
+  PtrLock(const PtrLock& p) : PtrLockBase(p) {}
+
+  inline void unlock_and_set(T* val) {
+    _unlock_and_set((uintptr_t)val);
+  }
+  
+  inline T* getValue() const {
+    return (T*)_getValue();
+  }
+
+  inline void setValue(T* val) {
+    _setValue((uintptr_t)val);
+  }
+
   //! CAS only works on unlocked values
   //! the lock bit will prevent a successful cas
   inline bool CAS(T* oldval, T* newval) {
-    assert(!((uintptr_t)oldval & 1) && !((uintptr_t)newval & 1));
-    uintptr_t old = (uintptr_t)oldval;
-    return _lock.compare_exchange_strong(old, (uintptr_t)newval);
+    return _CAS((uintptr_t)oldval, (uintptr_t)newval);
   }
 
   //! CAS that works on locked values; this can be very dangerous
   //! when used incorrectly
   inline bool stealing_CAS(T* oldval, T* newval) {
-    uintptr_t old = 1 | (uintptr_t)oldval;
-    return _lock.compare_exchange_strong(old, 1 | (uintptr_t)newval);
+    return _stealing_CAS((uintptr_t)oldval, (uintptr_t)newval);
   }
 };
 
 template<typename T>
-class PtrLock<T, false> {
-  T* _lock;
+class DummyPtrLock {
+  T* ptr;
 public:
-  PtrLock() : _lock() {}
-  
-  inline void lock() {}
-  inline void unlock() {}
-  inline void unlock_and_clear() { _lock = 0; }
-  inline void unlock_and_set(T* val) { _lock = val; }
-  inline T* getValue() const { return _lock; }
-  inline void setValue(T* val) { _lock = val; }
-  inline bool try_lock() const { return true; }
-  inline bool is_locked() const { return false; }
-  inline bool CAS(T* oldval, T* newval) {
-    if (_lock == oldval) {
-      _lock = newval;
+  T* getValue() const { return ptr; }
+  void setValue(T* v) { ptr = v; }
+  void lock() {}
+  void unlock() {}
+  void unlock_and_clear() { ptr = nullptr; }
+  void unlock_and_set(T* v) { ptr = v; }
+  bool CAS(T* oldval, T* newval) {
+    if (ptr == oldval) {
+      ptr = oldval;
       return true;
     }
     return false;
-  }
-  inline bool stealing_CAS(T* oldval, T* newval) {
-    return CAS(oldval, newval);
   }
 };
 
