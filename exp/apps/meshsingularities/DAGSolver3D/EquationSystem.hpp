85dc503cdda929b0a5aece58ef506ec167945d89
Konrad Jopek <kjopek@gilbert.ices.utexas.edu>
2015-08-11 11:33:09 -0500

    Make Cholesky elimination faster. Style.

--- exp/apps/meshsingularities/DAGSolver3D/EquationSystem.hpp
+++ exp/apps/meshsingularities/DAGSolver3D/EquationSystem.hpp
@@ -24,28 +24,28 @@ class EquationSystem {
   private:
         // needed because of implementation of swapRows
         double *origPtr;
-        SolverMode mode = OLD;
   public:
 	// this variables _should_ be public
 	// Productions will use them directly
+        SolverMode mode = OLD;
 
-	unsigned long n;
-	double ** matrix;
-	double *rhs;
+        unsigned long n;
+        double ** matrix;
+        double *rhs;
 
-	EquationSystem() {} ;
-    EquationSystem(unsigned long n, SolverMode mode=OLD);
-	virtual ~EquationSystem();
-    void allocate();
+        EquationSystem() {} ;
+        EquationSystem(unsigned long n, SolverMode mode=OLD);
+        virtual ~EquationSystem();
+        void allocate();
 
-	void swapRows(const int i, const int j);
-	void swapCols(const int i, const int j);
+        void swapRows(const int i, const int j);
+        void swapCols(const int i, const int j);
 
-    int eliminate(const int rows);
-	void backwardSubstitute(const int startingRow);
+        int eliminate(const int rows);
+        void backwardSubstitute(const int startingRow);
 
-	void checkRow(int row_nr, int* values, int values_cnt);
-	void print() const;
+        void checkRow(int row_nr, int* values, int values_cnt);
+        void print() const;
 };
 
 #endif /* EQUATIONSYSTEM_H_ */
