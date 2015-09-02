c8c1d1f00bf58a9a0a9cee30aba2f42151df5bc6
Andrew Lenharth <andrew@lenharth.org>
2015-09-02 13:58:33 -0500

    finish removing barrier state from substrate

---
+++ libsubstrate/include/Galois/Substrate/BarrierImpl.h
@@ -0,0 +1,65 @@
+/** Barriers -*- C++ -*-
+ * @file
+ * @section License
+ *
+ * This file is part of Galois.  Galoisis a gramework to exploit
+ * amorphous data-parallelism in irregular programs.
+ *
+ * Galois is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Galois is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Galois.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * @section Copyright
+ *
+ * Copyright (C) 2015, The University of Texas at Austin. All rights
+ * reserved.
+ *
+ * @section Description
+ *
+ * Public API for interacting with barriers
+ *
+ * @author Donald Nguyen <ddn@cs.utexas.edu>
+ */
+#ifndef GALOIS_SUBSTRATE_BARRIERIMPL_H
+#define GALOIS_SUBSTRATE_BARRIERIMPL_H
+
+#include <memory>
+
+#include "Galois/Substrate/Barrier.h"
+
+namespace Galois {
+namespace Substrate {
+
+/**
+ * Create specific types of barriers.  For benchmarking only.  Use
+ * getSystemBarrier() for all production code
+ */
+std::unique_ptr<Barrier> createPthreadBarrier(unsigned);
+std::unique_ptr<Barrier> createMCSBarrier(unsigned);
+std::unique_ptr<Barrier> createTopoBarrier(unsigned);
+std::unique_ptr<Barrier> createCountingBarrier(unsigned);
+std::unique_ptr<Barrier> createDisseminationBarrier(unsigned);
+
+/**
+ * Creates a new simple barrier. This barrier is not designed to be fast but
+ * does gaurantee that all threads have left the barrier before returning
+ * control. Useful when the number of active threads is modified to avoid a
+ * race in {@link getSystemBarrier()}.  Client is reponsible for deallocating
+ * returned barrier.
+ */
+std::unique_ptr<Barrier> createSimpleBarrier(unsigned);
+
+} // end namespace Substrate
+} // end namespace Galois
+
+#endif
