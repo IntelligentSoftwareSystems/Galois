b15fea02cf38c4aabcc15464c2a02cac406ee8ed
Andrew Lenharth <andrew@lenharth.org>
2015-10-02 12:40:57 -0500

    remove empty directories

--- include/Galois/c++11-compat/unordered_set.h
+++
@@ -1,6 +0,0 @@
-#ifndef GALOIS_C__11_COMPAT_UNORDERED_SET_H
-#define GALOIS_C__11_COMPAT_UNORDERED_SET_H
-
-#include <boost/tr1/unordered_set.hpp>
-namespace std { using namespace std::tr1; }
-#endif
--- include/Galois/c++11-compat/vector.h
+++
@@ -1,7 +0,0 @@
-#ifndef GALOIS_C__11_COMPAT_VECTOR_H
-#define GALOIS_C__11_COMPAT_VECTOR_H
-
-#include <vector>
-#define GALOIS_CXX11_VECTOR_HAS_NO_EMPLACE
-
-#endif
