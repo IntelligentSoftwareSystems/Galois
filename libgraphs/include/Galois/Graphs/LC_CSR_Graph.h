13581710d2208164570f518222e582f3c67ab14a
Andrew Lenharth <andrew@lenharth.org>
2015-10-05 12:43:17 -0500

    more license fix

--- libgraphs/include/Galois/Graphs/LC_CSR_Graph.h
+++ libgraphs/include/Galois/Graphs/LC_CSR_Graph.h
@@ -7,8 +7,8 @@
  *
  * Galois is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * published by the Free Software Foundation, version 2.1 of the
+ * License.
  *
  * Galois is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -29,6 +29,7 @@
  * @author Andrew Lenharth <andrewl@lenharth.org>
  * @author Donald Nguyen <ddn@cs.utexas.edu>
  */
+
 #ifndef GALOIS_GRAPH__LC_CSR_GRAPH_H
 #define GALOIS_GRAPH__LC_CSR_GRAPH_H
 
--- libgraphs/include/Galois/Graphs/memScalGraph.h
+++ libgraphs/include/Galois/Graphs/memScalGraph.h
@@ -1,8 +1,30 @@
-/*
- * memScalGraph.h
+/** -*- C++ -*-
+ * @file
+ * @section License
  *
- *  Created on: Mar 4, 2013
- *      Author: nyadav
+ * This file is part of Galois.  Galoisis a gramework to exploit
+ * amorphous data-parallelism in irregular programs.
+ *
+ * Galois is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, version 2.1 of the
+ * License.
+ *
+ * Galois is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Galois.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * @section Copyright
+ *
+ * Copyright (C) 2015, The University of Texas at Austin. All rights
+ * reserved.
+ *
+ * @author nyadav
  */
 
 #ifndef GALOIS_GRAPH_MEMSCALGRAPH_H
