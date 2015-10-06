49f42699ea114cb6267e4bed49533773c9dc7dcc
Andrew Lenharth <andrew@lenharth.org>
2015-10-06 10:52:33 -0500

    Fix spelling

--- libruntime/include/Galois/TwoLevelIterator.h
+++ libruntime/include/Galois/TwoLevelIterator.h
@@ -2,7 +2,7 @@
  * @file
  * @section License
  *
- * This file is part of Galois.  Galoisis a gramework to exploit
+ * This file is part of Galois.  Galoisis a framework to exploit
  * amorphous data-parallelism in irregular programs.
  *
  * Galois is free software: you can redistribute it and/or modify it
