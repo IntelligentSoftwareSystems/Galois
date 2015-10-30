b705db1b18c210c58572c21fc94e4e16c888555f
Gurbinder Gill <gill@ices.utexas.edu>
2015-10-30 15:55:11 -0500

    Final HSSSP compiler generated

--- exp/apps/hsssp/HSSSP_gen3.cpp
+++ exp/apps/hsssp/HSSSP_gen3.cpp
@@ -31,7 +31,16 @@ struct InitializeGraph {
   Graph *graph;
 
   void static go(Graph& _graph) {
-    Galois::do_all(_graph.begin(), _graph.end(), InitializeGraph {&_graph}, Galois::loopname("InitGraph"), Galois::write_set( "this->graph", "struct NodeData &", "dist_current" , "sync_pull"));
+
+     struct SyncerPull_0 {
+    	static int extract( const struct NodeData & node){ return node.dist_current; }
+    	static void setVal (struct NodeData & node, int y) {node.dist_current = y; }
+    	typedef int ValTy;
+    };
+    Galois::do_all(_graph.begin(), _graph.end(), InitializeGraph {&_graph}, Galois::loopname("InitGraph"), Galois::write_set("sync_pull", "this->graph", "struct NodeData &", "", "dist_current" , "int"));
+    _graph.sync_pull<SyncerPull_0>();
+    
+
   }
 
   void operator()(GNode src) const {
@@ -44,27 +53,31 @@ struct SSSP {
   Graph* graph;
 
   void static go(Graph& _graph){
+
      struct Syncer_0 {
     	static int extract( const struct NodeData & node){ return node.dist_current; }
     	static void reduce (struct NodeData & node, int y) {Galois::min(node.dist_current, y);}
     	static void reset (struct NodeData & node ) { node.dist_current = std::numeric_limits<int>::max(); }
     	typedef int ValTy;
     };
-    Galois::do_all(_graph.begin(), _graph.end(), SSSP { &_graph }, Galois::loopname("sssp"), Galois::write_set( "this->graph", "struct NodeData &", "dist_current" , "sync_pull"), Galois::write_set( "this->graph", "struct NodeData &", "struct std::atomic<int> &" , "dist_current", "int" , "{Galois::min(node.dist_current, y);}",  "std::numeric_limits<int>::max()", "sync_push"));
+ 
+    Galois::do_all(_graph.begin(), _graph.end(), SSSP { &_graph }, Galois::loopname("sssp"), Galois::write_set("sync_push", "this->graph", "struct NodeData &", "std::atomic<int> &" , "dist_current", "int" , "{Galois::min(node.dist_current, y);}",  "std::numeric_limits<int>::max()"));
     _graph.sync_push<Syncer_0>();
     
      }
 
   void operator()(GNode src) const {
     NodeData& snode = graph->getData(src);
+    auto& net = Galois::Runtime::getSystemNetworkInterface();
     auto& sdist = snode.dist_current;
+    //std::cout << "[" << net.ID << "] [" << src <<"] : " << sdist << "\n";
 
     for (auto jj = graph->edge_begin(src), ej = graph->edge_end(src); jj != ej; ++jj) {
       GNode dst = graph->getEdgeDst(jj);
       auto& dnode = graph->getData(dst);
       std::atomic<int>& ddist = dnode.dist_current;
       int old_dist = ddist;
-      int new_dist = graph->getEdgeData(jj) + sdist;
+      int new_dist = graph->getEdgeData(jj) + sdist + 1;
       while (ddist > new_dist){
         ddist.compare_exchange_strong(old_dist, new_dist);
       }
@@ -83,8 +96,33 @@ int main(int argc, char** argv) {
 
     Graph hg(inputFile, net.ID, net.Num);
 
+    std::cout << "InitializeGraph::go called\n";
     InitializeGraph::go(hg);
-    SSSP::go(hg);
+
+    // Set node 0 to be source.
+    if(net.ID == 0)
+      hg.getData(src_node).dist_current = 0;
+
+    // Verify
+  /*  if(net.ID == 0) {
+      for(auto ii = hg.begin(); ii != hg.end(); ++ii) {
+        std::cout << "[" << *ii << "]  " << hg.getData(*ii).dist_current << "\n";
+      }
+    }
+    */
+
+    std::cout << "SSSP::go called\n";
+    for (int i = 0; i < maxIterations; ++i) {
+      std::cout << " Iteration : " << i << "\n";
+      SSSP::go(hg);
+    }
+
+    // Verify
+    if(net.ID == 0) {
+      for(auto ii = hg.begin(); ii != hg.begin() + 8; ++ii) {
+        std::cout << "[" << *ii << "]  " << hg.getData(*ii).dist_current << "\n";
+      }
+    }
 
     return 0;
   } catch(const char* c) {
