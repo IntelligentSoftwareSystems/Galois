1fc11c59fc88e7e098e18fb8e618fec502473179
Roshan Dathathri <roshan@cs.utexas.edu>
2015-11-03 15:38:55 -0600

    IrGL Translator: transforming array-of-structure to structure-of-arrays

--- exp/plugins/GaloisFunctions/IrGLTranslator.cpp
+++ exp/plugins/GaloisFunctions/IrGLTranslator.cpp
@@ -27,6 +27,7 @@ using namespace clang;
 using namespace clang::ast_matchers;
 using namespace llvm;
 
+#define VARIABLE_NAME_CHARACTERS "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_"
 namespace {
 
 class IrGLFunctionsVisitor : public RecursiveASTVisitor<IrGLFunctionsVisitor> {
@@ -35,18 +36,49 @@ private:
   Rewriter &rewriter; // FIXME: is this necessary? can ASTContext give source code?
   std::unordered_set<const Stmt *> skipStmts;
   std::unordered_set<const Decl *> skipDecls;
+  std::map<std::string, std::string> nodeMap;
+  bool entryOnce;
+  std::stringstream declString, bodyString;
+  std::map<std::string, std::string> parameterToTypeMap;
 
 public:
   explicit IrGLFunctionsVisitor(CompilerInstance &CI, Rewriter &R) : 
     astContext(&(CI.getASTContext())),
-    rewriter(R)
+    rewriter(R),
+    entryOnce(false)
   {}
+  
+  void WriteCBlock(std::string text) {
+    assert(text.find("graph.getData") == std::string::npos);
+
+    // replace node variables: array of structures to structure of arrays
+    // FIXME: does not handle scoped variables (nesting with same variable name)
+    for (auto& nodeVar : nodeMap) {
+      std::size_t pos = text.find(nodeVar.first);
+      if (pos != std::string::npos) {
+        std::size_t end = text.find(".", pos);
+        text.erase(pos, end - pos + 1);
+        end = text.find_first_not_of(VARIABLE_NAME_CHARACTERS, pos); 
+        text.insert(end, "[" + nodeVar.second + "]");
+        text.insert(pos, "p_");
+      }
+    }
+
+    bodyString << "CBlock([\"" << text << "\"]),\n";
+  }
 
   virtual bool TraverseDecl(Decl *D) {
     bool traverse = RecursiveASTVisitor<IrGLFunctionsVisitor>::TraverseDecl(D);
-    if (traverse && D && isa<CXXMethodDecl>(D)) {
-      llvm::errs() << "]),\n"; // end ForAll
-      llvm::errs() << "]),\n"; // end kernel
+    if (traverse && D && isa<FunctionDecl>(D)) {
+      bodyString << "]),\n"; // end ForAll
+      bodyString << "]),\n"; // end kernel
+      for (auto& param : parameterToTypeMap) {
+        declString << ", ('" << param.second << " *', 'p_" << param.first << "')";
+      }
+      declString << "],\n";
+      llvm::errs() << declString.str();
+      llvm::errs() << bodyString.str();
+      entryOnce = false;
     }
     return traverse;
   }
@@ -54,22 +86,23 @@ public:
   virtual bool TraverseStmt(Stmt *S) {
     bool traverse = RecursiveASTVisitor<IrGLFunctionsVisitor>::TraverseStmt(S);
     if (traverse && S && isa<CXXForRangeStmt>(S)) {
-      llvm::errs() << "]),\n"; // end ForAll
+      bodyString << "]),\n"; // end ForAll
     }
     return traverse;
   }
 
   virtual bool VisitFunctionDecl(FunctionDecl* func) {
+    assert(!entryOnce);
+    entryOnce = true;
+    skipStmts.clear();
+    skipDecls.clear();
+    nodeMap.clear();
+    declString.str("");
+    bodyString.str("");
     std::string funcName = func->getNameAsString();
     llvm::errs() << "\nIrGL Function:\n";
-    llvm::errs() << "Kernel(\"" << funcName << "\", [G.param()";
-    for (auto param : func->params()) {
-      // FIXME: support for multiple arguments
-      // FIXME: fix type to node's datatype
-      llvm::errs() << ", ('" << param->getOriginalType().getAsString() << " *', 'graph_data')";
-    }
-    llvm::errs() << "],\n";
-    llvm::errs() << "[ForAll(\"vertex\", G.nodes(),\n[\n";
+    declString << "Kernel(\"" << funcName << "\", [G.param()";
+    bodyString << "[ForAll(\"vertex\", G.nodes(),\n[\n";
     return true;
   }
 
@@ -82,7 +115,7 @@ public:
         skipStmts.insert(expr);
       }
     }
-    llvm::errs() << "ForAll(\"" << forStmt->getLoopVariable()->getNameAsString() 
+    bodyString << "ForAll(\"" << forStmt->getLoopVariable()->getNameAsString() 
       << "\", G.edges(\"vertex\"),\n[\n";
     return true;
   }
@@ -93,13 +126,27 @@ public:
         const Expr *expr = varDecl->getAnyInitializer();
         skipStmts.insert(expr);
         if (skipDecls.find(varDecl) == skipDecls.end()) {
-          llvm::errs() << "CDecl([(\"" << varDecl->getType().getAsString() 
-            << "\", \"" << varDecl->getNameAsString() << "\", \"\")]),\n";
-
+          std::size_t pos = std::string::npos;
+          std::string initText;
           if (expr != NULL) {
             SourceRange range(expr->getLocStart(), expr->getLocEnd());
-            llvm::errs() << "CBlock([\"" << varDecl->getNameAsString() 
-              << " = " << rewriter.getRewrittenText(range) << "\"]),\n";
+            initText = rewriter.getRewrittenText(range);
+            pos = initText.find("graph.getData");
+          }
+          if (pos == std::string::npos) {
+            bodyString << "CDecl([(\"" << varDecl->getType().getAsString() 
+              << "\", \"" << varDecl->getNameAsString() << "\", \"\")]),\n";
+
+            if (!initText.empty()) {
+              WriteCBlock(varDecl->getNameAsString() + " = " + initText);
+            }
+          }
+          else {
+            // get the first argument to graph.getData()
+            std::size_t begin = initText.find("(", pos);
+            std::size_t end = initText.find(",", pos);
+            std::string index = initText.substr(begin+1, end - begin - 1);
+            nodeMap[varDecl->getNameAsString()] = index;
           }
         }
       }
@@ -113,7 +160,7 @@ public:
     if (skipStmts.find(binaryOp) == skipStmts.end()) {
       assert(binaryOp.isAssignmentOp());
       SourceRange range(binaryOp->getLocStart(), binaryOp->getLocEnd());
-      llvm::errs() << "CBlock([\"" << rewriter.getRewrittenText(range) << "\"]),\n";
+      WriteCBlock(rewriter.getRewrittenText(range));
     }
     return true;
   }
@@ -121,7 +168,7 @@ public:
   virtual bool VisitCXXMemberCallExpr(CXXMemberCallExpr *callExpr) {
     if (skipStmts.find(callExpr) == skipStmts.end()) {
       SourceRange range(callExpr->getLocStart(), callExpr->getLocEnd());
-      llvm::errs() << "CBlock([\"" << rewriter.getRewrittenText(range) << "\"]),\n";
+      WriteCBlock(rewriter.getRewrittenText(range));
     }
     return true;
   }
@@ -143,6 +190,15 @@ public:
   virtual bool VisitParenExpr(ParenExpr *parenExpr) {
     if (skipStmts.find(parenExpr) != skipStmts.end()) {
       skipStmts.insert(parenExpr->getSubExpr());
+    }
+    return true;
+  }
+
+  virtual bool VisitMemberExpr(MemberExpr *memberExpr) {
+    SourceRange range(memberExpr->getBase()->getLocStart(), memberExpr->getBase()->getLocEnd());
+    std::string objectName = rewriter.getRewrittenText(range);
+    if (nodeMap.find(objectName) != nodeMap.end()) {
+      parameterToTypeMap[memberExpr->getMemberNameInfo().getAsString()] = memberExpr->getMemberDecl()->getType().getAsString();
     }
     return true;
   }
