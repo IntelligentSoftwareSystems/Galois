e5638c6ee013759a3cfcb5d2a5f5f6dbf95cd33b
Roshan Dathathri <roshan@cs.utexas.edu>
2015-12-08 17:29:00 -0600

    IrGL Translator: adding support for worklists (data-driven algorithms)

--- exp/plugins/GaloisFunctions/IrGLTranslator.cpp
+++ exp/plugins/GaloisFunctions/IrGLTranslator.cpp
@@ -34,13 +34,21 @@ using namespace llvm;
 
 namespace {
 
+// finds and replaces all occurrences
+// ensures progress even if replaceWith contains toReplace
 void findAndReplace(std::string &str, 
     const std::string &toReplace, 
     const std::string &replaceWith) 
 {
-  size_t found = str.find(toReplace);
-  if (found != std::string::npos) {
-    str = str.replace(found, toReplace.length(), replaceWith);
+  std::size_t pos = 0;
+  while (1) {
+    std::size_t found = str.find(toReplace, pos);
+    if (found != std::string::npos) {
+      str = str.replace(found, toReplace.length(), replaceWith);
+      pos = found + replaceWith.length();
+    } else {
+      break;
+    }
   }
 }
 
@@ -48,6 +56,7 @@ class IrGLOperatorVisitor : public RecursiveASTVisitor<IrGLOperatorVisitor> {
 private:
   ASTContext* astContext;
   Rewriter &rewriter; // FIXME: is this necessary? can ASTContext give source code?
+  bool isTopological;
   std::unordered_set<const Stmt *> skipStmts;
   std::unordered_set<const Decl *> skipDecls;
   std::map<std::string, std::string> nodeMap;
@@ -63,17 +72,19 @@ private:
 
 public:
   explicit IrGLOperatorVisitor(ASTContext *context, Rewriter &R, 
+      bool isTopo,
       std::map<std::string, std::string> &globalVariables,
       std::map<std::string, std::string> &sharedVariables,
       std::map<std::string, std::vector<std::string> > &kernels) : 
     astContext(context),
     rewriter(R),
+    isTopological(isTopo),
     GlobalVariablesToTypeMap(globalVariables),
     SharedVariablesToTypeMap(sharedVariables),
     KernelToArgumentsMap(kernels)
   {}
   
-  void WriteCBlock(std::string text) {
+  std::string FormatCBlock(std::string text) {
     assert(text.find("graph.getData") == std::string::npos);
 
     // replace node variables: array of structures to structure of arrays
@@ -92,19 +103,43 @@ public:
         pos = text.find(nodeVar.first);
       }
     }
+    // FIXME: use & (address-of) operator for atomicu variables in atomicAdd() etc
 
-    // FIXME: assumes only one occurrence
+    while (1) {
+      std::size_t found = text.find("nout(");
+      if (found != std::string::npos) {
+        std::size_t begin = text.find(",", found);
+        std::size_t end = text.find(",", begin+1);
+        std::string var = text.substr(begin+1, end - begin - 1);
+        std::string replace = "graph.getOutDegree(" + var + ")";
+        end = text.find(")", end);
+        text = text.replace(found, end - found + 1, replace);
+      } else {
+        break;
+      }
+    } 
+
     findAndReplace(text, "getEdgeDst", "getAbsDestination");
     findAndReplace(text, "getEdgeData", "getAbsWeight");
     findAndReplace(text, "std::fabs", "fabs");
-    // FIXME: should it be only withint quoted strings?
-    findAndReplace(text, "\\", "\\\\");
+    findAndReplace(text, "ctx.push", "WL.push"); // FIXME: insert arguments within quotes
+    findAndReplace(text, "\\", "\\\\"); // FIXME: should it be only within quoted strings?
 
-    size_t found = text.find("printf");
+    return text;
+  }
+
+  void WriteCBlock(std::string text) {
+    text = FormatCBlock(text);
+    std::size_t found = text.find("printf");
     if (found != std::string::npos) {
       bodyString << "CBlock(['" << text << "']),\n";
     } else {
-      bodyString << "CBlock([\"" << text << "\"]),\n";
+      found = text.find("WL.push");
+      if (found != std::string::npos) {
+        bodyString << text << ",\n";
+      } else {
+        bodyString << "CBlock([\"" << text << "\"]),\n";
+      }
     }
   }
 
@@ -132,8 +167,12 @@ public:
 
   virtual bool TraverseStmt(Stmt *S) {
     bool traverse = RecursiveASTVisitor<IrGLOperatorVisitor>::TraverseStmt(S);
-    if (traverse && S && isa<CXXForRangeStmt>(S)) {
-      bodyString << "]),\n"; // end ForAll
+    if (traverse && S) {
+      if (isa<CXXForRangeStmt>(S)) {
+        bodyString << "]),\n"; // end ForAll
+      } else if (isa<IfStmt>(S)) {
+        bodyString << "]),\n"; // end If
+      }
     }
     return traverse;
   }
@@ -144,8 +183,17 @@ public:
     bodyString.str("");
     funcName = func->getParent()->getNameAsString();
     declString << "Kernel(\"" << funcName << "\", [G.param()";
-    bodyString << "ForAll(\"vertex\", G.nodes(),\n[\n";
-    symbolTable.insert("vertex");
+    // FIXME: assuming first parameter is GNode
+    std::string vertexName = func->getParamDecl(0)->getNameAsString();
+    if (isTopological) {
+      bodyString << "ForAll(\"" << vertexName << "\", G.nodes(),\n[\n";
+    } else {
+      bodyString << "ForAll(\"wlvertex\", WL.items(),\n[\n";
+      bodyString << "CDecl([(\"int\", \"" << vertexName << "\", \"\")]),\n";
+      bodyString << "CDecl([(\"bool\", \"pop\", \"\")]),\n";
+      bodyString << "WL.pop(\"pop\", \"wlvertex\", \"" << vertexName << "\"),\n";
+    }
+    symbolTable.insert(vertexName);
     return true;
   }
 
@@ -159,8 +207,16 @@ public:
       }
     }
     symbolTable.insert(forStmt->getLoopVariable()->getNameAsString());
+    const Expr *expr = forStmt->getRangeInit();
+    SourceRange range(expr->getLocStart(), expr->getLocEnd());
+    std::string vertexName = rewriter.getRewrittenText(range);
+    std::size_t found = vertexName.find("graph.edges");
+    assert(found != std::string::npos);
+    std::size_t begin = vertexName.find("(", found);
+    std::size_t end = vertexName.find(",", begin);
+    vertexName = vertexName.substr(begin+1, end - begin - 1);
     bodyString << "ForAll(\"" << forStmt->getLoopVariable()->getNameAsString() 
-      << "\", G.edges(\"vertex\"),\n[\n";
+      << "\", G.edges(\"" << vertexName << "\"),\n[\n";
     return true;
   }
 
@@ -192,6 +248,9 @@ public:
             // get the first argument to graph.getData()
             std::size_t begin = initText.find("(", pos);
             std::size_t end = initText.find(",", pos);
+            if (end == std::string::npos) {
+              end = initText.find(")", pos);
+            }
             std::string index = initText.substr(begin+1, end - begin - 1);
             nodeMap[varDecl->getNameAsString()] = index;
           }
@@ -212,13 +271,17 @@ public:
     return true;
   }
 
-  virtual bool VisitCXXMemberCallExpr(CXXMemberCallExpr *callExpr) {
+  virtual bool VisitCallExpr(CallExpr *callExpr) {
     symbolTable.insert(callExpr->getCalleeDecl()->getAsFunction()->getNameAsString());
     if (skipStmts.find(callExpr) == skipStmts.end()) {
-      CXXRecordDecl *record = callExpr->getRecordDecl();
-      std::string recordName = record->getNameAsString();
-      if (recordName.compare("GReducible") == 0) {
-        Expr *implicit = callExpr->getImplicitObjectArgument();
+      CXXMemberCallExpr *cxxCallExpr = dyn_cast<CXXMemberCallExpr>(callExpr);
+      CXXRecordDecl *record = NULL;
+      if (cxxCallExpr) {
+        record = cxxCallExpr->getRecordDecl();
+      }
+      if (record && 
+          (record->getNameAsString().compare("GReducible") == 0)) {
+        Expr *implicit = cxxCallExpr->getImplicitObjectArgument();
         if (CastExpr *cast = dyn_cast<CastExpr>(implicit)) {
           implicit = cast->getSubExpr();
         }
@@ -249,12 +312,12 @@ public:
     return true;
   }
 
-  virtual bool VisitCallExpr(CallExpr *callExpr) {
-    if (skipStmts.find(callExpr) != skipStmts.end()) {
-      for (unsigned i = 0; i < callExpr->getNumArgs(); ++i) {
-        skipStmts.insert(callExpr->getArg(i));
-      }
-    }
+  virtual bool VisitIfStmt(IfStmt *ifStmt) {
+    const Expr *expr = ifStmt->getCond();
+    SourceRange range(expr->getLocStart(), expr->getLocEnd());
+    std::string text = FormatCBlock(rewriter.getRewrittenText(range));
+    bodyString << "If(\"" << text << "\",\n[\n";
+    skipStmts.insert(expr);
     return true;
   }
 
@@ -283,8 +346,14 @@ public:
     SourceRange range(memberExpr->getBase()->getLocStart(), memberExpr->getBase()->getLocEnd());
     std::string objectName = rewriter.getRewrittenText(range);
     if (nodeMap.find(objectName) != nodeMap.end()) {
-      parameterToTypeMap[memberExpr->getMemberNameInfo().getAsString()] = 
+      std::string type = 
         memberExpr->getMemberDecl()->getType().getUnqualifiedType().getAsString();
+      if (type.compare(0, 11, "std::atomic") == 0) {
+        std::size_t begin = type.find("<");
+        std::size_t end = type.find_last_of(">");
+        type = type.substr(begin+1, end - begin - 1);
+      }
+      parameterToTypeMap[memberExpr->getMemberNameInfo().getAsString()] = type;
     }
     return true;
   }
@@ -294,6 +363,7 @@ public:
     const std::string &varName = decl->getNameAsString();
     if (symbolTable.find(varName) == symbolTable.end()) {
       if (GlobalVariablesToTypeMap.find(varName) == GlobalVariablesToTypeMap.end()) {
+        // FIXME: get initialization value if any
         GlobalVariablesToTypeMap[varName] = decl->getType().getAsString();
       }
     }
@@ -325,9 +395,11 @@ public:
     IrGLAST.open(filename);
     IrGLAST << "from gg.ast import *\n";
     IrGLAST << "from gg.lib.graph import Graph\n";
+    IrGLAST << "from gg.lib.wl import Worklist\n";
     IrGLAST << "from gg.ast.params import GraphParam\n";
     IrGLAST << "import cgen\n";
     IrGLAST << "G = Graph(\"graph\")\n";
+    IrGLAST << "WL = Worklist()\n";
     IrGLAST << "ast = Module([\n";
     IrGLAST << "CBlock([cgen.Include(\"kernels/reduce.cuh\", "
       << "system = False)], parse = False),\n";
@@ -351,7 +423,7 @@ public:
           type = var.second.substr(start+1, end-start-1);
         }
         // FIXME: initialization value?
-        IrGLAST << "CDecl([(\"extern " << type 
+        IrGLAST << "CDeclGlobal([(\"extern " << type 
           << "\", \"" << var.first << "\", \"\")]),\n";
       }
     }
@@ -368,6 +440,7 @@ public:
       IrGLAST << "CDecl((\"Shared<" << var.second << ">\", \"p_" << var.first << "\"";
       IrGLAST << ", \"= Shared<" << var.second << "> (hg.nnodes)\")),\n";
     }
+    // FIXME: initialize shared variables to zero if they are atomic
     std::ifstream orchestrator(ORCHESTRATOR_FILENAME);
     IrGLAST << orchestrator.rdbuf();
     orchestrator.close();
@@ -385,8 +458,7 @@ public:
   }
 
   void WriteCBlock(std::string text) {
-    // FIXME: assumes only one occurrence
-    // FIXME: should it be only withint quoted strings?
+    // FIXME: should it be only within quoted strings?
     findAndReplace(text, "\\", "\\\\");
 
     size_t found = text.find("printf");
@@ -537,25 +609,49 @@ public:
         if (!skip) WriteCBlock(text);
       } else {
         // generate kernel for operator
+        bool isTopological = true;
         assert(callExpr->getNumArgs() > 1);
         Expr *op = callExpr->getArg(1);
         RecordDecl *record = op->getType().getTypePtr()->getAsStructureType()->getDecl();
         CXXRecordDecl *cxxrecord = dyn_cast<CXXRecordDecl>(record);
         for (auto method : cxxrecord->methods()) {
+          // FIXME: handle overloading/polymorphism
           if (method->getNameAsString().compare("operator()") == 0) {
-            // FIXME: determine whether it is topological or data-driven
-            if (method->getNumParams() == 1) {
-              //llvm::errs() << "\nVertex Operator:\n" << 
-              //  rewriter.getRewrittenText(method->getSourceRange()) << "\n";
-              //method->dump();
-              IrGLOperatorVisitor operatorVisitor(astContext, rewriter,
-                  GlobalVariablesToTypeMap,
-                  SharedVariablesToTypeMap,
-                  KernelToArgumentsMap);
-              operatorVisitor.TraverseDecl(method);
-              break;
+            // FIXME: be precise in determining data-driven algorithms
+            if (callExpr->getNumArgs() > 2) {
+              std::string type = callExpr->getArg(2)->getType().getAsString();
+              if (type.find("Galois::WorkList") != std::string::npos) {
+                isTopological = false;
+              }
             }
+            //llvm::errs() << "\nVertex Operator:\n" << 
+            //  rewriter.getRewrittenText(method->getSourceRange()) << "\n";
+            //method->dump();
+            // FIXME: handle arguments to object of CXXRecordDecl?
+            IrGLOperatorVisitor operatorVisitor(astContext, rewriter,
+                isTopological,
+                GlobalVariablesToTypeMap,
+                SharedVariablesToTypeMap,
+                KernelToArgumentsMap);
+            operatorVisitor.TraverseDecl(method);
+            break;
           }
+        }
+
+        if (!isTopological) { // data driven
+          // FIXME: generate precise initial worklist
+          std::ofstream compute;
+          compute.open(COMPUTE_FILENAME, std::ios::app);
+          compute << "Kernel(\"__init_worklist__\", [G.param()],\n[\n";
+          compute << "ForAll(\"vertex\", G.nodes(),\n[\n";
+          compute << "WL.push(\"vertex\"),\n";
+          compute << "]),\n";
+          compute << "]),\n";
+          compute.close();
+
+          Output << "Pipe([\n";
+          Output << "Invoke(\"__init_worklist__\", (\"gg\", )),\n";
+          Output << "Pipe([\n";
         }
 
         // generate call to kernel
@@ -574,6 +670,11 @@ public:
           Output << ", \"p_" << argument << ".gpu_wr_ptr()\"";
         }
         Output << ")),\n";
+
+        if (!isTopological) { // data driven
+          Output << "], ),\n";
+          Output << "], once=True, wlinit=WLInit(\"hg.nedges\", [])),\n";
+        }
       }
     }
     for (unsigned i = 0; i < callExpr->getNumArgs(); ++i) {
