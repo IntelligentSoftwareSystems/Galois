0f689078d2462ac212cdd6690c49240d4153a604
Gurbinder Gill <gill@ices.utexas.edu>
2016-01-27 19:22:49 -0600

    For clang compilation

--- exp/include/Galois/Runtime/Serialize.h
+++ exp/include/Galois/Runtime/Serialize.h
@@ -38,6 +38,10 @@
 //from libc++, clang specific
 namespace std {
 #ifdef __clang__
+template <class T> struct is_trivially_copyable;
+template <class _Tp> struct is_trivially_copyable
+  : public std::integral_constant<bool, __is_trivially_copyable(_Tp)>
+{};
 #else
 #if __GNUC__ < 5
 template<class T>
