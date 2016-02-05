926703ca2f68c1f252b5b2d9cd263396868fe341
Andrew Lenharth <andrew@lenharth.org>
2016-02-05 08:25:18 -0600

    home changes

--- exp/plugins/distifier/distifier.cpp
+++ exp/plugins/distifier/distifier.cpp
@@ -33,8 +33,15 @@ static cl::extrahelp MoreHelp("\nMore help text...");
 class LoopMatcher : public MatchFinder::MatchCallback {
 public:
 
-  StatementMatcher Matcher;
+  struct LoopInfo {
+    const CallExpr* CallSite;
 
+    LoopInfo(const CallExpr* cs) : CallSite(cs) {}
+  };
+  
+  StatementMatcher Matcher;
+  std::vector<LoopInfo> Loops;
+  
   LoopMatcher()
     :Matcher{
     callExpr(callee(functionDecl(
@@ -57,6 +64,8 @@ public:
     llvm::outs() << "\n**\n";
     FS->dump();
     llvm::outs() << "\n\n";
+
+    Loops.emplace_back(FS);
   }
 };
 
