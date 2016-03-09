0efa1b44eee0c62129ebc24196fe0d85d5915103
Rashid Kaleem <rashid@ices.utexas.edu>
2016-03-09 12:07:47 -0600

    Move tool to standalone binary. Fix/hack around with incomplete decl bug.

---
+++ exp/plugins/clify/cl_transforms.h
@@ -0,0 +1,165 @@
+//@author Rashid Kaleem <rashid.kaleem@gmail.com>
+#include "utils.h"
+#include "gAST.h"
+#ifndef _CLIFY_CL_TRANSFORMS_H_
+#define _CLIFY_CL_TRANSFORMS_H_
+
+using namespace clang;
+using namespace clang::ast_matchers;
+using namespace llvm;
+using namespace std;
+
+
+
+/*********************************************************************************************************
+ *
+ *********************************************************************************************************/
+class GraphTypedefRewriter: public MatchFinder::MatchCallback {
+public:
+   Rewriter &rewriter;
+   ASTContext & ctx;
+public:
+   GraphTypedefRewriter(Rewriter &rewriter, ASTContext & c) :
+         rewriter(rewriter), ctx(c) {
+   }
+   ~GraphTypedefRewriter(){
+   }
+
+   //TODO RK - Fix the missing part of the declaration. Matcher returns 'G' instead of 'Graph'.
+   virtual void run(const MatchFinder::MatchResult &Results) {
+         TypedefDecl * decl = const_cast<TypedefDecl*>(Results.Nodes.getNodeAs<TypedefDecl>("GraphTypeDef"));
+         if (decl) {
+            llvm::outs() << " Rewriting typedef for graph \n";
+            decl->dump(llvm::outs());
+            llvm::outs() << " =================\n";
+            llvm::outs() << decl->getUnderlyingType().getAsString()<<"\n";
+            string s = "#include \"CL_Header.h\"\nusing namespace Galois::OpenCL;\n";
+            {
+               //TODO - RK - A hack. Since the begin/end location of the decl are not returning
+               // the entire decl (terminating at 'G' instead of at 'Graph', we try to hack around
+               // it by scanning the end.
+               char * ptr=  ast_utility.get_src_ptr(decl->getSourceRange().getBegin());
+               char * end = ptr;
+               while (*end!=';') end++;
+               string str(ptr, std::distance(ptr, end));
+               s+=str;
+            }
+            //s+=ast_utility.get_string(decl->getSourceRange().getBegin(), decl->getSourceRange().getEnd());
+            string old_name =decl->getUnderlyingType().getAsString();
+            old_name = old_name.substr(0, old_name.find("<"));
+            s.replace(s.find(old_name), old_name.length(), "Graphs::CL_LC_Graph");
+            rewriter.ReplaceText(SourceRange(decl->getSourceRange()), s);
+         }
+   }
+};
+
+/*********************************************************************************************************
+ *
+ *********************************************************************************************************/
+class GraphTypeMapper: public MatchFinder::MatchCallback {
+public:
+   Rewriter &rewriter;
+   std::vector<CXXRecordDecl *> graphDecls;
+   std::set<Type *> graph_types;
+   std::map<CXXRecordDecl *, std::vector<Type *>*> typeDecls;
+public:
+   GraphTypeMapper(Rewriter &rewriter) :
+         rewriter(rewriter) {
+   }
+   virtual ~GraphTypeMapper() {
+
+   }
+   virtual void run(const MatchFinder::MatchResult &Results) {
+      CXXRecordDecl* decl = const_cast<CXXRecordDecl*>(Results.Nodes.getNodeAs<clang::CXXRecordDecl>("GraphType"));
+      VarDecl * vd= const_cast<VarDecl*>(Results.Nodes.getNodeAs<VarDecl>("graphDecl"));
+      assert(decl!=nullptr);
+      assert(vd !=nullptr);
+      if(vd){
+//         rewriter.ReplaceText(vd->getTypeSourceInfo()->getTypeLoc().getSourceRange(), " CLGraph ");
+      }
+      llvm::outs() << "GraphClass :: " << decl->getNameAsString() << ", " << decl->getCanonicalDecl()->getNameAsString() << "\n";
+      graphDecls.push_back(decl);
+      graph_types.insert(const_cast<Type *>(decl->getCanonicalDecl()->getTypeForDecl()));
+      for (auto m : decl->methods()) {
+//         if (m->isConst() && m->param_size() == 1)
+         {
+            std::string mname = m->getNameAsString();
+            if (mname.compare("edge_begin") == 0) {
+               llvm::outs() << "EdgeBegin method found " << m->getNameAsString() << "\n";
+            } else if (mname.compare("edge_end") == 0) {
+               llvm::outs() << "EdgeEnd method found " << m->getNameAsString() << "\n";
+            } else if (mname.compare("getData") == 0) {
+               llvm::outs() << "GetData method found " << m->getNameAsString() << "\n";
+            } else if (mname.compare("getEdgeData") == 0) {
+               llvm::outs() << "GetEdgeData method found " << m->getNameAsString() << "\n";
+            } else if (mname.compare("getEdgeDst") == 0) {
+               llvm::outs() << "GetEdgeDst method found " << m->getNameAsString() << "\n";
+            }
+         } //End if ( m->isConst() && m->param_size()==2)
+      }
+   }
+
+};
+
+/*********************************************************************************************************
+ *
+ *********************************************************************************************************/
+
+class DoAllHandler: public MatchFinder::MatchCallback {
+public:
+   Rewriter &rewriter;
+   Galois::GAST::GaloisApp & app_data;
+public:
+   DoAllHandler(Rewriter &rewriter, Galois::GAST::GaloisApp & _ad) :
+         rewriter(rewriter), app_data(_ad) {
+   }
+/*
+   void add_know_types(const std::set<Type *> & kt){
+      app_data.add_known_types(kt);
+   }
+*/
+   virtual void run(const MatchFinder::MatchResult &Results) {
+      CallExpr* callFS = const_cast<CallExpr*>(Results.Nodes.getNodeAs<clang::CallExpr>("galoisLoop"));
+//      VarDecl * decl = const_cast<VarDecl*>(Results.Nodes.getNodeAs<VarDecl>("graphDecl"));
+//      if(decl)
+//         rewriter.ReplaceText(decl->getTypeSourceInfo()->getTypeLoc().getSourceRange(), " CLGraph ");
+
+      llvm::outs() << "GaloisLoop found  - #Args :: " << callFS->getNumArgs() << "\n";
+
+      if (callFS) {
+         CXXRecordDecl * kernel = const_cast<CXXRecordDecl*>(Results.Nodes.getNodeAs<CXXRecordDecl>("kernelType"));
+         assert(kernel!=nullptr && "KernelType cast failed.");
+         app_data.add_doAll_call(callFS, kernel);
+         llvm::outs() << "Galois::do_All loop found " << callFS->getCalleeDecl()->getCanonicalDecl()->getAsFunction()->getNameAsString() << "\n";
+         //Get the arguments:
+         clang::LangOptions LangOpts;
+         LangOpts.CPlusPlus = true;
+         clang::PrintingPolicy Policy(LangOpts);
+//         unsigned write_setNum = 0;
+//         string GraphNode;
+         //Print the important arguments::
+         if (false && callFS->getNumArgs() >= 3) {
+            llvm::outs() << "Begin iterator :: ";
+            callFS->getArg(0)->printPretty(llvm::outs(), nullptr, Policy);
+            llvm::outs() << ", Type :: " << QualType::getAsString(callFS->getArg(0)->getType().split()) << "\n";
+            llvm::outs() << "End iterator :: ";
+            callFS->getArg(1)->printPretty(llvm::outs(), nullptr, Policy);
+            llvm::outs() << ", Type :: " << QualType::getAsString(callFS->getArg(1)->getType().split()) << "\n";
+            llvm::outs() << "Operator instance :: ";
+            callFS->getArg(2)->printPretty(llvm::outs(), nullptr, Policy);
+            llvm::outs() << ", Type :: " << QualType::getAsString(callFS->getArg(2)->getType().split()) << "\n";
+            llvm::outs()<<"-------->OPERATOR CALLED IN DO_ALL::\n";
+            callFS->getArg(2)->getBestDynamicClassType()->dump();
+            llvm::outs() << ", Type :: " << callFS->getArg(2)->getBestDynamicClassType()->getName() << "\n";
+
+         }
+         rewriter.ReplaceText(SourceRange(callFS->getCallee()->getLocStart(), callFS->getCallee()->getLocEnd()), "do_all_cl");
+      }
+   }
+};
+/*********************************************************************************************************
+ *
+ *********************************************************************************************************/
+
+
+#endif//_CLIFY_CL_TRANSFORMS_H_
