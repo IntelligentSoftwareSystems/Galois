cc404f7a75dc547de7164b05bb680ca43bf8ee5a
Roshan Dathathri <roshan@cs.utexas.edu>
2016-05-26 11:59:52 -0500

    Repacking abelian compiler script

--- exp/apps/compiler_outputs/compiler.sh
+++ exp/apps/compiler_outputs/compiler.sh
@@ -1,23 +1,42 @@
 #!/bin/sh
 
-SRC=input.cpp
-LLVM_BUILD=/h2/roshan/SourceCode/llvm/build
-GGC_ROOT=/h2/roshan/SourceCode/ggc
-GALOIS_ROOT=/h2/roshan/SourceCode/GaloisCpp
+SRC=$1
 
-CXX_DEFINES="-DGALOIS_COPYRIGHT_YEAR=2015 -DGALOIS_USE_EXP -DGALOIS_VERSION=2.3.0 -DGALOIS_VERSION_MAJOR=2 -DGALOIS_VERSION_MINOR=3 -DGALOIS_VERSION_PATCH=0 -D__STDC_LIMIT_MACROS"
-CXX_FLAGS="-g -Wall -fcolor-diagnostics -Wall -g -I$GALOIS_ROOT/exp/include -I/opt/apps/ossw/libraries/mpich2/mpich2-1.5/c7/clang-system/include -I/net/faraday/workspace/local/modules/tbb-4.2/include -I/opt/apps/ossw/libraries/boost/boost-1.58.0/c7/clang-system/include -I$GALOIS_ROOT/lonestar/include -I$GALOIS_ROOT/libruntime/include -I$GALOIS_ROOT/libnet/include -I$GALOIS_ROOT/libsubstrate/include -I$GALOIS_ROOT/libllvm/include -I$GALOIS_ROOT/build/debug/libllvm/include -I$GALOIS_ROOT/libgraphs/include -std=gnu++11"
-GGC_FLAGS="--cuda-worklist basic --cuda-graph basic"
-if [ -f "GGCFLAGS" ]; then
-  GGC_FLAGS+=$(head -n 1 "GGCFLAGS")
+if [ -z "$ABELIAN_LLVM_BUILD" ]; then
+  ABELIAN_LLVM_BUILD=/net/velocity/workspace/SourceCode/llvm/build
+fi
+if [ -z "$ABELIAN_GALOIS_ROOT" ]; then
+  ABELIAN_GALOIS_ROOT=/net/velocity/workspace/SourceCode/GaloisCpp
+fi
+if [ -z "$ABELIAN_NON_HETEROGENEOUS" ]; then
+  if [ -z "$ABELIAN_GGC_ROOT" ]; then
+    ABELIAN_GGC_ROOT=/net/velocity/workspace/SourceCode/ggc
+  fi
 fi
 
-CXX=$LLVM_BUILD/bin/clang++
-IRGL_CXX="$CXX -Xclang -load -Xclang $LLVM_BUILD/lib/GaloisFunctions.so -Xclang -plugin -Xclang irgl"
-GPREPROCESS_CXX="$CXX -Xclang -load -Xclang $LLVM_BUILD/lib/GaloisFunctionsPreProcess.so -Xclang -plugin -Xclang galois-preProcess"
-GANALYSIS_CXX="$CXX -Xclang -load -Xclang $LLVM_BUILD/lib/GaloisFunctionsAnalysis.so -Xclang -plugin -Xclang galois-analysis"
-GFUNCS_CXX="$CXX -Xclang -load -Xclang $LLVM_BUILD/lib/GaloisFunctions.so -Xclang -plugin -Xclang galois-fns"
-GGC="$GGC_ROOT/src/ggc"
+echo "Using LLVM build:" $ABELIAN_LLVM_BUILD
+echo "Using Galois:" $ABELIAN_GALOIS_ROOT
+if [ -z "$ABELIAN_NON_HETEROGENEOUS" ]; then
+  echo "Using GGC:" $ABELIAN_GGC_ROOT
+fi
+
+CXX_DEFINES="-DGALOIS_COPYRIGHT_YEAR=2015 -DGALOIS_USE_EXP -DGALOIS_VERSION=2.3.0 -DGALOIS_VERSION_MAJOR=2 -DGALOIS_VERSION_MINOR=3 -DGALOIS_VERSION_PATCH=0 -D__STDC_LIMIT_MACROS"
+CXX_FLAGS="-g -Wall -fcolor-diagnostics -Wall -g -I$ABELIAN_GALOIS_ROOT/exp/include -I/opt/apps/ossw/libraries/mpich2/mpich2-1.5/c7/clang-system/include -I/net/faraday/workspace/local/modules/tbb-4.2/include -I/opt/apps/ossw/libraries/boost/boost-1.58.0/c7/clang-system/include -I$ABELIAN_GALOIS_ROOT/lonestar/include -I$ABELIAN_GALOIS_ROOT/libruntime/include -I$ABELIAN_GALOIS_ROOT/libnet/include -I$ABELIAN_GALOIS_ROOT/libsubstrate/include -I$ABELIAN_GALOIS_ROOT/libllvm/include -I$ABELIAN_GALOIS_ROOT/build/debug/libllvm/include -I$ABELIAN_GALOIS_ROOT/libgraphs/include -std=gnu++11"
+if [ -z "$ABELIAN_NON_HETEROGENEOUS" ]; then
+  GGC_FLAGS="--cuda-worklist basic --cuda-graph basic"
+  if [ -f "GGCFLAGS" ]; then
+    GGC_FLAGS+=$(head -n 1 "GGCFLAGS")
+  fi
+fi
+
+CXX=$ABELIAN_LLVM_BUILD/bin/clang++
+GPREPROCESS_CXX="$CXX -Xclang -load -Xclang $ABELIAN_LLVM_BUILD/lib/GaloisFunctionsPreProcess.so -Xclang -plugin -Xclang galois-preProcess"
+GANALYSIS_CXX="$CXX -Xclang -load -Xclang $ABELIAN_LLVM_BUILD/lib/GaloisFunctionsAnalysis.so -Xclang -plugin -Xclang galois-analysis"
+GFUNCS_CXX="$CXX -Xclang -load -Xclang $ABELIAN_LLVM_BUILD/lib/GaloisFunctions.so -Xclang -plugin -Xclang galois-fns"
+if [ -z "$ABELIAN_NON_HETEROGENEOUS" ]; then
+  IRGL_CXX="$CXX -Xclang -load -Xclang $ABELIAN_LLVM_BUILD/lib/GaloisFunctions.so -Xclang -plugin -Xclang irgl"
+  GGC="$ABELIAN_GGC_ROOT/src/ggc"
+fi
 
 log=.log
 
@@ -33,10 +52,18 @@ $GANALYSIS_CXX $CXX_DEFINES $CXX_FLAGS -o .temp.o -c gen.cpp >>$log 2>&1
 echo "Generating communication code"
 $GFUNCS_CXX $CXX_DEFINES $CXX_FLAGS -o .temp.o -c gen.cpp >>$log 2>&1
 
-echo "Generating IrGL code"
-$IRGL_CXX $CXX_DEFINES $CXX_FLAGS -o .temp.o -c gen.cpp >>$log 2>&1
-echo "Generating CUDA code from IrGL"
-$GGC $GGC_FLAGS -o gen_cuda.cu gen_cuda.py >>$log 2>&1
+if [ -z "$ABELIAN_NON_HETEROGENEOUS" ]; then
+  echo "Generating IrGL code"
+  $IRGL_CXX $CXX_DEFINES $CXX_FLAGS -o .temp.o -c gen.cpp >>$log 2>&1
+  echo "Generating CUDA code from IrGL"
+  $GGC $GGC_FLAGS -o gen_cuda.cu gen_cuda.py >>$log 2>&1
+fi
+
+if [ -z "$ABELIAN_NON_HETEROGENEOUS" ]; then
+  echo "Generated files: gen.cpp gen_cuda.py gen_cuda.h gen_cuda.cuh gen_cuda.cu" 
+else
+  echo "Generated files: gen.cpp" 
+fi
 
 rm -f Entry-*.dot
 
