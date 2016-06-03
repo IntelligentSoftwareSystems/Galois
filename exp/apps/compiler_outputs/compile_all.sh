633e3940462cbf83c54447e5ab26cf370bb52448
Roshan Dathathri <roshan@cs.utexas.edu>
2016-06-03 12:25:15 -0500

    Improving reusability of compiler scripts

--- exp/apps/compiler_outputs/compile_all.sh
+++ exp/apps/compiler_outputs/compile_all.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-INPUT_DIR=../compiler_inputs
+if ! [ -z "$ABELIAN_GALOIS_ROOT" ]; then
+  BASE_DIR=${ABELIAN_GALOIS_ROOT}/exp/apps
+else
+  BASE_DIR=..
+fi
+INPUT_DIR=${BASE_DIR}/compiler_inputs
+OUTPUT_DIR=${BASE_DIR}/compiler_outputs
 
 for input in $INPUT_DIR/*.cpp; do
   name=$(basename "$input" ".cpp")
-  ./compiler.sh $input $name
+  ./compiler.sh $input ${OUTPUT_DIR}/$name
 done
 
