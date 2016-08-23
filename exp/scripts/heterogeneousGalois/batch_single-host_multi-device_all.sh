e130b413d156e6d2381aecdee17081a1d10881b0
Roshan Dathathri <roshan@cs.utexas.edu>
2016-08-22 20:13:45 -0500

    Fixing run scripts

--- exp/scripts/heterogeneousGalois/batch_single-host_multi-device_all.sh
+++ exp/scripts/heterogeneousGalois/batch_single-host_multi-device_all.sh
@@ -2,7 +2,7 @@
 
 EXECS=( "bfs_pull-topological_edge-cut" "pagerank_pull-topological_edge-cut" "cc_pull-topological_edge-cut" "sssp_pull-topological_edge-cut" "bfs_push-worklist_edge-cut" "pagerank_push-worklist_edge-cut" "cc_push-worklist_edge-cut" "sssp_push-worklist_edge-cut" "bfs_push-filter_edge-cut" "pagerank_push-filter_edge-cut" "cc_push-filter_edge-cut" "sssp_push-filter_edge-cut" "bfs_push-topological_edge-cut" "pagerank_push-topological_edge-cut" "cc_push-topological_edge-cut" "sssp_push-topological_edge-cut" )
 
-INPUTS=( "rmat25.gr" "twitter-WWW10-component.gr" "twitter-ICWSM10-component.gr" "road-USA.gr" )
+INPUTS=( "rmat25" "twitter-WWW10-component" "twitter-ICWSM10-component" "road-USA" )
 
 for j in "${INPUTS[@]}"
 do
