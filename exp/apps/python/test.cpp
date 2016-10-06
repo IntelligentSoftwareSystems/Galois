e27259ada59a52e1e3857e4eac58baa629d48567
Yi-Shan Lu <ylu@ices.utexas.edu>
2016-10-06 17:35:57 -0500

    test for validity of edge_iterator after addMultiEdge

--- exp/apps/python/test.cpp
+++ exp/apps/python/test.cpp
@@ -1,16 +1,46 @@
 #include "PythonGraph.h"
 
+#include <vector>
+#include <iostream>
+
 int main(int argc, char *argv[]) {
+  std::vector<GNode> nodes;
+  std::vector<edge_iterator> edges;
+
   Graph *g = createGraph();
-  auto n1 = createNode(g);
-  addNode(g, n1);
-  addNodeAttr(g, n1, "color", "red");
-  auto n2 = createNode(g);
-  addNode(g, n2);
-  addNodeAttr(g, n2, "language", "english");
-  auto n1n2e1 = addMultiEdge(g, n1, n2);
-  addEdgeAttr(g, n1n2e1, "weight", "3.0");
+
+  nodes.push_back(createNode(g));
+  addNode(g, nodes[0]);
+  addNodeAttr(g, nodes[0], "color", "red");
+
+  nodes.push_back(createNode(g));
+  addNode(g, nodes[1]);
+  addNodeAttr(g, nodes[1], "language", "english");
+
+  edges.push_back(addMultiEdge(g, nodes[0], nodes[1]));
+  addEdgeAttr(g, edges[0], "weight", "3.0");
+
+  edges.push_back(addMultiEdge(g, nodes[0], nodes[1]));
+  addEdgeAttr(g, edges[1], "place", "texas");
+
+  for(auto n: nodes) {
+    for(auto i: g->getData(n)) {
+      std::cout << i.first << ": " << i.second << std::endl;
+    }
+  }
+
+  for(auto e: edges) {
+    for(auto i: g->getEdgeData(e)) {
+      std::cout << i.first << ": " << i.second << std::endl;
+    }
+  }
+
   analyzeBFS(g, 1);
+
+  deleteGraph(g);
+  nodes.clear();
+  edges.clear();
+
   return 0;
 }
 
