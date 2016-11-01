475bc1ea2110af7ea57e060ac1349e92f8c874fa
Yi-Shan Lu <ylu@ices.utexas.edu>
2016-11-01 16:23:01 -0500

    report2.py parses into csv current Galois output generated with run.py

---
+++ scripts/report2.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+import sys
+import collections
+
+def main():
+  class Row:
+    def __init__(self):
+      self.reset()
+    def reset(self):
+      self.r = collections.defaultdict(str)
+      self.header = None
+    def get(self, token, key):
+      return token[self.header.index(key)]
+
+  row = Row()
+  rows = []
+  cols = set()
+  in_stat = False
+  in_parse = False
+
+  for line in sys.stdin:
+    try:
+      param_token = [i.strip() for i in line.split()]
+      stat_token = [i.strip() for i in line.split(",")]
+
+      # empty line
+      if param_token == []:
+        continue
+
+      # parameter setting by run.py
+      if param_token[0] == "RUN:":
+        if param_token[1] == "Start":
+          if in_parse == True:
+            if row.r:
+              rows.append(row.r)
+              row.reset()
+          else:
+            in_parse = True
+          in_stat = False
+        elif param_token[1] == "Variable":
+          key = param_token[2]
+          cols.add(key)
+          row.r[key] = param_token[4] # param_token[3] is "="
+        elif param_token[1] == "CommandLine":
+          cmd_token = [i.strip() for i in line.split(None, 2)]
+          key = cmd_token[1]
+          cols.add(key)
+          row.r[key] = cmd_token[2]
+
+      # stat header returned by Galois
+      elif stat_token[0] == "LOOP":
+        in_stat = True
+        row.header = stat_token
+
+      # stat lines
+      elif in_stat == True:
+        loop_name = row.get(stat_token, "LOOP")
+        key = row.get(stat_token, "CATEGORY")
+        if loop_name != "(NULL)":
+          key = loop_name + "-" + key
+        cols.add(key)
+        row.r[key] = row.get(stat_token, "VAL")
+
+    except:
+      sys.stderr.write("Error parsing line: %s" % line)
+      raise
+
+  if row.r:
+    rows.append(row.r)
+  cols = sorted(cols)
+
+  print(','.join(cols))
+  for r in rows:
+    print(','.join([str(r[c]) for c in cols]))
+
+
+if __name__ == "__main__":
+  main()
