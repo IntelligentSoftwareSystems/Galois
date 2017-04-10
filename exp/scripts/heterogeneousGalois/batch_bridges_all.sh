b736ad9bb9534d826514b6e5726535242e3e394b
Roshan Dathathri <roshand@cs.utexas.edu>
2017-04-10 13:49:57 -0400

    Updating bridges scripts to use statOutputFile and new pset

--- exp/scripts/heterogeneousGalois/batch_bridges_all.sh
+++ exp/scripts/heterogeneousGalois/batch_bridges_all.sh
@@ -3,7 +3,7 @@
 EXECS=( "bfs_pull-topological" "pagerank_pull-topological" "cc_pull-topological" "sssp_pull-topological" "bfs_push-filter" "pagerank_push-filter" "cc_push-filter" "sssp_push-filter" "bfs_push-worklist" "pagerank_push-worklist" "cc_push-worklist" "sssp_push-worklist" "bfs_push-topological" "pagerank_push-topological" "cc_push-topological" "sssp_push-topological" )
 EXECS=( "bfs_push-filter_comm-updated-only" "pagerank_push-filter_comm-updated-only" "cc_push-filter_comm-updated-only" "sssp_push-filter_comm-updated-only" )
 
-SET="1,c,4:00:00 2,cc,03:30:00 4,cccc,03:00:00 8,cccccccc,02:30:00 16,cccccccccccccccc,02:00:00 32,cccccccccccccccccccccccccccccccc,01:45:00 64,cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc,01:30:00 128,cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc,01:00:00"
+SET="1,4:00:00 2,03:30:00 4,03:00:00 8,02:30:00 16,02:00:00 32,01:45:00 64,01:30:00 128,01:00:00"
 
 INPUTS=("rmat20;\"${SET}\"")
 INPUTS=("uk-2007;\"${SET}\"")
--- exp/scripts/heterogeneousGalois/run_bridges_all.sh
+++ exp/scripts/heterogeneousGalois/run_bridges_all.sh
@@ -4,8 +4,7 @@ EXEC=$1
 INPUT=$2
 SET=$3
 QUEUE=$4
-
-EXECDIR=/pylon2/ci4s88p/roshand/GaloisCpp-build/exp/apps/compiler_outputs
+HET=$5 # not supported for now
 
 SET="${SET%\"}"
 SET="${SET#\"}"
@@ -14,58 +13,58 @@ for task in $SET; do
   IFS=",";
   set $task;
   cp run_bridges.template.sbatch run_bridges.sbatch 
-  if [ $QUEUE == "GPU" ]; then
+  if [ $QUEUE == "GPU" ]; then # should add HET option
     ntasks=4
     ntasks=$((ntasks*$1))
-    sed -i "2i#SBATCH -t $3" run_bridges.sbatch
+    sed -i "2i#SBATCH -t $2" run_bridges.sbatch
     sed -i "2i#SBATCH --gres=gpu:k80:4" run_bridges.sbatch
     sed -i "2i#SBATCH --ntasks-per-node 4" run_bridges.sbatch
     sed -i "2i#SBATCH -N $1" run_bridges.sbatch
     sed -i "2i#SBATCH -p $QUEUE" run_bridges.sbatch
-    sed -i "2i#SBATCH -o ${EXEC}_${INPUT}_${ntasks}_${2}_%j.out" run_bridges.sbatch
-    sed -i "2i#SBATCH -J ${EXEC}_${INPUT}_${ntasks}_${2}" run_bridges.sbatch
+    sed -i "2i#SBATCH -o ${EXEC}_${INPUT}_${ntasks}_g_%j.out" run_bridges.sbatch
+    sed -i "2i#SBATCH -J ${EXEC}_${INPUT}_${ntasks}_g" run_bridges.sbatch
     threads=7
-    echo -n "multi-GPU-only " $EXEC $INPUT $1 $ntasks $2 $threads $3 " "
-    sbatch run_bridges.sbatch ${EXECDIR}/$EXEC $INPUT $ntasks $2 $threads
-  elif [ $QUEUE == "GPU-shared" ]; then
-    if [[ ($2 == *"gc"*) || ($2 == *"cg"*) ]]; then
+    echo -n "multi-GPU-only " $EXEC $INPUT $1 $ntasks "gggg" $threads $2 " "
+    sbatch run_bridges.sbatch $EXEC $INPUT $ntasks gggg $threads
+  elif [ $QUEUE == "GPU-shared" ]; then # should be fixed
+    if [[ $HET == 1 ]]; then
       threads=28
       ngpus=$1
       ngpus=$((ngpus-1))
-      sed -i "2i#SBATCH -t $3" run_bridges.sbatch
+      sed -i "2i#SBATCH -t $2" run_bridges.sbatch
       sed -i "2i#SBATCH --gres=gpu:$ngpus" run_bridges.sbatch
       sed -i "2i#SBATCH --ntasks-per-node $threads" run_bridges.sbatch
       sed -i "2i#SBATCH -N 1" run_bridges.sbatch
       sed -i "2i#SBATCH -p $QUEUE" run_bridges.sbatch
-      sed -i "2i#SBATCH -o ${EXEC}_${INPUT}_${1}_${2}_%j.out" run_bridges.sbatch
-      sed -i "2i#SBATCH -J ${EXEC}_${INPUT}_${1}_${2}" run_bridges.sbatch
+      sed -i "2i#SBATCH -o ${EXEC}_${INPUT}_${1}_cg_%j.out" run_bridges.sbatch
+      sed -i "2i#SBATCH -J ${EXEC}_${INPUT}_${1}_cg" run_bridges.sbatch
       threads=$((threads-$ngpus))
       threads=$((threads-$ngpus))
-      echo -n "CPU+GPU" $EXEC $INPUT $1 $2 $threads $3 " "
-      sbatch run_bridges.sbatch ${EXECDIR}/$EXEC $INPUT $1 $2 $threads
+      echo -n "CPU+GPU" $EXEC $INPUT $1 "cgggg" $threads $2 " "
+      sbatch run_bridges.sbatch $EXEC $INPUT $1 cgggg $threads
     else
       threads=7
       threads=$((threads*$1))
-      sed -i "2i#SBATCH -t $3" run_bridges.sbatch
+      sed -i "2i#SBATCH -t $2" run_bridges.sbatch
       sed -i "2i#SBATCH --gres=gpu:$1" run_bridges.sbatch
       sed -i "2i#SBATCH --ntasks-per-node $threads" run_bridges.sbatch
       sed -i "2i#SBATCH -N 1" run_bridges.sbatch
       sed -i "2i#SBATCH -p $QUEUE" run_bridges.sbatch
-      sed -i "2i#SBATCH -o ${EXEC}_${INPUT}_${1}_${2}_%j.out" run_bridges.sbatch
-      sed -i "2i#SBATCH -J ${EXEC}_${INPUT}_${1}_${2}" run_bridges.sbatch
-      echo -n "GPU-only" $EXEC $INPUT $1 $2 $threads $3 " "
-      sbatch run_bridges.sbatch ${EXECDIR}/$EXEC $INPUT $1 $2 $threads
+      sed -i "2i#SBATCH -o ${EXEC}_${INPUT}_${1}_g_%j.out" run_bridges.sbatch
+      sed -i "2i#SBATCH -J ${EXEC}_${INPUT}_${1}_g" run_bridges.sbatch
+      echo -n "GPU-only" $EXEC $INPUT $1 "gggg" $threads $2 " "
+      sbatch run_bridges.sbatch $EXEC $INPUT $1 gggg $threads
     fi
   elif [ $QUEUE == "RM" ]; then
-    sed -i "2i#SBATCH -t $3" run_bridges.sbatch
+    sed -i "2i#SBATCH -t $2" run_bridges.sbatch
     sed -i "2i#SBATCH --ntasks-per-node 1" run_bridges.sbatch
     sed -i "2i#SBATCH -N $1" run_bridges.sbatch
     sed -i "2i#SBATCH -p $QUEUE" run_bridges.sbatch
-    sed -i "2i#SBATCH -o ${EXEC}_${INPUT}_${1}_${2}_%j.out" run_bridges.sbatch
-    sed -i "2i#SBATCH -J ${EXEC}_${INPUT}_${1}_${2}" run_bridges.sbatch
+    sed -i "2i#SBATCH -o ${EXEC}_${INPUT}_${1}_c_%j.out" run_bridges.sbatch
+    sed -i "2i#SBATCH -J ${EXEC}_${INPUT}_${1}_c" run_bridges.sbatch
     threads=28
-    echo -n "CPU-only" $EXEC $INPUT $1 $2 $threads $3 " "
-    sbatch run_bridges.sbatch ${EXECDIR}/$EXEC $INPUT $1 $2 $threads
+    echo -n "CPU-only" $EXEC $INPUT $1 "c" $threads $2 " "
+    sbatch run_bridges.sbatch $EXEC $INPUT $1 c $threads
   fi
   rm run_bridges.sbatch
 done
