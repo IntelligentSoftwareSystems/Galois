a0d09d3040c23ecbe622dd6d0ba98e8e1ce99993
Yi-Shan Lu <ylu@ices.utexas.edu>
2017-06-16 12:46:56 -0500

    asdd bspIm algorithm (improved bsp)

--- exp/apps/ktruss/K-Truss.cpp
+++ exp/apps/ktruss/K-Truss.cpp
@@ -42,6 +42,7 @@
 
 enum Algo {
   bsp,
+  bspIm,
   async
 };
 
@@ -58,6 +59,7 @@ static cll::opt<std::string> outName("o", cll::desc("output file for the edgelis
 static cll::opt<Algo> algo("algo", cll::desc("Choose an algorithm:"), 
   cll::values(
     clEnumValN(Algo::bsp, "bsp", "Bulk-synchronous parallel (default)"), 
+    clEnumValN(Algo::bspIm, "bspIm", "Bulk-synchronous parallel improved"),
     clEnumValN(Algo::async, "async", "Asynchronous"), 
     clEnumValEnd), cll::init(Algo::bsp));
 
@@ -258,6 +260,129 @@ struct BSPAlgo {
   } // end operator()
 }; // end struct BSPAlgo
 
+struct BSPImprovedAlgo {
+  // edge weight: (# triangles supported << 1) | removal
+  //   set LSB of an edge weight to indicate the removal of the edge.
+  //   << 1 to track # triangles an edge supports, 
+  //   >> 1 when computing edge supports
+  typedef Galois::Graph::LC_CSR_Graph<void, uint32_t>
+    ::template with_numa_alloc<true>::type
+    ::template with_no_lockable<true>::type Graph;
+  typedef Graph::GraphNode GNode;
+
+  typedef std::pair<GNode, GNode> Edge;
+  typedef Galois::InsertBag<Edge> EdgeVec;
+
+  std::string name() { return "bspIm"; }
+
+  struct KeepSupportedEdges {
+    Graph& g;
+    unsigned int j;
+    EdgeVec& s;
+
+    KeepSupportedEdges(Graph& g, unsigned int j, EdgeVec& s)
+      : g(g), j(j), s(s) {}
+
+    bool isSupportNoLessThanJ(GNode src, GNode dst) {
+      size_t numValidEqual = 0;
+      auto srcI = g.edge_begin(src, Galois::MethodFlag::UNPROTECTED), 
+        srcE = g.edge_end(src, Galois::MethodFlag::UNPROTECTED), 
+        dstI = g.edge_begin(dst, Galois::MethodFlag::UNPROTECTED), 
+        dstE = g.edge_end(dst, Galois::MethodFlag::UNPROTECTED);
+
+      while (true) {
+        // find the first valid edge
+        while (srcI != srcE && (g.getEdgeData(srcI) & removed)) {
+          ++srcI;
+        }
+        while (dstI != dstE && (g.getEdgeData(dstI) & removed)) {
+          ++dstI;
+        }
+
+        if (srcI == srcE || dstI == dstE) {
+          return numValidEqual >= j;
+        }
+
+        // check for intersection
+        auto sN = g.getEdgeDst(srcI), dN = g.getEdgeDst(dstI);
+        if (sN < dN) {
+          ++srcI;
+        } else if (dN < sN) {
+          ++dstI;
+        } else {
+          numValidEqual += 1;
+          if (numValidEqual >= j) {
+            return true;
+          }
+          ++srcI;
+          ++dstI;
+        }
+      }
+      return numValidEqual >= j;
+    }
+
+    void operator()(Edge e) {
+      if (isSupportNoLessThanJ(e.first, e.second)) {
+        s.push_back(e);
+      } else {
+        g.getEdgeData(g.findEdgeSortedByDst(e.first, e.second)) = removed;
+        g.getEdgeData(g.findEdgeSortedByDst(e.second, e.first)) = removed;
+      }
+    }
+  };
+
+  void operator()(Graph& g, unsigned int k) {
+    if (0 == k-2) {
+      return;
+    }
+
+    EdgeVec work[2];
+    EdgeVec *cur = &work[0], *next = &work[1];
+    size_t curSize, nextSize;
+
+    // symmetry breaking: 
+    // consider only edges (i, j) where i < j
+    Galois::do_all_local(g, 
+      [&g, cur] (GNode n) {
+        for (auto e: g.edges(n)) {
+          auto dst = g.getEdgeDst(e);
+          if (dst > n) {
+            cur->push_back(std::make_pair(n, dst));
+          }
+        }
+      },
+      Galois::do_all_steal<true>()
+    );
+    curSize = std::distance(cur->begin(), cur->end());
+
+    size_t iter = 0;
+
+    while (true) {
+      std::cout << "Iteration " << iter << ": ";
+      std::cout << countValidNodes(g) << " valid nodes, ";
+      std::cout << std::distance(cur->begin(), cur->end()) << " valid edges" << std::endl;
+
+      Galois::do_all_local(*cur, 
+        KeepSupportedEdges{g, k-2, *next},
+        Galois::do_all_steal<true>()
+      );
+      nextSize = std::distance(next->begin(), next->end());
+
+      if (curSize == nextSize) {
+        // every edge in *cur is kept, done
+        break;
+      }
+
+      cur->clear();
+      curSize = nextSize;
+      std::swap(cur, next);
+      ++iter;
+    } 
+
+    std::cout << "Ends at iteration " << iter << std::endl;
+  } // end operator()
+}; // end struct BSPImprovedAlgo
+
 template<typename Algo>
 void run() {
   Algo algo;
@@ -292,6 +417,9 @@ int main(int argc, char **argv) {
   switch (algo) {
   case bsp: 
     run<BSPAlgo>(); 
+    break;
+  case bspIm:
+    run<BSPImprovedAlgo>();
     break;
   case async: 
 //    run<AsyncAlgo>(); 
