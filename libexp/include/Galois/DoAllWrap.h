55339e52b66a90ce1559cc7a8a9e08147cea1516
Roshan Dathathri <roshan@cs.utexas.edu>
2017-07-19 11:41:56 -0500

    Making do_all_choice options available through the command line

--- libexp/include/Galois/DoAllWrap.h
+++ libexp/include/Galois/DoAllWrap.h
@@ -53,7 +53,19 @@ enum DoAllTypes {
 };
 
 namespace cll = llvm::cl;
-extern cll::opt<DoAllTypes> doAllKind;
+//extern cll::opt<DoAllTypes> doAllKind;
+cll::opt<DoAllTypes> doAllKind (
+    cll::desc ("DoAll Implementation"),
+    cll::values (
+      clEnumVal (DOALL_GALOIS, "DOALL_GALOIS"),
+      clEnumVal (DOALL_GALOIS_STEAL, "DOALL_GALOIS_STEAL"),
+      clEnumVal (DOALL_GALOIS_FOREACH, "DOALL_GALOIS_FOREACH"),
+      clEnumVal (DOALL_COUPLED, "DOALL_COUPLED"),
+      clEnumVal (DOALL_CILK, "DOALL_CILK"),
+      clEnumVal (DOALL_OPENMP, "DOALL_OPENMP"),
+      clEnumValEnd),
+    cll::init (DOALL_COUPLED));
+
 
 void setDoAllImpl (const DoAllTypes& type);
 
