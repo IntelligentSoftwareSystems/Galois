543984233deac140324f1bbcddd01a37c646f963
Roshan Dathathri <roshan@cs.utexas.edu>
2017-08-19 02:29:14 -0500

    stampede2 scripts

--- dist_exp/scripts/heterogeneousGalois/batch_stampede_all.sh
+++ dist_exp/scripts/heterogeneousGalois/batch_stampede_all.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 
-EXECS=( "bfs_pull-topological" "pagerank_pull-topological" "cc_pull-topological" "sssp_pull-topological" "bfs_push-filter" "pagerank_push-filter" "cc_push-filter" "sssp_push-filter" "bfs_push-worklist" "pagerank_push-worklist" "cc_push-worklist" "sssp_push-worklist" "bfs_push-topological" "pagerank_push-topological" "cc_push-topological" "sssp_push-topological" )
-EXECS=( "bfs_push-filter_comm-updated-only" "pagerank_push-filter_comm-updated-only" "cc_push-filter_comm-updated-only" "sssp_push-filter_comm-updated-only" )
+EXECS=( "bfs_push-filter" "pagerank_pull-topological" "kcore_push-filter" "cc_push-filter" "sssp_push-filter" )
 
 SET="1,2:00:00 2,01:30:00 4,01:00:00 8,00:45:00 16,00:30:00 32,00:20:00"
+SET="128,00:30:00 64,00:45:00 32,01:00:00"
 
-INPUTS=("clueweb12;\"${SET}\"")
 INPUTS=("uk-2007;\"${SET}\"")
-INPUTS=("rmat28;\"${SET}\"")
 INPUTS=("twitter-ICWSM10-component;\"${SET}\"")
+INPUTS=("rmat24;\"${SET}\"")
+INPUTS=("rmat28;\"${SET}\"")
+INPUTS=("clueweb12;\"${SET}\"")
 
 QUEUE=gpu
-QUEUE=normal
 QUEUE=development
+QUEUE=normal
+
+PART=oec
+PART=hivc
+PART=2dvc
+PART=cvc
 
 for j in "${INPUTS[@]}"
 do
@@ -20,8 +26,8 @@ do
   set $j;
   for i in "${EXECS[@]}"
   do
-      echo "./run_stampede_all.sh ${i} ${1} ${2} $QUEUE"
-      ./run_stampede_all.sh ${i} ${1} ${2} $QUEUE |& tee -a jobs
+      echo "./run_stampede_all.sh ${i} ${1} ${2} $QUEUE $PART"
+      ./run_stampede_all.sh ${i} ${1} ${2} $QUEUE $PART |& tee -a jobs
   done
 done
 
--- dist_exp/scripts/heterogeneousGalois/run_stampede_all.sh
+++ dist_exp/scripts/heterogeneousGalois/run_stampede_all.sh
@@ -4,7 +4,8 @@ EXEC=$1
 INPUT=$2
 SET=$3
 QUEUE=$4
-HET=$5 # not supported for now
+PART=$5
+HET=$6 # not supported for now
 
 SET="${SET%\"}"
 SET="${SET#\"}"
@@ -18,7 +19,7 @@ for task in $SET; do
     sed -i "2i#SBATCH -p $QUEUE" run_stampede.sbatch
     sed -i "2i#SBATCH -N $1 -n $1" run_stampede.sbatch
     sed -i "2i#SBATCH -o ${EXEC}_${INPUT}_${1}_g_%j.out" run_stampede.sbatch
-    sed -i "2i#SBATCH -J stamped_${EXEC}_${INPUT}_${1}_g" run_stampede.sbatch
+    sed -i "2i#SBATCH -J ${EXEC}_${INPUT}_${1}_g" run_stampede.sbatch
     threads=16
     echo "multi-GPU-only " $EXEC $INPUT $1 "g" $threads $2
     sbatch run_stampede.sbatch $EXEC $INPUT g $threads
@@ -26,11 +27,11 @@ for task in $SET; do
     sed -i "2i#SBATCH -t $2" run_stampede.sbatch
     sed -i "2i#SBATCH -p $QUEUE" run_stampede.sbatch
     sed -i "2i#SBATCH -N $1 -n $1" run_stampede.sbatch
-    sed -i "2i#SBATCH -o ${EXEC}_${INPUT}_${1}_c_%j.out" run_stampede.sbatch
-    sed -i "2i#SBATCH -J stamped_${EXEC}_${INPUT}_${1}_c" run_stampede.sbatch
-    threads=16
-    echo "CPU-only " $EXEC $INPUT $1 "c" $threads $2
-    sbatch run_stampede.sbatch $EXEC $INPUT c $threads
+    sed -i "2i#SBATCH -o ${EXEC}_${INPUT}_${PART}_${1}_c_%j.out" run_stampede.sbatch
+    sed -i "2i#SBATCH -J ${EXEC}_${INPUT}_${PART}_${1}_c" run_stampede.sbatch
+    threads=272
+    echo "CPU-only " $EXEC $INPUT $PART $1 "c" $threads $2
+    sbatch run_stampede.sbatch $EXEC $INPUT $PART c $threads 
   fi
   rm run_stampede.sbatch
 done
