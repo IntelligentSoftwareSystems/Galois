ab4c8166d7118ddbc52b9e2e7adc8dbe4069320f
Roshan Dathathri <roshan@cs.utexas.edu>
2017-08-19 21:20:57 -0500

    rmat30 stampede scripts

--- dist_exp/scripts/heterogeneousGalois/batch_stampede_all.sh
+++ dist_exp/scripts/heterogeneousGalois/batch_stampede_all.sh
@@ -4,19 +4,21 @@ EXECS=( "bfs_push-filter" "pagerank_pull-topological" "kcore_push-filter" "cc_pu
 
 SET="1,2:00:00 2,01:30:00 4,01:00:00 8,00:45:00 16,00:30:00 32,00:20:00"
 SET="128,00:30:00 64,00:45:00 32,01:00:00"
-SET="64,00:45:00 32,01:00:00 16,01:30:00"
+SET="64,01:45:00 32,02:00:00 16,02:30:00" #clueweb12 rmat30
 
 INPUTS=("uk-2007;\"${SET}\"")
 INPUTS=("twitter-ICWSM10-component;\"${SET}\"")
 INPUTS=("rmat24;\"${SET}\"")
 INPUTS=("rmat28;\"${SET}\"")
 INPUTS=("clueweb12;\"${SET}\"")
+INPUTS=("rmat30;\"${SET}\"")
 
 QUEUE=gpu
 QUEUE=development
 QUEUE=normal
 
-PARTS=( "cvc" "hivc" "2dvc" "oec" )
+PARTS=( "cvc" "hivc" "2dvc" "oec" ) #clueweb12
+PARTS=( "cvc" "hovc" "2dvc" "iec" ) #rmat30
 
 for j in "${INPUTS[@]}"
 do
