83345f5e6b1bf2f58b919335b9d24483b376b6d9
Gurbinder Gill <gill@ices.utexas.edu>
2017-08-27 02:14:39 -0500

    Need this for now for distributed

--- libruntime/include/Galois/Traits.h
+++ libruntime/include/Galois/Traits.h
@@ -186,6 +186,16 @@ struct default_loopname: public loopname {
 };
 
 /**
+   - * Indicate run-number to appear in statistics. Optional argument to {@link do_all()}
+   - */
+// TODO remove numrun
+struct numrun_tag {};
+struct numrun: public trait_has_value<const std::string>, numrun_tag {
+   numrun(const std::string& p = ""): trait_has_value<const std::string>(p) { }
+};
+
+
+/**
  * Indicate whether @{link do_all()} loops should perform work-stealing. Optional
  * argument to {@link do_all()} loops.
  */
