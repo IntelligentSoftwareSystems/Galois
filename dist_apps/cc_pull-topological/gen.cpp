99afb444d05d97a555a9037cf97a380c714d4d5e
Gurbinder Gill <gill@ices.utexas.edu>
2017-09-07 11:48:01 -0500

    adding explicit printDistStat call

--- dist_apps/cc_pull-topological/gen.cpp
+++ dist_apps/cc_pull-topological/gen.cpp
@@ -438,6 +438,9 @@ int main(int argc, char** argv) {
 
     }
     Galois::Runtime::getHostBarrier().wait();
+    G.printDistStats();
+    Galois::Runtime::getHostBarrier().wait();
+
 
     return 0;
   } catch(const char* c) {
