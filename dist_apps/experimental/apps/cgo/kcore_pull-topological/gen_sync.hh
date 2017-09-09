26f7dd63b654d890d976f9d862f35ffecb856d14
Loc Hoang <l_hoang@utexas.edu>
2017-09-09 08:35:36 -0500

    kcore pull versions v2-5 (untested build)

---
+++ dist_apps/experimental/apps/cgo/kcore_pull-topological/gen_sync.hh
@@ -0,0 +1,27 @@
+#include "Galois/Runtime/sync_structures.h"
+
+////////////////////////////////////////////////////////////////////////////////
+// current_degree
+////////////////////////////////////////////////////////////////////////////////
+
+GALOIS_SYNC_STRUCTURE_REDUCE_ADD(current_degree, uint32_t);
+GALOIS_SYNC_STRUCTURE_REDUCE_SET(current_degree, uint32_t);
+GALOIS_SYNC_STRUCTURE_BROADCAST(current_degree, uint32_t);
+
+////////////////////////////////////////////////////////////////////////////////
+// trim
+////////////////////////////////////////////////////////////////////////////////
+
+GALOIS_SYNC_STRUCTURE_REDUCE_ADD(trim, uint32_t);
+GALOIS_SYNC_STRUCTURE_BROADCAST(trim, uint32_t);
+
+
+GALOIS_SYNC_STRUCTURE_BITSET(current_degree);
+#if __OPT_VERSION__ >= 3
+GALOIS_SYNC_STRUCTURE_BITSET(trim);
+#endif
+
+#if __OPT_VERSION__ == 5
+FieldFlags Flags_current_degree;
+FieldFlags Flags_trim;
+#endif
