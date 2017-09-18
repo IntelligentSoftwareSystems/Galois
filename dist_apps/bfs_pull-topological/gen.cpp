8b53c25f94d293b9a972de2dc52b0ee92ec57804
M. Amber Hassaan <ahassaan@ices.utexas.edu>
2017-09-18 18:09:11 -0500

    remove stat print hack

--- dist_apps/bfs_pull-topological/gen.cpp
+++ dist_apps/bfs_pull-topological/gen.cpp
@@ -454,10 +454,6 @@ int main(int argc, char** argv) {
     }
 
     }
-    Galois::Runtime::getHostBarrier().wait();
-    G.printDistStats();
-    Galois::Runtime::getHostBarrier().wait();
-
 
     return 0;
   } catch(const char* c) {
