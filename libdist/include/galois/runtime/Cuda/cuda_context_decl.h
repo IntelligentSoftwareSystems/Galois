217d0efa62ec83c133b9c3128b4b148fad27c400
Loc Hoang <lhoang@ices.utexas.edu>
2017-09-26 11:45:41 -0500

    added reset_CUDA_context to forward cuda decl

--- libdist/include/galois/runtime/Cuda/cuda_context_decl.h
+++ libdist/include/galois/runtime/Cuda/cuda_context_decl.h
@@ -5,3 +5,4 @@ struct CUDA_Context;
 struct CUDA_Context *get_CUDA_context(int id);
 bool init_CUDA_context(struct CUDA_Context *ctx, int device);
 void load_graph_CUDA(struct CUDA_Context *ctx, MarshalGraph &g, unsigned num_hosts);
+void reset_CUDA_context(struct CUDA_Context *ctx);
