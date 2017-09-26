2aa7fca96caad669c0daa962375ca227cbd6af5a
Loc Hoang <lhoang@ices.utexas.edu>
2017-09-26 12:02:21 -0500

    header + ifndef guard for cuda_context_decl

--- libdist/include/galois/runtime/Cuda/cuda_context_decl.h
+++ libdist/include/galois/runtime/Cuda/cuda_context_decl.h
@@ -1,3 +1,38 @@
+/** Forward declarations for cuda context -*- C++ -*-
+ * @file cuda_context_decl.h
+ * @section License
+ *
+ * This file is part of Galois.  Galois is a framework to exploit
+ * amorphous data-parallelism in irregular programs.
+ *
+ * Galois is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Galois is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Galois.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * @section Copyright
+ *
+ * Copyright (C) 2017, The University of Texas at Austin. All rights
+ * reserved.
+ *
+ * @section Description
+ *
+ * Forward declarations for common CUDA context functions
+ *
+ * @author Loc Hoang <l_hoang@utexas.edu>
+ */
+
+#ifndef __CUDA_CONTEXT_DECL__
+#define __CUDA_CONTEXT_DECL__
 #include "galois/runtime/Cuda/cuda_mtypes.h"
 
 struct CUDA_Context;
@@ -6,3 +41,4 @@ struct CUDA_Context *get_CUDA_context(int id);
 bool init_CUDA_context(struct CUDA_Context *ctx, int device);
 void load_graph_CUDA(struct CUDA_Context *ctx, MarshalGraph &g, unsigned num_hosts);
 void reset_CUDA_context(struct CUDA_Context *ctx);
+#endif
