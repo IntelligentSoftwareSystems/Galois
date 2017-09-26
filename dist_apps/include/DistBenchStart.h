6b9f62cb08f7d891d6d1927d4741ff5be1e800ac
Loc Hoang <lhoang@ices.utexas.edu>
2017-09-25 20:08:42 -0500

    moved some common distapp code to DistBenchStart includes command line args as well

--- dist_apps/include/DistBenchStart.h
+++ dist_apps/include/DistBenchStart.h
@@ -2,7 +2,7 @@
  * @file
  * @section License
  *
- * This file is part of Galois.  Galoisis a framework to exploit
+ * This file is part of Galois.  Galois is a framework to exploit
  * amorphous data-parallelism in irregular programs.
  *
  * Galois is free software: you can redistribute it and/or modify it
@@ -21,7 +21,7 @@
  *
  * @section Copyright
  *
- * Copyright (C) 2015, The University of Texas at Austin. All rights
+ * Copyright (C) 2017, The University of Texas at Austin. All rights
  * reserved.
  *
  * @section Description
@@ -29,6 +29,7 @@
  * Common benchmark initialization
  *
  * @author Andrew Lenharth <andrewl@lenharth.org>
+ * @author Loc Hoang <l_hoang@utexas.edu>
  */
 #ifndef DIST_BENCH_START_H
 #define DIST_BENCH_START_H
@@ -36,19 +37,84 @@
 #include "galois/Galois.h"
 #include "galois/Version.h"
 #include "llvm/Support/CommandLine.h"
+#include "galois/runtime/dGraphLoader.h"
+
+#ifdef __GALOIS_HET_CUDA__
+#include "galois/runtime/Cuda/cuda_device.h"
+#include "galois/runtime/Cuda/cuda_context_decl.h"
+#endif
 
 //! standard global options to the benchmarks
-extern llvm::cl::opt<bool> skipVerify;
-extern llvm::cl::opt<int> numThreads;
-extern llvm::cl::opt<int> numRuns;
-extern llvm::cl::opt<bool> savegraph;
-extern llvm::cl::opt<std::string> outputFile;
-extern llvm::cl::opt<bool> verifyMax;
-extern llvm::cl::opt<std::string> statFile;
+namespace cll = llvm::cl;
 
+extern cll::opt<bool> skipVerify;
+extern cll::opt<int> numThreads;
+extern cll::opt<int> numRuns;
+extern cll::opt<bool> savegraph;
+extern cll::opt<std::string> outputFile;
+extern cll::opt<bool> verifyMax;
+extern cll::opt<std::string> statFile;
+extern cll::opt<unsigned int> enforce_metadata;
+extern cll::opt<bool> verify;
 
-void DistBenchStart(int argc, char** argv, 
-                   const char* app, const char* desc = nullptr, const char* url = nullptr);
+#ifdef __GALOIS_HET_CUDA__
+enum Personality {
+   CPU, GPU_CUDA, GPU_OPENCL
+};
 
+std::string personality_str(Personality p);
+
+extern cll::opt<int> gpudevice;
+extern cll::opt<Personality> personality;
+extern cll::opt<unsigned> scalegpu;
+extern cll::opt<unsigned> scalecpu;
+extern cll::opt<int> num_nodes;
+extern cll::opt<std::string> personality_set;
+#endif
+
+void DistBenchStart(int argc, char** argv, const char* app, 
+                    const char* desc = nullptr, const char* url = nullptr);
+
+#ifdef __GALOIS_HET_CUDA__
+void SetupHetero(std::vector<unsigned>& scaleFactor);
+#endif
+
+template <typename NodeData, typename EdgeData>
+hGraph<NodeData, EdgeData>* LoadDGraph(std::vector<unsigned>& scaleFactor,
+                                       struct CUDA_Context** cuda_ctx) {
+  galois::StatTimer dGraphTimer("TIMER_HG_INIT"); 
+  dGraphTimer.start();
+
+  hGraph<NodeData, EdgeData>* loadedGraph = 
+      constructGraph<NodeData, EdgeData>(scaleFactor);
+
+  // graph marshalling
+  #ifdef __GALOIS_HET_CUDA__
+  auto& net = galois::runtime::getSystemNetworkInterface();
+  const unsigned my_host_id = galois::runtime::getHostID();
+
+  galois::StatTimer marshalTimer("TIMER_GRAPH_MARSHAL"); 
+  marshalTimer.start();
+
+  if (personality == GPU_CUDA) {
+    *cuda_ctx = get_CUDA_context(my_host_id);
+
+    if (!init_CUDA_context(*cuda_ctx, gpudevice)) {
+      GALOIS_DIE("Failed to initialize CUDA context");
+    }
+
+    MarshalGraph m = (*loadedGraph).getMarshalGraph(my_host_id);
+    load_graph_CUDA(*cuda_ctx, m, net.Num);
+  } else if (personality == GPU_OPENCL) {
+    //galois::opencl::cl_env.init(cldevice.Value);
+  }
+
+  marshalTimer.stop();
+  #endif
+
+  dGraphTimer.stop();
+
+  return loadedGraph;
+}
 
 #endif
--- dist_apps/src/DistBenchStart.cpp
+++ dist_apps/src/DistBenchStart.cpp
@@ -21,7 +21,7 @@
  *
  * @section Copyright
  *
- * Copyright (C) 2015, The University of Texas at Austin. All rights
+ * Copyright (C) 2017, The University of Texas at Austin. All rights
  * reserved.
  *
  * @section Description
@@ -29,6 +29,7 @@
  * Perform common setup tasks for the lonestar benchmarks
  *
  * @author Gurbinder Gill <gill@cs.utexas.edu>
+ * @author Loc Hoang <l_hoang@utexas.edu>
  */
 
 #include "DistBenchStart.h"
@@ -40,34 +41,98 @@
 #include <iostream>
 #include <sstream>
 
-//! standard global options to the benchmarks
-llvm::cl::opt<bool> skipVerify("noverify", llvm::cl::desc("Skip verification step"), llvm::cl::init(false));
-llvm::cl::opt<int> numThreads("t", llvm::cl::desc("Number of threads"), llvm::cl::init(1));
+////////////////////////////////////////////////////////////////////////////////
+// Command line args
+////////////////////////////////////////////////////////////////////////////////
 
-llvm::cl::opt<int> numRuns("runs", llvm::cl::desc("Number of runs"), llvm::cl::init(3));
+cll::opt<bool> skipVerify("noverify", cll::desc("Skip verification step"), 
+                                      cll::init(false));
+cll::opt<int> numThreads("t", cll::desc("Number of threads"), 
+                              cll::init(1));
+cll::opt<int> numRuns("runs", cll::desc("Number of runs"), 
+                              cll::init(3));
+cll::opt<bool> savegraph("savegraph", 
+                         cll::desc("Bool flag to enable save graph"),
+                         cll::init(false));
+cll::opt<std::string> outputFile("outputFile", 
+              cll::desc("Output file name to store the local graph structure"), 
+              cll::init("local_graph"));
+cll::opt<bool> verifyMax("verifyMax", 
+              cll::desc("Just print the max value of nodes fields"),
+              cll::init(false));
+cll::opt<std::string> statFile("statFile", 
+         cll::desc("output file to print stats to "), 
+         cll::init(""));
+cll::opt<unsigned int> enforce_metadata("metadata", 
+          cll::desc("Enforce communication metadata: 0 - auto (default), "
+                         "1 - bitset, 2 - indices, 3 - no metadata"), 
+          cll::init(0));
 
-llvm::cl::opt<bool> savegraph("savegraph", llvm::cl::desc("Bool flag to enable save graph"), llvm::cl::init(false));
-llvm::cl::opt<std::string> outputFile("outputFile", llvm::cl::desc("Output file name to store the local graph structure"), llvm::cl::init("local_graph"));
+cll::opt<bool> verify("verify", 
+                     cll::desc("Verify results by outputting results to file"), 
+                     cll::init(false));
 
-llvm::cl::opt<bool> verifyMax("verifyMax", llvm::cl::desc("Just print the max value of nodes fields"), llvm::cl::init(false));
-llvm::cl::opt<std::string> statFile("statFile", llvm::cl::desc("ouput file to print stats to "), llvm::cl::init(""));
-llvm::cl::opt<unsigned int> enforce_metadata("metadata", llvm::cl::desc("Enforce communication metadata: 0 - auto (default), 1 - bitset, 2 - indices, 3 - no metadata"), llvm::cl::init(0));
+
+#ifdef __GALOIS_HET_CUDA__
+std::string personality_str(Personality p) {
+  switch (p) {
+    case CPU:
+      return "CPU";
+    case GPU_CUDA:
+      return "GPU_CUDA";
+    case GPU_OPENCL:
+      return "GPU_OPENCL";
+  }
+
+  assert(false && "Invalid personality");
+  return "";
+}
+
+cll::opt<int> gpudevice("gpu", 
+                                cll::desc("Select GPU to run on, "
+                                          "default is to choose automatically"), 
+                                cll::init(-1));
+cll::opt<Personality> personality("personality", cll::desc("Personality"),
+      cll::values(clEnumValN(CPU, "cpu", "Galois CPU"), 
+                  clEnumValN(GPU_CUDA, "gpu/cuda", "GPU/CUDA"), 
+                  clEnumValN(GPU_OPENCL, "gpu/opencl", "GPU/OpenCL"), 
+                  clEnumValEnd),
+      cll::init(CPU));
+cll::opt<unsigned> scalegpu("scalegpu", 
+      cll::desc("Scale GPU workload w.r.t. CPU, default is proportionally "
+                "equal workload to CPU and GPU (1)"), 
+      cll::init(1));
+cll::opt<unsigned> scalecpu("scalecpu", 
+      cll::desc("Scale CPU workload w.r.t. GPU, default is proportionally "
+                "equal workload to CPU and GPU (1)"), 
+      cll::init(1));
+cll::opt<int> num_nodes("num_nodes", 
+      cll::desc("Num of physical nodes with devices (default = num of hosts): " 
+                "detect GPU to use for each host automatically"), 
+      cll::init(-1));
+cll::opt<std::string> personality_set("pset", 
+      cll::desc("String specifying personality for hosts on each physical "
+                "node. 'c'=CPU,'g'=GPU/CUDA and 'o'=GPU/OpenCL"), 
+      cll::init("c"));
+#endif
 
 DataCommMode enforce_data_mode = noData;
 
 static void PrintVersion() {
-  std::cout << "Galois Benchmark Suite v" << galois::getVersion() << " (" << galois::getRevision() << ")\n";
+  std::cout << "Galois Benchmark Suite v" << galois::getVersion() << " (" 
+            << galois::getRevision() << ")\n";
 }
 
-//! initialize lonestar benchmark
-void DistBenchStart(int argc, char** argv, 
-                   const char* app, const char* desc, const char* url) {
-  
+////////////////////////////////////////////////////////////////////////////////
+//! initialize benchmark + functions to help initialization
+////////////////////////////////////////////////////////////////////////////////
+
+void DistBenchStart(int argc, char** argv, const char* app, const char* desc, 
+                    const char* url) {
   llvm::cl::SetVersionPrinter(PrintVersion);
   llvm::cl::ParseCommandLineOptions(argc, argv);
   numThreads = galois::setActiveThreads(numThreads); 
   galois::runtime::setStatFile(statFile);
-
 
   assert(enforce_metadata <= 3);
   enforce_data_mode = static_cast<DataCommMode>((unsigned int)enforce_metadata);
@@ -76,32 +141,87 @@ void DistBenchStart(int argc, char** argv,
   
   if (net.ID == 0) {
     PrintVersion();
-    std::cout << "Copyright (C) " << galois::getCopyrightYear() << " The University of Texas at Austin\n";
+    std::cout << "Copyright (C) " << galois::getCopyrightYear() 
+              << " The University of Texas at Austin\n";
     std::cout << "http://iss.ices.utexas.edu/galois/\n\n";
     std::cout << "application: " <<  (app ? app : "unspecified") << "\n";
-    if (desc)
+
+    if (desc) {
       std::cout << desc << "\n";
-    if (url)
-      std::cout << "http://iss.ices.utexas.edu/?p=projects/galois/benchmarks/" << url << "\n";
+    }
+    if (url) {
+      std::cout << "http://iss.ices.utexas.edu/?p=projects/galois/benchmarks/" 
+                << url << "\n";
+    }
     std::cout << "\n";
 
     std::ostringstream cmdout;
+
     for (int i = 0; i < argc; ++i) {
       cmdout << argv[i];
       if (i != argc - 1)
         cmdout << " ";
     }
-    galois::runtime::reportParam("(NULL)", "CommandLine", cmdout.str());
-    galois::runtime::reportParam("(NULL)", "Threads", (unsigned long)numThreads);
-    galois::runtime::reportParam("(NULL)", "Hosts", (unsigned long)net.Num);
-    galois::runtime::reportParam("(NULL)", "Runs", (unsigned long)numRuns);
-    galois::runtime::reportParam("(NULL)", "Run_UUID", galois::runtime::getRandUUID());
+
+    galois::runtime::reportParam("DistBench", "CommandLine", cmdout.str());
+    galois::runtime::reportParam("DistBench", "Threads", 
+                                 (unsigned long)numThreads);
+    galois::runtime::reportParam("DistBench", "Hosts", (unsigned long)net.Num);
+    galois::runtime::reportParam("DistBench", "Runs", (unsigned long)numRuns);
+    galois::runtime::reportParam("DistBench", "Run_UUID", 
+                                 galois::runtime::getRandUUID());
   }
 
   char name[256];
   gethostname(name, 256);
-  galois::runtime::reportParam("(NULL)", "Hostname", name);
+  galois::runtime::reportParam("DistBench", "Hostname", name);
 
-  if(savegraph)
-    galois::runtime::reportParam("(NULL)", "outputFile", outputFile.c_str());
+  if (savegraph) {
+    galois::runtime::reportParam("DistBench", "outputFile", outputFile.c_str());
+  }
 }
+
+#ifdef __GALOIS_HET_CUDA__
+void SetupHetero(std::vector<unsigned>& scaleFactor) {
+  const unsigned my_host_id = galois::runtime::getHostID();
+
+  // Parse arg string when running on multiple hosts and update/override 
+  // personality with corresponding value.
+  auto& net = galois::runtime::getSystemNetworkInterface();
+
+  if (num_nodes == -1) num_nodes = net.Num;
+
+  assert((net.Num % num_nodes) == 0);
+
+  if (personality_set.length() == (net.Num / num_nodes)) {
+    switch (personality_set.c_str()[my_host_id % (net.Num / num_nodes)]) {
+      case 'g':
+        personality = GPU_CUDA;
+        break;
+      case 'o':
+        assert(0);
+        personality = GPU_OPENCL;
+        break;
+      case 'c':
+      default:
+        personality = CPU;
+        break;
+    }
+
+    if ((personality == GPU_CUDA) && (gpudevice == -1)) {
+      gpudevice = get_gpu_device_id(personality_set, num_nodes);
+    }
+
+    // scale factor setup
+    if ((scalecpu > 1) || (scalegpu > 1)) {
+      for (unsigned i = 0; i < net.Num; ++i) {
+        if (personality_set.c_str()[i % num_nodes] == 'c') {
+          scaleFactor.push_back(scalecpu);
+        } else {
+          scaleFactor.push_back(scalegpu);
+        }
+      }
+    }
+  }
+}
+#endif
