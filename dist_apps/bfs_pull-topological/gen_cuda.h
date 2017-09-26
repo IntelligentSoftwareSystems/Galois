e6bed17c3e81955116d7f7cc24f1084154d43cc2
Loc Hoang <lhoang@ices.utexas.edu>
2017-09-26 10:30:36 -0500

    dist bfs pull works with new refactored code

--- dist_apps/bfs_pull-topological/gen_cuda.h
+++ dist_apps/bfs_pull-topological/gen_cuda.h
@@ -1,12 +1,7 @@
 #pragma once
 #include "galois/runtime/Cuda/cuda_mtypes.h"
 #include "galois/runtime/DataCommMode.h"
-
-struct CUDA_Context;
-
-struct CUDA_Context *get_CUDA_context(int id);
-bool init_CUDA_context(struct CUDA_Context *ctx, int device);
-void load_graph_CUDA(struct CUDA_Context *ctx, MarshalGraph &g, unsigned num_hosts);
+#include "galois/runtime/Cuda/cuda_context_decl.h"
 
 void reset_CUDA_context(struct CUDA_Context *ctx);
 
