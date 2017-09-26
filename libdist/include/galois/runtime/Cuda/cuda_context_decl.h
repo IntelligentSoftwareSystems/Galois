62065ee7673650fdda184f65c907035c69b13e4b
Loc Hoang <lhoang@ices.utexas.edu>
2017-09-25 20:11:28 -0500

    forward declaration for some cuda init calls

---
+++ libdist/include/galois/runtime/Cuda/cuda_context_decl.h
@@ -0,0 +1,7 @@
+#include "galois/runtime/Cuda/cuda_mtypes.h"
+
+struct CUDA_Context;
+
+struct CUDA_Context *get_CUDA_context(int id);
+bool init_CUDA_context(struct CUDA_Context *ctx, int device);
+void load_graph_CUDA(struct CUDA_Context *ctx, MarshalGraph &g, unsigned num_hosts);
