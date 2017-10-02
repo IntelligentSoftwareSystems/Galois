2a026e981c9403addf8d9205ad2bc0208a981454
Loc Hoang <lhoang@ices.utexas.edu>
2017-10-02 16:23:02 -0500

    compilerhelperfunctions split into 2 files atomic helpers = atomic help, compiler helpers = compiler help

--- libgalois/include/galois/runtime/AtomicHelpers.h
+++ libgalois/include/galois/runtime/AtomicHelpers.h
@@ -1,11 +1,11 @@
-/** Galois Remote Object Store -*- C++ -*-
+/** Galois Atomic Helper Functions-*- C++ -*-
  * @file
  * @section License
  *
  * Galois, a framework to exploit amorphous data-parallelism in irregular
  * programs.
  *
- * Copyright (C) 2014, The University of Texas at Austin. All rights reserved.
+ * Copyright (C) 2017, The University of Texas at Austin. All rights reserved.
  * UNIVERSITY EXPRESSLY DISCLAIMS ANY AND ALL WARRANTIES CONCERNING THIS
  * SOFTWARE AND DOCUMENTATION, INCLUDING ANY WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR ANY PARTICULAR PURPOSE, NON-INFRINGEMENT AND WARRANTIES OF
@@ -21,20 +21,19 @@
  * @author Gurbinder Gill <gurbinder533@gmail.com>
  * @author Loc Hoang <l_hoang@utexas.edu>
  */
-#ifndef GALOIS_RUNTIME_COMPILER_HELPER_FUNCTIONS_H
-#define GALOIS_RUNTIME_COMPILER_HELPER_FUNCTIONS_H
-
+#ifndef __GALOIS_RUNTIME_ATOMIC_HELPER_FUNCTIONS_H__
+#define __GALOIS_RUNTIME_ATOMIC_HELPER_FUNCTIONS_H__
 #include <atomic>
 #include <algorithm>
 #include <vector>
 namespace galois {
   /** galois::atomicMax + non-atomic max calls **/
-
   template<typename Ty>
-  const Ty atomicMax(std::atomic<Ty>& a, const Ty b){
+  const Ty atomicMax(std::atomic<Ty>& a, const Ty b) {
     Ty old_a = a;
     // if old value is less than new value, atomically exchange
-    while (old_a < b && !a.compare_exchange_weak(old_a, b, std::memory_order_relaxed));
+    while (old_a < b && !a.compare_exchange_weak(old_a, b, 
+                                                 std::memory_order_relaxed));
     return old_a;
   }
 
@@ -49,10 +48,10 @@ namespace galois {
   }
 
   template<typename Ty>
-  const Ty max(Ty& a, const Ty& b){
+  const Ty max(Ty& a, const Ty& b) {
     Ty old_a = a;
 
-    if(a < b){
+    if (a < b) {
       a = b;
     }
     return old_a;
@@ -60,80 +59,82 @@ namespace galois {
 
   /** galois::atomicMin **/
   template<typename Ty>
-    const Ty atomicMin(std::atomic<Ty>& a, const Ty b){
-      Ty old_a = a;
-      while(old_a > b && !a.compare_exchange_weak(old_a,b, std::memory_order_relaxed));
-      return old_a;
-    }
+  const Ty atomicMin(std::atomic<Ty>& a, const Ty b) {
+    Ty old_a = a;
+    while (old_a > b && !a.compare_exchange_weak(old_a, b, 
+                                                 std::memory_order_relaxed));
+    return old_a;
+  }
 
   template<typename Ty>
-    const Ty min(std::atomic<Ty>& a, const Ty& b) {
-      Ty old_a = a;
-      if(a > b){
-        a = b;
-      }
-      return old_a;
+  const Ty min(std::atomic<Ty>& a, const Ty& b) {
+    Ty old_a = a;
+    if (a > b) {
+      a = b;
     }
+    return old_a;
+  }
 
   template<typename Ty>
-    const Ty min(Ty& a, const Ty& b) {
-      Ty old_a = a;
-      if(a > b){
-        a = b;
-      }
-      return old_a;
+  const Ty min(Ty& a, const Ty& b) {
+    Ty old_a = a;
+    if (a > b) {
+      a = b;
     }
+    return old_a;
+  }
 
   /** galois::atomicAdd **/
   template<typename Ty>
-    const Ty atomicAdd(std::atomic<Ty>& val, Ty delta){
-      Ty old_val = val;
-      while(!val.compare_exchange_weak(old_val, old_val + delta, std::memory_order_relaxed));
-      return old_val;
-    }
+  const Ty atomicAdd(std::atomic<Ty>& val, Ty delta) {
+    Ty old_val = val;
+    while (!val.compare_exchange_weak(old_val, old_val + delta, 
+                                      std::memory_order_relaxed));
+    return old_val;
+  }
 
   template<typename Ty>
-    const Ty add(std::atomic<Ty>& a, const Ty& b) {
-      Ty old_a = a;
-      a = a + b;
-      return old_a;
-    }
+  const Ty add(std::atomic<Ty>& a, const Ty& b) {
+    Ty old_a = a;
+    a = a + b;
+    return old_a;
+  }
 
   template<typename Ty>
-    const Ty add(Ty& a, std::atomic<Ty>& b) {
-      Ty old_a = a;
-      a = a + b.load();
-      return old_a;
-    }
+  const Ty add(Ty& a, std::atomic<Ty>& b) {
+    Ty old_a = a;
+    a = a + b.load();
+    return old_a;
+  }
 
   template<typename Ty>
-    const Ty add(Ty& a, const Ty& b) {
-      Ty old_a = a;
-      a += b;
-      return old_a;
-    }
+  const Ty add(Ty& a, const Ty& b) {
+    Ty old_a = a;
+    a += b;
+    return old_a;
+  }
 
   template<typename Ty>
-    const Ty set(Ty& a, const Ty& b) {
-      a = b;
-      return a;
-    }
+  const Ty set(Ty& a, const Ty& b) {
+    a = b;
+    return a;
+  }
 
   template<typename Ty>
-    const Ty set(std::atomic<Ty>& a, const Ty& b) {
-      a = b;
-      return a;
-    }
+  const Ty set(std::atomic<Ty>& a, const Ty& b) {
+    a = b;
+    return a;
+  }
 
   /** Pair Wise Average function **/
   template<typename Ty>
   const Ty pairWiseAvg(Ty a, Ty b) {
-    return (a+b)/2.0;
+    return (a + b) / 2.0;
   }
 
   template<typename Ty>
   void pairWiseAvg_vec(std::vector<Ty>& a_vec, std::vector<Ty>& b_vec) {
-    for(unsigned i = 0; i < a_vec.size(); ++i) {
+    for (unsigned i = 0; i < a_vec.size(); ++i) {
       a_vec[i] = (a_vec[i] + b_vec[i])/2.0;
     }
   }
@@ -147,29 +148,38 @@ namespace galois {
   template<typename Ty>
   void pairWiseAvg_vec(Ty& a_arr, Ty& b_arr) {
     for(unsigned i = 0; i < a_arr.size(); ++i) {
-      a_arr[i] = (a_arr[i] + b_arr[i])/2.0;
+      a_arr[i] = (a_arr[i] + b_arr[i]) / 2.0;
     }
   }
 
   template<typename Ty>
   void addArray(Ty& a_arr, Ty& b_arr) {
-    for(unsigned i = 0; i < a_arr.size(); ++i) {
+    for (unsigned i = 0; i < a_arr.size(); ++i) {
       a_arr[i] = (a_arr[i] + b_arr[i]);
     }
   }
 
   template<typename Ty>
   void resetVec(std::vector<Ty>& a_vec) {
-    std::for_each(galois::iterate(a_vec.begin(), a_vec.end()),
+    std::for_each(a_vec.begin(), a_vec.end(),
                   [] (Ty &ele) { ele = 0; } ); 
   }
 
-
-  //like std::inner_product
+  // like std::inner_product
   template<typename ItrTy, typename Ty >
   Ty innerProduct(ItrTy a_begin, ItrTy a_end, ItrTy b_begin, Ty init_value) {
     auto jj = b_begin;
-    for(auto ii = a_begin; ii != a_end; ++ii, ++jj){
+    for (auto ii = a_begin; ii != a_end; ++ii, ++jj) {
+      init_value += (*ii) * (*jj);
+    }
+    return init_value;
+  }
+
+  // like std::inner_product
+  template<typename ItrTy, typename Ty >
+  Ty innerProduct(ItrTy& a_arr, ItrTy& b_arr, Ty init_value) {
+    auto jj = b_arr.begin();
+    for (auto ii = a_arr.begin(); ii != a_arr.end(); ++ii, ++jj) {
       init_value += (*ii) * (*jj);
     }
     return init_value;
@@ -184,27 +194,5 @@ namespace galois {
   void reset(std::atomic<Ty>& var, Ty val) {
     var = val;
   }
-
-  // like std::inner_product
-  template<typename ItrTy, typename Ty >
-  Ty innerProduct(ItrTy& a_arr, ItrTy& b_arr, Ty init_value) {
-    auto jj = b_arr.begin();
-    for(auto ii = a_arr.begin(); ii != a_arr.end(); ++ii, ++jj){
-      init_value += (*ii) * (*jj);
-    }
-    return init_value;
-  }
-
-  template<typename... Args>
-  int read_set(Args... args) {
-    // Nothing for now.
-    return 0;
-  }
-
-  template<typename... Args>
-  int write_set(Args... args) {
-    // Nothing for now.
-    return 0;
-  }
-} //End namespace galois
+} // end namespace galois
 #endif
