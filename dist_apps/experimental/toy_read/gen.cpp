7136e02e2715b111a30b9c7e6f75cca4b87eff01
Loc Hoang <lhoang@ices.utexas.edu>
2017-10-04 13:22:56 -0500

    mpiread the entire file at once

--- dist_apps/experimental/toy_read/gen.cpp
+++ dist_apps/experimental/toy_read/gen.cpp
@@ -19,7 +19,6 @@ int main(int argc, char** argv) {
 
   printf("File to read: %s\n", argv[1]);
 
-  char dummyBuf[1];
 
   int numRuns = 1;
 
@@ -31,7 +30,10 @@ int main(int argc, char** argv) {
   MPI_Offset fileSize;
   MPI_File_get_size(mpiFile, &fileSize);
   printf("MPI says size is %llu\n", fileSize);
+
+  char dummyBuf[fileSize+1];
   uint64_t mpiBytesRead = 0;
+  dummyBuf[fileSize] = '\0';
 
   galois::Timer fileReadTimer;
 
@@ -39,7 +41,7 @@ int main(int argc, char** argv) {
 
   while (true) {
     MPI_Status mpiStat;
-    MPI_File_read_at(mpiFile, mpiBytesRead, (void*)dummyBuf, 1, MPI_BYTE, &mpiStat); 
+    MPI_File_read_at(mpiFile, mpiBytesRead, (void*)dummyBuf, fileSize, MPI_BYTE, &mpiStat); 
 
     //if (mpiStat.MPI_ERROR != MPI_SUCCESS) {
     //  //char dumBuf[1000];
@@ -55,9 +57,9 @@ int main(int argc, char** argv) {
     //  }
 
     //} else {
-      mpiBytesRead++;
-      //printf("%c", dummyBuf[0]);
-      if (mpiBytesRead == (unsigned)fileSize) {
+      mpiBytesRead+=fileSize;
+      //printf("%s", dummyBuf);
+      if (mpiBytesRead >= (unsigned)fileSize) {
         break;
       }
     //}
