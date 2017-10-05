2a61c61586b41715d3bbb61efcd6d528fc2a236c
Loc Hoang <lhoang@ices.utexas.edu>
2017-10-05 15:34:45 -0500

    toyinspect batch reading functionality

--- dist_apps/experimental/toyinspect/gen.cpp
+++ dist_apps/experimental/toyinspect/gen.cpp
@@ -12,9 +12,34 @@
 #include "galois/DistGalois.h"
 
 static uint64_t numBytesRead = 0;
-//static uint64_t* outIndex = nullptr;
-//static uint32_t* edgeDest = nullptr;
+static uint64_t* outIndex = nullptr;
+static uint32_t* edgeDest = nullptr;
 
+#define _BATCH
+
+#ifdef _BATCH
+uint64_t mpi_out_index(MPI_File mpiFile, uint64_t nodeIndex) {
+  numBytesRead += sizeof(uint64_t);
+  return outIndex[nodeIndex];
+}
+
+uint64_t mpi_edge_begin(MPI_File mpiFile, uint64_t nodeIndex) {
+  if (nodeIndex != 0) {
+    return mpi_out_index(mpiFile, nodeIndex - 1);
+  } else {
+    return 0;
+  }
+}
+
+uint64_t mpi_edge_end(MPI_File mpiFile, uint64_t nodeIndex) {
+  return mpi_out_index(mpiFile, nodeIndex);
+}
+
+uint64_t mpi_edge_dest(MPI_File mpiFile, uint64_t edgeIndex, uint64_t numNodes) {
+  numBytesRead += sizeof(uint32_t);
+  return edgeDest[edgeIndex];
+}
+#else
 uint64_t mpi_out_index(MPI_File mpiFile, uint64_t nodeIndex) {
   uint64_t position = (4 + nodeIndex) * sizeof(uint64_t);
 
@@ -52,6 +77,7 @@ uint64_t mpi_edge_dest(MPI_File mpiFile, uint64_t edgeIndex, uint64_t numNodes) 
   numBytesRead += sizeof(uint32_t);
   return retval;
 }
+#endif
 
 
 int main(int argc, char** argv) {
@@ -86,29 +112,39 @@ int main(int argc, char** argv) {
   MPI_File_open(MPI_COMM_WORLD, argv[1], MPI_MODE_RDONLY, 
                 MPI_INFO_NULL, &mpiFile);
 
+
+  printf("[%d] file open\n", hostID);
   galois::Timer timer;
   timer.start();
+
 
   // vector to hold a prefix sum for use in thread work distribution
   std::vector<uint64_t> prefixSumOfEdges(nodeEnd - nodeBegin);
 
+
+  uint64_t position = 4 * sizeof(uint64_t);
+  outIndex = (uint64_t*)malloc(sizeof(uint64_t)*numGlobalNodes);
+  edgeDest = (uint32_t*)malloc(sizeof(uint32_t) * g.sizeEdges());
+  if (outIndex == nullptr || edgeDest == nullptr) {
+    printf("[%d] null ptr mlalc\n", hostID);
+  }
+  MPI_Barrier(MPI_COMM_WORLD);
+  //printf("[%d] malloc success\n", hostID);
+
+  MPI_Status mpiStat;
+  MPI_File_read_at(mpiFile, position, (char*)outIndex, 
+                   sizeof(uint64_t)*numGlobalNodes, MPI_BYTE, &mpiStat); 
+
+  position = (4 + numGlobalNodes) * sizeof(uint64_t);
+
+  MPI_File_read_at(mpiFile, position, (char*)edgeDest, 
+                   sizeof(uint32_t)*g.sizeEdges(), MPI_BYTE, &mpiStat); 
+
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  //printf("[%d] Get nodes now\n", hostID);
+
   auto edgeOffset = mpi_edge_begin(mpiFile, nodeBegin);
-
-  //uint64_t position = 4 * sizeof(uint64_t);
-  //outIndex = (uint64_t*)malloc(sizeof(uint64_t)*numGlobalNodes);
-  //edgeDest = (uint32_t*)malloc(sizeof(uint32_t) * g.sizeEdges());
-  //assert(outIndex != nullptr);
-  //assert(edgeDest != nullptr);
-
-  //MPI_Status mpiStat;
-  //MPI_File_read_at(mpiFile, position, (char*)outIndex, 
-  //                 sizeof(uint64_t)*numGlobalNodes, MPI_BYTE, &mpiStat); 
-
-  //position = (4 + numGlobalNodes) * sizeof(uint64_t);
-
-  //MPI_File_read_at(mpiFile, position, (char*)edgeDest, 
-  //                 sizeof(uint32_t)*g.sizeEdges(), MPI_BYTE, &mpiStat); 
-
 
   galois::do_all(galois::iterate(nodeBegin, nodeEnd),
     [&] (auto n) {
@@ -129,6 +165,11 @@ int main(int argc, char** argv) {
   fprintf(stderr, "[%d] Edge inspection time : %f seconds to read %lu bytes (%f MBPS)\n", 
       hostID, timer.get_usec()/1000000.0f, numBytesRead, 
       numBytesRead/(float)timer.get_usec());
+
+  free(outIndex);
+  free(edgeDest);
+
+  MPI_Barrier(MPI_COMM_WORLD);
 
   return 0;
 }
