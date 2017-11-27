914732c62cc54d62ccb3700f40f024e541a7fe71
Loc Hoang <lhoang@ices.utexas.edu>
2017-11-27 10:41:26 -0600

    bmkprops distbmk2 setup/changes

--- scripts/experimental/distbmk2/bmkprops.py
+++ scripts/experimental/distbmk2/bmkprops.py
@@ -6,22 +6,23 @@ TIME_FMT = "%Y-%m-%d %H:%M:%S"
 class GraphBMKDistApp(bmk2.Binary):
   """Base class for dist apps to inherit from. Subclasses specify benchmark
   name + number of threads + number of hosts."""
-    def __init__(self):
-        """Initialize shared mem properties."""
-        self.props = GraphBMKSharedMemProps(self.benchmark)
+  def __init__(self):
+    """Initialize dist app properties."""
+    self.props = GraphBMKDistAppProps(self.benchmark)
         
-    def get_id(self):
-        """Return the id of this benchmark."""
-        return "%s" % (self.benchmark)
+  def get_id(self):
+    """Return the id of this benchmark."""
+    return "%s" % (self.benchmark)
 
-    def getUniqueStatFile(self, numThreads, graphName):
-        """Get a statfile name given num threads + graph name being used."""
-        timeNow = datetime.datetime.now().strftime(TIME_FMT).replace(" ", "_")
+  # TODO add hosts
+  def getUniqueStatFile(self, numThreads, numHosts, graphName):
+    """Get a statfile name given num threads + graph name being used."""
+    timeNow = datetime.datetime.now().strftime(TIME_FMT).replace(" ", "_")
 
-        return ("%s_%d_%s_%s.log" % (self.benchmark, numThreads, graphName,
-                               timeNow))
+    return ("%s_t=%d_n=%d_%s_%s.log" % (self.benchmark, numThreads, numHosts,
+                                        graphName, timeNow))
 
 class GraphBMKDistAppProps(bmk2.Properties):
   """Properties pertaining to a dist app."""
-  def __init(self, benchmark):
+  def __init__(self, benchmark):
     self.benchmark = benchmark
