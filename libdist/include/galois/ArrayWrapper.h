0614c89bc7c1d7e01d6b522b5b4fdec8d0b2ccc9
Loc Hoang <lhoang@ices.utexas.edu>
2017-12-06 13:25:24 -0600

    libdist galois namespace header cleanup

--- libdist/include/galois/ArrayWrapper.h
+++ libdist/include/galois/ArrayWrapper.h
@@ -1,19 +1,42 @@
+/** Galois Copyable Array Wrapper -*- C++ -*-
+ * @file
+ * @section License
+ *
+ * Galois, a framework to exploit amorphous data-parallelism in irregular
+ * programs.
+ *
+ * Copyright (C) 2017, The University of Texas at Austin. All rights reserved.
+ * UNIVERSITY EXPRESSLY DISCLAIMS ANY AND ALL WARRANTIES CONCERNING THIS
+ * SOFTWARE AND DOCUMENTATION, INCLUDING ANY WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR ANY PARTICULAR PURPOSE, NON-INFRINGEMENT AND WARRANTIES OF
+ * PERFORMANCE, AND ANY WARRANTY THAT MIGHT OTHERWISE ARISE FROM COURSE OF
+ * DEALING OR USAGE OF TRADE.  NO WARRANTY IS EITHER EXPRESS OR IMPLIED WITH
+ * RESPECT TO THE USE OF THE SOFTWARE OR DOCUMENTATION. Under no circumstances
+ * shall University be liable for incidental, special, indirect, direct or
+ * consequential damages or loss of profits, interruption of business, or
+ * related expenses which may arise from use of Software or Documentation,
+ * including but not limited to those resulting from defects in Software and/or
+ * Documentation, or loss or inaccuracy of data of any kind.
+ *
+ * @section Description
+ *
+ * Wrapper to make an array copyable.
+ *
+ * @author Gurbinder Gill <gurbinder533@gmail.com>
+ */
 #ifndef _ARRAY_WRAPPER_H_
 #define _ARRAY_WRAPPER_H_
 
 #include <array>
-#include <type_traits>
 #include "galois/runtime/Extra_dist_traits.h"
 
 namespace galois {
-
   template<class T, size_t N>
-    class CopyableArray : public std::array<T, N>
-  {
-    public:
-
-      // only typedef if T is trivially copyable to use mem copy in serialize/deserialize.
-      typedef typename std::enable_if<galois::runtime::is_memory_copyable<T>::value, int>::type tt_is_copyable;
+  class CopyableArray : public std::array<T, N> {
+  public:
+    // only typedef if T is trivially copyable to use mem copy in serialize/deserialize.
+    using tt_is_copyable = 
+      typename std::enable_if<galois::runtime::is_memory_copyable<T>::value, int>::type 
   };
 }
 #endif
--- libdist/include/galois/AtomicWrapper.h
+++ libdist/include/galois/AtomicWrapper.h
@@ -1,36 +1,55 @@
+/** Galois Copyable Atomic Wrapper -*- C++ -*-
+ * @file
+ * @section License
+ *
+ * Galois, a framework to exploit amorphous data-parallelism in irregular
+ * programs.
+ *
+ * Copyright (C) 2017, The University of Texas at Austin. All rights reserved.
+ * UNIVERSITY EXPRESSLY DISCLAIMS ANY AND ALL WARRANTIES CONCERNING THIS
+ * SOFTWARE AND DOCUMENTATION, INCLUDING ANY WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR ANY PARTICULAR PURPOSE, NON-INFRINGEMENT AND WARRANTIES OF
+ * PERFORMANCE, AND ANY WARRANTY THAT MIGHT OTHERWISE ARISE FROM COURSE OF
+ * DEALING OR USAGE OF TRADE.  NO WARRANTY IS EITHER EXPRESS OR IMPLIED WITH
+ * RESPECT TO THE USE OF THE SOFTWARE OR DOCUMENTATION. Under no circumstances
+ * shall University be liable for incidental, special, indirect, direct or
+ * consequential damages or loss of profits, interruption of business, or
+ * related expenses which may arise from use of Software or Documentation,
+ * including but not limited to those resulting from defects in Software and/or
+ * Documentation, or loss or inaccuracy of data of any kind.
+ *
+ * @section Description
+ *
+ * Wrapper to make atomics copyable.
+ *
+ * @author Gurbinder Gill <gurbinder533@gmail.com>
+ */
 #ifndef _ATOMIC_WRAPPER_H_
 #define _ATOMIC_WRAPPER_H_
 
 #include <atomic>
-#include <type_traits>
 #include "galois/runtime/Extra_dist_traits.h"
 
 namespace galois {
-
   template<class T>
-    class CopyableAtomic : public std::atomic<T>
-  {
-    public:
+  class CopyableAtomic : public std::atomic<T> {
+  public:
+    CopyableAtomic() : std::atomic<T>(T{}) {}
 
-      CopyableAtomic() : 
-        std::atomic<T>(T{})
-        {}
+    constexpr CopyableAtomic(T desired) : std::atomic<T>(desired) {}
 
-      constexpr CopyableAtomic(T desired) : 
-        std::atomic<T>(desired) 
-    {}
+    constexpr CopyableAtomic(const CopyableAtomic<T>& other) 
+      : CopyableAtomic(other.load(std::memory_order_relaxed)) {}
 
-      constexpr CopyableAtomic(const CopyableAtomic<T>& other) :
-        CopyableAtomic(other.load(std::memory_order_relaxed))
-    {}
+    CopyableAtomic& operator=(const CopyableAtomic<T>& other) {
+      this->store(other.load(std::memory_order_relaxed), 
+                  std::memory_order_relaxed);
+      return *this;
+    }
 
-      CopyableAtomic& operator=(const CopyableAtomic<T>& other) {
-        this->store(other.load(std::memory_order_relaxed), std::memory_order_relaxed);
-        return *this;
-      }
-
-      // only typedef if T is trivially copyable to use mem copy in serialize/deserialize.
-      typedef typename std::enable_if<std::is_trivially_copyable<T>::value, int>::type tt_is_copyable;
+    // only typedef if T is trivially copyable to use mem copy in serialize/deserialize.
+    using tt_is_copyable = 
+     typename std::enable_if<std::is_trivially_copyable<T>::value, int>::type;
   };
 }
 #endif
