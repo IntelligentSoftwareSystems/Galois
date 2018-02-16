76c77064582021af75128b8912f79b7f10626fda
Gurbinder Gill <gill@ices.utexas.edu>
2018-02-15 21:27:06 -0600

    Generating BC: Requires minor fixes

--- libdist/experimental/plugins/GaloisFunctionsAnalysis/callback_handlers/sync_pull_call_handler.h
+++ libdist/experimental/plugins/GaloisFunctionsAnalysis/callback_handlers/sync_pull_call_handler.h
@@ -48,51 +48,71 @@ class SyncPullInsideForLoopHandler : public MatchFinder::MatchCallback {
         for(auto j : i.second){
           /** Sync pull variables **/
           string str_syncPull_var = "syncPullVar_" + j.NODE_NAME + "_" + j.FIELD_NAME + "_" + i.first;
+          string str_memExpr = "memExpr_" + j.NODE_NAME + "_" + j.FIELD_NAME + i.first;
           /** Sync pull for += operator. **/
           string str_plusOp = "plusEqualOp_" + j.NODE_NAME + "_" + j.FIELD_NAME + "_" + i.first;
           string str_binaryOp_lhs = "binaryOp_LHS_" + j.NODE_NAME + "_" + j.FIELD_NAME + "_" + i.first;
+          string str_lhs_argument = "argument_" + j.NODE_NAME + "_" + j.FIELD_NAME + "_" + i.first;
+
+          /** Is field modified inside or outside edge loop **/
+          string str_modified_insideEdgeLoop = "insideEdgeLoop_LHS_" + j.NODE_NAME + "_" + j.FIELD_NAME + "_" + i.first;
+          string str_modified_outsideEdgeLoop = "outsideEdgeLoop_LHS_" + j.NODE_NAME + "_" + j.FIELD_NAME + "_" + i.first;
 
           /** Sync Pull variables **/
           auto syncPull_var = Results.Nodes.getNodeAs<clang::VarDecl>(str_syncPull_var);
           auto syncPull_plusOp = Results.Nodes.getNodeAs<clang::Stmt>(str_plusOp);
           auto syncPull_binary_var = Results.Nodes.getNodeAs<clang::VarDecl>(str_binaryOp_lhs);
+          auto syncPull_lhs_argument = Results.Nodes.getNodeAs<clang::MemberExpr>(str_lhs_argument);
+          auto syncPull_memExpr = Results.Nodes.getNodeAs<clang::MemberExpr>(str_memExpr);
+          auto syncPull_insideEdgeLoop = Results.Nodes.getNodeAs<clang::Stmt>(str_modified_insideEdgeLoop);
+          auto syncPull_outsideEdgeLoop = Results.Nodes.getNodeAs<clang::Stmt>(str_modified_outsideEdgeLoop);
+
 
           syncPull_var = (syncPull_var != NULL) ? syncPull_var : syncPull_binary_var;
 
+          //Note: FLag to decide if sync pull should be added or not
+          bool flag = false;
+          //if syncPull_var is not NULL, then it must be referenced
           if(syncPull_var){
-            //syncPull_var->dumpColor();
-
             if(syncPull_var->isReferenced()){
+              flag = true;
+            }
+          }
 
-              llvm::outs() << "ADDING FOR SYNC PULL\n";
-              ReductionOps_entry reduceOP_entry;
-              reduceOP_entry.GRAPH_NAME = j.GRAPH_NAME;
-              reduceOP_entry.NODE_TYPE = j.NODE_TYPE;
-              reduceOP_entry.FIELD_TYPE = j.NODE_TYPE;
-              reduceOP_entry.FIELD_NAME = j.FIELD_NAME;
-              reduceOP_entry.VAL_TYPE = j.VAL_TYPE;
-              reduceOP_entry.SYNC_TYPE = "sync_pull";
+          //if syncPull_lhs_argument is not NULL
+          if(syncPull_lhs_argument || syncPull_memExpr){
+            flag = true;
+          }
 
-              reduceOP_entry.OPERATION_EXPR = j.OPERATION_EXPR;
-              reduceOP_entry.RESETVAL_EXPR = j.RESETVAL_EXPR;
 
-              llvm::outs() << " sync for struct " << i.first << " Field name : "  << j.FIELD_NAME << " sync type : " << reduceOP_entry.SYNC_TYPE << "\n";
-              /** check for duplicate **/
-              if(!syncPull_reduction_exists(reduceOP_entry, i.second)){
-                info->reductionOps_map[i.first].push_back(reduceOP_entry);
-              }
+          if(flag){
+            llvm::outs() << "ADDING FOR SYNC PULL\n";
+            ReductionOps_entry reduceOP_entry;
+            reduceOP_entry.GRAPH_NAME = j.GRAPH_NAME;
+            reduceOP_entry.NODE_TYPE = j.NODE_TYPE;
+            reduceOP_entry.FIELD_TYPE = j.NODE_TYPE;
+            reduceOP_entry.FIELD_NAME = j.FIELD_NAME;
+            reduceOP_entry.VAL_TYPE = j.VAL_TYPE;
+            reduceOP_entry.SYNC_TYPE = "sync_pull";
+
+            reduceOP_entry.OPERATION_EXPR = j.OPERATION_EXPR;
+            reduceOP_entry.RESETVAL_EXPR = j.RESETVAL_EXPR;
+
+            if(syncPull_insideEdgeLoop){
+              reduceOP_entry.MODIFIED_IN_EDGE_LOOP = "__INSIDE";
+            } else if (syncPull_outsideEdgeLoop){
+              reduceOP_entry.MODIFIED_IN_EDGE_LOOP = "__OUTSIDE";
+            } else {
+              reduceOP_entry.MODIFIED_IN_EDGE_LOOP = "__OUTSIDE";
+            }
+
+            llvm::outs() << " sync for struct " << i.first << " Field name : "  << j.FIELD_NAME << " sync type : " << reduceOP_entry.SYNC_TYPE << "\n";
+            /** check for duplicate **/
+            if(!syncPull_reduction_exists(reduceOP_entry, i.second)){
+              info->reductionOps_map[i.first].push_back(reduceOP_entry);
             }
             break;
           }
-#if 0
-          else if(syncPull_binary_var){
-            syncPull_binary_var->dumpColor();
-            llvm::outs() << "BINARY IS referenced : " <<  syncPull_binary_var->isReferenced() <<"  " <<i.first << "\n";
-
-            llvm::outs() << j.NODE_NAME << ", " << j.FIELD_NAME << "\n";
-            break;
-          }
-#endif
         }
       }
     }
