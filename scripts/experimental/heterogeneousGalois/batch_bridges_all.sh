278a527bd1511d175f6760f997951290169f7f13
Roshan Dathathri <roshan@cs.utexas.edu>
2018-02-27 20:27:24 -0600

    Updating run scripts

--- scripts/experimental/heterogeneousGalois/batch_bridges_all.sh
+++ scripts/experimental/heterogeneousGalois/batch_bridges_all.sh
@@ -1,32 +1,33 @@
 #!/bin/sh
 
 # all benchmarks
-EXECS=( "bfs_push" "bfs_pull" "pagerank_pull" "pagerank_push" "kcore_push" "kcore_pull" "cc_push" "cc_pull" "sssp_push" "sssp_pull" )
+EXECS=( "bfs_push" "bfs_pull" "cc_push" "cc_pull" "kcore_push" "kcore_pull" "pagerank_push" "pagerank_pull" "sssp_push" "sssp_pull" )
 # fastest variants
-EXECS=( "bfs_push" "pagerank_pull" "kcore_push" "cc_push" "sssp_push" )
+EXECS=( "bfs_push" "cc_push" "pagerank_pull" "sssp_push" )
 
 SET="1,02:00:00 2,01:30:00 4,01:00:00" #rmat28 gpu
 SET="4,03:30:00 8,03:30:00 16,03:00:00 32,02:45:00 64,02:30:00 128,02:00:00" #clueweb12
 SET="1,2:00:00 2,01:30:00 4,01:00:00 8,01:00:00 16,01:00:00 32,00:45:00 64,00:30:00 128,00:30:00" #rmat28
+SETt="1,02:00:00" #twitter40 gpu
+SETc="8,02:00:00 16,02:00:00" #clueweb12 gpu
+SETk="8,02:00:00 16,02:00:00" #kron30 gpu
+SETr="1,02:00:00 2,01:30:00 4,01:00:00 8,01:00:00 16,01:00:00" #rmat28 gpu
 
-INPUTS=("twitter-ICWSM10-component;\"${SET}\"")
-INPUTS=("twitter-WWW10-component;\"${SET}\"")
-INPUTS=("rmat30;\"${SET}\"")
-INPUTS=("uk-2007;\"${SET}\"")
-INPUTS=("kron30;\"${SET}\"")
-INPUTS=("clueweb12;\"${SET}\"")
-INPUTS=("rmat28;\"${SET}\"")
+INPUTS=("twitter40;\"${SETt}\"")
+INPUTS=("kron30;\"${SETk}\"")
+INPUTS=("clueweb12;\"${SETc}\"")
+INPUTS=("rmat28;\"${SETr}\"")
 
 QUEUE=GPU-shared
 QUEUE=RM
 QUEUE=GPU
 #HET=1
 
-PARTS=( "cvc" "hovc" "2dvc" "iec" ) #rmat30
-PARTS=( "cvc" "hivc" "2dvc" "oec" ) #clueweb12
-PARTS=( "oec" ) #clueweb12 pr
-PARTS=( "iec" ) #kron30 or rmat28 pr
-PARTS=( "cvc" ) #default
+PARTS=( "cvc" "hovc" "2dvc" "iec" ) #rmat28/kron30
+PARTS=( "cvc" "hivc" "2dvc" "oec" ) #clueweb12/twitter40
+PARTS=( "cvc" )
+PARTS=( "oec" ) #clueweb12/twitter40
+PARTS=( "iec" ) #kron30/rmat28
 
 for j in "${INPUTS[@]}"
 do
--- scripts/experimental/heterogeneousGalois/batch_single-host_multi-device_all.sh
+++ scripts/experimental/heterogeneousGalois/batch_single-host_multi-device_all.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-# fastest variants
-EXECS=( "bfs_push" "pagerank_pull" "kcore_push" "cc_push" "sssp_push" )
 # all benchmarks
-EXECS=( "bfs_push" "bfs_pull" "kcore_push" "kcore_pull" "cc_push" "cc_pull" "sssp_push" "sssp_pull" )
+EXECS=( "bfs_push" "bfs_pull" "cc_push" "cc_pull" "kcore_push" "kcore_pull" "pagerank_push" "pagerank_pull" "sssp_push" "sssp_pull" )
+# fastest variants
+EXECS=( "bfs_push" "cc_push" "pagerank_pull" "sssp_push" )
 
-INPUTS=( "rmat25" "twitter-WWW10-component" "twitter-ICWSM10-component" "road-USA" )
+INPUTS=( "twitter40" "rmat26" "twitter50" "rmat28" "uk2007" )
+INPUTS=( "twitter40" "rmat26" "rmat28" "uk2007" )
+INPUTS=( "rmat28" "twitter40" )
 
 for j in "${INPUTS[@]}"
 do
@@ -13,6 +15,8 @@ do
   do
     echo "./run_single-host_multi-device_all.sh ${i} ${j}"
     ./run_single-host_multi-device_all.sh ${i} ${j}
+    #echo "ABELIAN_VERIFY=1 ./run_single-host_multi-device_all.sh ${i} ${j}"
+    #ABELIAN_VERIFY=1 ./run_single-host_multi-device_all.sh ${i} ${j}
   done
 done
 
