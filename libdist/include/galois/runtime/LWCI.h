333172a36587998a763706bf782cc75352d7cc85
Loc Hoang <l_hoang@utexas.edu>
2018-06-14 12:10:39 -0500

    lwci.h doxygen file header

--- libdist/include/galois/runtime/LWCI.h
+++ libdist/include/galois/runtime/LWCI.h
@@ -1,4 +1,4 @@
-/**
+/*
  * This file belongs to the Galois project, a C++ library for exploiting parallelism.
  * The code is being released under the terms of XYZ License (a copy is located in
  * LICENSE.txt at the top-level directory).
@@ -15,6 +15,13 @@
  * related expenses which may arise from use of Software or Documentation,
  * including but not limited to those resulting from defects in Software and/or
  * Documentation, or loss or inaccuracy of data of any kind.
+ */
+
+/**
+ * @file LWCI.h
+ *
+ * LWCI header that includes lc.h (LCI library) and internal helper functions
+ * on arrays.
  */
 
 #pragma once
