7713c0c47cb2583aa0939603bd2464dee2563a04
Loc Hoang <l_hoang@utexas.edu>
2018-06-14 12:13:17 -0500

    cuda device cpp doxygen file header

--- libdist/src/cuda_device.cpp
+++ libdist/src/cuda_device.cpp
@@ -1,9 +1,17 @@
+/*
+ */
+
+/** 
+ * @file cuda_device.cpp
+ *
+ * Contains implementation for function that gets gpu device ID.
+ */
 #include "galois/cuda/HostDecls.h"
 #include "galois/Galois.h"
 #include "galois/runtime/Network.h"
 #include <algorithm>
 
-int get_gpu_device_id(std::string personality_set, int num_nodes){
+int get_gpu_device_id(std::string personality_set, int num_nodes) {
   auto& net = galois::runtime::getSystemNetworkInterface();
   unsigned host_id = net.ID;
   unsigned num_hosts = net.Num;
@@ -11,7 +19,8 @@ int get_gpu_device_id(std::string personality_set, int num_nodes){
   assert((num_hosts % num_nodes) == 0);
   assert(personality_set.length() == num_hosts_per_node);
   unsigned num_gpus_per_node = std::count(personality_set.begin(), personality_set.end(), 'g');
-  unsigned num_gpus_before = std::count(personality_set.begin(), personality_set.begin() + (host_id % num_hosts_per_node), 'g');
+  unsigned num_gpus_before = std::count(personality_set.begin(), 
+                                        personality_set.begin() + 
+                                        (host_id % num_hosts_per_node), 'g');
   return (num_gpus_before % num_gpus_per_node);
 }
-
