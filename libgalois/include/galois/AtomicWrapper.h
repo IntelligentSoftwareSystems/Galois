5afcad749fc8f06b58064c91322fd6cb0762d25d
Loc Hoang <l_hoang@utexas.edu>
2018-06-15 18:33:44 -0500

    atomic wrapper doxygen

--- libgalois/include/galois/AtomicWrapper.h
+++ libgalois/include/galois/AtomicWrapper.h
@@ -1,4 +1,4 @@
-/**
+/*
  * This file belongs to the Galois project, a C++ library for exploiting parallelism.
  * The code is being released under the terms of XYZ License (a copy is located in
  * LICENSE.txt at the top-level directory).
@@ -17,27 +17,44 @@
  * Documentation, or loss or inaccuracy of data of any kind.
  */
 
+
+/**
+ * @file AtomicWrapper.h
+ * 
+ * Contains a copyable atomics class.
+ */
 #ifndef _ATOMIC_WRAPPER_H_
 #define _ATOMIC_WRAPPER_H_
 
 #include <atomic>
 
 namespace galois {
-  template<class T>
-  class CopyableAtomic : public std::atomic<T> {
-  public:
-    CopyableAtomic() : std::atomic<T>(T{}) {}
+/**
+ * Class that inherits from std::atomic to make it copyable by defining a copy
+ * constructor.
+ *
+ * @tparam T type of the atomic
+ */
+template<class T>
+class CopyableAtomic : public std::atomic<T> {
+public:
+  //! Default constructor
+  CopyableAtomic() : std::atomic<T>(T{}) {}
 
-    constexpr CopyableAtomic(T desired) : std::atomic<T>(desired) { }
+  //! Constructor initializing atomic to passed in data
+  constexpr CopyableAtomic(T desired) : std::atomic<T>(desired) { }
 
-    constexpr CopyableAtomic(const CopyableAtomic<T>& other) 
-      : CopyableAtomic(other.load(std::memory_order_relaxed)) { }
+  //! Copy constructor
+  constexpr CopyableAtomic(const CopyableAtomic<T>& other) 
+    : CopyableAtomic(other.load(std::memory_order_relaxed)) { }
 
-    CopyableAtomic& operator=(const CopyableAtomic<T>& other) {
-      this->store(other.load(std::memory_order_relaxed), 
-                  std::memory_order_relaxed);
-      return *this;
-    }
-  };
+  //! Copy constructor operator
+  CopyableAtomic& operator=(const CopyableAtomic<T>& other) {
+    this->store(other.load(std::memory_order_relaxed), 
+                std::memory_order_relaxed);
+    return *this;
+  }
+};
+
 }
 #endif
