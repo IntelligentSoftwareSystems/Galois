a3f0d821bd92f3da139a9e691aa749c3adff3684
Loc Hoang <l_hoang@utexas.edu>
2018-10-05 17:28:52 -0500

    partitioners for cartesian vertex cut

---
+++ libdist/include/galois/graphs/GenericPartitioners.h
@@ -0,0 +1,319 @@
+#ifndef _GALOIS_DIST_GENERICPARTS_H
+#define _GALOIS_DIST_GENERICPARTS_H
+
+#include "DistributedGraph.h"
+#include <utility>
+
+class GenericCVC {
+  std::vector<std::pair<uint64_t, uint64_t>> _gid2host;
+  uint32_t _hostID;
+  uint32_t _numHosts;
+  unsigned numRowHosts;
+  unsigned numColumnHosts;
+  unsigned _h_offset;
+
+  void factorizeHosts() {
+    numColumnHosts  = sqrt(_numHosts);
+
+    while ((_numHosts % numColumnHosts) != 0)
+      numColumnHosts--;
+
+    numRowHosts = _numHosts / numColumnHosts;
+    assert(numRowHosts >= numColumnHosts);
+
+    //if (moreColumnHosts) {
+    //  std::swap(numRowHosts, numColumnHosts);
+    //}
+
+    if (_hostID == 0) {
+      galois::gPrint("Cartesian grid: ", numRowHosts, " x ", numColumnHosts,
+                     "\n");
+    }
+  }
+
+  //! Returns the grid row ID of this host
+  unsigned gridRowID() const { return (_hostID / numColumnHosts); }
+  //! Returns the grid row ID of the specified host
+  unsigned gridRowID(unsigned id) const { return (id / numColumnHosts); }
+  //! Returns the grid column ID of this host
+  unsigned gridColumnID() const {
+    return (_hostID % numColumnHosts);
+  }
+  //! Returns the grid column ID of the specified host
+  unsigned gridColumnID(unsigned id) const {
+    return (id % numColumnHosts);
+  }
+
+  //! Find the column of a particular node
+  unsigned getColumnOfNode(uint64_t gid) const {
+    return gridColumnID(getMaster(gid));
+  }
+
+ public:
+  GenericCVC(uint32_t hostID, uint32_t numHosts) {
+    _hostID = hostID;
+    _numHosts = numHosts;
+    factorizeHosts();
+    _h_offset = gridRowID() * numColumnHosts;
+  }
+
+  void saveGIDToHost(std::vector<std::pair<uint64_t, uint64_t>>& gid2host) {
+    _gid2host = gid2host;
+  }
+
+  uint32_t getMaster(uint32_t gid) const {
+    for (auto h = 0U; h < _numHosts; ++h) {
+      uint64_t start, end;
+      std::tie(start, end) = _gid2host[h];
+      if (gid >= start && gid < end) {
+        return h;
+      }
+    }
+    assert(false);
+    return _numHosts;
+    //return gid % 2; // 0 or 1
+  }
+
+  uint32_t getEdgeOwner(uint32_t src, uint32_t dst, uint64_t numEdges) const {
+    int i         = getColumnOfNode(dst);
+    return _h_offset + i;
+  }
+
+  bool isVertexCut() const {
+    if ((numRowHosts == 1) || (numColumnHosts == 1)) return false;
+    return true;
+  }
+
+  constexpr static bool isCartCut() {
+    return true;
+  }
+
+  bool isNotCommunicationPartner(unsigned host, unsigned syncType,
+                                 WriteLocation writeLocation,
+                                 ReadLocation readLocation,
+                                 bool transposed) {
+    if (transposed) {
+      if (syncType == 0) {
+        switch (writeLocation) {
+        case writeSource:
+          return (gridColumnID() != gridColumnID(host));
+        case writeDestination:
+          return (gridRowID() != gridRowID(host));
+        case writeAny:
+          assert((gridRowID() == gridRowID(host)) ||
+                 (gridColumnID() == gridColumnID(host)));
+          return ((gridRowID() != gridRowID(host)) &&
+                  (gridColumnID() != gridColumnID(host))); // false
+        default:
+          assert(false);
+        }
+      } else { // syncBroadcast
+        switch (readLocation) {
+        case readSource:
+          return (gridColumnID() != gridColumnID(host));
+        case readDestination:
+          return (gridRowID() != gridRowID(host));
+        case readAny:
+          assert((gridRowID() == gridRowID(host)) ||
+                 (gridColumnID() == gridColumnID(host)));
+          return ((gridRowID() != gridRowID(host)) &&
+                  (gridColumnID() != gridColumnID(host))); // false
+        default:
+          assert(false);
+        }
+      }
+    } else {
+      if (syncType == 0) {
+        switch (writeLocation) {
+        case writeSource:
+          return (gridRowID() != gridRowID(host));
+        case writeDestination:
+          return (gridColumnID() != gridColumnID(host));
+        case writeAny:
+          assert((gridRowID() == gridRowID(host)) ||
+                 (gridColumnID() == gridColumnID(host)));
+          return ((gridRowID() != gridRowID(host)) &&
+                  (gridColumnID() != gridColumnID(host))); // false
+        default:
+          assert(false);
+        }
+      } else { // syncBroadcast, 1
+        switch (readLocation) {
+        case readSource:
+          return (gridRowID() != gridRowID(host));
+        case readDestination:
+          return (gridColumnID() != gridColumnID(host));
+        case readAny:
+          assert((gridRowID() == gridRowID(host)) ||
+                 (gridColumnID() == gridColumnID(host)));
+          return ((gridRowID() != gridRowID(host)) &&
+                  (gridColumnID() != gridColumnID(host))); // false
+        default:
+          assert(false);
+        }
+      }
+      return false;
+    }
+    return false;
+  }
+};
+
+// same as above, except columns are flipped (changes behavior of vertex cut
+// call as well)
+class GenericCVCColumnFlip {
+  std::vector<std::pair<uint64_t, uint64_t>> _gid2host;
+  uint32_t _hostID;
+  uint32_t _numHosts;
+  unsigned numRowHosts;
+  unsigned numColumnHosts;
+  unsigned _h_offset;
+
+  void factorizeHosts() {
+    numColumnHosts  = sqrt(_numHosts);
+
+    while ((_numHosts % numColumnHosts) != 0)
+      numColumnHosts--;
+
+    numRowHosts = _numHosts / numColumnHosts;
+    assert(numRowHosts >= numColumnHosts);
+
+    // column flip
+    std::swap(numRowHosts, numColumnHosts);
+
+    if (_hostID == 0) {
+      galois::gPrint("Cartesian grid: ", numRowHosts, " x ", numColumnHosts,
+                     "\n");
+    }
+  }
+
+  //! Returns the grid row ID of this host
+  unsigned gridRowID() const { return (_hostID / numColumnHosts); }
+  //! Returns the grid row ID of the specified host
+  unsigned gridRowID(unsigned id) const { return (id / numColumnHosts); }
+  //! Returns the grid column ID of this host
+  unsigned gridColumnID() const {
+    return (_hostID % numColumnHosts);
+  }
+  //! Returns the grid column ID of the specified host
+  unsigned gridColumnID(unsigned id) const {
+    return (id % numColumnHosts);
+  }
+
+  //! Find the column of a particular node
+  unsigned getColumnOfNode(uint64_t gid) const {
+    return gridColumnID(getMaster(gid));
+  }
+
+ public:
+  GenericCVCColumnFlip(uint32_t hostID, uint32_t numHosts) {
+    _hostID = hostID;
+    _numHosts = numHosts;
+    factorizeHosts();
+    _h_offset = gridRowID() * numColumnHosts;
+  }
+
+  void saveGIDToHost(std::vector<std::pair<uint64_t, uint64_t>>& gid2host) {
+    _gid2host = gid2host;
+  }
+
+  uint32_t getMaster(uint32_t gid) const {
+    for (auto h = 0U; h < _numHosts; ++h) {
+      uint64_t start, end;
+      std::tie(start, end) = _gid2host[h];
+      if (gid >= start && gid < end) {
+        return h;
+      }
+    }
+    assert(false);
+    return _numHosts;
+    //return gid % 2; // 0 or 1
+  }
+
+  uint32_t getEdgeOwner(uint32_t src, uint32_t dst, uint64_t numEdges) const {
+    int i         = getColumnOfNode(dst);
+    return _h_offset + i;
+  }
+
+  bool isVertexCut() const {
+    if ((numRowHosts == 1) && (numColumnHosts == 1)) return false;
+    return true;
+  }
+
+  constexpr static bool isCartCut() {
+    return true;
+  }
+
+  bool isNotCommunicationPartner(unsigned host, unsigned syncType,
+                                 WriteLocation writeLocation,
+                                 ReadLocation readLocation,
+                                 bool transposed) {
+    if (transposed) {
+      if (syncType == 0) {
+        switch (writeLocation) {
+        case writeSource:
+          return (gridColumnID() != gridColumnID(host));
+        case writeDestination:
+          return (gridRowID() != gridRowID(host));
+        case writeAny:
+          assert((gridRowID() == gridRowID(host)) ||
+                 (gridColumnID() == gridColumnID(host)));
+          return ((gridRowID() != gridRowID(host)) &&
+                  (gridColumnID() != gridColumnID(host))); // false
+        default:
+          assert(false);
+        }
+      } else { // syncBroadcast
+        switch (readLocation) {
+        case readSource:
+          return (gridColumnID() != gridColumnID(host));
+        case readDestination:
+          return (gridRowID() != gridRowID(host));
+        case readAny:
+          assert((gridRowID() == gridRowID(host)) ||
+                 (gridColumnID() == gridColumnID(host)));
+          return ((gridRowID() != gridRowID(host)) &&
+                  (gridColumnID() != gridColumnID(host))); // false
+        default:
+          assert(false);
+        }
+      }
+    } else {
+      if (syncType == 0) {
+        switch (writeLocation) {
+        case writeSource:
+          return (gridRowID() != gridRowID(host));
+        case writeDestination:
+          return (gridColumnID() != gridColumnID(host));
+        case writeAny:
+          assert((gridRowID() == gridRowID(host)) ||
+                 (gridColumnID() == gridColumnID(host)));
+          return ((gridRowID() != gridRowID(host)) &&
+                  (gridColumnID() != gridColumnID(host))); // false
+        default:
+          assert(false);
+        }
+      } else { // syncBroadcast, 1
+        switch (readLocation) {
+        case readSource:
+          return (gridRowID() != gridRowID(host));
+        case readDestination:
+          return (gridColumnID() != gridColumnID(host));
+        case readAny:
+          assert((gridRowID() == gridRowID(host)) ||
+                 (gridColumnID() == gridColumnID(host)));
+          return ((gridRowID() != gridRowID(host)) &&
+                  (gridColumnID() != gridColumnID(host))); // false
+        default:
+          assert(false);
+        }
+      }
+      return false;
+    }
+    return false;
+  }
+};
+
+
+
+
+#endif
