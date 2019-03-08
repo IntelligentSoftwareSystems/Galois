26d5e25ca2c56c87a46d11951dc7ee2181b02e2f
Hochan Lee <hochan@utexas.edu>
2019-03-08 02:09:45 -0600

    Optimize coreness peeling algorithm: remove useless loops and computations

--- lonestar/experimental/coreness/coreness.cpp
+++ lonestar/experimental/coreness/coreness.cpp
@@ -10,8 +10,7 @@ namespace cll = llvm::cl;
 
 enum Algo {
     naive = 0,
-    peeling,
-    peeling_flg_opt
+    peeling
 };
 
 static cll::opt<std::string>
@@ -22,27 +21,23 @@ static cll::opt<unsigned int>
 static cll::opt<Algo>
     algo("algo", cll::desc("Choose an algorithm:"),
             cll::values(clEnumVal(naive, "naive"),
-                       clEnumVal(peeling, "peeling"),
-                       clEnumVal(peeling_flg_opt, "peeling_flg_opt")),
+                       clEnumVal(peeling, "peeling")),
             cll::init(naive));
 
-typedef int64_t DegreeTy;
+typedef uint64_t DegreeTy;
+typedef std::atomic<DegreeTy> aDegreeTy;
 
-struct NodeData {
-    /* current number of valid degress */ 
-    std::atomic<DegreeTy> validDegree;
-    /* trimmed edges */
-    std::atomic<DegreeTy> trim;
-    /* possible active elements */
-    uint8_t flag;
-};
 DegreeTy* kcoreMap;
 
-constexpr static const DegreeTy K_INFINITY =
+constexpr static const DegreeTy MAX_DEGREE =
             std::numeric_limits<DegreeTy>::max();
+constexpr static const DegreeTy MIN_DEGREE =
+            std::numeric_limits<DegreeTy>::min();
+//! Chunksize for for_each worklist: best chunksize will depend on input
+constexpr static const unsigned CHUNK_SIZE = 64u;
 
 using Graph =
-    galois::graphs::LC_CSR_Graph<NodeData, void>::with_no_lockable<true>::type;
+    galois::graphs::LC_CSR_Graph<aDegreeTy, void>::with_no_lockable<true>::type;
 using GNode = Graph::GraphNode;
 
 /**
@@ -52,15 +47,15 @@ using GNode = Graph::GraphNode;
  */
 void initialize(Graph &graph) {
     galois::do_all(galois::iterate(graph.begin(), graph.end()),
-            [&](GNode node){
-                NodeData& currNode = graph.getData(node);
-                currNode.flag   = true;
-                currNode.validDegree = 0;
-                currNode.trim = 0;
-
-                for (auto edge : graph.edges(node))
-                   ++currNode.validDegree;
-            } );
+            [&](GNode cNode){
+                //NodeData& cNData = graph.getData(cNode);
+                aDegreeTy& cNDegree = graph.getData(cNode);
+                cNDegree.store(
+                    std::distance(graph.edge_begin(cNode),
+                                  graph.edge_end(cNode)));
+            },
+            galois::loopname("Initialization"),
+            galois::no_stats() );
 }
 
 /**
@@ -70,26 +65,20 @@ void initialize(Graph &graph) {
  * Not only initialize node data,
  * but also find minimum or maximum degrees.
  */
-void initialize(Graph &graph, DegreeTy& maxDegree, 
-                    std::atomic<DegreeTy>& minDegree) {
-    minDegree = K_INFINITY;
-    maxDegree = std::numeric_limits<DegreeTy>::min(); 
-    for (auto iter = graph.begin();
-            iter != graph.end();
-            ++iter) {
-        GNode node = *iter;
-        NodeData& currNode = graph.getData(node);
-        currNode.flag   = true;
-        currNode.trim = 0;
-
-        for (auto edge : graph.edges(node))
-            ++currNode.validDegree;
-
-        if (minDegree > currNode.validDegree)
-            minDegree = (DegreeTy) currNode.validDegree;
-        if (maxDegree < currNode.validDegree)
-            maxDegree = currNode.validDegree;
-    }
+void initialize(Graph &graph, aDegreeTy& maxDegree, aDegreeTy& minDegree) {
+    minDegree = MAX_DEGREE;
+    maxDegree = MIN_DEGREE;
+    galois::do_all(galois::iterate(graph.begin(), graph.end()),
+            [&](GNode cNode) {
+               aDegreeTy& cNDegree = graph.getData(cNode);
+               cNDegree.store(
+                   std::distance(graph.edge_begin(cNode),
+                                 graph.edge_end(cNode)));
+               galois::atomicMin(minDegree, (DegreeTy) cNDegree);
+               galois::atomicMax(maxDegree, (DegreeTy) cNDegree);
+            },
+            galois::loopname("Initialization"),
+            galois::no_stats() );
 }
 
 /**
@@ -100,46 +89,23 @@ void initialize(Graph &graph, DegreeTy& maxDegree,
  * but also find minimum or maximum degrees.
  * In addition, an initial bag is filled.
  */
-void initialize(Graph &graph, DegreeTy& maxDegree, 
-                    std::atomic<DegreeTy>& minDegree,
-                        galois::InsertBag<GNode>& curr) {
-    minDegree = K_INFINITY;
-    maxDegree = std::numeric_limits<DegreeTy>::min(); 
-    for (auto iter = graph.begin();
-            iter != graph.end();
-            ++iter) {
-        GNode node = *iter;
-        NodeData& currNode = graph.getData(node);
-        currNode.flag = true;
-        currNode.trim = 0;
+void initialize(Graph &graph, aDegreeTy& maxDegree,
+                  aDegreeTy& minDegree, galois::InsertBag<GNode>& curr) {
 
-        for (auto edge : graph.edges(node))
-            ++currNode.validDegree;
-
-        if (minDegree > currNode.validDegree)
-            minDegree = (DegreeTy) currNode.validDegree;
-        if (maxDegree < currNode.validDegree)
-            maxDegree = currNode.validDegree;
-
-        curr.push(node);
-    }
-}
-
-/**
- * copyToKcoreMap()
- * @graph: Target graph.
- *
- * Copy current degrees for each nodes to the kcoreMap.
- * This is because the graph keep changing,
- * while trying to find k+1 cores on the graph.
- */
-void copyToKcoreMap(Graph& graph) {
-    do_all(galois::iterate(graph.begin(), graph.end()),
+    minDegree = MAX_DEGREE;
+    maxDegree = MIN_DEGREE;
+    galois::do_all(galois::iterate(graph.begin(), graph.end()),
             [&](GNode cNode) {
-                NodeData& cNData = graph.getData(cNode);
-                kcoreMap[cNode] = cNData.validDegree;
+               aDegreeTy& cNDegree = graph.getData(cNode);
+               cNDegree.store(
+                   std::distance(graph.edge_begin(cNode),
+                                 graph.edge_end(cNode)));
+               galois::atomicMin(minDegree, (DegreeTy) cNDegree);
+               galois::atomicMax(maxDegree, (DegreeTy) cNDegree);
+               curr.push(cNode);
             },
-            galois::steal());
+            galois::loopname("Initialization"),
+            galois::no_stats() );
 }
 
 /**
@@ -155,54 +121,42 @@ void copyToKcoreMap(Graph& graph) {
  */
 uint8_t parFindKCore(Graph &graph, DegreeTy k) {
     uint8_t isFind = false;
-    using PSchunk = galois::worklists::PerSocketChunkFIFO<16>;
-    galois::InsertBag<GNode> initBag;
-    auto trimming =
-        [&](GNode cNode, auto& ctx) {
-            NodeData& cNData = graph.getData(cNode);
-            /* If the current node has degress that
-               lower than k,
-               then ignores and removes from
-               neighbor sets. */
-                cNData.flag = false;
-                for (auto e : graph.edges(cNode)) {
-                    GNode neigh = graph.getEdgeDst(e);
-                    NodeData& nghData = graph.getData(neigh);
-                    auto oldVal = galois::atomicAdd(nghData.validDegree,
-                                                        (DegreeTy) -1);
-                    if (oldVal == k+1) {
-                        ctx.push(neigh);
-                    }
-                }
-        };
+    galois::InsertBag<GNode> cur;
 
     // Update alive nodes to min degree.
     // Initialize candidnate nodes which are possible to be removed.
     galois::do_all(galois::iterate(graph.begin(), graph.end()),
             [&](GNode cNode) {
-                NodeData& cNData = graph.getData(cNode);
-                if (cNData.flag) {
+                aDegreeTy& cNDegree = graph.getData(cNode);
+                if (cNDegree >= k) {
                     kcoreMap[cNode] = k;
-                    if (cNData.validDegree == k)
-                        initBag.push(cNode);
+                    isFind = true;
+                    if (cNDegree == k)
+                        cur.emplace(cNode);
                 }
-            } , galois::steal() );
+            },
+            galois::steal(),
+            galois::no_stats() );
 
     // Perform trimming. 
-    galois::for_each(galois::iterate(initBag.begin(), initBag.end()),
-            trimming,
-            galois::wl<PSchunk>() );
-
-    // Find minimum degree.
-    galois::do_all(
-        galois::iterate(graph.begin(), graph.end()),
-        [&](GNode cNode) {
-            NodeData& cNData = graph.getData(cNode);
-            if (cNData.flag) {
-                /* If a node has degrees higher than k,
-                   at least it is in the k-core. */
-                isFind = true;
-           } } );
+    galois::for_each(galois::iterate(cur.begin(), cur.end()),
+            [&](GNode cNode, auto& ctx) {
+                /* If the current node has degress that
+                   lower than k,
+                   then ignores and removes from
+                   neighbor sets. */
+                for (auto e : graph.edges(cNode)) {
+                    GNode neigh = graph.getEdgeDst(e);
+                    aDegreeTy& nghDegree = graph.getData(neigh);
+                    auto oldVal = galois::atomicSubtract(
+                                            nghDegree, 1ul);
+                    if (oldVal == k+1) {
+                        ctx.push(neigh);
+                    }
+                }
+            },
+            galois::no_conflicts(),
+            galois::chunk_size<CHUNK_SIZE>() );
 
     return isFind;
 }
@@ -218,134 +172,57 @@ uint8_t parFindKCore(Graph &graph, DegreeTy k) {
  * Return: If it succeeds to find the k-cores,
  *         returns true. Otherwise, return false.
  */
-uint8_t parFindKCore(Graph &graph, DegreeTy k, 
-                   std::atomic<DegreeTy>& nextK,
+int8_t parFindKCore(Graph &graph, DegreeTy k, 
+                   aDegreeTy& nextK,
                    galois::InsertBag<GNode>& prev) {
-    uint8_t isFind = false;
-    using PSchunk = galois::worklists::PerSocketChunkFIFO<16>;
     galois::InsertBag<GNode> next, curr;
-    auto trimming =
-        [&](GNode cNode, auto& ctx) {
-            /* If the current node has degress that
-               lower than k,
-               then ignores and removes from
-               neighbor sets. */
-            NodeData& cNData = graph.getData(cNode);
-            for (auto e : graph.edges(cNode)) {
-                GNode neigh = graph.getEdgeDst(e);
-                NodeData& nghData = graph.getData(neigh);
-                auto oldVal = galois::atomicAdd(nghData.validDegree,
-                                                    (DegreeTy) -1);
-                if (oldVal == k+1) {
-                    ctx.push(neigh);
-                }
-            }
-        };
 
     // Update alive nodes to min degree.
     // Initialize candidnate nodes which are possible to be removed.
     // Do not iterate all nodes, but candidate nodes.
     galois::do_all(galois::iterate(prev.begin(), prev.end()),
             [&](GNode cNode) {
-                NodeData& cNData = graph.getData(cNode);
-                DegreeTy nDegree = cNData.validDegree;
-                if (nDegree >= k) {
-                    kcoreMap[cNode] = k;
-                    if (nDegree == k) curr.push(cNode);
-                    else next.push(cNode);
+                aDegreeTy& cNDegree = graph.getData(cNode);
+                if (cNDegree >= k) {
+                    if (cNDegree == k)
+                        curr.emplace(cNode);
+                    else if (cNDegree > k) {
+                        next.emplace(cNode);
+                        galois::atomicMin(nextK, (DegreeTy) cNDegree);
+                    }
                 }
-            } , galois::steal() );
+            },
+            galois::steal(),
+            galois::no_stats() );
 
     // Perform trimming. 
     galois::for_each(galois::iterate(curr.begin(), curr.end()),
-            trimming,
-            galois::wl<PSchunk>() );
+            [&](GNode cNode, auto& ctx) {
+                /* If the current node has degress that
+                   lower than k,
+                   then ignores and removes from
+                   neighbor sets. */
+                kcoreMap[cNode] = k;
+                for (auto e : graph.edges(cNode)) {
+                    GNode neigh = graph.getEdgeDst(e);
+                    aDegreeTy& nghDegree = graph.getData(neigh);
+                    auto oldVal = galois::atomicSubtract(
+                                        nghDegree, 1ul);
+                    if (oldVal == k+1) {
+                        ctx.push(neigh);
+                    } else if (oldVal > k+1) {
+                        galois::atomicMin(nextK, (DegreeTy) (oldVal-1));
+                    }
+                }
+            },
+            galois::no_conflicts(),
+            galois::chunk_size<CHUNK_SIZE>() );
 
     // Swap next to prev bag for the next phase.
     prev.clear();
     std::swap(prev, next);
 
-    // Find minimum degree.
-    galois::do_all(
-        galois::iterate(prev.begin(), prev.end()),
-        [&](GNode cNode) {
-            NodeData& cNData = graph.getData(cNode);
-            /* If a node has degrees higher than k,
-               at least it is in the k-core. */
-            isFind = true;
-
-            if (nextK > cNData.validDegree && cNData.validDegree > k)
-                galois::atomicMin(nextK, (DegreeTy) cNData.validDegree);
-           } );
-
-    return isFind;
-}
-
-/**
- * parFindKCore()
- * @graph: Target graph.
- * @k: target degree for k-cores
- *
- * Find whether the graph has k-cores or not in parallel.
- * NOTE: peeling based.
- *
- * Return: If it succeeds to find the k-cores,
- *         returns true. Otherwise, return false.
- */
-uint8_t parFindKCore(Graph &graph, DegreeTy k, std::atomic<DegreeTy>& nextK) {
-    uint8_t isFind = false;
-    using PSchunk = galois::worklists::PerSocketChunkFIFO<16>;
-    galois::InsertBag<GNode> initBag;
-    auto trimming =
-        [&](GNode cNode, auto& ctx) {
-            NodeData& cNData = graph.getData(cNode);
-            /* If the current node has degress that
-               lower than k,
-               then ignores and removes from
-               neighbor sets. */
-                cNData.flag = false;
-                for (auto e : graph.edges(cNode)) {
-                    GNode neigh = graph.getEdgeDst(e);
-                    NodeData& nghData = graph.getData(neigh);
-                    auto oldVal = galois::atomicAdd(nghData.validDegree,
-                                                    (DegreeTy) -1);
-                    if (oldVal == k+1) {
-                        ctx.push(neigh);
-                    }
-                }
-        };
-
-    // Update alive nodes to min degree.
-    // Initialize candidnate nodes which are possible to be removed.
-    galois::do_all(galois::iterate(graph.begin(), graph.end()),
-            [&](GNode cNode) {
-                NodeData& cNData = graph.getData(cNode);
-                if (cNData.flag) {
-                    kcoreMap[cNode] = k;
-                    if (cNData.validDegree == k)
-                        initBag.push(cNode);
-                }
-            } , galois::steal() );
-
-    // Perform trimming. 
-    galois::for_each(galois::iterate(initBag.begin(), initBag.end()),
-            trimming,
-            galois::wl<PSchunk>() );
-
-    // Find minimum degree.
-    galois::do_all(
-        galois::iterate(graph.begin(), graph.end()),
-        [&](GNode cNode) {
-            NodeData& cNData = graph.getData(cNode);
-            if (cNData.flag) {
-                /* If a node has degrees higher than k,
-                   at least it is in the k-core. */
-                isFind = true;
-                if (nextK > cNData.validDegree)
-                    galois::atomicMin(nextK, (DegreeTy) cNData.validDegree);
-           } } );
-
-    return isFind;
+    return (prev.begin() != prev.end());
 }
 
 /**
@@ -369,41 +246,6 @@ DegreeTy naiveCoreness(Graph &graph) {
         }
     }
 
-    for (size_t node = 0; node != graph.size(); node++) {
-        std::cout << node << "," << kcoreMap[node] << std::endl;
-    }
-
-    /* return maximal coreness number among nodes */
-    return cand_k;
-}
-
-/**
- * peelingCoreness()
- * @graph: Target graph.
- *
- * Iterate from 0 to a maximal possible k value for kcores 
- * (= the number of edges),
- * and check whether there exist any kcores.
- */
-DegreeTy peelingCoreness(Graph &graph) {
-    DegreeTy cand_k;
-    DegreeTy maxDegree;
-    std::atomic<DegreeTy> minDegree;
-
-    initialize(graph, maxDegree, minDegree);
-    cand_k = 0;
-    do {
-        cand_k = minDegree;
-        minDegree = K_INFINITY;
-        if(!parFindKCore(graph, cand_k, minDegree)) break;
-
-    } while (minDegree <= maxDegree);
-
-
-    for (size_t node = 0; node != graph.size(); node++) {
-        std::cout << node << "," << kcoreMap[node] << std::endl;
-    }
-
     /* return maximal coreness number among nodes */
     return cand_k;
 }
@@ -419,22 +261,17 @@ DegreeTy peelingCoreness(Graph &graph) {
  */
 DegreeTy peelingCoreness(Graph &graph, galois::InsertBag<GNode>& curr) {
     DegreeTy cand_k;
-    DegreeTy maxDegree;
-    std::atomic<DegreeTy> minDegree;
+    aDegreeTy maxDegree;
+    aDegreeTy minDegree;
 
     initialize(graph, maxDegree, minDegree, curr);
     cand_k = 0;
     do {
         cand_k = minDegree;
-        minDegree = K_INFINITY;
+        minDegree = MAX_DEGREE;
         if(!parFindKCore(graph, cand_k, minDegree, curr)) break;
 
     } while (minDegree <= maxDegree);
-
-
-    for (size_t node = 0; node != graph.size(); node++) {
-        std::cout << node << "," << kcoreMap[node] << std::endl;
-    }
 
     /* return maximal coreness number among nodes */
     return cand_k;
@@ -472,9 +309,6 @@ int main(int argc, char** argv) {
             coreness = naiveCoreness(graph);
         break;
         case peeling:
-            coreness = peelingCoreness(graph);
-        break;
-        case peeling_flg_opt:
             coreness = peelingCoreness(graph, initBag);
         break;
     }
@@ -482,6 +316,10 @@ int main(int argc, char** argv) {
     Tmain.stop();
     std::cout << "Graph [" << inputFName << "] has coreness of "
         << coreness << std::endl;
+
+    for (size_t node = 0; node != graph.size(); node++) {
+        std::cout << node << "," << kcoreMap[node] << std::endl;
+    }
 
     delete kcoreMap;
     return 0;
