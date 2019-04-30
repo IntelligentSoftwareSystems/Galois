b203934c573d812526ec18c7bd6b4e1c872a4def
Roshan Dathathri <roshan@cs.utexas.edu>
2019-04-30 13:29:10 -0500

    Rename dist_apps to lonestardist

--- scripts/experimental/heterogeneousGalois/batch_verify.sh
+++ scripts/experimental/heterogeneousGalois/batch_verify.sh
@@ -5,11 +5,13 @@ LOG=.verify_log
 # all benchmarks
 EXECS=( "bfs_push" "bfs_pull" "kcore_push" "kcore_pull" "cc_push" "cc_pull" "sssp_push" "sssp_pull" "pagerank_push" "pagerank_pull" )
 EXECS=( "bfs_push" "kcore_push" "cc_push" "sssp_push" "pagerank_pull" )
+EXECS=( "bfs_push" "bfs_push_async" )
 EXECS=( "bfs_push" "kcore_push" "cc_push" "sssp_push" "pagerank_pull" "bfs_push_async" "kcore_push_async" "cc_push_async" "sssp_push_async" "pagerank_pull_async" )
 
 INPUTS=( "rmat25" "twitter-WWW10-component" )
 INPUTS=( "rmat15" "rmat20" "rmat24" "road-USA" )
 INPUTS=( "rmat20" )
+INPUTS=( "rmat15" "rmat20" )
 INPUTS=( "rmat15" )
 
 rm -f $LOG
@@ -20,6 +22,7 @@ do
   for EXEC in "${EXECS[@]}"
   do
     $current_dir/verify.sh ${EXEC} ${input}
+    rm -f $LOG
   done
 done
 
