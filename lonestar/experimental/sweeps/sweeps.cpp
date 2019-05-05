f3d14db5c352b02af65646ae48d8b7a4bda403a6
Ian Henriksen <insertinterestingnamehere@gmail.com>
2019-05-04 23:09:46 -0500

    Add code for building graph to represent mesh in sweeps.

--- lonestar/experimental/sweeps/sweeps.cpp
+++ lonestar/experimental/sweeps/sweeps.cpp
@@ -17,15 +17,17 @@
  * Documentation, or loss or inaccuracy of data of any kind.
  */
 
-#include <cstddef>
+#include <array>
 #include <iostream>
 
 // Vendored from an old version of LLVM for Lonestar app command line handling.
 #include "llvm/Support/CommandLine.h"
 
 #include <galois/Galois.h>
-#include <galois/graphs/Graph.h>
 #include <galois/graphs/FileGraph.h>
+#include <galois/graphs/Graph.h>
+#include <galois/graphs/LCGraph.h>
+
 #include "Lonestar/BoilerPlate.h"
 
 static char const *name = "Boltzmann Equation sweeps";
@@ -49,8 +51,75 @@ static llvm::cl::opt<std::size_t> maxiters{"maxiters", llvm::cl::desc("maximum n
 // TODO: This is another example of an undirected graph being important.
 // We need one of those as well.
 
+// Note: I'm going to use a CSR graph, so each node will already have a
+// unique integer id that can be used to index other data structures, so
+// it's not necessary to store anything other than the actual topology
+// on the graph nodes. Since the rest of the data is dynamically sized,
+// it'll be in a separate array.
+// Each edge does need to store the unit normal going outward through
+// the corresponding face though.
+
+// TODO: Try making edge data separate and shared to see if it helps
+// locality. It's not clear that it will, but it's something to try.
+
+using graph_t = galois::graphs::LC_CSR_Graph<void, std::array<double, 3>>;
+
+// Routine to initialize graph topology and face normals.
+// Note: C++17 will allow just returning the graph since copy elision will be guaranteed.
+// For now, the reference is needed though, since the built graph may not be copyable.
+void generate_grid(graph_t &built_graph, std::size_t nx, std::size_t ny, std::size_t nz) noexcept {
+
+  galois::graphs::FileGraphWriter temp_graph;
+  std::size_t num_nodes = nx * ny * nz;
+  std::size_t num_edges = 2 * (nx * ny * (nz - 1) + nx * (ny - 1) * nz + (nx - 1) * ny * nz);
+  temp_graph.setNumNodes(num_nodes);
+  temp_graph.setNumEdges(num_edges);
+  temp_graph.setSizeofEdgeData(galois::LargeArray<graph_t::edge_data_type>::size_of::value);
+  galois::LargeArray<graph_t::edge_data_type> edge_data;
+  edge_data.create(num_edges);
+
+  temp_graph.phase1();
+  for (std::size_t i = 0; i < nx; i++) {
+    for (std::size_t j = 0; j < ny; j++) {
+      for (std::size_t k = 0; k < nz; k++) {
+        std::size_t id = i * nx * ny + j * ny + k;
+        if (i > 0) temp_graph.incrementDegree(id);
+        if (i < nx - 1) temp_graph.incrementDegree(id);
+        if (j > 0) temp_graph.incrementDegree(id);
+        if (j < ny - 1) temp_graph.incrementDegree(id);
+        if (k > 0) temp_graph.incrementDegree(id);
+        if (k < nz - 1) temp_graph.incrementDegree(id);
+      }
+    }
+  }
+
+  temp_graph.phase2();
+  for (std::size_t i = 0; i < nx; i++) {
+    for (std::size_t j = 0; j < ny; j++) {
+      for (std::size_t k = 0; k < nz; k++) {
+        std::size_t id = i * nx * ny + j * ny + k;
+        if (i > 0) edge_data.set(temp_graph.addNeighbor(id, id - ny * nz), {1., 0., 0.});
+        if (i < nx - 1) edge_data.set(temp_graph.addNeighbor(id, id + ny * nz), {-1., 0., 0.});
+        if (j > 0) edge_data.set(temp_graph.addNeighbor(id, id - nz), {0., 1., 0.});
+        if (j < ny - 1) edge_data.set(temp_graph.addNeighbor(id, id + nz), {0., -1., 0.});
+        if (k > 0) edge_data.set(temp_graph.addNeighbor(id, id - 1), {0., 0., 1.});
+        if (k < nz - 1) edge_data.set(temp_graph.addNeighbor(id, id + 1), {0., 0., -1.});
+      }
+    }
+  }
+
+  // TODO: is it possible to set the edge data during construction?
+  auto *rawEdgeData = temp_graph.finish<graph_t::edge_data_type>();
+  std::uninitialized_copy(std::make_move_iterator(edge_data.begin()), std::make_move_iterator(edge_data.end()), rawEdgeData);
+
+  galois::graphs::readGraph(built_graph, temp_graph);
+}
+
 int main(int argc, char**argv) noexcept {
   galois::SharedMemSys galois_system;
   LonestarStart(argc, argv, name, desc, url);
+
+  graph_t graph;
+  generate_grid(graph, nx, ny, nz);
 }
 
