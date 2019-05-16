c71a7afa5e4fd55dd0418805b4b1a42e884c04b5
Roshan Dathathri <roshan@cs.utexas.edu>
2019-05-16 10:20:29 -0500

    Gluon only sends updated values (does not send non-updated values to minimize communication volume)

--- libgluon/include/galois/runtime/DataCommMode.h
+++ libgluon/include/galois/runtime/DataCommMode.h
@@ -28,7 +28,7 @@
  */
 #pragma once
 
-//! Enumeration of data communication modes that can be used in sychronization
+//! Enumeration of data communication modes that can be used in synchronization
 //! @todo document the enums in doxygen
 enum DataCommMode {
   noData, //!< send no data
@@ -36,9 +36,8 @@ enum DataCommMode {
   offsetsData,
   gidsData,
   onlyData,
-  dataSplitFirst,
-  dataSplit,
-  neverOnlyData
+  dataSplitFirst, // NOT USED
+  dataSplit // NOT USED
 };
 
 //! If this is set, then always used the data mode it is set to
@@ -59,35 +58,17 @@ extern DataCommMode enforce_data_mode;
 template <typename DataType>
 DataCommMode get_data_mode(size_t num_selected, size_t num_total) {
   DataCommMode data_mode = noData;
-  // TODO clean up neverOnlyData path (integrate with main path in some way)
-  if (enforce_data_mode == neverOnlyData) {
-    if (num_selected == 0) {
-      data_mode = noData;
-    } else {
-      size_t bitset_alloc_size =
-          ((num_total + 63) / 64) * sizeof(uint64_t) + (2 * sizeof(size_t));
-
-      size_t bitsetDataSize = (num_selected * sizeof(DataType)) +
-                              bitset_alloc_size + sizeof(num_selected);
-      size_t offsetsDataSize = (num_selected * sizeof(DataType)) +
-                               (num_selected * sizeof(unsigned int)) +
-                               sizeof(size_t) + sizeof(num_selected);
-      if (bitsetDataSize < offsetsDataSize) {
-        data_mode = bitsetData;
-      } else {
-        data_mode = offsetsData;
-      }
-    }
-  } else if (enforce_data_mode != noData) {
+  if (enforce_data_mode != noData) {
     data_mode = enforce_data_mode;
   } else { // no enforced mode, so find an appropriate mode
     if (num_selected == 0) {
       data_mode = noData;
+    } else if (num_selected == num_total) {
+      data_mode = onlyData;
     } else {
       size_t bitset_alloc_size =
           ((num_total + 63) / 64) * sizeof(uint64_t) + (2 * sizeof(size_t));
 
-      size_t onlyDataSize   = (num_total * sizeof(DataType));
       size_t bitsetDataSize = (num_selected * sizeof(DataType)) +
                               bitset_alloc_size + sizeof(num_selected);
       size_t offsetsDataSize = (num_selected * sizeof(DataType)) +
@@ -95,17 +76,9 @@ DataCommMode get_data_mode(size_t num_selected, size_t num_total) {
                                sizeof(size_t) + sizeof(num_selected);
       // find the minimum size one
       if (bitsetDataSize < offsetsDataSize) {
-        if (bitsetDataSize < onlyDataSize) {
-          data_mode = bitsetData;
-        } else {
-          data_mode = onlyData;
-        }
+        data_mode = bitsetData;
       } else {
-        if (offsetsDataSize < onlyDataSize) {
-          data_mode = offsetsData;
-        } else {
-          data_mode = onlyData;
-        }
+        data_mode = offsetsData;
       }
     }
   }
