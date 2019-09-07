98291d65e7c9782bb36af0e772737ad6d5b18516
chenxuhao <chen_xuhao@126.com>
2019-09-07 10:49:42 -0500

    add header

--- lonestar/experimental/kcl1/kcl.cpp
+++ lonestar/experimental/kcl1/kcl.cpp
@@ -17,28 +17,7 @@
  * Documentation, or loss or inaccuracy of data of any kind.
  */
 
-#include "galois/Galois.h"
-#include "galois/Reduction.h"
-#include "galois/Bag.h"
-#include "galois/Timer.h"
-#include "galois/graphs/LCGraph.h"
-#include "galois/ParallelSTL.h"
-#include "llvm/Support/CommandLine.h"
-#include "Lonestar/BoilerPlate.h"
-#include "galois/runtime/Profile.h"
-#include <boost/iterator/transform_iterator.hpp>
-
-const char* name = "Kcl";
-const char* desc = "Counts the K-Cliques in a graph using BFS extension";
-const char* url  = 0;
-
-namespace cll = llvm::cl;
-static cll::opt<std::string> filetype(cll::Positional, cll::desc("<filetype: txt,adj,mtx,gr>"), cll::Required);
-static cll::opt<std::string> filename(cll::Positional, cll::desc("<filename: symmetrized graph>"), cll::Required);
-static cll::opt<unsigned> k("k", cll::desc("max number of vertices in k-clique (default value 3)"), cll::init(3));
-static cll::opt<unsigned> show("s", cll::desc("print out the details"), cll::init(0));
-typedef galois::graphs::LC_CSR_Graph<uint32_t, void>::with_numa_alloc<true>::type ::with_no_lockable<true>::type Graph;
-typedef Graph::GraphNode GNode;
+#include "pangolin.h"
 
 #define USE_DAG
 #define USE_SIMPLE
@@ -48,6 +27,10 @@ typedef Graph::GraphNode GNode;
 #define USE_BASE_TYPES
 #include "Mining/vertex_miner.h"
 #include "Mining/util.h"
+
+const char* name = "Kcl";
+const char* desc = "Counts the K-Cliques in a graph using BFS extension";
+const char* url  = 0;
 
 class AppMiner : public VertexMiner {
 public:
