8c1bd379d0dc2ba9d074233805bf270ed9b34d10
chenxuhao <chen_xuhao@126.com>
2019-10-03 09:55:43 -0500

    clean fsm

--- lonestar/include/BfsMining/engine.h
+++ lonestar/include/BfsMining/engine.h
@@ -1,4 +1,39 @@
 #ifndef TRIANGLE
+#ifdef EDGE_INDUCED
+void edgeSolver(AppMiner &miner, EmbeddingList& emb_list) {
+	unsigned level = 1;
+	if (debug) emb_list.printout_embeddings(1);
+	int num_freq_patterns = miner.init_aggregator();
+	if (num_freq_patterns == 0) {
+		std::cout << "No frequent pattern found\n\n";
+		return;
+	}
+	miner.inc_total_num(num_freq_patterns);
+	std::cout << "Number of frequent single-edge patterns: " << num_freq_patterns << "\n";
+	miner.init_filter(emb_list);
+	if (show) emb_list.printout_embeddings(level);
+
+	while (1) {
+		miner.extend_edge(level, emb_list);
+		level ++;
+		if (show) emb_list.printout_embeddings(level, debug);
+		miner.quick_aggregate(level, emb_list);
+		miner.merge_qp_map(level+1);
+		miner.canonical_aggregate();
+		miner.merge_cg_map(level+1);
+
+		num_freq_patterns = miner.support_count();
+		if (show) std::cout << "num_frequent_patterns: " << num_freq_patterns << "\n";
+		if (debug) miner.printout_agg();
+
+		miner.inc_total_num(num_freq_patterns);
+		if (num_freq_patterns == 0) break;
+		if (level == k) break;
+		miner.filter(level, emb_list);
+		if (show) emb_list.printout_embeddings(level, debug);
+	}
+}
+#else
 void solver(AppMiner &miner, EmbeddingList &emb_list) {
 	unsigned level = 1;
 	while (1) {
@@ -13,6 +48,8 @@ void solver(AppMiner &miner, EmbeddingList &emb_list) {
 	}
 }
 #endif
+#endif
+
 int main(int argc, char** argv) {
 	galois::SharedMemSys G;
 	LonestarStart(argc, argv, name, desc, url);
@@ -26,13 +63,24 @@ int main(int argc, char** argv) {
 	Tinitial.start();
 	read_graph(graph, filetype, filename, false, need_dag);
 	Tinitial.stop();
+	#ifdef EDGE_INDUCED
+	assert(k > 1);
+	#else
 	assert(k > 2);
+	#endif
 	std::cout << "num_vertices " << graph.size() << " num_edges " << graph.sizeEdges() << "\n";
+	//std::cout << "max_size = " << k << std::endl;
+	//std::cout << "min_support = " << minsup << std::endl;
+	//std::cout << "num_threads = " << numThreads << std::endl;
 
 	ResourceManager rm;
 
 	#ifdef TRIANGLE
 	AppMiner miner(&graph);
+	#else
+	#ifdef EDGE_INDUCED
+	AppMiner miner(&graph, k);
+	miner.set_threshold(minsup);
 	#else
 	int npatterns = 1;
 	#ifdef USE_MAP
@@ -40,27 +88,26 @@ int main(int argc, char** argv) {
 	#endif
 	AppMiner miner(&graph, k, npatterns);
 	#endif
-
-	#ifdef TRIANGLE
-	#ifdef USE_EMB_LIST
-	EmbeddingList emb_list;
-	galois::StatTimer Tinitemb("EmbListInitTime");
-	Tinitemb.start();
-	emb_list.init(graph, 2, need_dag);
-	Tinitemb.stop();
 	#endif
-	#else
+
 	#ifdef USE_EMB_LIST
 	EmbeddingList emb_list;
 	galois::StatTimer Tinitemb("EmbListInitTime");
 	Tinitemb.start();
+	#ifdef TRIANGLE
+	emb_list.init(graph, 2, need_dag);
+	#else
+	#ifdef EDGE_INDUCED
+	emb_list.init(graph, k+1);
+	#else
 	emb_list.init(graph, k, need_dag);
 	Tinitemb.stop();
+	#endif
+	#endif
 	#else
 	EmbeddingQueueType queue, queue2;
 	miner.init(queue); // insert single-edge (two-vertex) embeddings into the queue
 	if(show) queue.printout_embeddings(0);
-	#endif
 	#endif
 
 	galois::StatTimer Tcomp("Compute");
@@ -74,7 +121,11 @@ int main(int argc, char** argv) {
 	#endif // USE_EMB_LIST
 	#else
 	#ifdef USE_EMB_LIST
+	#ifdef EDGE_INDUCED
+	edgeSolver(miner, emb_list);
+	#else
 	solver(miner, emb_list);
+	#endif // EDGE_INDUCED
 	#else
 	unsigned level = 1;
 	while (1) {
@@ -87,6 +138,7 @@ int main(int argc, char** argv) {
 	}
 	#endif // USE_EMB_LIST
 	#endif // TRIANGLE
+
 	#ifdef USE_MAP
 	if (k >= 5) {
 		// TODO: need to use unsigned long for the counters
