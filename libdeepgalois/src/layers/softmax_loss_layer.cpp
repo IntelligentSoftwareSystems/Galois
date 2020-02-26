6336fe34b4fece4f2618fd534529c0e6451ca896
Loc Hoang <l_hoang@utexas.edu>
2020-02-25 18:16:21 -0600

    softmax missing functions

--- libdeepgalois/src/layers/softmax_loss_layer.cpp
+++ libdeepgalois/src/layers/softmax_loss_layer.cpp
@@ -56,11 +56,39 @@ void softmax_loss_layer::back_propagation(const float_t* in_data,
                  galois::chunk_size<CHUNK_SIZE>(), galois::steal(),
                  galois::loopname("softmax-loss-bw"));
 }
+
+acc_t softmax_loss_layer::get_masked_loss() {
+  AccumF total_loss;
+  AccumU valid_sample_count;
+  total_loss.reset();
+  valid_sample_count.reset();
+  galois::do_all(galois::iterate(begin_, end_),
+                 [&](const auto& i) {
+                   if (masks_[i]) {
+                     total_loss += loss[i];
+                     valid_sample_count += 1;
+                   }
+                 },
+                 galois::chunk_size<256>(), galois::steal(),
+                 galois::loopname("getMaskedLoss"));
+  assert(valid_sample_count.reduce() == count_);
+  return total_loss.reduce() / (acc_t)count_;
+}
+
+
+
 #else // GPU implementation
 void softmax_loss_layer::forward_propagation(const float_t* in_data,
                                              float_t* out_data) {
   softmax_cross_entropy_gpu(input_dims[0], input_dims[1], in_data, d_masks_,
                             context->d_labels, loss, out_data);
+}
+
+void softmax_loss_layer::back_propagation(const float_t* in_data,
+                                          const float_t* out_data,
+                                          float_t* out_grad, float_t* in_grad) {
+  d_softmax_cross_entropy_gpu(input_dims[0], input_dims[1], in_data, d_masks_,
+                              context->d_labels, out_data, in_grad);
 }
 
 acc_t softmax_loss_layer::get_masked_loss() {
