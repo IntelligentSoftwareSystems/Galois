d46fb46ed0eb256c4ff2690bb8ab69b618eaf688
Loc Hoang <l_hoang@utexas.edu>
2020-03-02 18:14:35 -0600

    aggregator.h: always include CPU version of update_all will not conflict with GPU since different signature

--- libdeepgalois/include/deepgalois/aggregator.h
+++ libdeepgalois/include/deepgalois/aggregator.h
@@ -1,13 +1,11 @@
 #pragma once
 #include "deepgalois/types.h"
-#ifdef CPU_ONLY
 #include "deepgalois/gtypes.h"
-
 namespace deepgalois {
 void update_all(size_t len, Graph& g, const float_t* in, float_t* out,
                 bool norm, const float_t* norm_factor);
 }
-#else
+#ifndef CPU_ONLY
 #include "graph_gpu.h"
 namespace deepgalois {
 void update_all(size_t len, CSRGraph& g, const float_t* in, float_t* out,
