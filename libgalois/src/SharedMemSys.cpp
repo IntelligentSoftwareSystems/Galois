0f17d7d78a1b5e881fc24ac45edefec7515d686a
Donald Nguyen <ddn0@users.noreply.github.com>
2020-03-31 12:52:03 -0700

    Rename SharedMem{Substrate,Runtime} to SharedMem
    These two classes are in different namespaces (galois::runtime,
    galois::substrate) and there is no reason for any client code to import
    both, so remove the redundant qualifier on their names. Along the way,
    rename source files to match their class declarations and fix various
    clang-tidy warnings.

--- libgalois/src/SharedMemSys.cpp
+++ libgalois/src/SharedMemSys.cpp
@@ -17,38 +17,8 @@
  * Documentation, or loss or inaccuracy of data of any kind.
  */
 
-#ifndef GALOIS_SUBSTRATE_INIT_H
-#define GALOIS_SUBSTRATE_INIT_H
+#include "galois/SharedMemSys.h"
 
-#include "galois/gIO.h"
-#include "galois/substrate/ThreadPool.h"
-#include "galois/substrate/Barrier.h"
-#include "galois/substrate/Termination.h"
+galois::SharedMemSys::SharedMemSys() = default;
 
-namespace galois {
-namespace substrate {
-
-class SharedMemSubstrate {
-
-  // Order is critical here
-  ThreadPool m_tpool;
-
-  internal::LocalTerminationDetection<>* m_termPtr;
-  internal::BarrierInstance<>* m_biPtr;
-
-public:
-  /**
-   * Initializes the Substrate library components
-   */
-  SharedMemSubstrate();
-
-  /**
-   * Destroys the Substrate library components
-   */
-  ~SharedMemSubstrate();
-};
-
-} // namespace substrate
-} // namespace galois
-
-#endif // GALOIS_SUBSTRATE_INIT_H
+galois::SharedMemSys::~SharedMemSys() = default;
