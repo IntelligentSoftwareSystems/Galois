ae090e13e9280a4a4837ce0d07c5f99a00e94cc7
Donald Nguyen <ddn0@users.noreply.github.com>
2020-04-25 10:53:48 -0700

    Remove gtuple
    icc fails to compile gtuple due to errors like the following:
    
      [...]gtuple.h(77): error: pack expansion does not make use of any argument packs
          using append = integer_seq<T, Is..., I>;
                                          ^
                detected during instantiation of class "galois::integer_seq<T, Is...> [with T=int, Is=<>]" at line 104
    
    Similar errors are reported by icc when it compiles <tuple> from the GCC
    9 STL library. Currently, icc 12 only supports GCC 8 as a backend.
    
    So, it appears that there are some C++17 features that still trip up
    icc. As a workaround, reformulate gtuple in terms of standard C++17 STL
    features. Since, gtuple itself was designed to provide transitional
    support for integer sequences and related functionality in mixed C++11
    codebases, this gives us an opportunity to remove custom code.

---
+++ libgalois/test/traits.cpp
@@ -0,0 +1,52 @@
+#include "galois/gIO.h"
+#include "galois/Traits.h"
+#include <iostream>
+#include <utility>
+
+struct A {};
+
+struct B : public A {
+  std::string name_;
+  B(std::string name) : name_(std::move(name)) {}
+  B() : B("") {}
+};
+
+struct Unrelated {};
+
+template <size_t... Ints, typename Tuple>
+void print(std::index_sequence<Ints...> seq, Tuple tup) {
+  (..., (std::cout << typeid(std::get<Ints>(tup)).name() << " ")) << "\n";
+}
+
+template <typename Tuple>
+void print(Tuple tup) {
+  print(std::make_index_sequence<std::tuple_size<Tuple>::value>(), tup);
+}
+
+int main() {
+  auto pull_from_default = galois::get_default_trait_values(
+      std::make_tuple(Unrelated{}), std::make_tuple(A{}), std::make_tuple(B{}));
+  static_assert(
+      std::is_same<decltype(pull_from_default), std::tuple<B>>::value);
+
+  auto no_pull_from_default_when_same = galois::get_default_trait_values(
+      std::make_tuple(A{}), std::make_tuple(A{}), std::make_tuple(B{}));
+  static_assert(std::is_same<decltype(no_pull_from_default_when_same),
+                             std::tuple<>>::value);
+
+  auto no_pull_from_default_when_derived = galois::get_default_trait_values(
+      std::make_tuple(B{}), std::make_tuple(A{}), std::make_tuple(B{}));
+  static_assert(std::is_same<decltype(no_pull_from_default_when_derived),
+                             std::tuple<>>::value);
+
+  auto empty_tuple = galois::get_default_trait_values(
+      std::make_tuple(), std::make_tuple(), std::make_tuple());
+  static_assert(std::is_same<decltype(empty_tuple), std::tuple<>>::value);
+
+  auto value_from_default = galois::get_default_trait_values(
+      std::make_tuple(), std::make_tuple(A{}), std::make_tuple(B{"name"}));
+  GALOIS_ASSERT(std::get<0>(value_from_default).name_ == "name");
+
+  auto get_value = galois::get_trait_value<A>(std::tuple<B>(B{"name"}));
+  GALOIS_ASSERT(get_value.name_ == "name");
+}
