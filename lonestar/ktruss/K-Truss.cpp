0f4e442d1e655c0818a0d24343c8303d0cb9a409
Hochan Lee <hochan@utexas.edu>
2020-04-28 23:06:53 -0500

    Modify comment alignments and clear header files

--- lonestar/ktruss/K-Truss.cpp
+++ lonestar/ktruss/K-Truss.cpp
@@ -1,7 +1,7 @@
 /*
- * This file belongs to the Galois project, a C++ library for exploiting parallelism.
- * The code is being released under the terms of the 3-Clause BSD License (a
- * copy is located in LICENSE.txt at the top-level directory).
+ * This file belongs to the Galois project, a C++ library for exploiting
+ * parallelism. The code is being released under the terms of the 3-Clause
+ * BSD License (a copy is located in LICENSE.txt at the top-level directory).
  *
  * Copyright (C) 2018, The University of Texas at Austin. All rights reserved.
  * UNIVERSITY EXPRESSLY DISCLAIMS ANY AND ALL WARRANTIES CONCERNING THIS
@@ -21,9 +21,9 @@
 #include "galois/Reduction.h"
 #include "galois/Bag.h"
 #include "galois/Timer.h"
-#include "galois/Timer.h"
 #include "galois/graphs/Graph.h"
 #include "galois/graphs/TypeTraits.h"
+#include "galois/runtime/Statistics.h"
 #include "llvm/Support/CommandLine.h"
 #include "Lonestar/BoilerPlate.h"
 
