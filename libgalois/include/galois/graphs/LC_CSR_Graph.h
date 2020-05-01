2e8c00df7a5061af9c3a6f0be4a8327392b0fc9e
Sepideh Maleki <smaleki@ghostwheel2.oden.utexas.edu>
2020-05-01 09:19:57 -0500

    LC_CSR

--- libgalois/include/galois/graphs/LC_CSR_Graph.h
+++ libgalois/include/galois/graphs/LC_CSR_Graph.h
@@ -153,6 +153,9 @@ public:
   typedef typename NodeInfoTypes::reference node_data_reference;
   using edge_iterator =
       boost::counting_iterator<typename EdgeIndData::value_type>;
+  // for hypergraphs
+  size_t hedges;
+  size_t hnodes;
   using iterator = boost::counting_iterator<typename EdgeDst::value_type>;
   typedef iterator const_iterator;
   typedef iterator local_iterator;
@@ -796,6 +799,43 @@ public:
      * Deallocate if reusing the graph
      */
     destroyAndAllocateFrom(numNodes, numEdges);
+    constructNodes();
+
+    galois::do_all(galois::iterate((uint32_t)0, numNodes),
+                  [&](uint32_t n) {
+                    edgeIndData[n] = prefix_sum[n];
+                    });
+
+    galois::do_all(galois::iterate((uint32_t)0, numNodes),
+                  [&](uint32_t n) {
+                    if( n == 0){
+                      if(edgeIndData[n] > 0){
+                        std::copy(edges_id[n].begin(), edges_id[n].end(), edgeDst.begin());
+                        std::copy(edges_data[n].begin(), edges_data[n].end(), edgeData.begin());
+                      }
+                    }
+                    else{
+                        if(edgeIndData[n] - edgeIndData[n-1] > 0){
+                          std::copy(edges_id[n].begin(), edges_id[n].end(), edgeDst.begin() + edgeIndData[n-1]);
+                          std::copy(edges_data[n].begin(), edges_data[n].end(), edgeData.begin() + edgeIndData[n-1]);
+                      }
+                      }
+                  });
+
+
+ galois::on_each(
+        [&](unsigned tid, unsigned total) {
+        std::vector<unsigned> dummy_scale_factor; // dummy passed in to function call
+
+          auto r = divideByNode(0, 1, tid, total).first;
+
+        //galois::gPrint("[", tid, "] : Ranges : ", *r.first, ", ", *r.second, "\n");
+        this->setLocalRange(*r.first, *r.second);
+        }
+        );
+  }
+  void constructFrom(uint32_t numNodes, uint64_t numEdges, std::vector<uint64_t>& prefix_sum, galois::gstl::Vector<galois::PODResizeableArray<uint32_t>>& edges_id, std::vector<std::vector<EdgeTy>>& edges_data) {
+    allocateFrom(numNodes, numEdges);
     constructNodes();
 
     galois::do_all(galois::iterate((uint32_t)0, numNodes),
