b412db3e5f2207e787dca2a3c6546c5a07544887
Donald Nguyen <ddn0@users.noreply.github.com>
2020-05-19 09:37:08 -0700

    Write outputs with libtsuba

--- lonestar/libdistbench/include/DistBench/Output.h
+++ lonestar/libdistbench/include/DistBench/Output.h
@@ -1,21 +1,42 @@
 #ifndef GALOIS_DISTBENCH_OUTPUT_H
 #define GALOIS_DISTBENCH_OUTPUT_H
 
-#include <string>
-#include <fstream>
+#include "galois/gIO.h"
 
-std::string makeOutputFilename(const std::string& outputDir);
+#include <arrow/api.h>
+#include <fstream>
+#include <string>
+#include <utility>
+
+void writeOutput(const std::string& outputDir, arrow::Table& table);
 
 template <typename T>
-void writeOutput(const std::string& outputDir, const std::string& /*fieldName*/,
+void writeOutput(const std::string& outputDir, const std::string& fieldName,
                  T* values, size_t length) {
-  std::string filename = makeOutputFilename(outputDir);
+  using ArrowType = typename arrow::CTypeTraits<T>::ArrowType;
 
-  std::ofstream outputFile(filename.c_str());
+  arrow::NumericBuilder<ArrowType> builder;
 
-  for (size_t i = 0; i < length; i++) {
-    outputFile << *(values++) << "\n";
+  auto appendStatus = builder.AppendValues(values, &values[length]);
+  if (!appendStatus.ok()) {
+    GALOIS_DIE("appending values", appendStatus.ToString());
   }
+
+  std::shared_ptr<arrow::Array> array;
+
+  auto finishStatus = builder.Finish(&array);
+  if (!finishStatus.ok()) {
+    GALOIS_DIE("finishing values", finishStatus.ToString());
+  }
+
+  auto typeFn = arrow::TypeTraits<ArrowType>::type_singleton();
+
+  std::shared_ptr<arrow::Schema> schema =
+      arrow::schema({arrow::field(fieldName, typeFn)});
+
+  std::shared_ptr<arrow::Table> table = arrow::Table::Make(schema, {array});
+
+  writeOutput(outputDir, *table);
 }
 
 #endif
--- lonestar/libdistbench/src/Output.cpp
+++ lonestar/libdistbench/src/Output.cpp
@@ -1,9 +1,12 @@
 #include "DistBench/Output.h"
 #include "galois/runtime/Network.h"
+#include "tsuba/tsuba_api.h"
 
+#include <parquet/arrow/writer.h>
 #include <iomanip>
 
 namespace {
+
 std::string zeroPad(int num, int width) {
   std::ostringstream out;
 
@@ -11,8 +14,6 @@ std::string zeroPad(int num, int width) {
 
   return out.str();
 }
-
-} // namespace
 
 std::string makeOutputFilename(const std::string& outputDir) {
   std::string filename = zeroPad(galois::runtime::getHostID(), 8);
@@ -25,4 +26,36 @@ std::string makeOutputFilename(const std::string& outputDir) {
   }
 
   return output;
+}
+
+} // namespace
+
+void writeOutput(const std::string& dir, arrow::Table& table) {
+  auto createResult = arrow::io::BufferOutputStream::Create();
+  if (!createResult.ok()) {
+    GALOIS_DIE("creating table", createResult.status().ToString());
+  }
+
+  std::shared_ptr<arrow::io::BufferOutputStream> out =
+      createResult.ValueOrDie();
+
+  auto writeStatus =
+      parquet::arrow::WriteTable(table, arrow::default_memory_pool(), out, 4);
+  if (!writeStatus.ok()) {
+    GALOIS_DIE("writing table", writeStatus.ToString());
+  }
+
+  auto finishResult = out->Finish();
+  if (!finishResult.ok()) {
+    GALOIS_DIE("finishing buffer", finishResult.status().ToString());
+  }
+
+  std::shared_ptr<arrow::Buffer> buf = finishResult.ValueOrDie();
+
+  std::string filename = makeOutputFilename(dir);
+
+  int err = 0;
+  if ((err = TsubaStore(filename.c_str(), buf->data(), buf->size())) != 0) {
+    GALOIS_DIE("store", err);
+  }
 }
