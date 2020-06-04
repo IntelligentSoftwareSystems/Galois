9d1935dac674a9138ae9d5cda02a16db9d4d9600
Ian Henriksen <insertinterestingnamehere@gmail.com>
2020-06-04 16:19:19 -0500

    Remove the experimental directory. Experimental development will take place on branches from now on.

--- lonestar/experimental/ordered/avi/dummy.f90
+++
@@ -1,9 +0,0 @@
-!***************************************************
-!  File:        Minimal95.F95
-!  Language:    Fortran 95
-!  Description: This program does nothing 
-!***************************************************
-
-PROGRAM Minimal95
-  print *, "Hello"
-END PROGRAM Minimal95
