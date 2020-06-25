11c9a2a0e5a996c88032b27c3297a82f48fffd96
Roshan Dathathri <roshan@cs.utexas.edu>
2020-06-25 16:18:35 -0500

    Removing unused option from triangle counting

--- lonestar/analytics/cpu/triangle-counting/Triangles.cpp
+++ lonestar/analytics/cpu/triangle-counting/Triangles.cpp
@@ -61,12 +61,6 @@ static cll::opt<bool>
                       "choose automatically)"),
             cll::init(false));
 
-static cll::opt<bool> storeRelabeledGraph(
-    "storeRelabeledGraph",
-    cll::desc("Write the relabeled graph to disk for future use with "
-              ".gr.triangles extension (default value true)"),
-    cll::init(true));
-
 typedef galois::graphs::LC_CSR_Graph<void, void>::with_numa_alloc<
     true>::type ::with_no_lockable<true>::type Graph;
 
