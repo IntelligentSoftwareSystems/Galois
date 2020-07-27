c9d964b9bf3ad993627de8060859f2f11817a9a5
Bozhi YOU <youbozhi@cs.utexas.edu>
2020-07-27 04:24:05 -0500

    Fix init value of earliest; update code structure w.r.t OBIM

--- libgalois/include/galois/worklists/AdaptiveObim.h
+++ libgalois/include/galois/worklists/AdaptiveObim.h
@@ -21,8 +21,6 @@
 #ifndef GALOIS_WORKLIST_ADAPTIVEOBIM_H
 #define GALOIS_WORKLIST_ADAPTIVEOBIM_H
 
-// #define UNMERGE_ENABLED
-
 #include <atomic>
 #include <cmath>
 #include <iostream>
@@ -30,6 +28,7 @@
 #include <type_traits>
 
 #include "galois/config.h"
+
 #include "galois/FlatMap.h"
 #include "galois/Timer.h"
 #include "galois/substrate/PaddedLock.h"
@@ -39,6 +38,32 @@
 
 namespace galois {
 namespace worklists {
+
+namespace internal {
+
+template <typename Index, bool UseDescending>
+struct AdaptiveOrderedByIntegerMetricComparator {
+  typedef std::less<Index> compare_t;
+  Index identity;
+  Index earliest;
+
+  AdaptiveOrderedByIntegerMetricComparator()
+      : identity(std::numeric_limits<Index>::max()),
+        earliest(std::numeric_limits<Index>::min()) {}
+};
+
+template <typename Index>
+struct AdaptiveOrderedByIntegerMetricComparator<Index, true> {
+  typedef std::greater<Index> compare_t;
+  Index identity;
+  Index earliest;
+
+  AdaptiveOrderedByIntegerMetricComparator()
+      : identity(std::numeric_limits<Index>::min()),
+        earliest(std::numeric_limits<Index>::max()) {}
+};
+
+} // namespace internal
 
 /**
  * Approximate priority scheduling. Indexer is a default-constructable class
@@ -66,7 +91,7 @@ namespace worklists {
  * @tparam BSP            Use back-scan prevention
  * @tparam uniformBSP     Use uniform back-scan prevention
  * @tparam T              Work item type
- * @tparam Index Indexer  return type
+ * @tparam Index          Indexer return type
  * @tparam UseMonotonic   Assume that an activity at priority p will not
  * schedule work at priority p or any priority p1 where p1 < p.
  * @tparam UseDescending  Use descending order instead
@@ -75,70 +100,91 @@ namespace worklists {
 template <class Indexer      = DummyIndexer<int>,
           typename Container = PerSocketChunkFIFO<>, int BlockPeriod = 0,
           bool BSP = true, bool uniformBSP = false, int chunk_size = 64,
-          typename T = int, typename Index = int, bool UseMonotonic = false,
-          bool UseDescending = false, bool Concurrent = true>
-struct AdaptiveOrderedByIntegerMetric : private boost::noncopyable {
+          typename T = int, typename Index = int, bool EnableUmerge = false,
+          bool UseMonotonic = false, bool UseDescending = false,
+          bool Concurrent = true>
+struct AdaptiveOrderedByIntegerMetric
+    : private boost::noncopyable,
+      public internal::AdaptiveOrderedByIntegerMetricComparator<Index,
+                                                                UseDescending> {
   template <typename _T>
   using retype = AdaptiveOrderedByIntegerMetric<
       Indexer, typename Container::template retype<_T>, BlockPeriod, BSP,
       uniformBSP, chunk_size, _T, typename std::result_of<Indexer(_T)>::type,
-      UseMonotonic, UseDescending, Concurrent>;
+      EnableUmerge, UseMonotonic, UseDescending, Concurrent>;
 
   template <bool _b>
   using rethread = AdaptiveOrderedByIntegerMetric<
       Indexer, typename Container::template rethread<_b>, BlockPeriod, BSP,
-      uniformBSP, chunk_size, T, Index, UseMonotonic, UseDescending, _b>;
+      uniformBSP, chunk_size, T, Index, EnableUmerge, UseMonotonic,
+      UseDescending, _b>;
 
   template <unsigned _period>
-  using with_block_period =
-      AdaptiveOrderedByIntegerMetric<Indexer, Container, _period, BSP,
-                                     uniformBSP, chunk_size, T, Index,
-                                     UseMonotonic, UseDescending, Concurrent>;
-
+  struct with_block_period {
+    typedef AdaptiveOrderedByIntegerMetric<
+        Indexer, Container, _period, BSP, uniformBSP, chunk_size, T, Index,
+        EnableUmerge, UseMonotonic, UseDescending, Concurrent>
+        type;
+  };
   template <typename _container>
-  using with_container =
-      AdaptiveOrderedByIntegerMetric<Indexer, _container, BlockPeriod, BSP,
-                                     uniformBSP, chunk_size, T, Index,
-                                     UseMonotonic, UseDescending, Concurrent>;
+  struct with_container {
+    typedef AdaptiveOrderedByIntegerMetric<
+        Indexer, _container, BlockPeriod, BSP, uniformBSP, chunk_size, T, Index,
+        EnableUmerge, UseMonotonic, UseDescending, Concurrent>
+        type;
+  };
 
-  template <typename Indexer_>
-  using with_indexer =
-      AdaptiveOrderedByIntegerMetric<Indexer_, Container, BlockPeriod, BSP,
-                                     uniformBSP, chunk_size, T, Index,
-                                     UseMonotonic, UseDescending, Concurrent>;
+  template <typename _indexer>
+  struct with_indexer {
+    AdaptiveOrderedByIntegerMetric<
+        _indexer, Container, BlockPeriod, BSP, uniformBSP, chunk_size, T, Index,
+        EnableUmerge, UseMonotonic, UseDescending, Concurrent>
+        type;
+  };
 
-  template <bool BSP_>
-  using with_back_scan_prevention =
-      AdaptiveOrderedByIntegerMetric<Indexer, Container, BlockPeriod, BSP_,
-                                     uniformBSP, chunk_size, T, Index,
-                                     UseMonotonic, UseDescending, Concurrent>;
+  template <bool _bsp>
+  struct with_back_scan_prevention {
+    typedef AdaptiveOrderedByIntegerMetric<
+        Indexer, Container, BlockPeriod, _bsp, uniformBSP, chunk_size, T, Index,
+        EnableUmerge, UseMonotonic, UseDescending, Concurrent>
+        type;
+  };
 
-  template <bool UseMonotonic_>
-  using with_monotonic =
-      AdaptiveOrderedByIntegerMetric<Indexer, Container, BlockPeriod, BSP,
-                                     uniformBSP, chunk_size, T, Index,
-                                     UseMonotonic_, UseDescending, Concurrent>;
+  template <bool _enable_unmerge>
+  struct with_unmerge {
+    AdaptiveOrderedByIntegerMetric<
+        Indexer, Container, BlockPeriod, BSP, uniformBSP, chunk_size, T, Index,
+        _enable_unmerge, UseMonotonic, UseDescending, Concurrent>
+        type;
+  };
 
-  template <bool UseDescending_>
-  using with_descending =
-      AdaptiveOrderedByIntegerMetric<Indexer, Container, BlockPeriod, BSP,
-                                     uniformBSP, chunk_size, T, Index,
-                                     UseMonotonic, UseDescending_, Concurrent>;
+  template <bool _use_monotonic>
+  struct with_monotonic {
+    AdaptiveOrderedByIntegerMetric<
+        Indexer, Container, BlockPeriod, BSP, uniformBSP, chunk_size, T, Index,
+        EnableUmerge, _use_monotonic, UseDescending, Concurrent>
+        type;
+  };
 
-  using compare_t =
-      std::conditional_t<UseDescending, std::greater<Index>, std::less<Index>>;
+  template <bool _use_descending>
+  struct with_descending {
+    AdaptiveOrderedByIntegerMetric<
+        Indexer, Container, BlockPeriod, BSP, uniformBSP, chunk_size, T, Index,
+        EnableUmerge, UseMonotonic, _use_descending, Concurrent>
+        type;
+  };
 
   typedef T value_type;
   typedef Index index_type;
+  typedef uint32_t delta_type;
 
 private:
-  static inline compare_t compare;
-  static constexpr Index earliest = UseDescending
-                                        ? std::numeric_limits<Index>::min()
-                                        : std::numeric_limits<Index>::max();
-  // static constexpr identity = UseDescending ?
-  // std::numeric_limits<Index>::max() : std::numeric_limits<Index>::min();
-  unsigned int delta;
+  typedef typename Container::template rethread<Concurrent> CTy;
+  typedef internal::AdaptiveOrderedByIntegerMetricComparator<Index,
+                                                             UseDescending>
+      Comparator;
+  static inline typename Comparator::compare_t compare;
+  delta_type delta;
   unsigned int counter;
   unsigned int maxIndex;
   unsigned int lastSizeMasterLog;
@@ -147,13 +193,12 @@ private:
   // smaller delta insertions are prioritirized
   struct deltaIndex {
     Index k; // note: original index is stored here
-    unsigned int d;
+    delta_type d;
     // taking the max of deltas and doing right shift eq. shifting priority with
     // d-max(d1, d2)
 
     deltaIndex() : k(0), d(0) {}
-    deltaIndex(Index k1, unsigned int d1) : k(k1), d(d1) {}
-    unsigned int id() { return k; }
+    deltaIndex(Index k1, delta_type d1) : k(k1), d(d1) {}
     bool operator==(const deltaIndex& a) const {
       unsigned delt = std::max(d, a.d);
       Index a1      = k >> delt;
@@ -214,7 +259,6 @@ private:
     }
   };
 
-  typedef typename Container::template rethread<Concurrent> CTy;
   typedef galois::flat_map<deltaIndex, CTy*> LMapTy;
 
   struct ThreadData {
@@ -225,32 +269,43 @@ private:
     unsigned int lastMasterVersion;
     unsigned int numPops;
 
-    unsigned long pmodAllDeq;
-
+    unsigned int popsLastFix;
+    unsigned int slowPopsLastPeriod;
+    unsigned int pushesLastPeriod;
+    unsigned int popsFromSameQ;
     struct {
-      unsigned int popsLastFix;
-      unsigned int slowPopsLastPeriod;
-      unsigned int pushesLastPeriod;
+      size_t pmodAllDeq;
       unsigned int priosLastPeriod;
-
-      unsigned int popsFromSameQ;
-
-      inline double slowPopFreq() {
-        return ((double)slowPopsLastPeriod / (double)popsLastFix);
-      }
-    } counters;
-    unsigned int ctr;
-
-    Index maxPrioDiffLastPeriod;
+      unsigned int numUmerges;
+      Index maxPrioDiffLastPeriod;
+    } stats;
     Index minPrio;
     Index maxPrio;
     substrate::PaddedLock<Concurrent> lock;
 
+    void cleanup() {
+      popsLastFix        = 0;
+      slowPopsLastPeriod = 0;
+      pushesLastPeriod   = 0;
+
+      stats.priosLastPeriod       = 0;
+      stats.maxPrioDiffLastPeriod = 0;
+
+      minPrio = std::numeric_limits<Index>::max();
+      maxPrio = std::numeric_limits<Index>::min();
+    }
+
+    inline bool isSlowPopFreq(double threshold) {
+      // return ((double)slowPopsLastPeriod / (double)popsLastFix) > threshold;
+      return ((double)slowPopsLastPeriod > (double)popsLastFix) * threshold;
+    }
+
     ThreadData(Index initial)
         : curIndex(initial, 0), scanStart(initial, 0), current(0),
-          lastMasterVersion(0), numPops(0),
-          pmodAllDeq(0), counters{0, 0, 0, 0, 0}, ctr(0),
-          maxPrioDiffLastPeriod(0), minPrio(std::numeric_limits<Index>::max()),
+          lastMasterVersion(0), numPops(0), popsLastFix(0),
+          slowPopsLastPeriod(0), pushesLastPeriod(0),
+          popsFromSameQ(0), stats{0, 0, 0, 0},
+          minPrio(std::numeric_limits<Index>::max()),
           maxPrio(std::numeric_limits<Index>::min()) {}
   };
 
@@ -289,63 +344,57 @@ private:
   GALOIS_ATTRIBUTE_NOINLINE
   galois::optional<T> slowPop(ThreadData& p) {
     // Failed, find minimum bin
-    p.counters.slowPopsLastPeriod++;
+    p.slowPopsLastPeriod++;
     unsigned myID = galois::substrate::ThreadPool::getTID();
 
     // first give it some time
     // then check the fdeq frequency
-    if (myID == 0 && p.counters.popsLastFix > counter &&
-        p.counters.slowPopFreq() > 1.0 / (double)(chunk_size)) {
+    if (myID == 0 && p.popsLastFix > counter &&
+        p.isSlowPopFreq(1.0 / (double)(chunk_size))) {
+      unsigned long numPushesThisStep      = p.pushesLastPeriod;
+      unsigned long priosCreatedThisPeriod = p.stats.priosLastPeriod;
+      unsigned long allPmodDeqCounts       = p.stats.pmodAllDeq;
+      Index minOfMin                       = p.minPrio;
+      Index maxOfMax                       = p.maxPrio;
+      p.cleanup();
       for (unsigned i = 1; i < runtime::activeThreads; ++i) {
         while (!data.getRemote(i)->lock.try_lock())
           ;
-      }
-      unsigned long priosCreatedThisPeriod = 0;
-      unsigned long numPushesThisStep      = 0;
-      unsigned long allPmodDeqCounts       = 0;
-      Index minOfMin                       = std::numeric_limits<Index>::max();
-      Index maxOfMax                       = std::numeric_limits<Index>::min();
-      for (unsigned i = 0; i < runtime::activeThreads; ++i) {
+
         Index& otherMinPrio = data.getRemote(i)->minPrio;
-        minOfMin = compare(minOfMin, otherMinPrio) ? minOfMin : otherMinPrio;
+        minOfMin            = std::min(minOfMin, otherMinPrio, compare);
         Index& otherMaxPrio = data.getRemote(i)->maxPrio;
-        maxOfMax = compare(otherMaxPrio, maxOfMax) ? maxOfMax : otherMaxPrio;
-        priosCreatedThisPeriod += data.getRemote(i)->counters.priosLastPeriod;
-        numPushesThisStep += data.getRemote(i)->counters.pushesLastPeriod;
-        allPmodDeqCounts += data.getRemote(i)->pmodAllDeq;
-        data.getRemote(i)->counters.popsLastFix        = 0;
-        data.getRemote(i)->counters.slowPopsLastPeriod = 0;
-        data.getRemote(i)->counters.pushesLastPeriod   = 0;
-        data.getRemote(i)->counters.priosLastPeriod    = 0;
-        data.getRemote(i)->maxPrioDiffLastPeriod       = 0;
+        maxOfMax            = std::max(otherMaxPrio, maxOfMax, compare);
+        numPushesThisStep += data.getRemote(i)->pushesLastPeriod;
+        priosCreatedThisPeriod += data.getRemote(i)->stats.priosLastPeriod;
+        allPmodDeqCounts += data.getRemote(i)->stats.pmodAllDeq;
 
-        data.getRemote(i)->minPrio = std::numeric_limits<Index>::max();
-        data.getRemote(i)->maxPrio = std::numeric_limits<Index>::min();
-      }
-
-      if ((double)numPushesThisStep &&
-          ((double)numPushesThisStep /
-           ((double)((maxOfMax >> delta) - (minOfMin >> delta)))) <
-              chunk_size / 2) {
-        double xx = ((double)(chunk_size) /
-                     ((double)numPushesThisStep /
-                      ((double)((maxOfMax >> delta) - (minOfMin >> delta)))));
-        delta += std::floor(std::log2(xx));
-        counter *= 2;
-      }
-
-      for (unsigned i = 1; i < runtime::activeThreads; ++i) {
+        data.getRemote(i)->cleanup();
         data.getRemote(i)->lock.unlock();
       }
+
+      if ((double)numPushesThisStep) {
+        Index prioRange = (maxOfMax >> delta) - (minOfMin >> delta);
+        // Division is expensive
+        // double fillRatio = ((double)numPushesThisStep / (double)prioRange);
+        if (numPushesThisStep < (chunk_size >> 1) * prioRange) {
+          // Ditto
+          // double xx = ((double)(chunk_size) / fillRatio);
+          double xx = std::log2(chunk_size) - std::log2(numPushesThisStep) +
+                      std::log2(prioRange);
+          assert(xx);
+          delta += std::floor(xx);
+          counter <<= 1;
+        }
+      }
     }
-#ifdef UNMERGE_ENABLED
     // serif added here
     // make sure delta is bigger than 0 so that we can actually unmerge things
     // give it some time and check the same queue pops
-    else if (delta > 0 && myID == 0 && p.counters.popsLastFix > counter &&
-             p.counters.popsFromSameQ > 4 * chunk_size) {
+    else if (EnableUmerge && delta > 0 && myID == 0 &&
+             p.popsLastFix > counter && p.popsFromSameQ > (chunk_size << 2)) {
       if (((p.maxPrio >> delta) - (p.minPrio >> delta)) < 16 &&
-          ((double)p.counters.pushesLastPeriod /
+          ((double)p.pushesLastPeriod /
            ((double)((p.maxPrio >> delta) - (p.minPrio >> delta)))) >
               4 * chunk_size) { // this is a check to make sure we are also
                                 // pushing with the same frequency end of
@@ -359,39 +408,24 @@ private:
         else
           delta = 0;
 
+        p.cleanup();
         for (unsigned i = 1; i < runtime::activeThreads; ++i) {
           while (!data.getRemote(i)->lock.try_lock())
             ;
-        }
-
-        for (unsigned i = 0; i < runtime::activeThreads; ++i) {
-
-          data.getRemote(i)->counters.popsLastFix        = 0;
-          data.getRemote(i)->counters.slowPopsLastPeriod = 0;
-          data.getRemote(i)->counters.pushesLastPeriod   = 0;
-          data.getRemote(i)->counters.priosLastPeriod    = 0;
-          data.getRemote(i)->maxPrioDiffLastPeriod       = 0;
-
-          data.getRemote(i)->minPrio = std::numeric_limits<Index>::max();
-          data.getRemote(i)->maxPrio = std::numeric_limits<Index>::min();
-        }
-
-        for (unsigned i = 1; i < runtime::activeThreads; ++i) {
+          data.getRemote(i)->cleanup();
           data.getRemote(i)->lock.unlock();
         }
-        p.ctr++;
+        p.stats.numUmerges++;
       }
-      p.counters.popsFromSameQ = 0;
+      p.popsFromSameQ = 0;
     }
-#endif
-    p.counters.popsFromSameQ = 0;
+    // p.popsFromSameQ = 0;
+
+    bool localLeader = substrate::ThreadPool::isLeader();
+    deltaIndex msS(this->earliest, 0);
 
     updateLocal(p);
-    bool localLeader = substrate::ThreadPool::isLeader();
 
-    deltaIndex msS;
-    msS.k = std::numeric_limits<Index>::min();
-    msS.d = 0;
     if (BSP && !UseMonotonic) {
       msS = p.scanStart;
       if (localLeader || uniformBSP) {
@@ -415,6 +449,7 @@ private:
         return item;
       }
     }
+
     p.lock.unlock();
     return galois::optional<value_type>();
   }
@@ -423,23 +458,23 @@ private:
   CTy* slowUpdateLocalOrCreate(ThreadData& p, deltaIndex i) {
     // update local until we find it or we get the write lock
     do {
-      CTy* lC;
       updateLocal(p);
+      CTy* lC;
       if ((lC = p.local[i]))
         return lC;
     } while (!masterLock.try_lock());
     // we have the write lock, update again then create
     updateLocal(p);
-    CTy*& lC2 = p.local[i];
-    if (!lC2) {
-      lC2                 = new (heap.allocate(sizeof(CTy))) CTy();
+    CTy*& C2 = p.local[i];
+    if (!C2) {
+      C2                  = new (heap.allocate(sizeof(CTy))) CTy();
       p.lastMasterVersion = masterVersion.load(std::memory_order_relaxed) + 1;
-      masterLog.push_back(std::make_pair(i, lC2));
+      masterLog.push_back(std::make_pair(i, C2));
       masterVersion.fetch_add(1);
-      p.counters.priosLastPeriod++;
+      p.stats.priosLastPeriod++;
     }
     masterLock.unlock();
-    return lC2;
+    return C2;
   }
 
   inline CTy* updateLocalOrCreate(ThreadData& p, deltaIndex i) {
@@ -454,7 +489,7 @@ private:
 
 public:
   AdaptiveOrderedByIntegerMetric(const Indexer& x = Indexer())
-      : data(earliest), heap(sizeof(CTy)), masterVersion(0), indexer(x) {
+      : data(this->earliest), heap(sizeof(CTy)), masterVersion(0), indexer(x) {
     delta   = 0;
     counter = chunk_size;
   }
@@ -475,19 +510,18 @@ public:
   void push(const value_type& val) {
     deltaIndex index;
     ThreadData& p = *data.getLocal();
-
     while (!p.lock.try_lock())
       ;
-    Index ind = indexer(val);
-    index.k   = ind;
-    index.d   = delta;
+
+    p.pushesLastPeriod++;
+    index.k = indexer(val);
+    index.d = delta;
     if (index.k > p.maxPrio) {
       p.maxPrio = index.k;
     }
     if (index.k < p.minPrio) {
       p.minPrio = index.k;
     }
-    p.counters.pushesLastPeriod++;
 
     // Fast path
     if (index == p.curIndex && p.current) {
@@ -503,12 +537,13 @@ public:
     // Opportunistically move to higher priority work
     if (index < p.curIndex) {
       // we moved to a higher prio
-      p.counters.popsFromSameQ = 0;
+      p.popsFromSameQ = 0;
 
       p.curIndex = index;
       p.current  = C;
     }
     C->push(val);
+
     p.lock.unlock();
   }
 
@@ -532,16 +567,16 @@ public:
       ;
     CTy* C = p.current;
 
-    p.counters.popsLastFix++;
-    unsigned myID = galois::substrate::ThreadPool::getTID();
-    data.getRemote(myID)->pmodAllDeq++;
+    p.popsLastFix++;
+    p.stats.pmodAllDeq++;
 
-    if (BlockPeriod && ((p.numPops++ & ((1ull << BlockPeriod) - 1)) == 0)) {
+    if (BlockPeriod && ((p.numPops++ & ((1ull << BlockPeriod) - 1)) == 0))
       return slowPop(p);
-    }
+
     galois::optional<value_type> item;
     if (C && (item = C->pop())) {
-      p.counters.popsFromSameQ++;
+      p.popsFromSameQ++;
+
       p.lock.unlock();
       return item;
     }
