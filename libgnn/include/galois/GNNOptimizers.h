27d807d336f803d0bc29b6e90dda46776ab0015b
Loc Hoang <l_hoang@utexas.edu>
2020-10-05 14:22:33 -0500

    Adam optimizer and test
    Added the Adam optimizer and a test for it. Notably different from the
    previous implementation is that there is now separate training variables
    for each layer instead of having them shared among all layers.

---
+++ libgnn/include/galois/GNNOptimizers.h
@@ -0,0 +1,70 @@
+#pragma once
+// Code inspired from this; actual code style is not the same + changed some
+// things such as adding params for every layer which TinyDNN does not seem to
+// do
+// https://github.com/tiny-dnn/tiny-dnn/blob/master/tiny_dnn/optimizers/optimizer.h
+// Copyright (c) 2013, Taiga Nomi and the respective contributors
+// All rights reserved.
+// Changed by Galois under 3-BSD
+#include "galois/GNNTypes.h"
+#include <vector>
+
+namespace galois {
+
+//! Virtual class; optimizers all need the descent function
+class BaseOptimizer {
+  virtual void GradientDescent(const std::vector<GNNFloat>& derivatives,
+                               std::vector<GNNFloat>* matrix,
+                               size_t layer_number) = 0;
+};
+
+//! Maintains a first and second moment for each weight in the weight matrix and
+//! does gradient descent invidiually on each weight
+class AdamOptimizer : public BaseOptimizer {
+public:
+  //! Struct for specifying adam config. Defaults based on the Adam paper.
+  struct AdamConfiguration {
+    GNNFloat alpha{0.001};
+    GNNFloat beta1{0.9};
+    GNNFloat beta2{0.999};
+    GNNFloat epsilon{1e-8};
+  };
+
+  //! Constructor allocates memory, initializes training vars for each layer
+  AdamOptimizer(const AdamConfiguration& config,
+                const std::vector<size_t>& trainable_layer_sizes,
+                size_t num_trainable_layers)
+      : config_(config), num_trainable_layers_(num_trainable_layers),
+        beta1_power_t_(num_trainable_layers_, config.beta1),
+        beta2_power_t_(num_trainable_layers_, config.beta2) {
+    assert(trainable_layer_sizes.size() == num_trainable_layers_);
+    // allocate vectors based on # of trainable layers
+    for (size_t layer_size : trainable_layer_sizes) {
+      first_moments_.emplace_back(layer_size, 0.0);
+      second_moments_.emplace_back(layer_size, 0.0);
+    }
+    assert(first_moments_.size() == num_trainable_layers_);
+    assert(second_moments_.size() == num_trainable_layers_);
+  }
+  //! Adam based gradient descent
+  void GradientDescent(const std::vector<GNNFloat>& derivatives,
+                       std::vector<GNNFloat>* matrix,
+                       size_t layer_number) final;
+
+private:
+  //! Configuration options for this layer
+  AdamConfiguration config_;
+  //! First moment vectors; one for each trainable layer
+  std::vector<std::vector<GNNFloat>> first_moments_;
+  //! Second moment vectors; one for each trainable layer
+  std::vector<std::vector<GNNFloat>> second_moments_;
+  //! Number of layers that can be trained (need moment vectors for each)
+  size_t num_trainable_layers_;
+  // power terms used in adam: updated by raising power every time update is
+  // called
+  // vector because one is necessary for each layer
+  std::vector<GNNFloat> beta1_power_t_;
+  std::vector<GNNFloat> beta2_power_t_;
+};
+
+} // namespace galois
---
+++ libgnn/src/GNNOptimizers.cpp
@@ -0,0 +1,44 @@
+#include "galois/Galois.h"
+#include "galois/GNNOptimizers.h"
+#include "galois/Logging.h"
+#include <cassert>
+
+void galois::AdamOptimizer::GradientDescent(
+    const std::vector<GNNFloat>& derivatives, std::vector<GNNFloat>* matrix,
+    size_t layer_number) {
+  assert(derivatives.size() == matrix->size());
+
+  // grab based on layer being used
+  std::vector<GNNFloat>& first_moment  = first_moments_[layer_number];
+  std::vector<GNNFloat>& second_moment = second_moments_[layer_number];
+  assert(derivatives.size() == first_moment.size());
+  assert(derivatives.size() == second_moment.size());
+
+  // individual weight updates via gradients
+  galois::do_all(
+      galois::iterate(static_cast<size_t>(0), matrix->size()),
+      [&](size_t i) {
+        // moment estimate updates
+        first_moment[i] = config_.beta1 * first_moment[i] +
+                          (1.0 - config_.beta1) * derivatives[i];
+        second_moment[i] =
+            config_.beta2 * second_moment[i] +
+            (1.0 - config_.beta2) * (derivatives[i] * derivatives[i]);
+        GALOIS_LOG_VERBOSE("{} {}", first_moment[i], second_moment[i]);
+        // bias corrected moments using beta power
+        GNNFloat bias_correct_first =
+            first_moment[i] / (1.0 - beta1_power_t_[layer_number]);
+        GNNFloat bias_correct_second =
+            second_moment[i] / (1.0 - beta2_power_t_[layer_number]);
+        GALOIS_LOG_VERBOSE("{} {}", bias_correct_first, bias_correct_second);
+        // weight update using bias corrected moments
+        (matrix->data())[i] -=
+            config_.alpha * bias_correct_first /
+            (std::sqrt(bias_correct_second) + config_.epsilon);
+      },
+      galois::loopname("AdamOptimizerGradientDescent"));
+
+  // update the power terms for next update call
+  beta1_power_t_[layer_number] *= config_.beta1;
+  beta2_power_t_[layer_number] *= config_.beta2;
+}
