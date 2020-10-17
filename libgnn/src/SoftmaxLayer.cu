d013d3593db41dd784b108924f9f078e699ab36a
chenxuhao <chen_xuhao@126.com>
2020-10-17 11:40:55 -0500

    add comments

--- libgnn/src/SoftmaxLayer.cu
+++ libgnn/src/SoftmaxLayer.cu
@@ -16,6 +16,10 @@ void galois::SoftmaxLayer::Forward(const galois::GNNFloat* in_tensor,
 // Input: out_tensor
 // Input: out_gradients
 // Output: in_gradients
+// Note: although out_gradients is an input data, 
+//       it is not const because it can be reused
+//       to hold intermediate data inside this function, 
+//       to avoid allocating more memory
 void galois::SoftmaxLayer::Backward(const galois::GNNFloat* in_tensor,
                                     const galois::GNNFloat* out_tensor,
                                     galois::GNNFloat* in_gradients,
