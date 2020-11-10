606750b85e131b08da97ff60f9ea621ee041a45e
Loc Hoang <l_hoang@utexas.edu>
2020-11-10 17:06:42 -0600

    GPU arch related defs, CUDA_TEST
    Adds compile time constants for various GPU things like warp size, block
    size, etc. Also adds CUDA_TEST which is a macro for checking if a CUDA
    call returned successfully.

--- libgnn/include/galois/CUDAUtil.h
+++ libgnn/include/galois/CUDAUtil.h
@@ -5,6 +5,14 @@
 #include <cuda.h>
 #include "galois/Logging.h"
 
+// TODO check these
+#define CHUNK_SIZE 256
+#define TB_SIZE 256
+#define BLOCK_SIZE 256
+#define WARP_SIZE 32
+#define MAX_NUM_CLASSES 128
+#define WARPS_PER_BLOCK (BLOCK_SIZE / WARP_SIZE)
+
 #define CUDA_CHECK(condition)                                                  \
   do {                                                                         \
     cudaError_t error = condition;                                             \
@@ -19,6 +27,17 @@
     if (ptr) {                                                                 \
       CUDA_CHECK(cudaFree(ptr));                                               \
       ptr = nullptr;                                                           \
+    }                                                                          \
+  } while (0)
+
+#define CUDA_TEST(msg)                                                         \
+  do {                                                                         \
+    cudaError_t e;                                                             \
+    cudaDeviceSynchronize();                                                   \
+    if (cudaSuccess != (e = cudaGetLastError())) {                             \
+      GALOIS_LOG_ERROR("{}: {}", msg, e);                                      \
+      GALOIS_LOG_ERROR("{}", cudaGetErrorString(e));                           \
+      exit(EXIT_FAILURE);                                                      \
     }                                                                          \
   } while (0)
 
