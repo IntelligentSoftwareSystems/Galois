8017d6a473efe0ce5fad02183656406569b016df
Loc Hoang <l_hoang@utexas.edu>
2020-11-13 19:38:52 -0600

    CUDA_KERNEL_LOOP and some helper calcs
    Added a few things from old codebase's CUDA utils to new one in
    preparation for using the newly added things to compute the softmax
    layer. Also added the original source of the old code: Caffe.

--- libgnn/include/galois/CUDAUtil.h
+++ libgnn/include/galois/CUDAUtil.h
@@ -2,9 +2,20 @@
 #define GALOIS_CUDA_UTIL
 //! @file CUDAUtil.h
 //! Contains various utility functions for CUDA.
+//! Taken and revised+added to from here
+//! https://github.com/BVLC/caffe/blob/master/include/caffe/util/device_alternate.hpp
 #include <cuda.h>
 #include <cublas_v2.h>
 #include "galois/Logging.h"
+
+// TODO check these too and make sure they make sense
+// CUDA: use 256 threads per block
+const int CUDA_NUM_THREADS = 256;
+
+// CUDA: number of blocks for threads.
+inline int CUDA_GET_BLOCKS(const int N) {
+  return (N + CUDA_NUM_THREADS - 1) / CUDA_NUM_THREADS;
+}
 
 // TODO check these
 #define CHUNK_SIZE 256
@@ -14,6 +25,7 @@
 #define MAX_NUM_CLASSES 128
 #define WARPS_PER_BLOCK (BLOCK_SIZE / WARP_SIZE)
 
+//! Wrap a CUDA call with this to auto-check if it returns any error
 #define CUDA_CHECK(condition)                                                  \
   do {                                                                         \
     cudaError_t error = condition;                                             \
@@ -22,6 +34,7 @@
     }                                                                          \
   } while (0)
 
+//! Frees a pointer allocated by cuda malloc
 #define CUDA_FREE(ptr)                                                         \
   do {                                                                         \
     if (ptr) {                                                                 \
@@ -30,6 +43,7 @@
     }                                                                          \
   } while (0)
 
+//! Call this after a cuda call to make sure it set any error flags
 #define CUDA_TEST(msg)                                                         \
   do {                                                                         \
     cudaError_t e;                                                             \
@@ -41,6 +55,13 @@
     }                                                                          \
   } while (0)
 
+//! Basic kernel loop for CUDA threads
+//! Caffe describes it as "grid stride"
+#define CUDA_KERNEL_LOOP(i, n)                                                 \
+  for (int i = blockIdx.x * blockDim.x + threadIdx.x; i < (n);                 \
+       i += blockDim.x * gridDim.x)
+
+//! Wrap a CuBLAS call with this to check if it threw any errors
 #define CUBLAS_CHECK(condition)                                                \
   do {                                                                         \
     cublasStatus_t status = condition;                                         \
