3a80d2483d8151f0c84b658d6e43a361271d7131
Loc Hoang <l_hoang@utexas.edu>
2021-06-09 13:59:46 -0500

    MKL microbenchmark additions and changes
    - Version that uses galois wrapper around single thread MKL calls
    - removal of unused things from original test

--- libgnn/test/single_mkl_micro.cpp
+++ libgnn/test/single_mkl_micro.cpp
@@ -12,27 +12,73 @@
 #include "galois/DistGalois.h"
 #include "galois/LargeArray.h"
 #endif
-#ifdef USE_SHARED_GALOIS_DELETE
-#include "galois/Galois.h"
-#endif
 
 #ifdef USE_OMP
 #include "omp.h"
 #endif
 
 // MKL wrapper
-void CBlasSGEMM(const CBLAS_TRANSPOSE trans_a, const CBLAS_TRANSPOSE trans_b,
+#ifdef USE_OMP
+void CBlasSGEMMOMP(const CBLAS_TRANSPOSE trans_a, const CBLAS_TRANSPOSE trans_b,
                 size_t input_rows, size_t input_columns, size_t output_columns,
                 const float* a, const float* b, float* output) {
   // set lead dimension based on cblas spec w.r.t. transpose setting
   size_t lead_dim_a = (trans_a == CblasNoTrans) ? input_columns : input_rows;
   size_t lead_dim_b =
       (trans_b == CblasNoTrans) ? output_columns : input_columns;
-  // do the MM
-  cblas_sgemm(CblasRowMajor, trans_a, trans_b, input_rows, output_columns,
-              input_columns, 1.0, a, lead_dim_a, b, lead_dim_b,
-              false ? 1.0 : 0.0, output, output_columns);
+
+  #pragma omp parallel for
+  for (int i = 0; i < omp_get_num_threads(); i++) {
+    unsigned chunk_size = input_rows / omp_get_num_threads();
+    unsigned my_start = chunk_size * i;
+    unsigned my_end = chunk_size * (i + 1);
+    if (omp_get_num_threads() - 1 == i) {
+      my_end = input_rows;
+    }
+    unsigned rows_to_use = my_end - my_start;
+
+    const float* my_a = a + (my_start * input_columns);
+    float* my_output = output + (my_start * output_columns);
+
+    // do the MM
+    cblas_sgemm(CblasRowMajor, trans_a, trans_b, rows_to_use, output_columns,
+                input_columns, 1.0, my_a, lead_dim_a, b, lead_dim_b,
+                false ? 1.0 : 0.0, my_output, output_columns);
+  }
 }
+#endif
+
+#if defined(USE_SHARED_GALOIS) || defined(USE_DIST_GALOIS)
+void CBlasSGEMMGalois(const CBLAS_TRANSPOSE trans_a, const CBLAS_TRANSPOSE trans_b,
+                size_t input_rows, size_t input_columns, size_t output_columns,
+                const float* a, const float* b, float* output) {
+  // set lead dimension based on cblas spec w.r.t. transpose setting
+  size_t lead_dim_a = (trans_a == CblasNoTrans) ? input_columns : input_rows;
+  size_t lead_dim_b =
+      (trans_b == CblasNoTrans) ? output_columns : input_columns;
+
+  galois::on_each(
+    [&] (size_t i, size_t num_threads) {
+    unsigned chunk_size = input_rows / num_threads;
+    unsigned my_start = chunk_size * i;
+    unsigned my_end = chunk_size * (i + 1);
+    if (num_threads - 1 == i) {
+      my_end = input_rows;
+    }
+    unsigned rows_to_use = my_end - my_start;
+
+    const float* my_a = a + (my_start * input_columns);
+    float* my_output = output + (my_start * output_columns);
+
+    // do the MM
+    cblas_sgemm(CblasRowMajor, trans_a, trans_b, rows_to_use, output_columns,
+                input_columns, 1.0, my_a, lead_dim_a, b, lead_dim_b,
+                false ? 1.0 : 0.0, my_output, output_columns);
+    }
+  );
+}
+#endif
+
 
 void CacheFlush(std::vector<float>* matrix) {
   for (size_t i = 0; i < matrix->size(); i++) {
@@ -50,21 +96,6 @@ int main(int argc, char* argv[]) {
   galois::setActiveThreads(std::stoi(argv[1]));
   printf("Initialized Galois Shared Mem with %u threads\n",
          galois::getActiveThreads());
-#endif
-
-#ifdef USE_SHARED_GALOIS_DELETE
-  std::unique_ptr<galois::SharedMemSys> G;
-  G = std::make_unique<galois::SharedMemSys>();
-
-  if (argc != 2) {
-    printf("Thread arg not specified\n");
-    exit(1);
-  }
-  galois::setActiveThreads(std::stoi(argv[1]));
-  printf("Initialized Galois Shared Mem with %u threads\n",
-         galois::getActiveThreads());
-  printf("Deleting galois\n");
-  G.reset();
 #endif
 
 #ifdef USE_DIST_GALOIS
@@ -89,8 +120,7 @@ int main(int argc, char* argv[]) {
   std::vector<float> matrix_1(a_dim * b_dim);
   std::vector<float> matrix_2(a_dim * c_dim);
   // output
-  //std::vector<float> matrix_3(a_dim * c_dim);
-  std::vector<float> matrix_3(b_dim * c_dim);
+  std::vector<float> matrix_3(a_dim * c_dim);
 
   size_t kBigSize = 1000000000;
   std::vector<float> very_big_matrix(kBigSize);
@@ -112,23 +142,21 @@ int main(int argc, char* argv[]) {
     // cache flush
     CacheFlush(&very_big_matrix);
 
-    // dummy OMP TBB loop
-#ifdef USE_OMP
-#pragma omp parallel for
-    for (size_t i = 0; i < very_big_matrix.size(); i++) {
-      very_big_matrix[i] = i;
-    }
-#endif
-
     printf("Rep %lu\n", reps);
 
     auto start = std::chrono::high_resolution_clock::now();
     // transpose because it's the same as the problematic call in GNN
     // TODO(loc) non transpose version
-    //CBlasSGEMM(CblasNoTrans, CblasNoTrans, a_dim, b_dim, c_dim, matrix_1.data(),
-    //           matrix_2.data(), matrix_3.data());
-    CBlasSGEMM(CblasTrans, CblasNoTrans, b_dim, a_dim, c_dim, matrix_1.data(),
+#ifdef USE_OMP
+    CBlasSGEMMOMP(CblasNoTrans, CblasNoTrans, a_dim, b_dim, c_dim, matrix_1.data(),
                matrix_2.data(), matrix_3.data());
+#endif
+#if defined(USE_SHARED_GALOIS) || defined(USE_DIST_GALOIS)
+    CBlasSGEMMGalois(CblasNoTrans, CblasNoTrans, a_dim, b_dim, c_dim, matrix_1.data(),
+               matrix_2.data(), matrix_3.data());
+#endif
+    //CBlasSGEMM(CblasTrans, CblasNoTrans, b_dim, a_dim, c_dim, matrix_1.data(),
+    //           matrix_2.data(), matrix_3.data());
     auto stop = std::chrono::high_resolution_clock::now();
 
     auto duration = std::chrono::time_point_cast<std::chrono::milliseconds>(stop) - 
