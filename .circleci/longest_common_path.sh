e991791f8ab097676b5126ebf6ee7d05b29c3e6c
Bozhi YOU <youbozhi@cs.utexas.edu>
2023-08-23 00:21:53 -0500

    Fix OS-specific issues.

--- .circleci/longest_common_path.sh
+++ .circleci/longest_common_path.sh
@@ -1,10 +1,9 @@
+#!/bin/bash
 # For PR build only; find the longest common path prefix as the build and test subset
 
 longest_common_prefix() {
     declare -a possible_prefix
     declare i=0
-
-    changes=("$@")
 
     path="${1%/}"
     while [ "$path" != "." ]; do
@@ -17,7 +16,7 @@ longest_common_prefix() {
 
     lcp="."
     for prefix in "${possible_prefix[@]}"; do
-        for path in "${changes[@]}"; do
+        for path in $@; do
             if [ "${path#$prefix}" = "${path}" ]; then
                 continue 2
             fi
@@ -28,7 +27,7 @@ longest_common_prefix() {
     echo $lcp
 }
 base=$( \
-    curl -s "https://api.github.com/repos/$(sed "s/\/pull\//\/pulls\//" <<< "${CIRCLE_PULL_REQUEST:19}")" \
+    wget -q -O - "https://api.github.com/repos/$(echo ${CIRCLE_PULL_REQUEST:19} | sed "s/\/pull\//\/pulls\//")" \
     | sed -n -e "s/^.*IntelligentSoftwareSystems://p" \
     | sed -n -e "s/\".*$//p" \
 )
